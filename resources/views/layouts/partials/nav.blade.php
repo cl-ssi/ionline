813ab7a11a83d727a72521b917e6e90f151b35e3
AquaroTorres <alvarotorres@gmail.com>
2020-08-27 15:13:49 -0400

    Test logout

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -117,7 +117,8 @@
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
+                                             document.getElementById('logout-form').submit();"
+                                             id="cierreSesion">
                                 {{ __('Logout') }}
                             </a>
 
