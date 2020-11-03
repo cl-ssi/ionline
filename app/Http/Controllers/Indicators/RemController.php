7a0ab78021e7dc78eba82898067965f47fb68a9d
Alvaro Lupa <alvaro.lupa@gmail.com>
2020-11-03 17:51:31 -0300

    Indicators->REM order by id_prestacion instead codigo_prestacion

--- app/Http/Controllers/Indicators/RemController.php
+++ app/Http/Controllers/Indicators/RemController.php
@@ -76,53 +76,10 @@ class RemController extends Controller
                 $seccion->prestaciones = Prestacion::year($year)->with(['rems' => function($q) use ($establecimiento, $periodo){
                                                     $q->whereIn('IdEstablecimiento', $establecimiento)->whereIn('Mes', $periodo);
                                             }])
-                                            ->whereIn('codigo_prestacion', $seccion->cods)->orderBy('codigo_prestacion')->get();
+                                            ->whereIn('codigo_prestacion', $seccion->cods)->orderBy('id_prestacion')->get();
             }
         }
         
-        // $filter = ['02010101', '02010201', '02010103', '02010105'];
-        // $estabs = ['102300', '102301'];
-        // $months = [1];
-        
-        // foreach($prestaciones as $prestacion){
-        //     return $prestacion->rems->sum('Col01');
-        // }
-
-        /*
-        $query = 'SELECT a.codigo_prestacion,a.descripcion
-                              ,sum(ifnull(b.Col01,0)) Col01
-                              ,sum(ifnull(b.Col02,0)) Col02
-                              ,sum(ifnull(b.Col03,0)) Col03
-                              ,sum(ifnull(b.Col04,0)) Col04
-                              ,sum(ifnull(b.Col05,0)) Col05
-                              ,sum(ifnull(b.Col06,0)) Col06
-                              ,sum(ifnull(b.Col07,0)) Col07
-                              ,sum(ifnull(b.Col08,0)) Col08
-                              ,sum(ifnull(b.Col09,0)) Col09
-                              ,sum(ifnull(b.Col10,0)) Col10
-                              ,sum(ifnull(b.Col11,0)) Col11
-                              ,sum(ifnull(b.Col12,0)) Col12
-                              ,sum(ifnull(b.Col13,0)) Col13
-                              ,sum(ifnull(b.Col14,0)) Col14
-                              ,sum(ifnull(b.Col15,0)) Col15
-                              ,sum(ifnull(b.Col16,0)) Col16
-                              ,sum(ifnull(b.Col17,0)) Col17
-                              ,sum(ifnull(b.Col18,0)) Col18
-                              ,sum(ifnull(b.Col19,0)) Col19
-                              ,sum(ifnull(b.Col20,0)) Col20
-                              ,sum(ifnull(b.Col21,0)) Col21
-                              ,sum(ifnull(b.Col22,0)) Col22
-                              ,sum(ifnull(b.Col23,0)) Col23
-                              ,sum(ifnull(b.Col24,0)) Col24
-                              ,sum(ifnull(b.Col25,0)) Col25
-                          FROM (select c.* from 2020prestaciones c where c.codigo_prestacion in("01010101","01010103","01010201","01010203","01080001","01080002",
-                                                                                            "01110106","01110107","01080030","01080040","01010601","01010603",
-                                                                                            "01010901","01010903","01010401","01010403") AND c.serie = "A" ) a
-                          left join 2020rems b on (a.id_prestacion=b.prestacion_id_prestacion)
-                          AND (b.Mes in ('.$mes.')) AND (b.establecimiento_id_establecimiento in ('.$estab.'))
-                          group by  a.codigo_prestacion,a.descripcion, a.id_prestacion
-                          order by a.id_prestacion';
-        */
         return view('indicators.rem.show', compact('year', 'establecimientos', 'prestacion', 'establecimiento', 'periodo', 'secciones'));
     }
 
