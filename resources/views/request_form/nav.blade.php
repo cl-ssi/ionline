e4d2d40cd043460f39723480a70067a678fc7081
Esteban <tebiccr@gmail.com>
2020-11-20 13:28:52 -0300

    se quita el active en el nav del request form

--- resources/views/request_form/nav.blade.php
+++ resources/views/request_form/nav.blade.php
@@ -1,25 +1,25 @@
 <ul class="nav nav-tabs mb-3 d-print-none">
     <li class="nav-item active">
-        <a class="nav-link {{ active('request_forms.create') }}"
+        <a class="nav-link"
             href="{{ route('request_forms.create') }}">
             <i class="fas fa-pencil-alt"></i> Nuevo
         </a>
     </li>
     <li class="nav-item active">
-        <a class="nav-link {{ active('request_forms.index') }}"
+        <a class="nav-link"
             href="{{ route('request_forms.index') }}">
             <i class="fas fa-inbox"></i> Mis Formularios
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link {{ active('request_forms.my_request_inbox') }}"
+        <a class="nav-link"
             href="{{ route('request_forms.my_request_inbox') }}">
             <i class="fas fa-inbox"></i> Mis Solicitudes
             <span class="badge badge-secondary">{{ App\Utilities::getPendingSignature() }}</span></a>
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link {{ active('request_forms.authorize_inbox') }}"
+        <a class="nav-link"
             href="{{ route('request_forms.authorize_inbox') }}">
             <i class="fas fa-inbox"></i> Autorizar
             <span class="badge badge-secondary">{{ App\Utilities::getPendingSignatureAuthorize() }}</span></a>
@@ -27,7 +27,7 @@
     </li>
     @if(App\Utilities::getAmIDirector() == 1)
     <li class="nav-item">
-        <a class="nav-link {{ active('request_forms.director_inbox') }}"
+        <a class="nav-link"
             href="{{ route('request_forms.director_inbox') }}">
             <i class="fas fa-inbox"></i> Director
             <span class="badge badge-secondary">{{ App\Utilities::getPendingDirectorAuthorize() }}</span></a>
@@ -36,7 +36,7 @@
     @endif
     @can('Request Forms: Finance add item code')
     <li class="nav-item">
-        <a class="nav-link {{ active('request_forms.finance_inbox') }}"
+        <a class="nav-link"
             href="{{ route('request_forms.finance_inbox') }}">
             <i class="fas fa-inbox"></i> Finanzas
             <span class="badge badge-secondary">{{ App\Utilities::getPendingDirectorAuthorize() }}</span></a>
