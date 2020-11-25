19de68502f1b23845eb68d357fc5ecc9e7e3c069
jcortesf <juliocortesf@gmail.com>
2020-11-25 01:30:26 -0300

    add: final countdown

--- resources/views/programmings/programmings/index.blade.php
+++ resources/views/programmings/programmings/index.blade.php
@@ -12,7 +12,8 @@
     <a href="{{ route('programmings.create') }}" class="btn btn-info mb-4">Comenzar Nueva Programación</a>
  @endcan
 
-
+ <div class="float-right text-center" id="timer"></div>
+ 
 <table class="table table-sm  ">
     <thead>
         <tr class="small ">
@@ -87,3 +88,55 @@
 
 
 @endsection
+
+@section('custom_js')
+
+
+<script type="text/javascript">
+
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 1, 2020 00:00:01").getTime();
+
+function timePart(val,text,color="black"){
+  return `<h6 class="timer" style="color:${color};">${val}<div>${text}</div></h6 >`
+}
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+ var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+ // Display the result in the element with id="demo"
+
+ let res = timePart(days,'Días') + timePart(hours,'Horas') + timePart(minutes,'Min.')  + timePart(seconds,'Seg.','red');
+document.getElementById("timer").innerHTML = res
+
+  // If the count down is finished, write some text 
+ if (distance < 0) {
+    clearInterval(x);
+
+document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+</script>
+<style type="text/css">
+.timer{
+  display:inline-block;
+  padding:10px;
+}
+</style>
+@endsection
+
+
+
