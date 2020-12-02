92cc53fa9e98fe3c5b258ade2f02f04b3a05c383
jcortesf <juliocortesf@gmail.com>
2020-12-02 01:41:47 -0300

    fix: encoding in consolidated report excel 6

--- resources/views/programmings/reports/reportConsolidated.blade.php
+++ resources/views/programmings/reports/reportConsolidated.blade.php
@@ -84,7 +84,7 @@
 <script>
     function exportTableToExcel(tableID, filename = ''){
         var downloadLink;
-        var dataType = 'application/vnd.ms-excel';
+        var dataType = 'data:application/vnd.ms-excel';
         var tableSelect = document.getElementById(tableID);
         var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
         
@@ -103,7 +103,7 @@
             navigator.msSaveOrOpenBlob( blob, filename);
         }else{
             // Create a link to the file
-            downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+            downloadLink.href = 'data:' + dataType + ', ' + escape(tableHTML);
         
             // Setting the file name
             downloadLink.download = filename;
