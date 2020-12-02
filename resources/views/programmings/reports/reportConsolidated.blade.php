0a197a255b1676f21209f3c9bde122284fbb4668
jcortesf <juliocortesf@gmail.com>
2020-12-02 13:31:40 -0300

    fix: consolidated report only tracer activities

--- resources/views/programmings/reports/reportConsolidated.blade.php
+++ resources/views/programmings/reports/reportConsolidated.blade.php
@@ -85,37 +85,6 @@
 </script>
 
 <script>
-    function exportTableToExcel(tableID, filename = ''){
-        var downloadLink;
-        var dataType = 'data:application/vnd.ms-excel';
-        var tableSelect = document.getElementById(tableID);
-        var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
-        
-        // Specify file name
-        filename = filename?filename+'.xls':'Informe_consolidado.xls';
-        
-        // Create download link element
-        downloadLink = document.createElement("a");
-        
-        document.body.appendChild(downloadLink);
-        
-        if(navigator.msSaveOrOpenBlob){
-            var blob = new Blob(['\ufeff', escape(tableHTML)], {
-                type: dataType
-            });
-            navigator.msSaveOrOpenBlob( blob, filename);
-        }else{
-            // Create a link to the file
-            downloadLink.href = 'data:' + dataType + ', ' + escape(tableHTML);
-        
-            // Setting the file name
-            downloadLink.download = filename;
-            
-            //triggering the function
-            downloadLink.click();
-        }
-    }
-
     function tableExcel(type, fn, dl) {
           var elt = document.getElementById('tblData');
           const filename = 'Informe_consolidado'
