e8c1de14be6ac2d0f66af633f31c9c586add23bd
Alvaro Lupa <alvaro.lupa@gmail.com>
2021-02-05 13:06:59 -0300

    Documentos->Convenios formato convenio 2021 arreglos capitalizado las letras de los montos en vez de todo en mayusculas

--- app/Http/Controllers/Agreements/WordTestController.php
+++ app/Http/Controllers/Agreements/WordTestController.php
@@ -74,8 +74,9 @@ class WordTestController extends Controller
 
         // SE CONVIERTE EL VALOR TOTAL DEL CONVENIO EN PALABRAS
         $formatter = new NumeroALetras;
+        $formatter->apocope = true;
         $totalConvenio = $agreements->agreement_amounts->sum('amount');
-        $totalConvenioLetras = $formatter->toMoney($totalConvenio,0, 'pesos','');
+        $totalConvenioLetras = ucwords(strtolower($formatter->toMoney($totalConvenio,0, 'pesos','')));
  
         // ARRAY PARA OBTENER LAS CUOTAS ASOCIADAS AL TOTAL DEL CONVENIO
         foreach ($quotas as $key => $quota) {
@@ -83,7 +84,7 @@ class WordTestController extends Controller
                 $arrayQuota[] = array('index' => ($this->ordinal($key+1))
                                       ,'cuotaDescripcion' => $quota->description . ($key+1 == 1 ? ' del total de los recursos del convenio una vez aprobada la resolución exenta que aprueba el presente instrumento y recibidos los recursos del Ministerio de Salud.' : ' restante del total de recursos y se enviará en el mes de octubre, según resultados obtenidos en la primera evaluación definida en la cláusula anterior. Así también, dependerá de la recepción de dichos recursos desde Ministerio de Salud y existencia de rendición financiera según lo establece la resolución N°30/2015 que fija normas sobre procedimiento de rendición de cuentas de la Contraloría General de la Republica, por parte de la “MUNICIPALIDAD”.')
                                       ,'cuotaMonto' => number_format($quota->amount,0,",",".")
-                                      ,'cuotaLetra' => $cuotaConvenioLetras);
+                                      ,'cuotaLetra' => ucwords(strtolower($cuotaConvenioLetras)));
              } 
 
              //dd($arrayQuota);
