a0f05e7dc31b8f21c96288f12098ecebe889307a
AquaroTorres <alvarotorres@gmail.com>
2021-02-10 00:05:21 -0300

    Welcome maquillaje

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm container bg-nav-gobierno">
         <h5 class="my-0 mr-md-auto font-weight-normal"> <img src="{{ asset('images/gob-header.svg') }}" alt="Logo del gobierno de chile"> </h5>
         <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2" href="http://www.saludiquique.cl">Web Servicio de Salud</a>
+            <a class="p-2" href="http://www.saludiquique.cl">Servicio de Salud</a>
             <a class="p-2 btn btn-sm btn-outline-primary" href="{{ route('claveunica.autenticar') }}?redirect=L3NpcmVteC9sb2dpbmN1">Siremx</a>
             <a class="p-2 btn btn-sm btn-outline-primary" href="http://intranet.saludiquique.cl">Intranet</a>
             <a class="p-2 btn btn-sm btn-outline-primary" href="{{ route('login') }}">iOnline</a>
