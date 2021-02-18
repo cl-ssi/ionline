4f58edd5130601e372225ea16720cbbe5f3b770f
Alvaro Lupa <alvaro.lupa@gmail.com>
2021-02-18 17:53:14 -0300

    Indicators->REM 2021 serie A01 completed

--- resources/views/indicators/rem/index.blade.php
+++ resources/views/indicators/rem/index.blade.php
@@ -15,7 +15,7 @@
                 <strong>Serie A</strong>
             </div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item text-muted">{{--<a href="{{ route('indicators.rem.index', [2021, 'A']) }}">2021</a>--}} 2021</li>
+                <li class="list-group-item text-muted"><a href="{{ route('indicators.rem.index', [2021, 'A']) }}">2021</a></li>
                 <li class="list-group-item text-muted"><a href="{{ route('indicators.rems.year.serie.index', [2020, 'serie_a']) }}">2020</a> </li>
                 <li class="list-group-item text-muted"><a href="{{ route('indicators.rems.year.serie.index', [2019, 'serie_a']) }}">2019</a> </li>
                 <li class="list-group-item text-muted"><a href="{{ route('indicators.rem.index', [2018, 'A']) }}">2018</a></li>
