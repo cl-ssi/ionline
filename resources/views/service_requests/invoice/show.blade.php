53a68f6d760290966aed43a246fd85972ca0c530
Esteban <tebiccr@gmail.com>
2021-03-02 19:31:00 -0300

    maqueteo boleta

--- resources/views/service_requests/invoice/show.blade.php
+++ resources/views/service_requests/invoice/show.blade.php
@@ -4,16 +4,41 @@
 
 @section('content')
 
-@if(empty($sr))
-
+@if($sr === null)
 <div class="alert alert-danger">
         <h4 class="alert-heading">No Posee Solicitudes de Pago de Honorario con este RUT.</h4>
 </div>
-
 @else
-Si Existe Boleta de Honorario
-
-
+<h5 class="mb-3">información de sus solicitudes de pago de Honorario.</h5>
+<hr>
+<div class="table-responsive">
+        <table class="table table-sm table-bordered small">
+                <thead>
+                        <tr>
+                                <th>Id</th>
+                                <th>Tipo de Contrato</th>
+                                <th>Descripción de Servicio</th>
+                                <th>Fecha de Solicitud</th>
+                                <th>Inicio de Contrato</th>
+                                <th>Fin de Contrato</th>
+                                <th>Subir Boleta</th>
+                        </tr>
+                </thead>
+                <tbody>
+                        @foreach ($sr as $servicerequest)
+                        <tr>
+                                <td class="small">{{ $servicerequest->id ?? '' }}</td>
+                                <td>{{ $servicerequest->type ?? '' }}</td>
+                                <td class="small">{{ $servicerequest->service_description ?? '' }}</td>
+                                <td>{{ $servicerequest->request_date->format('d-m-Y') ?? '' }}</td>
+                                <td>{{ $servicerequest->start_date->format('d-m-Y') ?? '' }}</td>
+                                <td>{{ $servicerequest->end_date->format('d-m-Y') ?? '' }}</td>
+                                <td> <i class="fas fa-paperclip"></i></td>
+                        </tr>
+                        @endforeach
+                </tbody>
+        </table>
+</div>
 @endif
 
 
