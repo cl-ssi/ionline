44ecc30f2f4d5e02b2ffcd49503b395b98fa8689
GermanZun <german.zuniga@gmail.com>
2021-03-03 17:10:11 -0300

    boton firmar fulfillments

--- resources/views/documents/signatures/partials/sign_file.blade.php
+++ resources/views/documents/signatures/partials/sign_file.blade.php
@@ -1,9 +1,9 @@
-<div class="modal fade" id="signPdfModal{{$signedSignaturesFlow->id}}" tabindex="-1" role="dialog"
+<div class="modal fade" id="signPdfModal{{$idSignModal}}" tabindex="-1" role="dialog"
      aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Modal incrustado {{$signedSignaturesFlow->id}} </h5>
+                <h5 class="modal-title" id="exampleModalLongTitle">Firmar documento {{$idSignModal}} </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -20,8 +20,8 @@
                             <input type="text" class="form-control form-control-sm" id="forotp"
                                    name="otp" maxlength="6" autocomplete="off" required/>
 {{--                            <input type="hidden" name="file_path" value="modulo1/1.pdf">--}}
-                            <input type="hidden" name="route" value="/rrhh/service_requests/resolution-pdf/2">
-                            <input type="hidden" name="return_url" value="documents.callbackFirma">
+                            <input type="hidden" name="route" value={{$routePdfSignModal}}>
+                            <input type="hidden" name="return_url" value={{$returnUrlSignModal}}>
                         </div>
                     </div>
                 </div>
@@ -36,4 +36,45 @@
             </form>
         </div>
     </div>
+</div>
+
+
+{{--<div class="modal fade" id="signPdfModal{{$signedSignaturesFlow->id}}" tabindex="-1" role="dialog"--}}
+{{--     aria-labelledby="exampleModalCenterTitle" aria-hidden="true">--}}
+{{--    <div class="modal-dialog modal-dialog-centered" role="document">--}}
+{{--        <div class="modal-content">--}}
+{{--            <div class="modal-header">--}}
+{{--                <h5 class="modal-title" id="exampleModalLongTitle">Modal incrustado {{$signedSignaturesFlow->id}} </h5>--}}
+{{--                <button type="button" class="close" data-dismiss="modal" aria-label="Close">--}}
+{{--                    <span aria-hidden="true">&times;</span>--}}
+{{--                </button>--}}
+{{--            </div>--}}
+{{--            <form method="POST" class="form-horizontal"--}}
+{{--                  action="{{route('signPdf')}}"--}}
+{{--                  enctype="multipart/form-data">--}}
+{{--                <div class="modal-body">--}}
+{{--                @csrf <!-- input hidden contra ataques CSRF -->--}}
+{{--                    @method('POST')--}}
+{{--                    <div class="form-row">--}}
+{{--                        <div class="form-group col-12">--}}
+{{--                            <label for="forotp">Ingrese número OTP.</label>--}}
+{{--                            <input type="text" class="form-control form-control-sm" id="forotp"--}}
+{{--                                   name="otp" maxlength="6" autocomplete="off" required/>--}}
+{{--                            --}}{{--                            <input type="hidden" name="file_path" value="modulo1/1.pdf">--}}
+{{--                            <input type="hidden" name="route" value="/rrhh/service_requests/resolution-pdf/2">--}}
+{{--                            <input type="hidden" name="return_url" value="documents.callbackFirma">--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="modal-footer">--}}
+{{--                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar--}}
+{{--                    </button>--}}
+
+{{--                    <button class="btn btn-primary" type="submit">--}}
+{{--                        <i class="fas fa-edit"></i> Firmar--}}
+{{--                    </button>--}}
+{{--                </div>--}}
+{{--            </form>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 </div>
