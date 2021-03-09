9a715f0fe6693f2b612b8a7a73f824662c42f815
Esteban <sick_iqq@hotmail.com>
2021-03-09 19:51:14 -0300

    fix pending requests - se agrega cumplimientos

--- resources/views/service_requests/requests/pending_requests.blade.php
+++ resources/views/service_requests/requests/pending_requests.blade.php
@@ -6,23 +6,24 @@
 
 <h3 class="mb-3">Reporte estado de solicitudes</h3>
 
-<form method="GET" class="form-horizontal" action="{{ route('rrhh.service_requests.pending_requests') }}">
+<form method="GET" class="form-horizontal" action="{{ route('rrhh.service_requests.consolidated_data') }}">
 	<div class="input-group mb-3">
 		<div class="input-group-prepend">
 			<span class="input-group-text">Rango de fechas (Inicio de contrato)</span>
 		</div>
-		<input type="date" class="form-control" id="for_dateFrom" name="dateFrom" value="2021-01-01" required >
-		<input type="date" class="form-control" id="for_dateTo" name="dateTo" value="2021-01-31" required>
+		<!-- <input type="date" class="form-control" id="for_dateFrom" name="dateFrom" value="2021-01-01" required >
+		<input type="date" class="form-control" id="for_dateTo" name="dateTo" value="2021-01-31" required> -->
+		<input type="text" value="Todos los datos" disabled>
     <div class="input-group-append">
         <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Buscar</button>
     </div>
 	</div>
 </form>
 
-<hr>
+<!-- <hr> -->
 
-<h4>Subtotales</h4>
-<hr>
+<h4>Usuarios con hojas de ruta pendientes por visar</h4>
+<!-- <hr> -->
 <table class="table table-sm small" >
     <thead>
         <tr>
@@ -41,12 +42,12 @@
       @endforeach
       <tr>
         <td nowrap>Total</td>
-        <td nowrap><b>{{$falta_aprobar}}</b></td>
+        <td nowrap><b>{{$hoja_ruta_falta_aprobar}}</b></td>
       </tr>
     </tbody>
 </table>
 
-<h4>Detalle</h4>
+<!-- <h4>Detalle</h4>
 <hr>
 <table class="table table-sm small" >
     <thead>
@@ -67,7 +68,31 @@
         </tr>
       @endforeach
     </tbody>
+</table> -->
+
+<h4>Usuarios con cumplimientos pendientes por ingresar</h4>
+<!-- <hr> -->
+<table class="table table-sm small" >
+    <thead>
+        <tr>
+            <th>FUNCIONARIO</th>
+            <th>CANTIDAD</th>
+        </tr>
+    </thead>
+    <tbody>
+      @foreach($fulfillments_missing as $key => $fulfillment)
+        <tr>
+          <td nowrap>{{$key}}</td>
+          <td nowrap>{{$fulfillment}}</td>
+        </tr>
+      @endforeach
+			<tr>
+        <td nowrap>Total</td>
+        <td nowrap><b>{{$cumplimiento_falta_ingresar}}</b></td>
+      </tr>
+    </tbody>
 </table>
+
 
 @endsection
 
