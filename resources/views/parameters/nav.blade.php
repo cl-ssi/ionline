b884e01710a91e5c8aa571ad30fc4e25c3951659
Esteban <tebiccr@gmail.com>
2021-03-11 01:47:42 -0300

    mantenedor valor hora/jornada

--- resources/views/parameters/nav.blade.php
+++ resources/views/parameters/nav.blade.php
@@ -22,11 +22,11 @@
             href="{{ route('parameters.permissions.index', 'web') }}">
             <i class="fas fa-chalkboard-teacher"></i> Permisos Internos</a>
     </li>
-
+    
     <li class="nav-item">
         <a class="nav-link"
             href="{{ route('parameters.permissions.index', 'external') }}">
-            <i class="fas fa-chalkboard-teacher"></i> Permisos Externos</a>
+            <i class="fas fa-external-link-alt"></i> Permisos Externos</a>
     </li>
 
     <li class="nav-item">
@@ -51,5 +51,11 @@
         <a class="nav-link"
             href="{{ route('parameters.phrases.index') }}">
             <i class="fas fa-smile-beam"></i> Frases del día</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link"
+            href="{{ route('parameters.values.index') }}">            
+            <i class="fas fa-money-bill-alt"></i> Valor Hora/Jornada</a>
     </li>
 </ul>
