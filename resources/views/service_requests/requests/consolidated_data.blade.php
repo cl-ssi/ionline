9eec55129a0c187675633611d66fe6aaeb6fbf89
Esteban <sick_iqq@hotmail.com>
2021-03-11 13:18:12 -0300

    descargar excel consolidado

--- resources/views/service_requests/requests/consolidated_data.blade.php
+++ resources/views/service_requests/requests/consolidated_data.blade.php
@@ -4,29 +4,36 @@
 
 @section('content')
 
-<h3 class="mb-3">Reporte consolidado</h3>
+	<h3 class="mb-3">Reporte consolidado</h3>
 
-<form method="GET" class="form-horizontal" action="{{ route('rrhh.service_requests.consolidated_data') }}">
-	<div class="input-group mb-3">
-		<div class="input-group-prepend">
-			<span class="input-group-text">Rango de fechas (Inicio de contrato)</span>
+	<form method="GET" class="form-horizontal" action="{{ route('rrhh.service_requests.consolidated_data') }}">
+		<div class="input-group mb-3">
+			<div class="input-group-prepend">
+				<span class="input-group-text">Rango de fechas (Inicio de contrato)</span>
+			</div>
+			<!-- <input type="date" class="form-control" id="for_dateFrom" name="dateFrom" value="2021-01-01" required >
+			<input type="date" class="form-control" id="for_dateTo" name="dateTo" value="2021-01-31" required> -->
+			<input type="text" value="Todos los datos" disabled>
+			<div class="input-group-append">
+					<button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Buscar</button>
+			</div>
 		</div>
-		<!-- <input type="date" class="form-control" id="for_dateFrom" name="dateFrom" value="2021-01-01" required >
-		<input type="date" class="form-control" id="for_dateTo" name="dateTo" value="2021-01-31" required> -->
-		<input type="text" value="Todos los datos" disabled>
-    <div class="input-group-append">
-        <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Buscar</button>
-    </div>
-	</div>
-</form>
+	</form>
 
 <hr>
 
 </main><main class="py-4">
 
-<h4>Solicitudes Activas <b>({{count($serviceRequests)}})</b></h4>
+	<h4>Solicitudes Activas <b>({{count($serviceRequests)}})</b></h4><br>
+	<!-- <a type="button"
+		 class="btn btn-outline-success" onclick="fnExcelReport();">
+		 Descargar
+		 <i class="far fa-file-excel"></i>
+	 </a><br> -->
+	 <a class="btn btn-outline-success btn-sm mb-3" id="downloadLink" onclick="exportF(this)">Descargar en excel</a>
 
-<table class="table table-sm table-bordered table-responsive small" >
+<iframe id="txtArea1" style="display:none"></iframe>
+<table class="table table-sm table-bordered table-responsive small" id="table_id">
     <thead>
         <tr class="text-center">
             <th>ID</th>
@@ -100,7 +107,7 @@
 							</tr>
 						@else
 							@if($fulfillment->bill_number != NULL || $fulfillment->total_hours_paid != NULL || $fulfillment->total_paid != NULL ||
-						      $fulfillment->payment_date != NULL || $fulfillment->contable_month != NULL))
+						      $fulfillment->payment_date != NULL || $fulfillment->contable_month != NULL)
 									<tr class="table-success">
 										<td nowrap>{{$serviceRequest->id}}</td>
 										<td nowrap>{{$serviceRequest->contract_number}}</td>
@@ -292,6 +299,14 @@
 
 @section('custom_js_head')
 <script type="text/javascript">
-
+    function exportF(elem) {
+        var table = document.getElementById("table_id");
+        var html = table.outerHTML;
+        var html_no_links = html.replace(/<a[^>]*>|<\/a>/g, ""); //remove if u want links in your table
+        var url = 'data:application/vnd.ms-excel,' + escape(html_no_links); // Set your html table into url
+        elem.setAttribute("href", url);
+        elem.setAttribute("download", "reporte_consolidado.xls"); // Choose the file name
+        return false;
+    }
 </script>
 @endsection
