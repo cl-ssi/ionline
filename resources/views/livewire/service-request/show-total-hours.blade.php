f38bdc2f4d097a378d173d7c0ce2acd399b93044
GermanZun <german.zuniga@gmail.com>
2021-03-12 12:47:27 -0300

    amount from service request, error msg

--- resources/views/livewire/service-request/show-total-hours.blade.php
+++ resources/views/livewire/service-request/show-total-hours.blade.php
@@ -1,18 +1,29 @@
-<table class="table table-sm">
-    <thead>
-    <tr>
-        <th>Horas Diurno</th>
-        <th>Horas Nocturno</th>
-        <th>Horas Total</th>
-        <th>Monto</th>
-    </tr>
-    </thead>
-    <tbody>
+@if(!$errorMsg)
+    <table class="table table-sm">
+        <thead>
+        <tr>
+            <th>Horas Diurno</th>
+            <th>Horas Nocturno</th>
+            <th>Horas Total</th>
+            <th>Monto</th>
+        </tr>
+        </thead>
+        <tbody>
         <tr>
             <td>{{$totalHoursDay}}</td>
             <td>{{$totalHoursNight}}</td>
             <td>{{$totalHours}}</td>
             <td>{{ '$'.number_format($totalAmount, 0, ',', '.') }}</td>
         </tr>
-    </tbody>
-</table>
+        </tbody>
+    </table>
+
+@else
+    <div>
+        <div class="alert alert-warning" role="alert">
+            {{$errorMsg}}
+        </div>
+    </div>
+@endif
+
+
