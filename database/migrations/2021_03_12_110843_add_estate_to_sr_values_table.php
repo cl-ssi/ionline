390ca2e9fe4d97e5814c6bf17ec0910dec0346fb
GermanZun <german.zuniga@gmail.com>
2021-03-12 11:17:38 -0300

    add estate

---
+++ database/migrations/2021_03_12_110843_add_estate_to_sr_values_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddEstateToSrValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sr_values', function (Blueprint $table) {
+            $table->enum('estate', ['Profesional Médico', 'Profesional', 'Técnico', 'Administrativo', 'Farmaceutico', 'Odontólogo', 'Bioquímico', 'Auxiliar', 'Otro (justificar)'])
+                ->after('validity_from')
+                ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sr_values', function (Blueprint $table) {
+            $table->dropColumn('estate');
+        });
+    }
+}
