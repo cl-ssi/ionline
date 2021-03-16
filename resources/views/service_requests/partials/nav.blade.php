e7cf6427a70340448c2bacbd4651f591d64711f8
AquaroTorres <alvarotorres@gmail.com>
2021-03-16 15:11:02 -0300

    Active para los menus

--- resources/views/service_requests/partials/nav.blade.php
+++ resources/views/service_requests/partials/nav.blade.php
@@ -2,7 +2,7 @@
     
 
     <li class="nav-item">
-        <a class="nav-link"
+        <a class="nav-link {{ active('rrhh.service_requests.index') }}"
             href="{{ route('rrhh.service_requests.index') }}">
             <i class="fas fa-pencil-alt"></i> Solicitudes
             <span class="badge badge-secondary">{{ App\Models\ServiceRequests\ServiceRequest::getPendingRequests() }}</span>
@@ -11,7 +11,7 @@
 
     @canany(['Service Request: fulfillments'])
     <li class="nav-item">
-        <a class="nav-link"
+        <a class="nav-link {{ active('rrhh.fulfillments.index') }}"
             href="{{ route('rrhh.fulfillments.index') }}">
             <i class="fas fa-clipboard-check"></i> Cumplimientos
         </a>
@@ -20,7 +20,7 @@
 
     @canany(['Service Request: additional data'])
     <li class="nav-item">
-        <a class="nav-link"
+        <a class="nav-link {{ active('rrhh.service_requests.aditional_data_list') }}"
             href="{{ route('rrhh.service_requests.aditional_data_list') }}">
             <i class="fas fa-file-alt"></i> Información adicional
         </a>
@@ -28,19 +28,20 @@
     @endcan
 
     <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle {{ active('rrhh.service_requests.report.*') }}" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-archive"></i> Reportes
         </a>
         <div class="dropdown-menu">
             
             @canany(['Service Request: pending requests'])
-            <a class="dropdown-item"
+            <a class="dropdown-item {{ active('rrhh.service_requests.pending_requests') }}"
                 href="{{ route('rrhh.service_requests.pending_requests') }}">
                 <i class="fas fa-bomb"></i> Estado de firmas
             </a>
             @endcan
             
-            <a class="dropdown-item" href="{{ route('rrhh.service_requests.report.toPay') }}">
+            <a class="dropdown-item {{ active('rrhh.service_requests.report.toPay') }}" 
+                href="{{ route('rrhh.service_requests.report.toPay') }}">
                 <i class="fas fa-file-invoice-dollar"></i> Reporte para pagos
             </a>
 
