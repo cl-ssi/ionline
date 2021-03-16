caa716ffc08438f0a1330b49b376e34000659014
Esteban <sick_iqq@hotmail.com>
2021-03-16 18:27:44 -0300

    fix report

--- app/Http/Controllers/ServiceRequests/ReportController.php
+++ app/Http/Controllers/ServiceRequests/ReportController.php
@@ -20,12 +20,18 @@ class ReportController extends Controller
         // $week = $request->week;
         // $current_week = $now->format('Y').'-W'.$now->week();
 
-        $current_week = $request->week;
+        if ($request->week != NULL) {
+          $current_week = $request->week;
+        }else{
+          $current_week = date('Y').'-W'.date('W');
+        }
+
         $now = Carbon::now();
-        list($year, $week) = explode('-W',$request->week);
+        list($year, $week) = explode('-W',$current_week);
         $now->setISODate($year,$week);
         $from = $now->startOfWeek()->format('Y-m-d 00:00:00');
         $to   = $now->endOfWeek()->format('Y-m-d 23:59:59');
+
 
         $fulfillments = Fulfillment::whereHas("ServiceRequest", function($subQuery) use($from,$to){
                                        $subQuery->whereBetween('request_date',[$from,$to]);
