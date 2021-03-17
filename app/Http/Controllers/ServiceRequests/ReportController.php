1a72ca906040fd48612fe015d98dacdeb27eafb2
GermanZun <german.zuniga@gmail.com>
2021-03-17 16:16:26 -0300

    archivo pago banco test

--- app/Http/Controllers/ServiceRequests/ReportController.php
+++ app/Http/Controllers/ServiceRequests/ReportController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ServiceRequests\Fulfillment;
 use Carbon\Carbon;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ReportController extends Controller
 {
@@ -41,4 +43,36 @@ class ReportController extends Controller
         // dd($fulfillments);
         return view('service_requests.reports.to_pay', compact('current_week','fulfillments'));
     }
+
+    public function bankPaymentFile($selected_week)
+    {
+        $now = Carbon::now();
+        list($year, $week) = explode('-W',$selected_week);
+        $now->setISODate($year,$week);
+        $from = $now->startOfWeek()->format('Y-m-d 00:00:00');
+        $to   = $now->endOfWeek()->format('Y-m-d 23:59:59');
+        $fromFormatted = $now->startOfWeek()->format('d-m-Y');
+
+        $fulfillments = Fulfillment::whereHas("ServiceRequest", function($subQuery) use($from,$to){
+            $subQuery->whereBetween('request_date',[$from,$to]);
+        })
+            ->get();
+
+        $txt = '';
+        foreach ($fulfillments as $fulfillment) {
+            $txt .= "{$fulfillment->serviceRequest->rut} {$fulfillment->serviceRequest->name} 012 30 1919434511 {$fulfillment->total_paid}\n";
+        }
+
+        $response = new StreamedResponse();
+        $response->setCallBack(function () use($txt) {
+            echo $txt;
+        });
+        $response->headers->set('Content-Type', 'text/plain');
+        $disposition = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, "pago-banco-semana-del-$fromFormatted.txt");
+        $response->headers->set('Content-Disposition', $disposition);
+
+        return $response;
+
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -287,10 +287,10 @@ Route::prefix('rrhh')->as('rrhh.')->group(function () {
 
         Route::get('service_requests/resolution/{ServiceRequest}', 'ServiceRequests\ServiceRequestController@resolution')->middleware('auth');
 
-        
-        
-        
-        
+
+
+
+
 
 
         Route::prefix('shift-control')->name('shift-control.')->group(function () {
@@ -315,18 +315,18 @@ Route::prefix('rrhh')->as('rrhh.')->group(function () {
 
             Route::get('fulfillments/refuseFulfillment/{fulfillment}', 'ServiceRequests\FulfillmentController@refuseFulfillment')->name('fulfillments.refuseFulfillment')->middleware('auth');
 
-            
 
-            
-            
-            
+
+
+
+
         });
 
         Route::prefix('report')->name('report.')->group(function () {
             // Rutas a los reportes
             Route::get('/to-pay', [ServiceRequests\ReportController::class, 'toPay'])->name('toPay');
 
-            
+
         });
 
         Route::prefix('signature-flow')->name('signature-flow.')->group(function () {
@@ -336,8 +336,8 @@ Route::prefix('rrhh')->as('rrhh.')->group(function () {
         });
     });
     */
-    
-    
+
+
 
     //Route::resource('shift_control', 'ServiceRequests\ShiftControlController')->middleware('auth');
     Route::post('service_requests.derive','ServiceRequests\ServiceRequestController@derive')->name('service_requests.derive')->middleware('auth');
@@ -354,6 +354,7 @@ Route::prefix('rrhh')->as('rrhh.')->group(function () {
     Route::get('filfillments/download-invoice/{fulfillment}','ServiceRequests\FulfillmentController@downloadInvoice')->name('fulfillments.download.invoice')->middleware('auth');
     Route::get('filfillments/download-resolution/{fulfillment}','ServiceRequests\FulfillmentController@downloadResolution')->name('fulfillments.download.resolution')->middleware('auth');
     Route::get('service_requests/report/to-pay','ServiceRequests\ReportController@toPay')->name('service_requests.report.toPay')->middleware('auth');
+    Route::get('service_requests/report/bank-payment-file/{selected_week}','ServiceRequests\ReportController@bankPaymentFile')->name('service_requests.report.bankPaymentFile')->middleware('auth');
 
     Route::resource('fulfillments', 'ServiceRequests\FulfillmentController')->middleware('auth');
     Route::get('fulfillments/certificate-pdf/{fulfillment}', 'ServiceRequests\FulfillmentController@certificatePDF')->name('fulfillments.certificate-pdf')->middleware('auth');
@@ -365,7 +366,7 @@ Route::prefix('rrhh')->as('rrhh.')->group(function () {
     Route::get('service_requests/resolution/{ServiceRequest}', 'ServiceRequests\ServiceRequestController@resolution')->middleware('auth');
     Route::get('service_requests/resolution-pdf/{ServiceRequest}', 'ServiceRequests\ServiceRequestController@resolutionPDF')->name('service_requests.resolution-pdf')->middleware('auth');
     Route::resource('signature_flow', 'ServiceRequests\SignatureFlowController')->middleware('auth');
-    
+
     Route::resource('authorities', 'Rrhh\AuthorityController')->middleware(['auth']);
 
     Route::prefix('organizational-units')->name('organizational-units.')->group(function () {
