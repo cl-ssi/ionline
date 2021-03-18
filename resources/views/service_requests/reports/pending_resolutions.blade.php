abf882f804cd85c8ee0aa533d80da043b3a3ffe7
Esteban <sick_iqq@hotmail.com>
2021-03-18 10:12:31 -0300

    fix report pending resolutions

--- resources/views/service_requests/reports/pending_resolutions.blade.php
+++ resources/views/service_requests/reports/pending_resolutions.blade.php
@@ -11,23 +11,31 @@
 <table class="table table-sm table-bordered">
     <tr>
         <th>Id</th>
-        <th>Tipo</th>
-        <th>Jornada</th>
         <th nowrap>Rut</th>
         <th>Nombre</th>
-        <th>Resolución</th>
+        <th scope="col">F. Inicio</th>
+        <th scope="col">F. Término</th>
+        <th>Tipo</th>
+        <th>Jornada</th>
         <th></th>
     </tr>
     @foreach($serviceRequests as $key => $serviceRequest)
       <tr>
           <td>{{$serviceRequest->id}}</td>
-          <td>{{$serviceRequest->program_contract_type}}</td>
-          <td>{{$serviceRequest->working_day_type}}</td>
           <td>{{$serviceRequest->rut}}</td>
           <td>{{$serviceRequest->name}}</td>
-          <td>
-
-          </td>
+          <td nowrap>{{ $serviceRequest->start_date->format('d-m-Y') }}</td>
+    			<td nowrap>{{ $serviceRequest->end_date->format('d-m-Y') }}</td>
+          <td>{{$serviceRequest->program_contract_type}}</td>
+          <td>{{$serviceRequest->working_day_type}}</td>
+          <!-- <td>
+            @if($serviceRequest->has_resolution_file)
+              <a href="{{route('rrhh.fulfillments.download.resolution', $serviceRequest)}}"
+                 target="_blank" class="mr-4">
+                 <i class="fas fa-paperclip"></i>
+              </a>
+            @endif
+          </td> -->
           <td>
               <a href="{{ route('rrhh.fulfillments.edit_fulfillment',$serviceRequest) }}">
       					<span class="fas fa-edit" aria-hidden="true"></span>
