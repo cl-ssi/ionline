bf2ae9900777c32525902f2939613fae7f188902
AquaroTorres <alvarotorres@gmail.com>
2021-03-18 09:56:34 -0300

    SR has_resolution_file to servicerequest

--- resources/views/service_requests/requests/fulfillments/edit_hours_others.blade.php
+++ resources/views/service_requests/requests/fulfillments/edit_hours_others.blade.php
@@ -56,8 +56,7 @@
 
     <div class="row">
         <div class="col-12 col-md-5">
-          <strong>Cargar Resolución:</strong>
-          @livewire('service-request.upload-resolution', ['fulfillment' => $serviceRequest->fulfillments->first()])
+
         </div>
         <div class="col-12 col-md-5">
           <strong>Cargar Boleta:</strong>
