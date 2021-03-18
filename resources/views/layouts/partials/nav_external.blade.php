1f1233d8aaf9054a40d2b4ba4f10913567454d7d
Esteban <tebiccr@gmail.com>
2021-03-18 01:37:37 -0300

    login external como el unisalud

--- resources/views/layouts/partials/nav_external.blade.php
+++ resources/views/layouts/partials/nav_external.blade.php
@@ -1,67 +1,75 @@
-<nav class="navbar navbar-expand-md navbar-dark shadow-sm bg-nav-gobierno">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
+<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-1 mb-1 text-muted">
+    <span>Mi información</span>
+    <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
+    <span data-feather="plus-circle"></span>
+    </a>
+</h6>
+<ul class="nav flex-column">   
+    <li class="nav-item">
+        <a class="nav-link {{ active('profile.observation.index') }}" href="#">
+        <span data-feather="user"></span>
+        Mis exámenes<span class="sr-only"></span>
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link {{ active(['profile.show', 'profile.edit']) }}" href="#">
+        <span data-feather="user"></span>
+        Mi perfíl<span class="sr-only"></span>
+        </a>
+    </li>
+</ul>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
 
-            <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-book-reader"></i> Ver Test de Idoneidad Pendientes
-                    </a>
-                    @php
-                    $psirequests = App\Models\Suitability\PsiRequest::where('user_id',Auth::id())->where('status','Esperando Test')->get();
-                    @endphp
-                    @foreach($psirequests as $psirequest)
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item"
-                           href="{{ route('suitability.test',$psirequest->id) }}">
-                            <i class="fas fa-desktop fa-fw"></i> Solicitud N° {{$psirequest->id}} para el cargo de {{$psirequest->job}}
-                        </a>
-                        </div>
-                    @endforeach
-                    
-                </li>
-                
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar Sesión') }}</a>
-                    </li>
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->firstName }} <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">                              
+<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-1 mb-1 text-muted">
+    <span>Pacientes</span>
+    <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
+    <span data-feather="plus-circle"></span>
+    </a>
+</h6>
+<ul class="nav flex-column">
+    <li class="nav-item">
+        <a class="nav-link {{ active('patient.index') }}" href="#">
+        <span data-feather="users"></span>
+        Ver todos<span class="sr-only">(current)</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link {{ active('patient.create') }}" href="#">
+        <span data-feather="plus-circle"></span>
+        Ingresar nuevo
+        </a>
+    </li>
+</ul>
 
 
 
-                            
+<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-1 mb-1 text-muted">
+    <span>Administrador</span>
+    <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
+    <span data-feather="plus-circle"></span>
+    </a>
+</h6>
+<ul class="nav flex-column">
+    <li class="nav-item">
+        <a class="nav-link {{ active('parameter.permission.index') }}" href="#">
+        <span data-feather="lock"></span>
+        Permisos<span class="sr-only">(current)</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link {{ active('user.edit') }}" href="#">
+        <span data-feather="unlock"></span>
+        Mis permisos
+        </a>
+    </li>
+</ul>
 
-                            <a class="dropdown-item" role="button" onclick="logout()" id="cierreSesion" href="{{ route('logout') }}">
-                                {{ __('Cerrar sesión') }}
-                            </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </div>
-    </div>
-</nav>
+<ul class="nav flex-column">   
+    <li class="nav-item border-top">
+        <a class="nav-link" href="#">
+        <span data-feather="log-out"></span>
+        Cerrar sesión
+        </a>
+    </li>
+</ul>
