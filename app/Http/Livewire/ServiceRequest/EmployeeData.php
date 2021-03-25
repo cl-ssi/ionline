df659170db41030fa07d4e3c2adadf68510f3187
AquaroTorres <alvarotorres@gmail.com>
2021-03-25 10:18:37 -0300

    SR: bank account en create

--- app/Http/Livewire/ServiceRequest/EmployeeData.php
+++ app/Http/Livewire/ServiceRequest/EmployeeData.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\ServiceRequest;
 
 use Livewire\Component;
 use App\Models\ServiceRequests\ServiceRequest;
+use App\Models\Parameters\Bank;
 
 class EmployeeData extends Component
 {
@@ -13,11 +14,13 @@ class EmployeeData extends Component
     {
         $var = $this->var;
         $ServiceRequest = new ServiceRequest();
+        $banks = Bank::all();
+        
         if ($var > 3000000) {
           $ServiceRequest = ServiceRequest::where('rut','like',$var.'%')->whereNotNull('address')->first();
           // dd($ServiceRequest);
         }
 
-        return view('livewire.service-request.employee-data',compact('ServiceRequest'));
+        return view('livewire.service-request.employee-data',compact('ServiceRequest','banks'));
     }
 }
