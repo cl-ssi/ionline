a7bf24ccf6ad9435f785cbe95df31ffdd108c00f
GermanZun <german.zuniga@gmail.com>
2021-03-25 17:40:37 -0300

    report with resolucion add fecha pago , nro.

--- resources/views/service_requests/reports/index_with_resolution_file.blade.php
+++ resources/views/service_requests/reports/index_with_resolution_file.blade.php
@@ -10,17 +10,21 @@
 
 <table class="table table-sm table-bordered">
     <tr>
-        <th>Id</th>
+        <th>Nro.</th>
+        <th>Id. solicitud</th>
         <th>Nombre</th>
         <th scope="col">F. Inicio</th>
+        <th scope="col">F. Pago</th>
         <th>Boleta</th>
     </tr>
     @foreach($serviceRequests as $key => $serviceRequest)
         <tr>
+            <td rowspan="{{$serviceRequest->fulfillments->count()}}">{{$key + 1}}</td>
             <td rowspan="{{$serviceRequest->fulfillments->count()}}">{{$serviceRequest->id}}</td>
             <td rowspan="{{$serviceRequest->fulfillments->count()}}">{{$serviceRequest->name}}</td>
             @foreach($serviceRequest->fulfillments as $fulfillment)
                 <td nowrap>{{ $fulfillment->start_date->format('d-m-Y') }}</td>
+                <td nowrap>{{ ($fulfillment->payment_date) ? $fulfillment->payment_date->format('d-m-Y') : ''}}</td>
                 <td> @if($fulfillment->has_invoice_file == 1) Sí @else No @endif</td>
             </tr>
         @endforeach
