6869cb299da4983d1f99146b5ca523665bf403bc
AquaroTorres <alvarotorres@gmail.com>
2021-03-28 12:37:36 -0300

    Command Parte Files Update

---
+++ app/Console/Commands/ParteFiles.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Documents\Parte;
+
+class ParteFiles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'parte:files';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $partes = Parte::whereHas('files')->get();
+        foreach($partes as $parte) {
+            foreach($parte->files as $file) {
+                list($folder,$name) = explode('/',$file->file);
+                echo $name."\n";
+                $file = Storage::disk('local')->get($file->file);
+                Storage::disk('gcs')->put('ionline/partes/'.$name, $file);
+            }
+        }
+        return 0;
+    }
+}
