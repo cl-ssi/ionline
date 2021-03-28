7829d9d72ac41b44c0ed17685bdac61614f3b2a6
AquaroTorres <atorresf@gmail.com>
2021-03-28 18:43:40 -0300

    update files to gcs

--- app/Console/Commands/DocumentsFiles.php
+++ app/Console/Commands/DocumentsFiles.php
@@ -3,8 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use app\Documents\Document;
 use Illuminate\Support\Facades\Storage;
+use App\Documents\Document;
 
 class DocumentsFiles extends Command
 {
@@ -42,10 +42,11 @@ class DocumentsFiles extends Command
         $documents = Document::whereNotNull('file')->get();
         foreach($documents as $document) {
             list($folder,$name) = explode('/',$document->file);
-            echo $name."\n";
             $file = Storage::disk('local')->get($document->file);
             Storage::disk('gcs')->put('ionline/documents/documents/'.$name, $file);
-            $document->update(['file' => 'ionline/documents/documents/'.$name]);
+	    $document->file = 'ionline/documents/documents/'.$name;
+	    $document->save();
+	    echo $document->file."\n";
         }
         return 0;
     }
