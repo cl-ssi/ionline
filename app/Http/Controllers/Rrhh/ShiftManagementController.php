af7624041cb7c756529d2ab424b1450ba6f46577
Armando Barra P <armando.barra.p@gmail.com>
2021-03-30 10:22:57 -0300

    controlador e index para la gestion de turnos

---
+++ app/Http/Controllers/Rrhh/ShiftManagementController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Rrhh;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\User;
+use Spatie\Permission\Models\Role;
+
+class ShiftManagementController extends Controller
+{
+     public function index(Request $request)
+    {
+    	// echo "Shift Management";
+    	$users = User::Search($request->get('name'))->orderBy('name','Asc')->paginate(500);
+        return view('rrhh.shift_management.index', compact('users'));
+    }
+ 
+
+}
