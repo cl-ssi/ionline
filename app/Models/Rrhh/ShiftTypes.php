980d11c426a672bff71de717d278fafd1464513b
Armando Barra P <armando.barra.p@gmail.com>
2021-03-30 15:56:35 -0300

    add model shiftTypes and seeder

---
+++ app/Models/Rrhh/ShiftTypes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Rrhh;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShiftTypes extends Model
+{
+    use HasFactory;
+    protected $fillable = [ 'name', 'shortname', 'day_series', 'status'];
+	protected $table = 'rrhh_shift_types';
+}
