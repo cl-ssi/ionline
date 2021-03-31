d07e6096364b7763f683c04744482a112716f911
Esteban <tebiccr@gmail.com>
2021-03-31 17:04:50 -0300

    mantenedor profesiones

---
+++ app/Http/Controllers/Parameters/ProfessionController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Parameters;
+
+use App\Http\Controllers\Controller;
+use App\Models\Parameters\Profession;
+use Illuminate\Http\Request;
+
+class ProfessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $professions = Profession::All();
+        return view('parameters.professions.index', compact('professions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('parameters.professions.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $profession = new Profession($request->All());
+        $profession->save();
+        session()->flash('success', 'Professión Creada');
+        return redirect()->route('parameters.professions.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Paramaters\Professional  $professional
+     * @return \Illuminate\Http\Response
+     */
+    
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Paramaters\Professional  $professional
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Profession $profession)
+    {
+        return view('parameters.professions.edit', compact('profession'));
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Paramaters\Professional  $professional
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Profession $profession)
+    {
+        //
+        $profession->fill($request->all());
+        $profession->save();
+        session()->flash('success', 'Professión: '.$profession->name.' ha sido actualizado.');
+        return redirect()->route('parameters.professions.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Paramaters\Professional  $professional
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Professional $professional)
+    {
+        //
+    }
+}
---
+++ resources/views/parameters/professions/edit.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('title', 'Crear nuevo Permisos')
+
+@section('content')
+
+@include('parameters/nav')
+
+<h3 class="mb-3">Editar Professión</h3>
+
+<form method="POST" class="form-horizontal" action="{{ route('parameters.professions.update', $profession) }}">
+    @csrf
+    @method('PUT')
+
+    <div class="row">
+
+    <fieldset class="form-group col-6 col-md-3">
+            <label for="for_name">Nombre*</label>
+            <input type="text" class="form-control" id="for_name" name="name" value="{{$profession->name}}" autocomplete="off" required>
+        </fieldset>        
+
+        <fieldset class="form-group col-12 col-md-7">
+            <label for="for_category">Categoría</label>
+            <input type="text" class="form-control" id="for_category" name="category" value="{{$profession->category}}" autocomplete="off">
+        </fieldset>
+    </div>
+    
+
+    <button type="submit" class="btn btn-primary">Actualizar</button>
+
+</form>
+
+@endsection
+
+@section('custom_js')
+
+@endsection
