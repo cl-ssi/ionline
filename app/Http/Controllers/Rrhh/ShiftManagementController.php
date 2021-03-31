922e2f700f0405ed8502452154eaf4db2e19ad30
Armando Barra P <armando.barra.p@gmail.com>
2021-03-31 17:43:48 -0300

    added new interfaces for shift types, and modify view shift manag index

--- app/Http/Controllers/Rrhh/ShiftManagementController.php
+++ app/Http/Controllers/Rrhh/ShiftManagementController.php
@@ -11,13 +11,18 @@ use Spatie\Permission\Models\Role;
 
 class ShiftManagementController extends Controller
 {
-     public function index(Request $request)
+     public function index(Request $r)
     {
     	// echo "Shift Management";
-    	$users = User::Search($request->get('name'))->orderBy('name','Asc')->paginate(500);
+    	$users = User::Search($r->get('name'))->orderBy('name','Asc')->paginate(500);
         return view('rrhh.shift_management.index', compact('users'));
     }
- 	
+ 	public function indexfiltered(Request $r){
+
+        
+        return view('rrhh.shift_management.index', compact('users'));
+
+ 	}
  	public function shiftstypesindex()
     {
         // return view('rrhh.shift_management.shiftstypes', compact('users'));
@@ -39,7 +44,15 @@ class ShiftManagementController extends Controller
 
     }
     public function newshifttype(){
-    	echo "create";
+    	// echo "create";
+    	$tiposJornada =   array(
+    							'F' => "Libre",
+								'D' => "Dia",
+								'L' => "Largo",
+								'N' => "Noche"
+						);
+        return view('rrhh.shift_management.createshifttype', compact('tiposJornada'));
+
     }
     public function storenewshift(Request $r){
 
@@ -47,6 +60,10 @@ class ShiftManagementController extends Controller
         $nSType->name = $r->name;
         $nSType->shortname = $r->shortname;
         $nSType->day_series = implode(",", $r->day_series);
+        $nSType->save();
+        session()->flash('info', 'El Turno tipo <i>"'.$r->name.'"</i> ha sido creado.');
+        return redirect()->route('rrhh.shiftsTypes.index');
+
     }
     public function updateshifttype(Request $r){
     	// echo "updateshifttype ".implode(",", $r->day_series); 
@@ -56,7 +73,7 @@ class ShiftManagementController extends Controller
 		$fSType->shortname = $r->shortname;
 		$fSType->day_series = implode(",", $r->day_series);
 		$fSType->update();
-        session()->flash('info', 'El Turno tipo "'.$r->name.'" ha sido modificado.');
+        session()->flash('info', 'El Turno tipo <i>"'.$r->name.'"</i> ha sido modificado.');
         return redirect()->route('rrhh.shiftsTypes.index');
 
     }
