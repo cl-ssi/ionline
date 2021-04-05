a717a11c979cb1ea154c2a987a2df326daa5dd66
Jorge Miranda <mirandal.jorge@gmail.com>
2021-04-05 00:07:00 -0400

    Replacement Staff: Request

---
+++ app/Http/Controllers/RequestSingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RequestSing;
+use Illuminate\Http\Request;
+
+class RequestSingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RequestSing  $requestSing
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RequestSing $requestSing)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RequestSing  $requestSing
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RequestSing $requestSing)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RequestSing  $requestSing
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RequestSing $requestSing)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RequestSing  $requestSing
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RequestSing $requestSing)
+    {
+        //
+    }
+}
