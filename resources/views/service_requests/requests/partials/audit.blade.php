267c541de33d02d582eb2613f5db4c58b379f070
AquaroTorres <alvarotorres@gmail.com>
2021-04-08 17:03:29 -0400

    SR Audit

--- resources/views/service_requests/requests/partials/audit.blade.php
+++ resources/views/service_requests/requests/partials/audit.blade.php
@@ -3,28 +3,25 @@
 <table class="table table-sm small text-muted mt-3">
     <thead>
         <tr>
-            <th>IP</th>
-            <th>URL</th>
-            <th>Modelo</th>
-            <th>Fecha</th>
-            <th>Usuario</th>
             <th>Evento</th>
+            <th>Acción</th>
             <th>Modificaciones</th>
         </tr>
     </thead>
     <tbody>
         @foreach($audits->sortByDesc('updated_at') as $audit)
         <tr>
-            <td>{{ $audit->ip_address }}</td>
-            <td>{{ $audit->url }}</td>
-            <td>{{ $audit->auditable_type }}</td>
-            <td>{{ $audit->created_at }}</td>
             <td>
-            @if ($audit->user)
-                {{ $audit->user->name }}
-            @endif
+                <strong>Usuario:</strong> {{ optional($audit->user)->fullName }}<br>
+                <strong>Fecha:</strong> {{ $audit->created_at }}<br>
+                <strong>Modelo:</strong> {{ $audit->auditable_type }}<br>
+                <strong>Id:</strong> {{ $audit->auditable_id }}<br>
+                <strong>URL:</strong> {{ $audit->url }}<br>
+                <strong>IP:</strong> {{ $audit->ip_address }}
             </td>
-            <td>{{ $audit->event }}</td>
+            <td>
+                {{ $audit->event }}
+            </td>
             <td>
             @if($audit->event != "deleted")
               @foreach($audit->getModified() as $attribute => $modified)
