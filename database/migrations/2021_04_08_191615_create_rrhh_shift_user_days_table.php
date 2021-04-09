84cd01d987c1080215e942e6d37be77dd3674b3a
Armando Barra P <armando.barra.p@gmail.com>
2021-04-09 10:34:03 -0300

    change view to new requeriments, added migrate for shift mngmnt

---
+++ database/migrations/2021_04_08_191615_create_rrhh_shift_user_days_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRrhhShiftUserDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rrhh_shift_user_days', function (Blueprint $table) {
+            $table->id();
+            $table->date('day');
+            $table->string('commentary');
+            $table->integer('status')->comment('they can be 1:assigned;2:completed,3:extra shift,4:shift change5: medical license,6: union jurisdiction,7: legal holiday,8: exceptional permit or did not belong to the service.');
+            
+            $table->unsignedBigInteger('shift_user_id');
+
+
+            $table->foreign('shift_types_id')->references('id')->on('rrhh_shift_users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rrhh_shift_user_days');
+    }
+}
