5d80d29076ea1da62ae2c39b670ae0efe2ac27e1
Armando Barra P <armando.barra.p@gmail.com>
2021-04-14 14:40:46 -0400

    added button and functionality for edit

--- app/Http/Livewire/Rrhh/ListOfShifts.php
+++ app/Http/Livewire/Rrhh/ListOfShifts.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Rrhh;
 
 use Livewire\Component;
+use App\Models\Rrhh\ShiftUserDay;
 
 class ListOfShifts extends Component
 {
@@ -10,6 +11,12 @@ class ListOfShifts extends Component
 	public $actuallyYear;
 	public $actuallyMonth;
 	public $days;
+	public $modalProperty ="hidden";
+
+	public function editStatusDay($sfId){
+		$this->modalProperty = "";
+	}
+
     public function render()
     {
         return view('livewire.rrhh.list-of-shifts');
