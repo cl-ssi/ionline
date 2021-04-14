a1a653370fdf932f0ee04bdca4a434295ece9482
Armando Barra P <armando.barra.p@gmail.com>
2021-04-14 12:06:29 -0400

    add livewire in shiftmanagment index for edit day status

---
+++ app/Http/Livewire/Rrhh/ListOfShifts.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Rrhh;
+
+use Livewire\Component;
+
+class ListOfShifts extends Component
+{
+    public function render()
+    {
+        return view('livewire.rrhh.list-of-shifts');
+    }
+}
