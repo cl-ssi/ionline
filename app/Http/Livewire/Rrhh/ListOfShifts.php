35b58890a7fc4941c7f4df17e2c7558ef01d5bb9
Armando Barra P <armando.barra.p@gmail.com>
2021-04-14 13:12:20 -0400

    replace original list of shifts to livewire

--- app/Http/Livewire/Rrhh/ListOfShifts.php
+++ app/Http/Livewire/Rrhh/ListOfShifts.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class ListOfShifts extends Component
 {
+	public $staffInShift;
+	public $actuallyYear;
+	public $actuallyMonth;
+	public $days;
     public function render()
     {
         return view('livewire.rrhh.list-of-shifts');
--- resources/views/livewire/rrhh/list-of-shifts.blade.php
+++ resources/views/livewire/rrhh/list-of-shifts.blade.php
@@ -1,3 +1,33 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    	@foreach($staffInShift as $sis)
+						<tr>
+						   <td class="bless br" >
+                            <form method="POST" action="{{ route('rrhh.shiftsTypes.deleteassign') }}">
+                                @csrf
+                                @method('POST')
+                                <input hidden name="idAssign" value="{{$sis->id}}">
+                            <button class="only-icon"><i class="fa fa-close" style="color:red"></i></button> {{ $sis->user->runFormat()}} - {{$sis->user->name}}  </td>
+                            </form>
+                          
+						    @for($j = 1; $j <= $days; $j++) 
+
+						    	@php
+						    		$date = \Carbon\Carbon::createFromFormat('Y-m-d',  $actuallyYear."-".$actuallyMonth."-".$j);
+						    		$date =explode(" ",$date);
+
+						    		$d = $sis->days->where('day',$date[0]);
+						    	@endphp
+                            <td  style="text-align:center;width:54px;height:54px">
+                                <div  class="bbd day" >{{ ( isset($d) && count($d) )? ( ($d->first()->working_day!="F")?$d->first()->working_day:"-" ) :"n/a" }}</div>
+                            </td>
+                            @endfor
+                           
+                            
+						</tr>	
+					@endforeach
+                                     
+                   	@if(count($staffInShift)<1)
+                   		<td style="text-align:  center;" colspan="{{$days}}">SIN PERSONAL ASIGNADO</td>
+                   	@endif
+
 </div>
