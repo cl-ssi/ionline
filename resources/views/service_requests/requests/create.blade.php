5945137c351730674d5eecfda36bb559e4f71775
Esteban <sick_iqq@hotmail.com>
2021-04-16 12:49:49 -0400

    add validation control de turnos inicio < termino

--- resources/views/service_requests/requests/create.blade.php
+++ resources/views/service_requests/requests/create.blade.php
@@ -172,11 +172,7 @@
 			            </tr>
 			        </thead>
 			        <tbody>
-			            <!-- <tr>
-			                <td><input type="checkbox" name="record"></td>
-			                <td>Peter Parker</td>
-			                <td>peterparker@mail.com</td>
-			            </tr> -->
+
 			        </tbody>
 			    </table>
 					<button type="button" class="btn btn-primary delete-row">Eliminar filas</button>
@@ -719,6 +715,15 @@
       var start_hour = $("#start_hour").val();
 			var shift_end_date = $("#shift_end_date").val();
 			var end_hour = $("#end_hour").val();
+
+			var start_date = new Date(shift_start_date + ' ' + start_hour);
+			var end_date = new Date(shift_end_date + ' ' + end_hour);
+
+			if (start_date > end_date) {
+				alert("La fecha de salida es menor a la fecha de inicio, revise la información.");
+				return;
+			}
+
 			var observation = $("#observation").val();
       var markup = "<tr><td><input type='checkbox' name='record'></td><td> <input type='hidden' class='form-control' name='shift_start_date[]' id='shift_start_date' value='"+ shift_start_date +"'>"+ shift_start_date +"</td><td> <input type='hidden' class='form-control' name='shift_start_hour[]' id='start_hour' value='"+ start_hour +"'>" + start_hour + "</td><td> <input type='hidden' class='form-control' name='shift_end_date[]' id='shift_end_date' value='"+ shift_end_date +"'>"+ shift_end_date +"</td><td> <input type='hidden' class='form-control' name='shift_end_hour[]' id='end_hour' value='"+ end_hour +"'>" + end_hour + "</td><td> <input type='hidden' class='form-control' name='shift_observation[]' id='observation' value='"+ observation +"'>" + observation + "</td></tr>";
       $("table tbody").append(markup);
