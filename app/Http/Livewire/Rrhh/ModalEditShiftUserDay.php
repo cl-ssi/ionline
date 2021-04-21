ba3d20384c0959e513aa4e50c9a88b03a7c2b7b8
Armando Barra P <armando.barra.p@gmail.com>
2021-04-21 12:04:17 -0400

    added functionality to  shift user day moda edit

---
+++ app/Http/Livewire/Rrhh/ModalEditShiftUserDay.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Livewire\Rrhh;
+
+use Livewire\Component;
+
+class ModalEditShiftUserDay extends Component
+{	
+	public $visibility = "xx";
+
+    public function render()
+    {
+        return view('livewire.rrhh.modal-edit-shift-user-day');
+    }
+}
