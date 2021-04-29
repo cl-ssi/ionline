ebe83077a9fe5be8008f9a28d66467ed257b9892
Jorge Miranda <mirandal.jorge@gmail.com>
2021-04-29 13:14:07 -0400

    fix unidad organizacionales

--- resources/views/requirements/create.blade.php
+++ resources/views/requirements/create.blade.php
@@ -80,28 +80,28 @@
                         @foreach($ouRoots as $ouRoot)
                             @if($ouRoot->name != 'Externos')
                                 <option value="{{ $ouRoot->id }}">
-                                    {{ $ouRoot->name }}
+                                    {{ $ouRoot->name }} ({{($ouRoot->establishment->name)}})
                                 </option>
                                 @foreach($ouRoot->childs as $child_level_1)
 
                                     <option value="{{ $child_level_1->id }}">
                                         &nbsp;&nbsp;&nbsp;
-                                        {{ $child_level_1->name }}
+                                        {{ $child_level_1->name }} ({{($child_level_1->establishment->name)}})
                                     </option>
                                     @foreach($child_level_1->childs as $child_level_2)
                                         <option value="{{ $child_level_2->id }}">
                                             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                            {{ $child_level_2->name }}
+                                            {{ $child_level_2->name }} ({{($child_level_2->establishment->name)}})
                                         </option>
                                         @foreach($child_level_2->childs as $child_level_3)
                                             <option value="{{ $child_level_3->id }}">
                                                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                                {{ $child_level_3->name }}
+                                                {{ $child_level_3->name }} ({{($child_level_3->establishment->name)}})
                                             </option>
                                             @foreach($child_level_3->childs as $child_level_4)
                                                 <option value="{{ $child_level_4->id }}">
                                                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                                    {{ $child_level_4->name }}
+                                                    {{ $child_level_4->name }} ({{($child_level_4->establishment->name)}})
                                                 </option>
                                             @endforeach
                                         @endforeach
