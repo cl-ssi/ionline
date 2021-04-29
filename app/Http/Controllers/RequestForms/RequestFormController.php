a19e3db48906a0bc9a9a953aad66bdac32932e74
oscarzavala <ozavala@gmail.com>
2021-04-28 20:15:45 -0400

    requestForm Controller index change

--- app/Http/Controllers/RequestForms/RequestFormController.php
+++ app/Http/Controllers/RequestForms/RequestFormController.php
@@ -29,8 +29,8 @@ class RequestFormController extends Controller
     public function index()
     {
         //MIS FORMULARIOS DE REQUERIMIENTO
-        $myRequestForms = RequestForm::with(['items','passages', 'requestformfiles', 'requestformevents'])
-            ->where('user_id', Auth::user()->id)->get();
+        $myRequestForms = RequestForm::with('items')
+            ->where('user_id', auth()->user()->id)->get();
         return view('request_form.index', compact('myRequestForms'));
     }
 
