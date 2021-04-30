825a3417e788bd8008de90e2ba45cb448a5b1282
Esteban <tebiccr@gmail.com>
2021-04-30 08:50:00 -0400

    solamente muestra distribución si hay dato

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -128,11 +128,12 @@
             </div>
 
 
-
+            @if($document->distributionHtml != null)
             <div class="firma seis" style="padding-top: 6px; padding-bottom: 8px; display: inline-block; vertical-align:top; width: 49%;">
                 <div style="padding-bottom: 4px;"><strong>DISTRIBUCIÓN:</strong></div>
                 <div style="padding-bottom: 4px;">{!! $document->distributionHtml !!}</div>
             </div>
+            @endif
 
             <div style="padding-bottom: 6px; display: inline-block; vertical-align:top; width: 49%; text-align: right">
 
