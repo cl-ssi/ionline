c215a68b9a4cdbe2befa812d252f9eb2669f95fe
Jorge Miranda <mirandal.jorge@gmail.com>
2021-05-03 19:33:38 -0400

    External nav disabled Replacement Staff

--- resources/views/layouts/partials/nav_external.blade.php
+++ resources/views/layouts/partials/nav_external.blade.php
@@ -42,12 +42,12 @@
 </ul> -->
 
 
-<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-1 mb-1 text-muted">
+<!-- <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-1 mb-1 text-muted">
     <span>Staff de Reemplazo</span>
     <a class="d-flex align-items-center text-muted" href="{{ route('replacement_staff.create') }}" aria-label="Ver Mis datos">
     <span data-feather="plus-circle"></span>
     </a>
-</h6>
+</h6> -->
 <ul class="nav flex-column">
     <!-- <li class="nav-item">
         <a class="nav-link {{ active('patient.index') }}" href="{{ route('replacement_staff.create') }}">
@@ -127,11 +127,11 @@ $psirequests = App\Models\Suitability\PsiRequest::where('user_external_id',Auth:
 </h6>
 
 <ul class="nav flex-column">
-    @foreach($psirequests as $psirequest)    
+    @foreach($psirequests as $psirequest)
     <li class="nav-item">
     <form action="{{ route('idoneidad.updateStatus', $psirequest) }}" method="POST">
         @csrf
-        @method('PATCH')        
+        @method('PATCH')
         <button type="submit" class="btn btn-danger float-left"><i  onclick="return confirm('Al momento de apretar en aceptar, usted tendrá 45 minutos para poder realizar el Test de Idoneidad, no tendrá más oportunidades, luego de realizado este test. Por favor asegurarse que posea buena conexión a internet. ¿Está seguro que desea rendir el test?')">Realizar test para cargo <br>{{$psirequest->job}} ({{$psirequest->school->name}})</i></button>
     </form>
     </li>
