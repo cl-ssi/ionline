6ae1249b084e89182b9fbf6d64d0981feb8f2f49
Esteban <tebiccr@gmail.com>
2021-05-04 19:15:35 -0400

    manuales de usuarios idoneidad

--- routes/web.php
+++ routes/web.php
@@ -89,6 +89,7 @@ Route::group(['middleware' => 'auth:external'], function () {
     Route::view('/external', 'external')->name('external');
     //Route::view('/external', 'external')->name('external');
     Route::prefix('idoneidad')->as('idoneidad.')->group(function(){
+    Route::get('/manual-usuario', [SuitabilityController::class, 'downloadManualUser'])->name('downloadManualUser');
     Route::get('/create/{school}', [SuitabilityController::class, 'createExternal'])->name('createExternal');
     Route::post('/', [SuitabilityController::class, 'storeSuitabilityExternal'])->name('storeSuitabilityExternal');
     Route::get('/list/{school}', [SuitabilityController::class, 'listOwn'])->name('listOwn');
