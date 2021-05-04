653acf55e07554818c604c3c158a3d7d9580c741
Jorge Miranda <mirandal.jorge@gmail.com>
2021-05-04 17:03:58 -0400

    Replacement Staff: Fix External Form

--- app/Http/Livewire/ReplacementStaff/Profile.php
+++ app/Http/Livewire/ReplacementStaff/Profile.php
@@ -8,24 +8,29 @@ class Profile extends Component
 {
     public $inputs = [];
     public $i = 1;
+    public $count = 0;
+
     public $replacementStaff;
     public $professionManage;
     public $profileManage;
-    public $estado = '';
-    //public $usersSelect ='readonly';
-    public $profileSelected =null;
+
+    public $profileSelected = null;
+    public $valueSelected = null;
     public $selectstate = 'disabled';
+
 
     public function add($i)
     {
         $i = $i + 1;
         $this->i = $i;
         array_push($this->inputs ,$i);
+        $this->count++;
     }
 
     public function remove($i)
     {
         unset($this->inputs[$i]);
+        $this->count--;
     }
 
     public function mount($replacementStaff, $professionManage, $profileManage)
@@ -33,33 +38,22 @@ class Profile extends Component
         $this->replacementStaff = $replacementStaff;
         $this->professionManage = $professionManage;
         $this->profileManage = $profileManage;
-        
+
     }
 
     public function updatedprofileSelected($profile_id){
-
-        
-        if($profile_id ==2 or $profile_id ==4)
-        {
-            $this->selectstate = '';        
-
+        if($profile_id == 2 or $profile_id == 4){
+            $this->selectstate = '';
         }
         else{
             $this->selectstate = 'disabled';
-            
-        }   
-        
-        
-        
-
-
+        }
     }
 
     public function render()
     {
-        
-        return view('livewire.replacement-staff.profile', compact($this->replacementStaff, $this->professionManage, $this->profileManage));
-        
+        return view('livewire.replacement-staff.profile',
+            compact($this->replacementStaff, $this->professionManage, $this->profileManage));
     }
 
 }
