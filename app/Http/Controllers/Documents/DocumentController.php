a89144fefda25f56002475340b9c97dd0407e0b2
GermanZun <german.zuniga@gmail.com>
2021-05-05 10:13:08 -0400

    document type preload

--- app/Http/Controllers/Documents/DocumentController.php
+++ app/Http/Controllers/Documents/DocumentController.php
@@ -251,24 +251,43 @@ class DocumentController extends Controller
     {
         $signature = new Signature();
         $signature->request_date = Carbon::now();
-//        $signature->document_type = 'Memorando'; //TODO variable
         $signature->subject = $document->subject;
         $signature->description = $document->antecedent;
-//        $signature->endorse_type = 'Visación en cadena de responsabilidad';
         $signature->recipients = $document->distribution;
+
+        switch ($document->type) {
+            case 'Memo':
+                $signature->document_type = 'Memorando';
+                break;
+            case 'Ordinario':
+            case 'Reservado':
+            case 'Oficio':
+                $signature->document_type = 'Oficio';
+                break;
+            case 'Circular':
+                $signature->document_type = 'Circular';
+                break;
+            case 'Acta de recepción':
+                $signature->document_type = 'Acta';
+                break;
+            case 'Resolución':
+                $signature->document_type = 'Resoluciones';
+                break;
+        }
+
+        if($signature->document_type = 'Memorando')
+
+//        $signature->endorse_type = 'Visación en cadena de responsabilidad';
 //        $signature->distribution = 'División de Atención Primaria MINSAL,Oficina de Partes SSI,'.$municipio;
 
 
         if($document->type == 'Acta de recepción') {
-//            return view('documents.reception')->withDocument($document);
             $documentFile = \PDF::loadView('documents.reception', compact('document'));
         }
         else if($document->type == 'Resolución') {
-//            return view('documents.resolution')->withDocument($document);
             $documentFile = \PDF::loadView('documents.resolution', compact('document'));
         }
         else {
-//            return view('documents.show')->withDocument($document);
             $documentFile = \PDF::loadView('documents.show', compact('document'));
         }
 
@@ -279,10 +298,8 @@ class DocumentController extends Controller
 
         $signature->signaturesFiles->add($signaturesFile);
         $documentId = $document->id;
-//        dd($signature);
 
         return view('documents.signatures.create', compact('signature', 'documentId' ));
-
     }
 
     public function signedDocumentPdf($id)
