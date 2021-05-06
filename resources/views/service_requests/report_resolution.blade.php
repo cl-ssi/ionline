d27dddaac8abbf732fa456c126c1ac92fc746514
Esteban <sick_iqq@hotmail.com>
2021-05-05 20:23:05 -0400

    ADD PHUQHAÑA CONTRATO

--- resources/views/service_requests/report_resolution.blade.php
+++ resources/views/service_requests/report_resolution.blade.php
@@ -132,7 +132,7 @@
     SUBDIRECCIÓN DE GESTIÓN Y DESARROLLO DE LAS PERSONAS
 </div>
 <div class="seis" style="padding-top: 4px;">
-    N.I. {{$ServiceRequest->id}} - {{\Carbon\Carbon::now()->format('d/m/Y')}}
+    N.I.PHUQHAÑA. {{$ServiceRequest->id}} - {{\Carbon\Carbon::now()->format('d/m/Y')}}
 </div>
 
 
