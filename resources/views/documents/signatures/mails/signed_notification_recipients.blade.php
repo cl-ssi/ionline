7b164665108d19591f6a226b9998329ec057fdd0
GermanZun <german.zuniga@gmail.com>
2021-05-10 14:32:16 -0400

    add subject mail signed notification

--- resources/views/documents/signatures/mails/signed_notification_recipients.blade.php
+++ resources/views/documents/signatures/mails/signed_notification_recipients.blade.php
@@ -6,7 +6,7 @@
         <p>Junto con saludar cordialmente.</p>
         <p>Adjunto documento indicado para conocimiento y fines.</p>
         <p> <strong>Tipo:</strong> {{ $signature->document_type }}</p>
-{{--        <p> <strong>Número:</strong> {{ $signature->signaturesFileDocument->id }}</p>--}}
+        <p> <strong>Asunto:</strong> {{ $signature->subject }}</p>
 {{--        <p> <strong>Firma del documento: </strong> {{ $signature->signaturesFlowSigner->signature_date->format('d-m-Y') }} </p>--}}
 {{--        <p> <strong>Archivo:</strong> SSI_{{ $document->type }}_{{ $document->number }}.pdf</p>--}}
         <br>
