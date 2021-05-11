9a79280a3d4323b445f791d663567a484be5c7e8
Alvaro Lupa <alvaro.lupa@gmail.com>
2021-05-11 19:01:29 -0400

    Indicators->MS 19664 enabled link

--- resources/views/indicators/19664/partials/card.blade.php
+++ resources/views/indicators/19664/partials/card.blade.php
@@ -3,11 +3,14 @@
         <strong>Metas Sanitarias Ley N° 19.664</strong>
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item"><a href="{{ route('indicators.19664.2020.index') }}">2020</a></li>
-      <li class="list-group-item"><a href="{{ route('indicators.19664.2019.index') }}">2019</a></li>
-      <li class="list-group-item">
-          <a href="{{ route('indicators.19664.index') }}">Años anteriores</a>
-      </li>
+        <li class="list-group-item">
+            <a href="{{ route('indicators.health_goals.list', [19664, 2021]) }}">2021</a> <span class="badge badge-warning">En Desarrollo</span>
+        </li>
+        <li class="list-group-item"><a href="{{ route('indicators.19664.2020.index') }}">2020</a></li>
+        <li class="list-group-item"><a href="{{ route('indicators.19664.2019.index') }}">2019</a></li>
+        <li class="list-group-item">
+            <a href="{{ route('indicators.19664.index') }}">Años anteriores</a>
+        </li>
       <!-- <li class="list-group-item"><a href="{{ route('indicators.19664.2018.index') }}">2018</a></li> -->
     </ul>
 </div>
