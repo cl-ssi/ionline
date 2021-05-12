eb1b99468ab1f3e111863d1e75c430c860a955dd
Jorge Miranda <mirandal.jorge@gmail.com>
2021-05-12 11:56:29 -0400

    Replacement Staff: Stand By

--- app/Http/Livewire/ReplacementStaff/Training.php
+++ app/Http/Livewire/ReplacementStaff/Training.php
@@ -9,17 +9,20 @@ class Training extends Component
     public $inputs = [];
     public $i = 1;
     public $replacementStaff;
+    public $count = 0;
 
     public function add($i)
     {
         $i = $i + 1;
         $this->i = $i;
         array_push($this->inputs ,$i);
+        $this->count++;
     }
 
     public function remove($i)
     {
         unset($this->inputs[$i]);
+        $this->count--;
     }
 
     public function mount($replacementStaff)
