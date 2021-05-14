24bcdf8b8f6224c099cd18bae1c79dd7bb21b9c6
Alvaro Lupa <alvaro.lupa@gmail.com>
2021-05-14 13:57:24 -0400

    Indicators->MS se agrega Programación médica a la lista de medios de verificación

--- resources/views/indicators/health_goals/ind/edit.blade.php
+++ resources/views/indicators/health_goals/ind/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @php
     $months = array (1=>'Ene',2=>'Feb',3=>'Mar',4=>'Abr',5=>'May',6=>'Jun',7=>'Jul',8=>'Ago',9=>'Sep',10=>'Oct',11=>'Nov',12=>'Dic');
-    $sources_type = array('REM', 'Programación anual', 'Datamart', 'Reporte RRHH', 'Certificado Hospital', 'SIGTE', 'UGCC Minsal', 'GRD Minsal');
+    $sources_type = array('REM', 'Programación médica', 'Programación anual', 'Datamart', 'Reporte RRHH', 'Certificado Hospital', 'SIGTE', 'UGCC Minsal', 'GRD Minsal');
 @endphp
 @section('title', 'Metas Sanitarias Ley N° ' . $healthGoal->law . ' / '. $healthGoal->year . ' - ' . $healthGoal->name)
 
