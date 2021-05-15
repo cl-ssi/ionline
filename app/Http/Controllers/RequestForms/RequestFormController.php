eb5f541fa81676a348d4efc5af1d48379eee2fe8
oscarzavala <ozavala@gmail.com>
2021-05-15 05:08:01 -0400

    filtro requestform con status en controller

--- app/Http/Controllers/RequestForms/RequestFormController.php
+++ app/Http/Controllers/RequestForms/RequestFormController.php
@@ -30,7 +30,7 @@ class RequestFormController extends Controller
     {
         //MIS FORMULARIOS DE REQUERIMIENTO
         //$myRequestForms = RequestForm::where('creator_user_id', auth()->user()->id)->get();
-        $myRequestForms = auth()->user()->applicantRequestForms()->get();
+        $myRequestForms = auth()->user()->applicantRequestForms()->where('status', 'created')->get();
         return view('request_form.index', compact('myRequestForms'));
     }
 
