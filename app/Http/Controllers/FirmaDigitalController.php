6fea9be65f4195f65645e28acd306897552288fa
Alvaro Torres <alvarotorres@gmail.com>
2021-05-17 10:39:12 -0400

    Firma test

--- app/Http/Controllers/FirmaDigitalController.php
+++ app/Http/Controllers/FirmaDigitalController.php
@@ -392,4 +392,152 @@ class FirmaDigitalController extends Controller
             'errorMsg' => '',
         ];
     }
+
+    public function test($otp) {
+        $pdf            = 'samples/original.pdf';
+        $pdfbase64      = base64_encode(file_get_contents(public_path($pdf)));
+        $checksum_pdf   = md5_file(public_path($pdf));
+        $signatureType = 'firmante';
+        $docId = 1;
+        $verificationCode = 'asaasf';
+
+
+        $actualDate = now()->format('d-m-Y H:i:s');
+        $fullName = Auth::user()->full_name;
+
+
+        /* Confección del cuadro imagen de la firma */
+        $font_light     = public_path('fonts/verdana-italic.ttf');
+        $font_bold      = public_path('fonts/verdana-bold-2.ttf');
+        $font_regular   = public_path('fonts/Verdana.ttf');
+
+        $marginTop  = 1;
+        $xAxis      = 5;
+        $yPading    = 16;
+        $fontSize   = 10;
+
+        if ($signatureType === 'firmante' || $visatorAsSignature === true) {
+            $im = @imagecreate(480, 80) or die("Cannot Initialize new GD image stream");
+            $background_color = imagecolorallocate($im, 204, 204, 204);
+            $white = imagecolorallocate($im, 255, 255, 255);
+            imagefilledrectangle($im, 1, 1, 398, 78, $white);
+            $text_color = imagecolorallocate($im, 0, 0, 0);
+
+            imagettftext($im, $fontSize, 0, $xAxis, $yPading * 1 + $marginTop,
+                $text_color, $font_light, "Firmado digitalmente de acuerdo con la ley Nº 19.799");
+            imagettftext($im, $fontSize + 1, 0, $xAxis, $yPading * 2 + $marginTop + 2,
+                $text_color, $font_bold, $fullName);
+            imagettftext($im, $fontSize, 0, $xAxis, $yPading * 3 + $marginTop + 3,
+                $text_color, $font_regular, env('APP_SS'));
+            imagettftext($im, $fontSize, 0, $xAxis, $yPading * 4 + $marginTop + 4,
+                $text_color, $font_regular, $actualDate . ($signatureType === 'firmante' ? "- ID: $docId - Código: $verificationCode" : ''));
+        } else {
+            $im = @imagecreate(400, 40) or die("Cannot Initialize new GD image stream");
+            // $background_color = imagecolorallocate($im, 204, 204, 204);
+            $white = imagecolorallocate($im, 255, 255, 255);
+            imagefilledrectangle($im, 0, 0, 400, 40, $white);
+            $text_color = imagecolorallocate($im, 0, 0, 0);
+            imagettftext($im, $fontSize, 0, $xAxis, $yPading * 1 + $marginTop,
+                $text_color, $font_light, Str::upper(Auth::user()->initials) . ' - ' . Str::upper(Auth::user()->organizationalUnit->initials));
+        }
+
+        $firma_gob = imagecreatefrompng(public_path('images/firma_gobierno_80.png'));
+        //imagecopyresized($thumb, $origen, 0, 0, 0, 0, $nuevo_ancho, $nuevo_alto, $ancho, $alto);
+        //( $dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct )
+        imagecopymerge($im, $firma_gob, 400, 0, 0, 0, 80, 84, 100);
+
+        /* Obtener Imagen de firma en variable $firma */
+        ob_start();
+        imagepng($im);
+        $firma = base64_encode(ob_get_clean());
+        imagedestroy($im);
+
+        $url = env('FIRMA_URL');
+        $api_token = env('FIRMA_API_TOKEN');
+        $secret = env('FIRMA_SECRET');
+        $otp = $otp;
+        $run = Auth::id();
+
+        $purpose = 'Propósito General';
+        $entity = 'Servicio de Salud Iquique';
+        /* Confección firma en JWT */
+        $payload = [
+            "purpose" => $purpose,
+            "entity" => $entity,
+            "expiration" => now()->add(30, 'minutes')->format('Y-m-d\TH:i:s'),
+            "run" => $run
+        ];
+
+        $jwt = JWT::encode($payload, $secret);
+        //die($jwt);
+
+        if ($signatureType == 'visador') {
+            // $ct_firmas = $signaturesFlow->signature->signaturesFlows->where('type', 'visador')->count();
+            // $pocision_firma = $signaturesFlow->sign_position;
+
+            if ($visatorAsSignature === true) {
+                $padding = 50;
+                $alto = 55;
+            } else {
+                $padding = 25;
+                $alto = 26;
+            }
+            $coordenada_x = 65;
+            $coordenada_y = 50 + $padding * $ct_firmas_visator - ($posicion_firma * $padding);
+            $ancho = 170 * 1.4;
+        } else if ($signatureType == 'firmante') {
+            $coordenada_x = 310;
+            $coordenada_y = 49;
+            $ancho = 170 * 1.4;
+            $alto = 55;
+        }
+
+        //header("Content-type: image/png");
+        //echo base64_decode($firma);
+
+        $data = [
+            'api_token_key' => $api_token,
+            'token' => $jwt,
+            'files' => [
+                [
+                    'content-type' => 'application/pdf',
+                    'content' => $pdfbase64,
+                    'description' => 'str',
+                    'checksum' => $checksum_pdf,
+                    'layout' => "
+                        <AgileSignerConfig>
+                            <Application id=\"THIS-CONFIG\">
+                                <pdfPassword/>
+                                <Signature>
+                                    <Visible active=\"true\" layer2=\"false\" label=\"true\" pos=\"1\">
+                                        <llx>" . $coordenada_x . "</llx>
+                                        <lly>" . $coordenada_y . "</lly>
+                                        <urx>" . ($coordenada_x + $ancho) . "</urx>
+                                        <ury>" . ($coordenada_y + $alto) . "</ury>
+                                        <page>LAST</page>
+                                        <image>BASE64</image>
+                                        <BASE64VALUE>$firma</BASE64VALUE>
+                                    </Visible>
+                                </Signature>
+                            </Application>
+                        </AgileSignerConfig>"
+                ]
+            ]
+        ];
+
+        print_r($data);
+        die();
+
+        $response = Http::withHeaders(['otp' => $otp])->post($url, $data);
+        $json = $response->json();
+        dd($json);
+        $json['files'][0]['content'];
+
+        header('Content-Type: application/pdf');
+        echo base64_decode($json);
+
+        die();
+
+    }
+
 }
