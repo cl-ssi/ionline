07239dc09a476069dab8f8f91a9489f805f32a8e
Alvaro Lupa <alvaro.lupa@gmail.com>
2021-05-26 13:11:00 -0400

    Documentos->Convenios: se puede seleccionar el apelativo del alcalde ya sea titular o subrogante y genero

--- app/Municipality.php
+++ app/Municipality.php
@@ -9,7 +9,7 @@ class Municipality extends Model
     protected $fillable = [
         'name','name_municipality','rut_municipality','adress_municipality','appellative_representative','decree_representative',
         'name_representative','rut_representative','name_representative_surrogate','rut_representative_surrogate', 
-        'decree_representative_surrogate', 'email_municipality'
+        'decree_representative_surrogate', 'email_municipality', 'appellative_representative_surrogate'
     ];
 
     protected $hidden = [
