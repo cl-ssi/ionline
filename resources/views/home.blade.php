5c0d2f3119b697d56949383c70934be84e7de601
Esteban <tebiccr@gmail.com>
2021-06-07 22:20:56 -0400

    home pasos solicitud firma electrónica

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,52 +4,69 @@
 
 @section('content')
 <div class="jumbotron mt-5">
-  <div class="row">
-  <div class="col-9">
-      <h1 class="display-4">Intranet Online</h1>      
-      <p class="lead">{{ env('APP_SS') }}</p>
-      <hr class="my-5">
-        <p>Contacto:
-            <a href="mailto:{{ env('APP_SS_EMAIL') }}">{{ env('APP_SS_EMAIL') }}</a>
-        </p>
+    <div class="row">
+        <div class="col-9">
+            <h1 class="display-4">Intranet Online</h1>
+            <p class="lead">{{ env('APP_SS') }}</p>
+            <hr class="my-5">
+            <p>Contacto:
+                <a href="mailto:{{ env('APP_SS_EMAIL') }}">{{ env('APP_SS_EMAIL') }}</a>
+            </p>
+            <p>
 
-  </div>
-  <div class="col-md-3 col-12">
-      <img src="{{ asset('images/logo_blanco.png') }}"
-          alt="Logo {{ env('APP_SS') }}"
-          style="background-color: rgb(0, 108, 183);"
-          class="img-thumbnail">
-  </div>
-</div>
+            <ol>
+                <h6>Pasos para solicitud de Firma Electrónica del Gobierno (OTP)</h6>
+                <li>
+                Solicitar a Yeannett Valdivia o Pamela Villagrán crear usuario en sistema de firma digital.
+                </li>
+                <li>
+                Ingresar al link <a href="https://firma.digital.gob.cl/ra/">firma.digital.gob.cl</a> usando clave única y obtener un certificado de "propósito general".
+                </li>
+                <li>                
+                Contactar al ministro de fe Paula Tapia o José Donoso para solicitar la aprobación por parte del ministro de fe.
+                </li>                
+                <li>
+                Volver a ingresar a <a href="https://firma.digital.gob.cl/ra/">firma.digital.gob.cl</a> y escanear el código QR de certificado generado, para escanear urilizar alguna aplicación autentificadora, ejemplo Google Authenticator.<br>
+                <a href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2">Android</a><br>
+                <a href="https://apps.apple.com/cl/app/google-authenticator/id388497605">I-Phone</a>
+                </li>
+            </ol>
+            </p>
 
-<div class="alert alert-light" style="display: none" role="alert" id="developers">
-    Hola {{ auth()->user()->firstName }}, soy el sistema <i class="fas fa-cog fa-spin fa-2x" style="color:green"></i>
-    , quiero contarte que fui desarrollado el año 2018 por <a href="mailto:alvaro.torres@redsalud.gob.cl">
-    Alvaro Torres</a> y <a href="mailto:jorge.mirandal@redsalud.gob.cl">Jorge Miranda</a> 
-    y hoy día soy mantenido por un excelente equipo de desarrollo del Departamento TIC,
-    dónde se incorporó los Estebanes (Rojas + Miranda), Germán Zuñiga, Álvaro Lupa y Oscar Zavala.
-    <br>
-    El equipo de combate en terreno está formado por 
-    Yeannett, Adriana, Cristian y Álvaro (si, hay tres Álvaros en el departamento).
-    <br>
-    Nuestro jefe de departamento es el glorioso Don José Don Oso. <br>
-    Y una mención especial a nuestra ex .... secretaría que aún nos apoya, Pamela.
+        </div>
+        <div class="col-md-3 col-12">
+            <img src="{{ asset('images/logo_blanco.png') }}" alt="Logo {{ env('APP_SS') }}" style="background-color: rgb(0, 108, 183);" class="img-thumbnail">
+        </div>
+    </div>
 
-    <hr>
+    <div class="alert alert-light" style="display: none" role="alert" id="developers">
+        Hola {{ auth()->user()->firstName }}, soy el sistema <i class="fas fa-cog fa-spin fa-2x" style="color:green"></i>
+        , quiero contarte que fui desarrollado el año 2018 por <a href="mailto:alvaro.torres@redsalud.gob.cl">
+            Alvaro Torres</a> y <a href="mailto:jorge.mirandal@redsalud.gob.cl">Jorge Miranda</a>
+        y hoy día soy mantenido por un excelente equipo de desarrollo del Departamento TIC,
+        dónde se incorporó los Estebanes (Rojas + Miranda), Germán Zuñiga, Álvaro Lupa y Oscar Zavala.
+        <br>
+        El equipo de combate en terreno está formado por
+        Yeannett, Adriana, Cristian y Álvaro (si, hay tres Álvaros en el departamento).
+        <br>
+        Nuestro jefe de departamento es el glorioso Don José Don Oso. <br>
+        Y una mención especial a nuestra ex .... secretaría que aún nos apoya, Pamela.
 
-    <pre>{{ $phrase ? $phrase->phrase : '' }}</pre>
-</div>
-@endsection
+        <hr>
 
-@section('custom_js')
+        <pre>{{ $phrase ? $phrase->phrase : '' }}</pre>
+    </div>
+    @endsection
 
-<script type="text/javascript">
-$(document).ready(function(){
-    $("body").keydown(function(event){
-    	/* 65=a, 74=j*/
-    	if(event.which == 65 || event.which ==74) $("#developers").toggle("slow");
-    });
-});
-</script>
+    @section('custom_js')
 
-@endsection
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("body").keydown(function(event) {
+                /* 65=a, 74=j*/
+                if (event.which == 65 || event.which == 74) $("#developers").toggle("slow");
+            });
+        });
+    </script>
+
+    @endsection
