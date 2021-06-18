f38a4cd3ce799a9fd8382aef72e0de5655422e21
oscarzavala <ozavala@gmail.com>
2021-06-18 19:35:19 -0400

    Purchasing Process

---
+++ app/Http/Livewire/RequestForm/PurchasingProcess.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Livewire\RequestForm;
+
+use Livewire\Component;
+use App\Models\RequestForms\RequestForm;
+use App\Models\RequestForms\EventRequestForm;
+use App\Models\Parameters\PurchaseUnit;
+use App\Models\Parameters\PurchaseType;
+use App\Rrhh\Authority;
+use Carbon\Carbon;
+use App\User;
+
+class PurchasingProcess extends Component
+{
+  public $requestForm, $purchase_mechanism, $purchaseUnit, $purchaseType, $lstPurchaseType, $lstPurchaseUnit, $vista, $index;
+
+    public function mount(RequestForm $requestForm)
+    {
+      $this->requestForm        = $requestForm;
+      $this->purchase_mechanism = $requestForm->purchase_mechanism;
+      $this->purchaseUnit       = $requestForm->purchaseUnit->id;
+      $this->purchaseType       = $requestForm->purchaseType->id;
+      $this->lstPurchaseType    = PurchaseType::all();
+      $this->lstPurchaseUnit    = PurchaseUnit::all();
+      $this->vista              = false;
+    }
+
+    public function resetError()
+    {
+    }
+
+    public function edit($index[])
+    {
+      if(!$this->vista)
+        $this->vista = true;
+      else
+        $this->vista = false;
+    }
+
+    public function render()
+    {
+        return view('livewire.request-form.purchasing-process');
+    }
+}
--- resources/views/request_form/purchasing_process.blade.php
+++ resources/views/request_form/purchasing_process.blade.php
@@ -13,122 +13,40 @@
 @include('request_form.nav')
 
 <div class="card mx-0">
-  <h6 class="card-header text-primary"><i class="fas fa-file-signature"></i> Formulario de Requerimiento N° {{ $requestForm->id }}</h6>
+  <h6 class="card-header text-primary"><i class="fas fa-shopping-cart"></i></a> Formulario de Requerimiento N° {{ $requestForm->id }}</h6>
   <div class="card-body mx-4 px-0">
 
-    <div class="container-fluid row mx-0 mb-3 mt-3 pt-0"> <!-- DIV para TABLA-->
-      <table class="table table-condensed table-sm small">
-          <tr class="d-flex">
-              <th class="text-muted col-3" scope="row">Gasto Estimado</th>
-              <td class="col-3">{{ $requestForm->estimatedExpense() }}</td>
-              <th class="text-muted col-3" scope="row">Nombre del Solicitante</th>
-              <td class="col-3">{{ $requestForm->creator->tinnyName()}}</td>
-          </tr>
-
-          <tr class="d-flex">
-              <th class="text-muted col-3" scope="row">Unidad Organizacional</th>
-              <td class="col-3">{{ $requestForm->organizationalUnit->getInitialsAttribute() }}</td>
-              <th class="text-muted col-3" scope="row">Mecanismo de Compra</th>
-              <td class="col-3">{{ $requestForm->getPurchaseMechanism()}}</td>
-          </tr>
-
-          <tr class="d-flex">
-              <th class="text-muted col-3" scope="row">Programa Asociado</th>
-              <td class="col-3">{{ $requestForm->program }}</td>
-              <th class="text-muted col-3" scope="row">Folio Requerimiento SIGFE</th>
-              <td class="col-3">{{ $requestForm->sigfe }}</td>
-          </tr>
-
-          <tr class="d-flex">
-              <th class="text-muted col-3" scope="row">Justificación de Adquisición</th>
-              <td class="col-3">{{ $requestForm->justification }}</td>
-              <th class="text-muted col-3" scope="row">Fecha de Creación</th>
-              <td class="col-3">{{ $requestForm->createdDate() }}</td>
-          </tr>
-
-          <tr class="d-flex">
-              <th class="text-muted col-3" scope="row">Archivos</th>
-              <td class="col-3">FILE01 - FILE02 - FILE03 - FILE04</td>
-              <th class="text-muted col-3" scope="row">Tiempo transcurrido</th>
-              <td class="col-3">{{ $requestForm->getElapsedTime() }}</td>
-          </tr>
-
-          <tr class="d-flex">
-              <th class="text-muted col-3" scope="row">Aprobación Abastecimiento</th>
-              <td class="col-3">{{ $requestForm->eventSignatureDate('supply_event', 'approved') }}</td>
-              <th class="text-muted col-3" scope="row">Visador Abastecimiento</th>
-              <td class="col-3">{{ $requestForm->eventSignerName('supply_event', 'approved') }}</td>
-          </tr>
-
-          <tr class="d-flex">
-              <th class="text-muted col-3" scope="row">Tipo de Compra</th>
-              <td class="col-3">{{ $requestForm->purchaseType->name }}</td>
-              <th class="text-muted col-3" scope="row">Unidad de Compra</th>
-              <td class="col-3">{{ $requestForm->purchaseUnit->name }}</td>
-          </tr>
-
-          <tr class="d-flex">
-              <th class="text-muted col-3" scope="row"></th>
-              <td class="col-3"></td>
-              <th class="text-muted col-3" scope="row"></th>
-              <td class="col-3"></td>
-          </tr>
-      </table>
-    </div><!-- div para TABLA -->
-
-    <div class="row mx-3 mb-3 mt-3 pt-0"> <!-- DIV para TABLA-->
-      <h6 class="card-subtitle mt-0 mb-2 text-primary">Lista de Bienes y/o Servicios:</h6>
-      <table class="table table-condensed table-hover table-bordered table-sm small">
-        <thead>
-          <tr>
-            <th>Item</th>
-            <th>ID</th>
-            <th>Cod.Presup.</th>
-            <th>Artículo</th>
-            <th>UM</th>
-            <th>Especificaciones Técnicas</th>
-            <th>Archivo</th>
-            <th>Cantidad</th>
-            <th>Valor U.</th>
-            <th>Impuestos</th>
-            <th>Total Item</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach($requestForm->itemRequestForms as $key => $item)
-                  <tr>
-                      <td>{{ $key+1 }}</td>
-                      <td>{{ $item->id }}</td>
-                      <td>{{ $item->budgetItem()->first()->fullName() }}</td>
-                      <td>{{ $item->article }}</td>
-                      <td>{{ $item->unit_of_measurement }}</td>
-                      <td>{{ $item->specification }}</td>
-                      <td>FILE</td>
-                      <td align='right'>{{ $item->quantity }}</td>
-                      <td align='right'>{{ $item->unit_value }}</td>
-                      <td>{{ $item->tax }}</td>
-                      <td align='right'>{{ $item->formatExpense() }}</td>
-                  </tr>
-          @endforeach
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colspan="7" rowspan="2"></td>
-            <td colspan="2" align='right'>Cantidad de Items</td>
-            <td colspan="2" align='right'>{{count($requestForm->itemRequestForms)}}</td>
-          </tr>
-          <tr>
-            <td colspan="2" align='right'>Valor Total</td>
-            <td colspan="2" align='right'>{{$requestForm->estimatedExpense()}}</td>
-          </tr>
-        </tfoot>
-      </table>
-    </div><!-- DIV para TABLA-->
-
-
+    <livewire:request-form.purchasing-process :requestForm="$requestForm" :eventType="$eventType" >
 
   </div><!-- card-body -->
 </div><!-- card-principal -->
 
+@endsection
+@section('custom_js')
 
+@endsection
+@section('custom_js_head')
+<style>
+table {
+  border-collapse: collapse;
+}
+.brd-l {
+  border-left: solid 1px #D6DBDF;
+}
+.brd-r {
+  border-right: solid 1px #D6DBDF;
+}
+.brd-b {
+  border-bottom: solid 1px #D6DBDF;
+}
+.brd-t {
+  border-top: solid 1px #D6DBDF;
+}
+oz {
+  color: red;
+  font-size: 60px;
+  background-color: yellow;
+  font-family: time new roman;
+}
+</style>
 @endsection
