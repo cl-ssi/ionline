35c07c8ffc2b1b5e4649ad5a0f006c5d87f6393b
Esteban <tebiccr@gmail.com>
2021-06-24 17:39:40 -0400

    solo se ve Subtitulo 31 para SSI

--- resources/views/service_requests/requests/create.blade.php
+++ resources/views/service_requests/requests/create.blade.php
@@ -436,13 +436,16 @@
 		</fieldset>
 	</div> -->
 
+	@if(Auth::user()->organizationalUnit->establishment_id == 1)
 
+	@else
 	<div class="form-row" id="div_subt31" style="display: none">
 		<fieldset class="form-group col">
 				<label for="for_subt31">Subtitulo 31 <small>(Aparecerá en resolución, luego del texto "El gasto corresponde")</small></label>
 				<textarea id="subt31" name="subt31" class="form-control" rows="4" cols="50" disabled></textarea>
 		</fieldset>
 	</div>
+	@endif
 	
 
 
