f9a85f3f82a6549b7db7e3332f1d3ea44a02937e
tebiccr <tebiccr@gmail.com>
2021-07-06 11:13:09 -0400

    fix resolucion

--- resources/views/service_requests/report_resolution.blade.php
+++ resources/views/service_requests/report_resolution.blade.php
@@ -313,7 +313,7 @@
       <p class="justify">
         <strong>SEGUNDO:</strong> En cumplimiento del presente convenio El prestador deberá llevar a cabo las siguientes prestaciones:
       <ul>
-        <li>{!! nl2br(e($ServiceRequest->service_description!)) !!}</li>
+        <li>{!! nl2br(e($ServiceRequest->service_description)) !!}</li>
       </ul>
       </p>
 
