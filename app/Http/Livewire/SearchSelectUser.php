3c956ea4f0bcde414042046c13506fa46d18c795
AquaroTorres <alvarotorres@gmail.com>
2021-07-20 16:53:29 -0400

    Search and Select User Livewire

---
+++ app/Http/Livewire/SearchSelectUser.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\User;
+
+class SearchSelectUser extends Component
+{
+    /** Uso:
+     * @livewire('search-select-user') 
+     * 
+     * Se puede definir el nombre del campo que almacenará el id de usuario
+     * @livewire('search-select-user', ['selected_id' => 'user_id'])
+     * 
+     * Si necesitas que aparezca precargado el usuario
+     * @livewire('search-select-user', ['user' => $user])
+     */
+    public $query;
+    public $users;
+    /** Para cuando viene precargado */     
+    public $user;
+    public $selectedName;
+    public $selected_id = 'user_id';
+    public $msg_too_many;
+
+    public function resetx()
+    {
+        $this->query = '';
+        $this->users = [];
+        $this->user = null;
+    }
+
+    public function mount()
+    {
+        if($this->user) {
+            $this->setUser($this->user);
+        }
+    }
+
+    public function setUser(User $user)
+    {
+        $this->resetx();
+        $this->user = $user;
+        $this->selectedName = $user->fullName;
+    }
+
+    public function updatedQuery()
+    {
+        $this->users = User::getUsersBySearch($this->query)
+            ->orderBy('name','Asc')
+            ->get();
+
+        /** Más de 50 resultados  */
+        if(count($this->users) >= 25)
+        {
+            $this->users = [];
+            $this->msg_too_many = true;
+        }
+        else {
+            $this->msg_too_many = false;
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.search-select-user');
+    }
+}
