83a6773b58d39920160519235482d4f70f1f94a6
Esteban <sick_iqq@hotmail.com>
2021-08-23 11:11:15 -0400

    farmacia: incorpora acta y lote en bincard

--- app/Pharmacies/Product.php
+++ app/Pharmacies/Product.php
@@ -145,6 +145,9 @@ class Product extends Model
                         //$matrix[$cont]['saldo'] = $saldo;
                         $matrix[$cont]['amount'] = $CollectionItem->amount;
                         $matrix[$cont]['notas'] = $CollectionItem->purchase->notes;
+
+                        $matrix[$cont]['act_number'] = null;
+                        $matrix[$cont]['product_batch'] = null;
                     }
                     if ($key1==1) {
                         //$saldo = $saldo + $CollectionItem->amount;
@@ -159,6 +162,9 @@ class Product extends Model
                         //$matrix[$cont]['saldo'] = $saldo;
                         $matrix[$cont]['amount'] = $CollectionItem->amount;
                         $matrix[$cont]['notas'] = $CollectionItem->receiving->notes;
+
+                        $matrix[$cont]['act_number'] = null;
+                        $matrix[$cont]['product_batch'] = null;
                     }
                     if ($key1==2) {
                         //$saldo = $saldo - $CollectionItem->amount;
@@ -173,6 +179,9 @@ class Product extends Model
                         //$matrix[$cont]['saldo'] = $saldo;
                         $matrix[$cont]['amount'] = $CollectionItem->amount;
                         $matrix[$cont]['notas'] = $CollectionItem->dispatch->notes;
+
+                        $matrix[$cont]['act_number'] = $CollectionItem->dispatch->id;
+                        $matrix[$cont]['product_batch'] = $CollectionItem->batch;
                     }
 
                     $cont = $cont + 1;
