f6fe45df56b07b083bea8eb0b12e382e8b304c9b
oscarzavala <ozavala@gmail.com>
2021-09-03 05:13:00 -0400

    Pivot Table on Purchase Mechanism

--- app/Models/Parameters/PurchaseMechanism.php
+++ app/Models/Parameters/PurchaseMechanism.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 //use App\Models\RequestForms\ItemRequestForm;
 use App\Models\RequestForms\RequestForm;
 use App\Models\RequestForms\PurchasingProcess;
+use App\Models\Parameters\PurchaseType;
 
 class PurchaseMechanism extends Model
 {
@@ -24,7 +25,12 @@ class PurchaseMechanism extends Model
 
   public function purchasingProcesses() {
       return $this->hasMany(PurchasingProcess::class, 'purchase_mechanism_id');
-  }  
+  }
+
+  public function purchaseTypes()
+  {
+      return $this->belongsToMany(PurchaseType::class, 'cfg_purchase_mechanism_type', 'purchase_mechanism_id', 'purchase_type_id')->withTimestamps();
+  }
 
   protected $table = 'cfg_purchase_mechanisms';
 
