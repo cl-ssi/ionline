3f3f6eb4322a6cdbcda0ae9ebcc0ff28a9f70812
oscarzavala <ozavala@gmail.com>
2021-09-07 10:22:24 -0300

    fix migrations arq_request_forms

--- database/migrations/2021_07_21_092717_create_arq_purchasing_processes_table.php
+++ database/migrations/2021_07_21_092717_create_arq_purchasing_processes_table.php
@@ -23,7 +23,7 @@ class CreateArqPurchasingProcessesTable extends Migration
         Schema::create('arq_purchasing_processes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('purchase_mechanism_id');
-            //$table->foreignId('purchase_type_id');
+            $table->foreignId('purchase_type_id');
             $table->foreignId('purchase_unit_id');
             $table->foreignId('item_request_form_id');
             $table->enum('status', ['in_progress', 'total', 'partial', 'desert',  'timed_out', 'not_available']);
@@ -43,7 +43,7 @@ class CreateArqPurchasingProcessesTable extends Migration
             $table->timestamps();
 
             $table->foreign('purchase_mechanism_id')->references('id')->on('cfg_purchase_mechanisms');
-            //$table->foreign('purchase_type_id')->references('id')->on('cfg_purchase_types');
+            $table->foreign('purchase_type_id')->references('id')->on('cfg_purchase_types');
             $table->foreign('purchase_unit_id')->references('id')->on('cfg_purchase_units');
             $table->foreign('item_request_form_id')->references('id')->on('arq_item_request_forms');
         });
