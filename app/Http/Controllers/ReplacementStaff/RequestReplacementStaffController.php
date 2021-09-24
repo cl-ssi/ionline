996bc4cfb2a69e52278068a3213d7919ee432f5c
Jorge Miranda <mirandal.jorge@gmail.com>
2021-09-24 01:07:30 -0300

    Replacement Staff: End of Request

--- app/Http/Controllers/ReplacementStaff/RequestReplacementStaffController.php
+++ app/Http/Controllers/ReplacementStaff/RequestReplacementStaffController.php
@@ -62,21 +62,20 @@ class RequestReplacementStaffController extends Controller
             ->WhereHas('technicalEvaluation', function($q) {
               $q->Where('technical_evaluation_status', 'pending');
             })
-             ->WhereHas('assignEvaluations', function($j) {
-               $j->Where('to_user_id', Auth::user()->id)
+            ->WhereHas('assignEvaluations', function($j) {
+              $j->Where('to_user_id', Auth::user()->id)
                ->where('status', 'assigned');
-             })
-             ->get();
+            })
+            ->get();
 
         $requests = RequestReplacementStaff::latest()
-            ->where(function ($q){
-                $q->whereHas('requestSign', function($j) {
-                    $j->Where('request_status', 'rejected');
-                })
-                ->orWhereHas('technicalEvaluation', function($y){
-                    $y->Where('technical_evaluation_status', 'complete')
-                    ->OrWhere('technical_evaluation_status', 'rejected');
-                });
+            ->WhereHas('technicalEvaluation', function($q) {
+              $q->Where('technical_evaluation_status', 'complete')
+              ->OrWhere('technical_evaluation_status', 'rejected');
+            })
+            ->WhereHas('assignEvaluations', function($j) {
+              $j->Where('to_user_id', Auth::user()->id)
+               ->where('status', 'assigned');
             })
             ->paginate(10);
 
@@ -423,7 +422,7 @@ class RequestReplacementStaffController extends Controller
         $requestReplacementStaff->fill($request->all());
         $requestReplacementStaff->save();
         session()->flash('success', 'Su solicitud ha sido sido correctamente actualizada.');
-        return redirect()->route('replacement_staff.edit', $requestReplacementStaff);
+        return redirect()->route('replacement_staff.request.edit', $requestReplacementStaff);
     }
 
     /**
