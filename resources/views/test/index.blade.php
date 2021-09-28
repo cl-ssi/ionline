d584550450c6b1f66ae9b67f28e844a8b1ba093e
AquaroTorres <alvarotorres@gmail.com>
2021-09-28 16:31:46 -0300

    test index

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+
+<form method="post" action="{{ route('test.post') }}">
+    @csrf
+    <textarea name="texto" id="tt"  
+        rows="18" 
+        class="form-control"  
+        style="white-space: pre-wrap;"></textarea>
+    <button class="btn btn-primary">Guardar</button>
+</form>
+
+@endsection
+
+@section('custom_js')
+<script>
+//Javascript
+var stringAsHtml = $('tt').val().Replace("\r\n", "<br />\r\n");
+console.log('stringAsHtml');
+
+</script>
+
+@endsection
