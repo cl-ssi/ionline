5628d3f840740b92c21f9cc8568650331ea65c96
Esteban <sick_iqq@hotmail.com>
2021-10-05 15:58:44 -0300

    updt: solicitudes tipo covid: se dejarán de utilizar (en contratos y cumplimientos) los montos registrados en mantenedor de valores, y desde ahora en adelante se realizarán los cálculos en base a lo registrado en las solicitudes (cuadro rojo)

--- resources/views/livewire/service-request/monthly-quotes.blade.php
+++ resources/views/livewire/service-request/monthly-quotes.blade.php
@@ -1,17 +1,21 @@
 <span>
-    @if($resultadoEnNumero)
-        @php $total = 0;
-            $cont = 1;
-        @endphp
-        @foreach($valores as $key => $valor)
-            {{"Cuota: ".$cont." - Mes: ".$key.": $".number_format($valor, 0, ',', '.')}} <br>
-            @php
-                $total = $total + $valor;
-                $cont++;
-            @endphp
-        @endforeach
-        <strong> Total: </strong>{{number_format($total, 0, ',', '.')}}
+    @if($parametroMes != null)
+      {{$array_valores_mensualizados[$parametroMes]}}
     @else
-        {{ $valores }}
+      @if($resultadoEnNumero)
+          @php $total = 0;
+              $cont = 1;
+          @endphp
+          @foreach($valores as $key => $valor)
+              {{"Cuota: ".$cont." - Mes: ".$key.": $".number_format($valor, 0, ',', '.')}} <br>
+              @php
+                  $total = $total + $valor;
+                  $cont++;
+              @endphp
+          @endforeach
+          <strong> Total: </strong>{{number_format($total, 0, ',', '.')}}
+      @else
+          {{ $valores }}
+      @endif
     @endif
 </span>
