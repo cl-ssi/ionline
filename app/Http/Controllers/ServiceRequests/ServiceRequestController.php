b956710b08f775097b70b5ce60fd2aa437c44407
Esteban <sick_iqq@hotmail.com>
2021-10-08 11:09:27 -0300

    fix, para que guarde hsa_schedule_detail al editar

--- app/Http/Controllers/ServiceRequests/ServiceRequestController.php
+++ app/Http/Controllers/ServiceRequests/ServiceRequestController.php
@@ -575,9 +575,9 @@ class ServiceRequestController extends Controller
   {
     //se guarda información de la solicitud
     $serviceRequest->fill($request->all());
-    if (isset($request->hsa_schedule_detail)) {
-      $serviceRequest->schedule_detail = $request->hsa_schedule_detail;
-    }
+    // if (isset($request->hsa_schedule_detail)) {
+    //   $serviceRequest->schedule_detail = $request->hsa_schedule_detail;
+    // }
 
     $serviceRequest->save();
 
