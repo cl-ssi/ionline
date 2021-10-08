7e2a6358a1c762a7e73b2d976f065ce5ebaf0e58
Jorge Miranda <mirandal.jorge@gmail.com>
2021-10-08 13:06:01 -0300

    Mammography: Migration

---
+++ database/migrations/2021_10_07_152611_create_mammography_days_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMammographyDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mammography_days', function (Blueprint $table) {
+            $table->id();
+            $table->date('day');
+            $table->unsignedInteger('exam_available')->default(0);
+            $table->unsignedInteger('exam_used')->default(0);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mammography_days');
+    }
+}
