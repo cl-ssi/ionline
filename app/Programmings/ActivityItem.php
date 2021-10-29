cda373940dc3501e36e2941a1cb07bea464aaf50
Alvaro Lupa <alvaro.lupa@gmail.com>
2021-10-29 15:16:56 -0300

    Programacion: activity items softdeletes enabled

--- app/Programmings/ActivityItem.php
+++ app/Programmings/ActivityItem.php
@@ -3,15 +3,17 @@
 namespace App\Programmings;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ActivityItem extends Model
 {
+    use SoftDeletes;
     protected $table = 'pro_activity_items';
     protected $fillable = [
         'id', 'int_code', 'vitakl_cycle', 'tracer', 'action_type', 'activity_name', 'def_target_population', 'verification_rem', 'professional', 'activity_id'
     ];
 
     public function program(){
-        return $this->belongsTo('App\Programmings\ActivityProgram');
+        return $this->belongsTo('App\Programmings\ActivityProgram', 'activity_id');
     }
 }
