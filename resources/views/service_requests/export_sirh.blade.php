8a3705c25634967d4632d103388e1515767fb85d
GinoPSC <92866955+GinoPSC@users.noreply.github.com>
2021-11-16 17:30:25 -0300

    mas ajustes

--- resources/views/service_requests/export_sirh.blade.php
+++ resources/views/service_requests/export_sirh.blade.php
@@ -7,41 +7,50 @@
 @include('service_requests.partials.nav')
 
 
-<form method="get" class="form-inline mb-3" action="{{ route('rrhh.service-request.report.export_sirh') }}">
+<form method="get" action="{{ route('rrhh.service-request.report.export_sirh') }}">
     <div class="form-row">
-        <div class="form-group ml-4">
+        <div class="form-group col-12 col-md-4">
             <label for="for_run">Run</label>
             <input name="run" class="form-control" id="for_run_id" placeholder="opcional. EJ: 123456" value="{{old('run')}}">
-
         </div>
-    </div>
-    <br>
-    <br>
-    <div class="form-row">
-        <div class="form-group ml-3">
+        <div class="form-group col-12 col-md-4">
             <label for="for_from">Fecha Inicio de Contrato Desde*</label>
-            <input type="datetime-local" class="form-control mx-sm-3" id="for_from" name="from" value="{{old('from') }}" >
+            <input type="datetime-local" class="form-control" id="for_from" name="from" value="{{old('from') }}" >
         </div>
-        <div class="form-group ml-3">
+        <div class="form-group col-12 col-md-4">
             <label for="for_to">Fecha Inicio de Contrato Hasta*</label>
-            <input type="datetime-local" class="form-control mx-sm-3" id="for_to" name="to" value="{{old('to') }}">
+            <input type="datetime-local" class="form-control" id="for_to" name="to" value="{{old('to') }}">
         </div>
         <!-- <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Buscar</button> -->
     </div>
 
     <div class="form-row">
-        <div class="form-group ml-6">
+        <div class="form-group col-6 col-md-3">
             <label for="for_from">ID Desde</label>
             <input type="number" class="form-control mx-sm-6" id="for_id_from" name="id_from" value="{{old('id_from') }}">
         </div>
-        <div class="form-group ml-6">
+        <div class="form-group col-6 col-md-3">
             <label for="for_from">ID hasta</label>
             <input type="number" class="form-control mx-sm-6" id="for_id_to" name="id_to" value="{{old('id_to') }}">
-            <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Buscar</button>
-            <a type="button" class="btn btn-success" target="_blank" href="{{ route('rrhh.service-request.report.export-sirh-txt', ['establishment' => $request->establishment, 'sirh' => $request->sirh, 'from' => $request->from, 'to' => $request->to , 'id_from' => $request->id_from, 'id_to' => $request->id_to]  ) }}">Formato SIRH (Descargar Todo) <i class="far fa-file-excel"></i>
+        </div>
+        <div class="form-group col-6 col-md-2">
+            <label>&nbsp;</label>
+            <button type="submit" class="form-control btn btn-primary"><i class="fas fa-search"></i> Buscar</button>
+        </div>
+        <div class="form-group col-6 col-md-4">
+            <label>&nbsp;</label>
+            <a type="button" class="form-control btn btn-success" target="_blank"
+                href="{{
+                    route('rrhh.service-request.report.export-sirh-txt',
+                    ['establishment' => $request->establishment,
+                    'sirh' => $request->sirh,
+                    'from' => $request->from,
+                    'to' => $request->to ,
+                    'id_from' => $request->id_from,
+                    'id_to' => $request->id_to]) 
+                }}">Formato SIRH (Descargar Todo) <i class="far fa-file-excel"></i>
             </a>
         </div>
-        
     </div>
 
     <!-- <div class="form-row">
@@ -63,7 +72,7 @@
 </form>
 
 
-<table class="table table-sm table-bordered table-responsive small text-uppercase" id="tabla_sirh">
+<table class="table table-sm table-bordered table-responsive-xl small text-uppercase" id="tabla_sirh">
     <thead>
         <tr>
             <th>ID Service Request</th>
