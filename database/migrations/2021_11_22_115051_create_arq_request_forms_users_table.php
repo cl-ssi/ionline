320af04159c2f28c06a501d31406cf987f5fdc01
Alvaro Lupa <alvaro.lupa@gmail.com>
2021-11-22 12:39:13 -0300

    request form: normalizacion tablas

---
+++ database/migrations/2021_11_22_115051_create_arq_request_forms_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArqRequestFormsUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('arq_request_forms_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('purchaser_user_id')->constrained('users');
+            $table->foreignId('request_form_id')->constrained('arq_request_forms');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('arq_request_forms_users');
+    }
+}
