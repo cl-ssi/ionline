e53abc83264d1ba6d6726063aa13a10412a05a60
Jorge Miranda <mirandal.jorge@gmail.com>
2021-11-25 09:57:29 -0300

    Request Forms: Fix Migration

--- app/Http/Livewire/RequestForm/RequestFormCreate.php
+++ app/Http/Livewire/RequestForm/RequestFormCreate.php
@@ -5,6 +5,7 @@ use Livewire\Component;
 use App\Models\RequestForms\RequestForm;
 use App\Models\RequestForms\RequestFormFile;
 use App\Models\RequestForms\ItemRequestForm;
+use App\Models\RequestForms\Passenger;
 use App\Models\RequestForms\EventRequestForm;
 use App\Models\Parameters\UnitOfMeasurement;
 use App\Models\Parameters\PurchaseMechanism;
@@ -268,7 +269,9 @@ class RequestFormCreate extends Component
           'estimated_expense'     =>  $this->totalDocument,
           'purchase_mechanism_id' =>  $this->purchaseMechanism,
           'program'               =>  $this->program,
-          'status'                =>  'created'
+          'status'                =>  'pending'
+
+          //'passenger_type'    =>  $this->passengerType,
       ]);
 
       // AQUI GUARDAR ARCHIVOS
@@ -336,26 +339,32 @@ class RequestFormCreate extends Component
       ]);
     }
 
-    private function savePassenger($item, $id){
+    private function savePassenger($passenger, $id){
         $now = Carbon::now()->format('Y_m_d_H_i_s');
         $file_name = $now.'art_file_'.$id;
-        $req = ItemRequestForm::updateOrCreate(
-          [
-            'id'                    =>      $item['id'],
-          ],
-          [
-            'request_form_id'       =>      $id,
-            'article'               =>      $item['article'],
-            'unit_of_measurement'   =>      $item['unitOfMeasurement'],
-            'specification'         =>      $item['technicalSpecifications'],
-            'quantity'              =>      $item['quantity'],
-            'unit_value'            =>      $item['unitValue'],
-            'tax'                   =>      $item['taxes'],
-            //'budget_item_id'        =>      '1',
-            'expense'               =>      $item['totalValue'],
-            'type_of_currency'      =>      $item['typeOfCurrency'],
-            'article_file'          =>      $item['articleFile'] ? $item['articleFile']->storeAs('/ionline/request_forms_dev/item_files/', $file_name.'.'.$item['articleFile']->extension(), 'gcs') : null
-      ]);
+        $req = Passenger::updateOrCreate(
+            [
+              'id'                =>  $passenger['id'],
+            ],
+            [
+              'user_id'           =>  Auth()->user()->id,
+              'run'               =>  $passenger['run'],
+              'dv'                =>  $passenger['dv'],
+              'name'              =>  $passenger['name'],
+              'fathers_family'    =>  $passenger['fathers_family'],
+              'mothers_family'    =>  $passenger['mothers_family'],
+              'birthday'          =>  $passenger['birthday'],
+              'phone_number'      =>  $passenger['phone_number'],
+              'email'             =>  $passenger['email'],
+              'round_trip'        =>  $passenger['round_trip'],
+              'origin'            =>  $passenger['origin'],
+              'destination'       =>  $passenger['destination'],
+              'departure_date'    =>  $passenger['departure_date'],
+              'return_date'       =>  $passenger['return_date'],
+              'baggage'           =>  $passenger['baggage'],
+              'unit_value'        =>  $passenger['unitValue'],
+              'request_form_id'   =>  $id
+            ]);
     }
 
     public function render(){
