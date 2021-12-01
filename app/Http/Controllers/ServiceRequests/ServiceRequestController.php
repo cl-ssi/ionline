d2ba45700a318d9bf1886de40e0c32884492844d
tebiccr <tebiccr@gmail.com>
2021-12-01 10:54:45 -0300

    fix subdirección

--- app/Http/Controllers/ServiceRequests/ServiceRequestController.php
+++ app/Http/Controllers/ServiceRequests/ServiceRequestController.php
@@ -546,7 +546,7 @@ class ServiceRequestController extends Controller
     $establishments = Establishment::orderBy('name', 'ASC')->get();
     $professions = Profession::orderBy('name', 'ASC')->get();
 
-    $subdirections = OrganizationalUnit::where('name', 'LIKE', '%direc%')->orderBy('name', 'ASC')->get();
+    $subdirections = OrganizationalUnit::where('name', 'LIKE', '%direc%')->where('establishment_id',Auth::user()->organizationalUnit->id)->orderBy('name', 'ASC')->get();
     $responsabilityCenters = OrganizationalUnit::orderBy('name', 'ASC')->get();
     $countries = Country::orderBy('name', 'ASC')->get();
 
