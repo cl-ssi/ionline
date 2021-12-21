393729701c3457c233ce3b140dde24f308508b94
AquaroTorres <atorresf@gmail.com>
2021-12-21 12:30:42 -0300

    Fix seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,9 +16,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
-        $ouTIC = OrganizationalUnit::Where('name','Unidad de Informática y Tecnología')->first();
-        $ouDPCR = OrganizationalUnit::Where('name','Departamento de Planificación y Control de Redes')->first();
+        $ou = OrganizationalUnit::first();
 
         $user = new User();
         $user->id = 12345678;
@@ -29,10 +27,10 @@ class UserSeeder extends Seeder
         $user->password = bcrypt('admin');
         $user->position = "Administrator";
         $user->email = "alvaro.torres@redsalud.gob.cl";
-        $user->organizationalUnit()->associate($ouTIC);
+        $user->organizationalUnit()->associate($ou);
         $user->save();
         $user->assignRole('god', 'dev','RRHH: admin');
-        //$user->givePermissionTo(Permission::all());
+        $user->givePermissionTo(Permission::all());
 
         $user = new User();
         $user->id = 15287582;
@@ -43,38 +41,38 @@ class UserSeeder extends Seeder
         $user->email = "sistemas.ssi@redsalud.gob.cl";
         $user->password = bcrypt('admin');
         $user->position = "Profesional SIDRA";
-        $user->organizationalUnit()->associate($ouTIC);
+        $user->organizationalUnit()->associate($ou);
         $user->save();
         $user->assignRole('god', 'dev');
         $user->givePermissionTo(Permission::all());
 
         $user = User::Create(['id'=>10278387, 'dv'=>5, 'name'=>'José', 'fathers_family'=>'Donoso', 'mothers_family' => 'Carrera',
-            'email'=>'jose.donosoc@redsalud.gob.cl','password'=>bcrypt('10278387'), 'position'=>'Jefe', 'organizational_unit_id'=>$ouDPCR->id]);
+            'email'=>'jose.donosoc@redsalud.gob.cl','password'=>bcrypt('10278387'), 'position'=>'Jefe', 'organizational_unit_id'=>$ou->id]);
         $user->assignRole('dev');
         $user->givePermissionTo(Permission::all());
 
         $user = User::Create(['id'=>14107361, 'dv'=>3, 'name'=>'Pamela', 'fathers_family'=>'Villagrán', 'mothers_family' => 'Alvarez',
-            'email'=>'pamela.villagran@redsalud.gob.cl','password'=>bcrypt('14107361'), 'position'=>'Administrativa', 'organizational_unit_id'=>$ouDPCR->id]);
+            'email'=>'pamela.villagran@redsalud.gob.cl','password'=>bcrypt('14107361'), 'position'=>'Administrativa', 'organizational_unit_id'=>$ou->id]);
         $user->assignRole('dev');
         $user->givePermissionTo(Permission::all());
 
         $user = User::Create(['id'=>16966444, 'dv'=>7, 'name'=>'Jorge', 'fathers_family'=>'Miranda', 'mothers_family' => 'Lopez',
-            'email'=>'jorge.mirandal@redsalud.gob.cl','password'=>bcrypt('16966444'), 'position'=>'Profesional SIDRA', 'organizational_unit_id'=>$ouTIC->id]);
+            'email'=>'jorge.mirandal@redsalud.gob.cl','password'=>bcrypt('16966444'), 'position'=>'Profesional SIDRA', 'organizational_unit_id'=>$ou->id]);
         $user->assignRole('god','dev');
         $user->givePermissionTo(Permission::all());
 
         $user = User::Create(['id'=>15924400, 'dv'=>8, 'name'=>'Cristian', 'fathers_family'=>'Carpio', 'mothers_family' => 'Diaz',
-            'email'=>'cristian.carpio@redsalud.gob.cl','password'=>bcrypt('15924400'), 'position'=>'Profesional SIDRA', 'organizational_unit_id'=>$ouTIC->id]);
+            'email'=>'cristian.carpio@redsalud.gob.cl','password'=>bcrypt('15924400'), 'position'=>'Profesional SIDRA', 'organizational_unit_id'=>$ou->id]);
         $user->assignRole('dev');
         $user->givePermissionTo(Permission::all());
 
         $user = User::Create(['id'=>16351236, 'dv'=>'k', 'name'=>'German', 'fathers_family'=>'Zuñiga', 'mothers_family' => 'Codocedo',
-            'email'=>'german.zuniga@redsalud.gob.cl','password'=>bcrypt('admin'), 'position'=>'Profesional SIDRA', 'organizational_unit_id'=>$ouTIC->id]);
+            'email'=>'german.zuniga@redsalud.gob.cl','password'=>bcrypt('admin'), 'position'=>'Profesional SIDRA', 'organizational_unit_id'=>$ou->id]);
         $user->assignRole('god','dev');
         $user->givePermissionTo(Permission::all());
 
         $user = User::Create(['id'=>16350137, 'dv'=>6, 'name'=>'Alvaro', 'fathers_family'=>'Lupa', 'mothers_family' => 'Huanca',
-            'email'=>'alvaro.lupa@redsalud.gob.cl','password'=>bcrypt('admin'), 'position'=>'Profesional SIDRA', 'organizational_unit_id'=>$ouTIC->id]);
+            'email'=>'alvaro.lupa@redsalud.gob.cl','password'=>bcrypt('admin'), 'position'=>'Profesional SIDRA', 'organizational_unit_id'=>$ou->id]);
         $user->assignRole('god','dev');
         $user->givePermissionTo(Permission::all());
     }
