6c4531f0ee13dd9942090f4e55446550c9afc300
AquaroTorres <atorresf@gmail.com>
2022-01-12 15:40:45 -0300

    Log index

---
+++ resources/views/parameters/logs/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('title', 'Logs')
+
+@section('content')
+
+@include('parameters.nav')
+
+<style>
+    pre {
+    white-space: pre-wrap;
+}
+</style>
+
+<h3 class="mb-3">Logs</h3>
+
+<table class="table table-sm">
+    <tbody>
+        @foreach($logs as $log)
+        <tr>
+            <th class="table-danger">Error</th>
+            <th class="table-danger">
+            <!-- <a href="{{ route('parameters.logs.edit', $log) }}">
+                    <i class="fas fa-edit"></i>
+                </a> -->
+                <a href="{{ route('parameters.logs.destroy', $log) }}">
+                    <i class="fas fa-trash text-danger"></i>
+                </a>
+            </th>
+        </tr>
+        <tr>
+            <td>
+                <pre>
+                    {{ print_r($log->toArray()) }} {{ optional($log->user)->fullName }}
+                </pre>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+{{ $logs->links() }}
+
+@endsection
+
+@section('custom_js')
+
+@endsection
