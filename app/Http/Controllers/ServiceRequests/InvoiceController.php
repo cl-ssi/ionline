52156af88500b69109f82d1d79404fc918f56d86
AquaroTorres <atorresf@gmail.com>
2022-01-12 12:00:08 -0300

    Fix sr invoice error

--- app/Http/Controllers/ServiceRequests/InvoiceController.php
+++ app/Http/Controllers/ServiceRequests/InvoiceController.php
@@ -32,7 +32,7 @@ class InvoiceController extends Controller
                 $response = Http::withToken($access_token)->post($url_base);
                 $user_cu = json_decode($response);
                 Log::info($response);
-                if($response)
+                if($user_cu->RolUnico)
                 {
                     $user_id = $user_cu->RolUnico->numero;
                 }
