dc2b743ba925e1aa7da6180fa071ffbba9d7e401
AquaroTorres <atorresf@gmail.com>
2022-01-13 00:56:04 -0300

    Fix invoice login

--- app/Http/Controllers/ServiceRequests/InvoiceController.php
+++ app/Http/Controllers/ServiceRequests/InvoiceController.php
@@ -24,28 +24,21 @@ class InvoiceController extends Controller
     public function login($access_token = null)
     {
         if ($access_token) {
-            // dd("");
+
             if (env('APP_ENV') == 'production' OR env('APP_ENV') == 'testing') {
-                // $access_token = session()->get('access_token');
                 $url_base = "https://www.claveunica.gob.cl/openid/userinfo/";
                 $response = Http::withToken($access_token)->post($url_base);
-                $user_cu = json_decode($response);
-                if($user_cu)
-                {
+
+                if($response->getStatusCode() == 200) {
+                    $user_cu = json_decode($response);
                     $user_id = $user_cu->RolUnico->numero;
                 }
-                else
-                {
-                    Log::info('Redireccionó a: ', ['access_token' => $access_token]);
-                    //Log::info($response);
+                else {
                     return redirect()->route('invoice.welcome');
                 }
-                // $user = $user.'-'.$user_cu->RolUnico->DV;
-
-
+                
             } else if (env('APP_ENV') == 'local') {
                 $user_id = $access_token;
-
             }
             return $this->show($user_id);
         }
