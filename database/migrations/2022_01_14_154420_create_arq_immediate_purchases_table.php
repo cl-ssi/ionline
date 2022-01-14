eec8949b2568d96b4108ca284b5e708be2b84dec
Jorge Miranda <mirandal.jorge@gmail.com>
2022-01-14 16:55:44 -0300

    Request Forms: fix migration

--- database/migrations/2022_01_14_154420_create_arq_immediate_purchases_table.php
+++ database/migrations/2022_01_14_154420_create_arq_immediate_purchases_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateImmediatePurchasesTable extends Migration
+class CreateArqImmediatePurchasesTable extends Migration
 {
     /**
      * Run the migrations.
