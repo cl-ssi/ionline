3627d6d73d944ecc2c7bbfd4f860a788f5f6ccff
Alvaro Lupa <alvaro.lupa@gmail.com>
2022-01-14 16:52:08 -0300

    RF: attachedFile model added

---
+++ app/Models/RequestForms/AttachedFile.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\RequestForms;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class AttachedFile extends Model implements Auditable
+{
+    use \OwenIt\Auditing\Auditable;
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'arq_attached_files';
+    protected $fillable = [
+        'file', 'document_type', 'tender_id'
+    ];
+
+
+}
