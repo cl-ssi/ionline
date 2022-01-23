19d87ad0837a1ae0e10528d37d15d0121534091d
Alvaro Lupa <alvaro.lupa@gmail.com>
2022-01-22 22:29:29 -0300

    Indicadores index se muestra estado en revisión

--- resources/views/indicators/19813/partials/card.blade.php
+++ resources/views/indicators/19813/partials/card.blade.php
@@ -4,7 +4,7 @@
     </div>
     <ul class="list-group list-group-flush">
         <li class="list-group-item">
-            <a href="{{ route('indicators.health_goals.list', [19813, 2021]) }}">2021</a> <span class="badge badge-warning">En Desarrollo</span>
+            <a href="{{ route('indicators.health_goals.list', [19813, 2021]) }}">2021</a> <span class="badge badge-warning">En revisión</span>
         </li>
         <li class="list-group-item">
             <a href="{{ route('indicators.19813.2020.index') }}">2020</a>
