e54c76be1f39680734fd9ca2f69f68b923c15d5a
AquaroTorres <atorresf@gmail.com>
2022-01-28 14:58:37 -0300

    Fix user ou

--- routes/web.php
+++ routes/web.php
@@ -704,7 +704,7 @@ Route::prefix('rrhh')->as('rrhh.')->group(function () {
         Route::delete('{organizationalUnit}/destroy', 'Rrhh\OrganizationalUnitController@destroy')->name('destroy')->middleware('auth');
     });
     Route::prefix('users')->name('users.')->group(function () {
-        Route::get('ou/{ou_id?}', 'UserController@getFromOu')->name('get.from.ou')->middleware('auth');
+        Route::get('ou/{ou_id?}', 'Rrhh\UserController@getFromOu')->name('get.from.ou')->middleware('auth');
         Route::get('autority/{ou_id?}', 'Rrhh\UserController@getAutorityFromOu')->name('get.autority.from.ou')->middleware('auth');
         Route::put('{user}/password', 'Rrhh\UserController@resetPassword')->name('password.reset')->middleware('auth');
         Route::get('{user}/switch', 'Rrhh\UserController@switch')->name('switch')->middleware('auth');
