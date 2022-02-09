2abd8ce9cd7bc048c9928f4e4bf61ec9751b12f8
Alvaro Lupa <alvaro.lupa@gmail.com>
2022-02-09 16:44:11 -0300

    RF: se agrega listado completo de los forms con permiso especial

--- routes/web.php
+++ routes/web.php
@@ -1376,6 +1376,7 @@ Route::prefix('request_forms')->as('request_forms.')->middleware('auth')->group(
 
     Route::prefix('supply')->as('supply.')->middleware('auth')->group(function () {
         Route::get('/', [PurchasingProcessController::class, 'index'])->name('index');
+        Route::get('/forms', [PurchasingProcessController::class, 'forms'])->name('forms');
         Route::get('/{requestForm}', [PurchasingProcessController::class, 'show'])->name('show');
         Route::get('/{requestForm}/purchase', [PurchasingProcessController::class, 'purchase'])->name('purchase');
         Route::post('/{requestForm}/create_internal_oc', [PurchasingProcessController::class, 'create_internal_oc'])->name('create_internal_oc');
