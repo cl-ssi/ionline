b191c06b43e5b3ea6e9385503820057913b35e22
Jorge Miranda <mirandal.jorge@gmail.com>
2022-02-18 17:14:17 -0300

    Ind: 2022 Index

--- resources/views/indicators/19813/index.blade.php
+++ resources/views/indicators/19813/index.blade.php
@@ -13,7 +13,13 @@
       </div>
       <ul class="list-group list-group-flush">
           <li class="list-group-item">
-              <a href="{{ route('indicators.19813.2020.index') }}">2020</a> <span class="badge badge-warning">En Revisión</span>
+              <a href="{{ route('indicators.health_goals.list', [19813, 2022]) }}">2022</a> <span class="badge badge-warning">En Desarrollo</span></li>
+          </li>
+          <li class="list-group-item">
+              <a href="{{ route('indicators.health_goals.list', [19813, 2021]) }}">2021</a>
+          </li>
+          <li class="list-group-item">
+              <a href="{{ route('indicators.19813.2020.index') }}">2020</a>
           </li>
           <li class="list-group-item">
               <a href="{{ route('indicators.19813.2019.index') }}">2019</a>
--- resources/views/indicators/19813/partials/card.blade.php
+++ resources/views/indicators/19813/partials/card.blade.php
@@ -4,14 +4,17 @@
     </div>
     <ul class="list-group list-group-flush">
         <li class="list-group-item">
-            <a href="{{ route('indicators.health_goals.list', [19813, 2021]) }}">2021</a> <span class="badge badge-warning">En revisión</span>
+            <a href="{{ route('indicators.health_goals.list', [19813, 2022]) }}">2022</a> <span class="badge badge-warning">En Desarrollo</span>
+        </li>
+        <li class="list-group-item">
+            <a href="{{ route('indicators.health_goals.list', [19813, 2021]) }}">2021</a>
         </li>
         <li class="list-group-item">
             <a href="{{ route('indicators.19813.2020.index') }}">2020</a>
         </li>
-        <li class="list-group-item">
+        <!-- <li class="list-group-item">
             <a href="{{ route('indicators.19813.2019.index') }}">2019</a>
-        </li>
+        </li> -->
         <!-- <li class="list-group-item">
             <a href="{{ route('indicators.19813.2018.index') }}">2018</a>
         </li> -->
