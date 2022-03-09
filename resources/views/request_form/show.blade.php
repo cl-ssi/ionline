13336a0e6a15f36ebe5c624640196a27a66ca4f4
Alvaro Lupa <alvaro.lupa@gmail.com>
2022-03-08 23:51:59 -0300

    FR: se agrega nuevo acceso para los administradores de contrato puedan ver formularios asignados a su nombre fix

--- resources/views/request_form/show.blade.php
+++ resources/views/request_form/show.blade.php
@@ -11,7 +11,7 @@
     <div class="col-sm-8">
         <div class="table-responsive">
             <h6><i class="fas fa-info-circle"></i> Detalle Formulario ID {{$requestForm->id}}
-@if($requestForm->eventRequestForms->first()->status == 'pending' && Auth()->user()->organizational_unit_id != 40)
+@if($requestForm->eventRequestForms->first()->status == 'pending' && Auth()->user()->organizational_unit_id != 40 && $requestForm->request_user_id == auth()->user()->id || Auth()->user()->hasPermissionTo('Request Forms: all'))
 <a class="btn btn-link btn-sm float-right font-weight-bold align-top" href="{{route('request_forms.edit', $requestForm)}}">
     <i class="fas fa-edit"></i> Editar formulario
 </a>
