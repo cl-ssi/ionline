9ae5b6efad910612c24ea1845ef16d9d32f14082
sick <esteban.rojasg@redsalud.cl>
2022-03-10 13:55:14 -0300

    Se finaliza mantenedor de asociación usuarios con farmacias

--- resources/views/pharmacies/admin_nav.blade.php
+++ resources/views/pharmacies/admin_nav.blade.php
@@ -2,7 +2,7 @@
 <ul class="nav nav-tabs mb-3">
       <li class="nav-item">
           <a class="nav-link"
-              href="{{ route('pharmacies.products.purchase.index') }}">
+              href="{{ route('pharmacies.pharmacy_users') }}">
               <i class="fas fa-user"></i> Asignación de farmacias</a>
       </li>
 </ul>
--- resources/views/pharmacies/admin_view.blade.php
+++ resources/views/pharmacies/admin_view.blade.php
@@ -6,35 +6,7 @@
 
 @include('pharmacies.admin_nav')
 
-@canany(['Pharmacy: SSI (id:1)', 'Pharmacy: REYNO (id:2)'])
-    <h3 class="mb-3">Bienvenido al módulo de bodega de medicamentos</h3>
-@endcan
-
-@can('Pharmacy: APS (id:3)')
-    <h3 class="mb-3">Bienvenido al módulo de bodega APS</h3>
-@endcan
-
-@can('Pharmacy: Servicios generales (id:4)')
-    <h3 class="mb-3">Bienvenido al módulo de bodega Servicios Generales</h3>
-@endcan
-
-
-
-@can('Pharmacy: SSI (id:1)')
-    <h4>Bodega selecionada: SSI</h4>
-@endcan
-
-@can('Pharmacy: REYNO (id:2)')
-    <h4>Bodega selecionada: Hector Reyno</h4>
-@endcan
-
-@can('Pharmacy: APS (id:3)')
-    <h4>Bodega selecionada: APS</h4>
-@endcan
-
-@can('Pharmacy: Servicios generales (id:4)')
-    <h4>Bodega selecionada: Servicios Generales</h4>
-@endcan
+<h3 class="mb-3">Bienvenido al área de administración del módulo de bodegas.</h3>
 
 @endsection
 
