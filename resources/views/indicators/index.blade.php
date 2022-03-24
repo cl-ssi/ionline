3fae7334776a2a5250684767ee2ac0ee247a50a1
Jorge Miranda <mirandal.jorge@gmail.com>
2022-03-24 17:24:47 -0300

    REM: D15

--- resources/views/indicators/index.blade.php
+++ resources/views/indicators/index.blade.php
@@ -101,6 +101,7 @@
                 <strong>Serie D</strong>
             </div>
             <ul class="list-group list-group-flush">
+                <li class="list-group-item text-muted"><a href="{{ route('indicators.rem.index', [2022, 'D']) }}">2022</a></li>
                 <li class="list-group-item text-muted"><a href="{{ route('indicators.rem.index', [2021, 'D']) }}">2021</a></li>
                 <li class="list-group-item"><a href="{{ route('indicators.rems.year.serie.index', [2020, 'serie_d']) }}">2020</a></li>
                 <li class="list-group-item text-muted"><a href="{{ route('indicators.rems.year.serie.index', [2019, 'serie_d']) }}">2019</a></li>
