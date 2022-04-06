2fa512210a24f531bfede97ec9e5fb03fe3e1cf5
GermanZun <german.zuniga@gmail.com>
2022-04-06 13:15:58 -0400

    ident 4 events

--- app/Http/Controllers/Requirements/EventController.php
+++ app/Http/Controllers/Requirements/EventController.php
@@ -64,139 +64,137 @@ class EventController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         //cuando es nulo, es porque el select viene disabled, y debe obtenerse nuevamente la info.
-        if($request->to_ou_id == null){
-          $lastEvent = Event::where('requirement_id',$request->requirement_id)
-                            ->where('from_user_id','<>',Auth::user()->id)->get()->last();
-          //si no existen respuestas de otras personas, se devuelve la ultima cualquiera.
-          if($lastEvent == null){
-            $lastEvent = Event::where('requirement_id',$request->requirement_id)->get()->last();
-          }
-          $firstEvent = Event::where('requirement_id',$request->requirement_id)
-                             //->where('from_user_id','<>',Auth::user()->id)->get()
-                             ->First();
-          if($request->status == 'respondido'){
-            $request['to_ou_id'] = $lastEvent->from_user->organizationalUnit->id;
-            $request['to_user_id'] = $lastEvent->from_user->id;
-          }else{
-            $request['to_ou_id'] = $firstEvent->from_user->organizationalUnit->id;
-            $request['to_user_id'] = $firstEvent->from_user->id;
-          }
+        if ($request->to_ou_id == null) {
+            $lastEvent = Event::where('requirement_id', $request->requirement_id)
+                ->where('from_user_id', '<>', Auth::user()->id)->get()->last();
+            //si no existen respuestas de otras personas, se devuelve la ultima cualquiera.
+            if ($lastEvent == null) {
+                $lastEvent = Event::where('requirement_id', $request->requirement_id)->get()->last();
+            }
+            $firstEvent = Event::where('requirement_id', $request->requirement_id)
+                //->where('from_user_id','<>',Auth::user()->id)->get()
+                ->First();
+            if ($request->status == 'respondido') {
+                $request['to_ou_id'] = $lastEvent->from_user->organizationalUnit->id;
+                $request['to_user_id'] = $lastEvent->from_user->id;
+            } else {
+                $request['to_ou_id'] = $firstEvent->from_user->organizationalUnit->id;
+                $request['to_user_id'] = $firstEvent->from_user->id;
+            }
         }
 
         //dd($request->users);
         //cuando no se agregó derivación a tabla temporal
-        if($request->users == null){
+        if ($request->users == null) {
 
-          //guarda evento.
-          $requirementEvent = new Event($request->All());
-          $requirementEvent->from_user()->associate(Auth::user());
-          $requirementEvent->from_ou_id = Auth::user()->organizationalUnit->id;
-          $requirementEvent->save();
+            //guarda evento.
+            $requirementEvent = new Event($request->All());
+            $requirementEvent->from_user()->associate(Auth::user());
+            $requirementEvent->from_ou_id = Auth::user()->organizationalUnit->id;
+            $requirementEvent->save();
 
-          //asocia evento con documentos
-          if($request->documents <> null){
-            foreach ($request->documents as $key => $document_aux) {
-              $document = Document::find($document_aux);
-              $requirementEvent->documents()->attach($document);
-            }
-          }
-
-          //modifica estado del requerimiento
-          $requirement = Requirement::find($request->requirement_id);
-          //$requirement->user_id = Auth::user()->id;
-          $requirement->status = $request->status;
-          $requirement->save();
-
-          //guarda archivos
-          if($request->hasFile('forfile')){
-            foreach($request->file('forfile') as $file) {
-              $filename = $file->getClientOriginalName();
-              $fileModel = New File;
-              $fileModel->file = $file->store('requirements');
-              $fileModel->name = $filename;
-              $fileModel->event_id = $requirementEvent->id;
-              //$fileModel->ticket()->associate($ticket);
-              $fileModel->save();
-            }
-          }
-        }
-
-        else{
-
-          //encuentra cuales son usuarios para derivar, y cuales son en copia.
-          $users_req = null;
-          $users_enCopia = null;
-          foreach ($request->enCopia as $key => $enCopia) {
-            if($enCopia == 0){
-              $users_req[] = $request->users[$key];
-            }
-            if($enCopia == 1){
-              $users_enCopia[] = $request->users[$key];
-            }
-          }
-
-          //dd($users_req, $users_enCopia);
-
-          //guarda eventos en copia
-          if ($users_enCopia <> null) {
-            foreach ($users_enCopia as $key => $user_) {
-              //guarda evento.
-              $user_aux = User::where('id',$user_)->get();
-              $requirementEvent = new Event($request->All());
-              $requirementEvent->to_ou_id = $user_aux->first()->organizational_unit_id;
-              $requirementEvent->to_user_id = $user_;
-              $requirementEvent->status = "en copia";
-              $requirementEvent->from_user()->associate(Auth::user());
-              $requirementEvent->from_ou_id = Auth::user()->organizationalUnit->id;
-              $requirementEvent->save();
-            }
-          }
-
-          //guarda otros eventos
-          if($users_req != null){
-            foreach ($users_req as $key => $user_) {
-              //guarda evento.
-              $user_aux = User::where('id',$user_)->get();
-              $requirementEvent = new Event($request->All());
-              $requirementEvent->to_ou_id = $user_aux->first()->organizational_unit_id;
-              $requirementEvent->to_user_id = $user_;
-              $requirementEvent->from_user()->associate(Auth::user());
-              $requirementEvent->from_ou_id = Auth::user()->organizationalUnit->id;
-              $requirementEvent->save();
-
-              //asocia evento con documentos
-              if($request->documents <> null){
+            //asocia evento con documentos
+            if ($request->documents <> null) {
                 foreach ($request->documents as $key => $document_aux) {
-                  $document = Document::find($document_aux);
-                  $requirementEvent->documents()->attach($document);
+                    $document = Document::find($document_aux);
+                    $requirementEvent->documents()->attach($document);
                 }
-              }
-
-              //modifica estado del requerimiento
-              $requirement = Requirement::find($request->requirement_id);
-              $requirement->status = $request->status;
-              $requirement->save();
-
-              //guarda archivos
-              if($request->hasFile('forfile')){
-                foreach($request->file('forfile') as $file) {
-                  $filename = $file->getClientOriginalName();
-                  $fileModel = New File;
-                  $fileModel->file = $file->store('requirements');
-                  $fileModel->name = $filename;
-                  $fileModel->event_id = $requirementEvent->id;
-                  //$fileModel->ticket()->associate($ticket);
-                  $fileModel->save();
-                }
-              }
             }
-          }
+
+            //modifica estado del requerimiento
+            $requirement = Requirement::find($request->requirement_id);
+            //$requirement->user_id = Auth::user()->id;
+            $requirement->status = $request->status;
+            $requirement->save();
+
+            //guarda archivos
+            if ($request->hasFile('forfile')) {
+                foreach ($request->file('forfile') as $file) {
+                    $filename = $file->getClientOriginalName();
+                    $fileModel = new File;
+                    $fileModel->file = $file->store('requirements');
+                    $fileModel->name = $filename;
+                    $fileModel->event_id = $requirementEvent->id;
+                    //$fileModel->ticket()->associate($ticket);
+                    $fileModel->save();
+                }
+            }
+        } else {
+
+            //encuentra cuales son usuarios para derivar, y cuales son en copia.
+            $users_req = null;
+            $users_enCopia = null;
+            foreach ($request->enCopia as $key => $enCopia) {
+                if ($enCopia == 0) {
+                    $users_req[] = $request->users[$key];
+                }
+                if ($enCopia == 1) {
+                    $users_enCopia[] = $request->users[$key];
+                }
+            }
+
+            //dd($users_req, $users_enCopia);
+
+            //guarda eventos en copia
+            if ($users_enCopia <> null) {
+                foreach ($users_enCopia as $key => $user_) {
+                    //guarda evento.
+                    $user_aux = User::where('id', $user_)->get();
+                    $requirementEvent = new Event($request->All());
+                    $requirementEvent->to_ou_id = $user_aux->first()->organizational_unit_id;
+                    $requirementEvent->to_user_id = $user_;
+                    $requirementEvent->status = "en copia";
+                    $requirementEvent->from_user()->associate(Auth::user());
+                    $requirementEvent->from_ou_id = Auth::user()->organizationalUnit->id;
+                    $requirementEvent->save();
+                }
+            }
+
+            //guarda otros eventos
+            if ($users_req != null) {
+                foreach ($users_req as $key => $user_) {
+                    //guarda evento.
+                    $user_aux = User::where('id', $user_)->get();
+                    $requirementEvent = new Event($request->All());
+                    $requirementEvent->to_ou_id = $user_aux->first()->organizational_unit_id;
+                    $requirementEvent->to_user_id = $user_;
+                    $requirementEvent->from_user()->associate(Auth::user());
+                    $requirementEvent->from_ou_id = Auth::user()->organizationalUnit->id;
+                    $requirementEvent->save();
+
+                    //asocia evento con documentos
+                    if ($request->documents <> null) {
+                        foreach ($request->documents as $key => $document_aux) {
+                            $document = Document::find($document_aux);
+                            $requirementEvent->documents()->attach($document);
+                        }
+                    }
+
+                    //modifica estado del requerimiento
+                    $requirement = Requirement::find($request->requirement_id);
+                    $requirement->status = $request->status;
+                    $requirement->save();
+
+                    //guarda archivos
+                    if ($request->hasFile('forfile')) {
+                        foreach ($request->file('forfile') as $file) {
+                            $filename = $file->getClientOriginalName();
+                            $fileModel = new File;
+                            $fileModel->file = $file->store('requirements');
+                            $fileModel->name = $filename;
+                            $fileModel->event_id = $requirementEvent->id;
+                            //$fileModel->ticket()->associate($ticket);
+                            $fileModel->save();
+                        }
+                    }
+                }
+            }
 
         }
 
@@ -207,7 +205,7 @@ class EventController extends Controller
                 ->send(new RequirementEventNotification($requirementEvent));
         }
 
-        session()->flash('info', 'El evento '.$requirementEvent->id.' ha sido ingresado.');
+        session()->flash('info', 'El evento ' . $requirementEvent->id . ' ha sido ingresado.');
 
         /*$requirementEvent->requirement->status = $requirementEvent->status;
         $requirementEvent->requirement->save();
@@ -224,7 +222,7 @@ class EventController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Requirements\RequirementEvent  $requirementEvent
+     * @param \App\Requirements\RequirementEvent $requirementEvent
      * @return \Illuminate\Http\Response
      */
     public function show(RequirementEvent $requirementEvent)
@@ -235,7 +233,7 @@ class EventController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Requirements\RequirementEvent  $requirementEvent
+     * @param \App\Requirements\RequirementEvent $requirementEvent
      * @return \Illuminate\Http\Response
      */
     public function edit(RequirementEvent $requirementEvent)
@@ -246,8 +244,8 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Requirements\RequirementEvent  $requirementEvent
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Requirements\RequirementEvent $requirementEvent
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, RequirementEvent $requirementEvent)
@@ -258,7 +256,7 @@ class EventController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Requirements\RequirementEvent  $requirementEvent
+     * @param \App\Requirements\RequirementEvent $requirementEvent
      * @return \Illuminate\Http\Response
      */
     public function destroy(RequirementEvent $requirementEvent)
@@ -268,6 +266,6 @@ class EventController extends Controller
 
     public function download(File $file)
     {
-        return Storage::response($file->file, mb_convert_encoding($file->name,'ASCII'));
+        return Storage::response($file->file, mb_convert_encoding($file->name, 'ASCII'));
     }
 }
