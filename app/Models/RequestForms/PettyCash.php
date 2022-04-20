59a3baa550c5a3bbfbca935381e87b254bd209b6
Alvaro Lupa <alvaro.lupa@gmail.com>
2022-04-20 18:23:06 -0400

    FR: se corrige documento OC interna y se agregar auditoria a mecanismos de compra faltantes

--- app/Models/RequestForms/PettyCash.php
+++ app/Models/RequestForms/PettyCash.php
@@ -5,9 +5,11 @@ namespace App\Models\RequestForms;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class PettyCash extends Model
+class PettyCash extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
     use HasFactory;
     use SoftDeletes;
 
