97eaa2ae1a8ae523652f0ac1d6197b9edd286bc7
Jorge Miranda <mirandal.jorge@gmail.com>
2022-04-26 17:19:07 -0400

    Indicators: APS 2022

--- resources/views/indicators/aps/partials/card.blade.php
+++ resources/views/indicators/aps/partials/card.blade.php
@@ -4,6 +4,9 @@
     </div>
 
     <ul class="list-group list-group-flush">
+      <li class="list-group-item">
+          <a href="{{ route('indicators.iaps.list', 2022) }}">2022</a> <span class="badge badge-warning">En Desarrollo</span>
+      </li>
         <li class="list-group-item">
             <a href="{{ route('indicators.iaps.list', 2021) }}">2021</a>
         </li>
