672176c8c18420fd931ceb0a5cd468af58844423
Jorge Miranda <mirandal.jorge@gmail.com>
2022-04-28 18:10:23 -0400

    Indicators: Serie BM18 2021

--- resources/views/indicators/rem/index.blade.php
+++ resources/views/indicators/rem/index.blade.php
@@ -33,7 +33,7 @@
                 <strong>Serie BM</strong>
             </div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item text-muted">{{--<a href="{{ route('indicators.rem.index', [2021, 'BM']) }}">2021</a>--}} 2021</li>
+                <li class="list-group-item text-muted"><a href="{{ route('indicators.rem.index', [2021, 'BM']) }}">2021</a></li>
                 <li class="list-group-item text-muted"><a href="{{ route('indicators.rems.year.serie.index', [2020, 'serie_bm']) }}">2020</a></li>
                 <li class="list-group-item text-muted"><a>2019</a> </li>
                 <li class="list-group-item text-muted"><a href="{{ route('indicators.rem.index', [2018, 'BM']) }}">2018</a></li>
