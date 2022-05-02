2b634e814af451a8e9af6b2628952f6a2a672353
Jorge Miranda <mirandal.jorge@gmail.com>
2022-05-02 14:12:29 -0400

    Indicators: Se agregar REM BM18 2022 en index REM

--- resources/views/indicators/rem/index.blade.php
+++ resources/views/indicators/rem/index.blade.php
@@ -33,6 +33,7 @@
                 <strong>Serie BM</strong>
             </div>
             <ul class="list-group list-group-flush">
+                <li class="list-group-item text-muted"><a href="{{ route('indicators.rem.index', [2022, 'BM']) }}">2022</a></li>
                 <li class="list-group-item text-muted"><a href="{{ route('indicators.rem.index', [2021, 'BM']) }}">2021</a></li>
                 <li class="list-group-item text-muted"><a href="{{ route('indicators.rems.year.serie.index', [2020, 'serie_bm']) }}">2020</a></li>
                 <li class="list-group-item text-muted"><a>2019</a> </li>
