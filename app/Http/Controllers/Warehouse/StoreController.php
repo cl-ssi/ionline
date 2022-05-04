487a5a0e902a563a03a937801af83972310c7a52
rafael1delgado <delgadorafael2011@gmail.com>
2022-05-04 16:29:51 -0400

    [feat] Add controllers for maintainer and Store of Warehouse

---
+++ app/Http/Controllers/Warehouse/StoreController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Warehouse;
+
+use App\Http\Controllers\Controller;
+use App\Models\Warehouse\Store;
+use App\Models\Warehouse\StoreUser;
+use Illuminate\Support\Facades\Auth;
+
+class StoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('warehouse.stores.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('warehouse.stores.create');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Warehouse\Store  $store
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Store $store)
+    {
+        return view('warehouse.stores.edit', compact('store'));
+    }
+
+    /**
+     * Manage store users.
+     *
+     * @param  \App\Models\Warehouse\Store  $store
+     * @return \Illuminate\Http\Response
+     */
+    public function users(Store $store)
+    {
+        return view('warehouse.stores.manage-users', compact('store'));
+    }
+
+    public function welcome()
+    {
+        return view('warehouse.stores.welcome');
+    }
+
+    public function activateStore(Store $store)
+    {
+        $storeActive = Auth::user()->active_store;
+
+        if($storeActive)
+        {
+            $storeActive->pivot->status = 0;
+            $storeActive->pivot->save();
+        }
+
+        $selectedActive = StoreUser::whereStoreId($store->id)->whereUserId(Auth::id())->first();
+        if($selectedActive)
+        {
+            $selectedActive->update(['status' => 1]);
+        }
+
+        return redirect()->route('warehouse.store.welcome');
+    }
+}
