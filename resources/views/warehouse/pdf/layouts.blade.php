61b3b62e670a92e277255cc4d36f976256ed4dc3
rafael1delgado <delgadorafael2011@gmail.com>
2022-05-04 16:38:43 -0400

    [feat] Add view for dispatch PDF report

---
+++ resources/views/warehouse/pdf/layouts.blade.php
@@ -0,0 +1,24 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>@yield('title')</title>
+        <meta name="description" content="Report Store">
+        <meta name="author" content="{{ env('APP_SS') }}">
+        <link href="{{ asset('css/report.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="content">
+            <img style="padding-bottom: 4px;" src="{{ asset('images/logo_pluma.jpg') }}"
+                width="120" alt="Logo {{ env('APP_SS') }}"><br>
+
+            @yield('content')
+
+            <div class="pie_pagina seis center">
+                <span class="uppercase">{{ env('APP_SS') }}</span><br>
+                {{ env('APP_SS_ADDRESS') }} -
+                Fono: 572406976 -
+                {{ env('APP_SS_WEBSITE') }}
+            </div>
+        </div>
+    </body>
+</html>
