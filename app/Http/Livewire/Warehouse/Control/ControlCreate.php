36eea63ea38678baae11b3e49f62cb8f1fc727f2
rafael1delgado <delgadorafael2011@gmail.com>
2022-05-06 13:39:03 -0400

    [feat] Add the program when creating the store

--- app/Http/Livewire/Warehouse/Control/ControlCreate.php
+++ app/Http/Livewire/Warehouse/Control/ControlCreate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Warehouse\Control;
 
 use App\Models\Warehouse\Control;
+use App\Pharmacies\Program;
 use Livewire\Component;
 
 class ControlCreate extends Component
@@ -11,20 +12,29 @@ class ControlCreate extends Component
     public $type;
     public $date;
     public $note;
+    public $program_id;
     public $origin_id;
     public $destination_id;
+    public $programs;
 
     public $rulesReceiving = [
-        'date'      => 'required|date_format:Y-m-d',
-        'note'      => 'required|string|min:2|max:255',
-        'origin_id' => 'required|exists:wre_origins,id',
+        'date'          => 'required|date_format:Y-m-d',
+        'note'          => 'required|string|min:2|max:255',
+        'program_id'    => 'required|exists:frm_programs,id',
+        'origin_id'     => 'required|exists:wre_origins,id',
     ];
 
     public $rulesDispatch = [
         'date'              => 'required|date_format:Y-m-d',
         'note'              => 'required|string|min:2|max:255',
+        'program_id'        => 'required|exists:frm_programs,id',
         'destination_id'    => 'required|exists:wre_destinations,id',
     ];
+
+    public function mount()
+    {
+        $this->programs = Program::all();
+    }
 
     public function render()
     {
