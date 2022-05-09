77b705e5d169184dae69146c0f2d73c3428534a7
rafael1delgado <delgadorafael2011@gmail.com>
2022-05-09 19:41:44 -0400

    [feat] Add programs table

---
+++ database/migrations/2022_05_09_103455_create_cfg_programs.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Models\Cfg\Program as CfgProgram;
+use App\Pharmacies\Program;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCfgPrograms extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cfg_programs', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 255)->nullable();
+            $table->date('start')->nullable();
+            $table->date('end')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        $pharmacyPrograms = Program::all();
+
+        foreach($pharmacyPrograms as $program)
+        {
+            CfgProgram::create([
+                'name' => $program->name,
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cfg_programs');
+    }
+}
