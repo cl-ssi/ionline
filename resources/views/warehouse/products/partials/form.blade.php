de6c53cc9f393c94dbfc3a7c6fc28ab673959f0d
rafael1delgado <delgadorafael2011@gmail.com>
2022-05-10 18:17:20 -0400

    [feat] Update partial view

--- resources/views/warehouse/products/partials/form.blade.php
+++ resources/views/warehouse/products/partials/form.blade.php
@@ -1,7 +1,13 @@
 <div class="form-row mt-3">
     <fieldset class="form-group col-md-4">
         <label for="store-id">Bodega</label>
-        <input type="text" id="store-id" class="form-control" value="{{ optional($store)->name }}" disabled>
+        <input
+            type="text"
+            id="store-id"
+            class="form-control"
+            value="{{ optional($store)->name }}"
+            disabled
+        >
     </fieldset>
 
     <fieldset class="form-group col-md-4">
@@ -12,45 +18,37 @@
             wire:model="name"
             placeholder="Ingresa el nombre"
             value="{{ old('name', optional($product)->name) }}"
-            required>
+            required
+        >
         @error('name')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
-    </fieldset>
-
-    <fieldset class="form-group col-md-2">
-        <label for="program-id">Programa</label>
-        <select id="program-id" class="form-control @error('program_id') is-invalid @enderror" wire:model="program_id" required>
-            <option value="">Selecciona el programa</option>
-            @foreach($programs as $program)
-                <option value="{{ $program->id }}" {{ optional($product)->program_id == $program->id ? 'selected' : '' }}>
-                    {{ $program->name }}
-                </option>
-            @endforeach
-        </select>
-        @error('program_id')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
         @enderror
     </fieldset>
 
     <fieldset class="form-group col-md-2">
         <label for="category-id">Categoría</label>
-        <select id="category-id" class="form-control @error('category_id') is-invalid @enderror" wire:model="category_id" required>
+        <select
+            id="category-id"
+            class="form-control @error('category_id') is-invalid @enderror"
+            wire:model="category_id"
+            required
+        >
             <option value="">Selecciona la categoría</option>
             @foreach($categories as $category)
-                <option value="{{ $category->id }}" {{ optional($product)->category_id == $category->id ? 'selected' : '' }}>
+                <option
+                    value="{{ $category->id }}"
+                    {{ optional($product)->category_id == $category->id ? 'selected' : '' }}
+                >
                     {{ $category->name }}
                 </option>
             @endforeach
         </select>
         @error('category_id')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
         @enderror
     </fieldset>
 </div>
