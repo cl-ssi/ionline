25a5f823ac50e32e6e72402f37b4b9edc8bf067a
Alvaro Lupa <alvaro.lupa@gmail.com>
2022-05-11 10:21:08 -0400

    Indicadores: se habilita metas ley 19.664 año 2022

--- resources/views/indicators/18834/partials/card.blade.php
+++ resources/views/indicators/18834/partials/card.blade.php
@@ -4,7 +4,7 @@
     </div>
     <ul class="list-group list-group-flush">
         <li class="list-group-item">
-            <a href="{{ route('indicators.health_goals.list', [18834, 2022]) }}">2022</a> <span class="badge badge-warning">En Desarrollo</span>
+            <a href="{{ route('indicators.health_goals.list', [18834, 2022]) }}">2022</a>
         </li>
         <li class="list-group-item">
             <a href="{{ route('indicators.health_goals.list', [18834, 2021]) }}">2021</a>
