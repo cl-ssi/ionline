2fa8e524000c8ee7dc8b1bc943f35b362138c6cb
rafael1delgado <delgadorafael2011@gmail.com>
2022-05-17 18:25:08 -0400

    [feat] Add type dispatch and type reception model

---
+++ app/Models/Warehouse/TypeReception.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models\Warehouse;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TypeReception extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'wre_type_receptions';
+
+    protected $fillable = [
+        'name',
+        'active',
+    ];
+
+    public static function receiving()
+    {
+        return 1;
+    }
+
+    public static function receiveFromStore()
+    {
+        return 2;
+    }
+
+    public static function return()
+    {
+        return 3;
+    }
+}
