919a197137f56e22497dc93d4a428f65f46e374a
rafael1delgado <delgadorafael2011@gmail.com>
2022-05-17 18:24:13 -0400

    [feat] Add type receptions adn type dispatches

---
+++ database/migrations/2022_05_09_145954_create_wre_type_dispatches_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Models\Warehouse\TypeDispatch;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWreTypeDispatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wre_type_dispatches', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 255)->nullable();
+            $table->boolean('active')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        $dispatches = [
+            [
+                'name' => 'Egreso Normal',
+                'active' => true,
+            ],
+            [
+                'name' => 'Ajuste de Inventario',
+                'active' => true,
+            ],
+
+            [
+                'name' => 'Enviar a Bodega',
+                'active' => true,
+            ]
+        ];
+
+        foreach($dispatches as $dispatch)
+        {
+            TypeDispatch::create($dispatch);
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wre_type_dispatches');
+    }
+}
