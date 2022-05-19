c3c285065e2eaf5866bfe1c981ec66d1699af560
rafael1delgado <delgadorafael2011@gmail.com>
2022-05-18 23:22:03 -0400

    [feat] Add ProgramController

---
+++ app/Http/Controllers/Cfg/ProgramController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Cfg;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProgramController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('cfg.programs.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+}
