98f57bfaf1fb75b524aa3e4bdd06a01b7c6c17e2
rafael1delgado <delgadorafael2011@gmail.com>
2022-05-31 11:56:47 -0400

    [feat] Add Inventory model

---
+++ app/Models/Inv/Inventory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models\Inv;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Inventory extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'inv_inventories';
+
+    protected $fillable = [
+        'number',
+        'useful_life',
+        'applicant',
+        'brand',
+        'model',
+        'serial_number',
+        'po_code',
+        'po_price',
+        'po_date',
+        'deliver_date',
+        'delivered_to',
+        'reception_confirmation',
+        'product_id',
+        'po_id',
+        'control_id',
+        'place_id',
+        'store_id',
+    ];
+}
