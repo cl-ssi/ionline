b2a07478c0f2fa7dae9479a2872b702e52d39162
rafael1delgado <delgadorafael2011@gmail.com>
2022-05-31 13:38:06 -0400

    [feat] Add program model

---
+++ app/Models/Parameters/Program.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models\Parameters;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Program extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'cfg_programs';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'start_date',
+        'end_date',
+    ];
+
+    protected $dates = [
+        'start_date',
+        'end_date'
+    ];
+
+    public function getStartDateFormatAttribute()
+    {
+        $date = '-';
+        if($this->start_date)
+            $date = $this->start_date->format('Y-m-d');
+        return $date;
+    }
+
+    public function getEndDateFormatAttribute()
+    {
+        $date = '-';
+        if($this->end_date != null)
+            $date = $this->end_date->format('Y-m-d');
+        return $date;
+    }
+}
