bdd59c5baa3dcc25c790865de469f075abb2a7e9
rafael1delgado <delgadorafael2011@gmail.com>
2022-05-31 11:54:43 -0400

    [feat] Add generate reception view

---
+++ resources/views/warehouse/stores/generate-reception.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Crear Ingreso')
+
+@section('content')
+
+@include('warehouse.nav')
+
+@livewire('warehouse.control.generate-reception', [
+    'store' => $store,
+])
+
+@endsection
