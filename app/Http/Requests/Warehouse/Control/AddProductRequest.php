f2de14dbe74bda9e46a422bd0450dca20da3deef
rafael1delgado <delgadorafael2011@gmail.com>
2022-06-02 13:42:29 -0400

    [feat] Add barcode unique & store_id

--- app/Http/Requests/Warehouse/Control/AddProductRequest.php
+++ app/Http/Requests/Warehouse/Control/AddProductRequest.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Requests\Warehouse\Control;
 
+use App\Models\Warehouse\Store;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class AddProductRequest extends FormRequest
 {
     public $max_quantity;
+    public $store_id;
 
-    public function __construct($max_quantity)
+    public function __construct($store_id, $max_quantity)
     {
+        $this->store_id = $store_id;
         $this->max_quantity = $max_quantity;
     }
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -32,7 +37,13 @@ class AddProductRequest extends FormRequest
         return [
             'quantity'      => 'required|integer|min:0|max:' . $this->max_quantity,
             'description'   => 'required',
-            'barcode'       => 'nullable', // definir
+            'barcode'       => [
+                'nullable',
+                'string',
+                'min:1',
+                'max:255',
+                Rule::unique('wre_products', 'barcode')->where('store_id', $this->store_id)
+            ],
         ];
     }
 }
