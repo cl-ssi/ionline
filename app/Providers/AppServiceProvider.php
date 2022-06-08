89df6e3dd8baea7083e49b35c476ccaca3247137
tebiccr <tebiccr@gmail.com>
2022-06-07 23:05:03 -0400

    se quiten las importaciones y codigo no usado

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,9 +7,7 @@ use Illuminate\Support\Facades\Blade;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;	
-use Illuminate\Support\Facades\File;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -52,33 +50,7 @@ class AppServiceProvider extends ServiceProvider
         Blade::directive('numero_decimal', function ($numero) {
             return "<?php echo number_format($numero, 2, '.', '.'); ?>";
         });
-
-
-        DB::listen(function ($query) {
-            // $location = collect(debug_backtrace())->filter(function ($trace) {
-            //     return !str_contains($trace['file'], 'vendor/');
-            // })->first(); // grab the first element of non vendor/ calls
-
-            // $bindings = implode(", ", $query->bindings); // format the bindings as string
-
-            // Log::info("
-            //        ------------
-            //        Sql: $query->sql
-            //        Bindings: $bindings
-            //        Time: $query->time
-            //        File: {$location['file']}
-            //        Line: {$location['line']}
-            //        ------------
-            // ");
-            // $query->sql; // the sql string that was executed
-            // $query->bindings; // the parameters passed to the sql query (this replace the '?'s in the sql string)
-            // $query->time; // the time it took for the query to execute;
-
-            // Log::info($query->sql);
-            // Log::info($query->bindings);
-            // Log::info($query->time);
-        });
-
+        
         Paginator::useBootstrap();
     }
 }
