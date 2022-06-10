91e50dda0bcdb98873b725dc8a8157248fc1a69d
rafael1delgado <delgadorafael2011@gmail.com>
2022-06-10 12:57:59 -0400

    [feat] Add relationship to the inventory model

--- app/Models/Inv/Inventory.php
+++ app/Models/Inv/Inventory.php
@@ -2,6 +2,14 @@
 
 namespace App\Models\Inv;
 
+use App\Models\RequestForms\PurchaseOrder;
+use App\Models\RequestForms\RequestForm;
+use App\Models\Warehouse\Control;
+use App\Models\Warehouse\Product;
+use App\Models\Warehouse\Store;
+use App\Parameters\Place;
+use App\Rrhh\OrganizationalUnit;
+use App\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -34,4 +42,54 @@ class Inventory extends Model
         'po_id',
         'request_form_id',
     ];
+
+    public function deliveredOrganizationalUnit()
+    {
+        return $this->belongsTo(OrganizationalUnit::class, 'delivered_user_ou_id');
+    }
+
+    public function deliveredUser()
+    {
+        return $this->belongsTo(User::class, 'delivered_user_id');
+    }
+
+    public function requestOrganizationalUnit()
+    {
+        return $this->belongsTo(OrganizationalUnit::class, 'request_user_ou_id');
+    }
+
+    public function requestUser()
+    {
+        return $this->belongsTo(User::class, 'request_user_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function control()
+    {
+        return $this->belongsTo(Control::class);
+    }
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+
+    public function place()
+    {
+        return $this->belongsTo(Place::class);
+    }
+
+    public function purchaseOrder()
+    {
+        return $this->belongsTo(PurchaseOrder::class, 'po_id');
+    }
+
+    public function requestForm()
+    {
+        return $this->belongsTo(RequestForm::class);
+    }
 }
