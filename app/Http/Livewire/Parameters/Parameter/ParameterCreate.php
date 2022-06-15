6b3aa5e71e50898710dcbb9e91ef2ebf7a72e3b4
rafael1delgado <delgadorafael2011@gmail.com>
2022-06-15 16:11:09 -0400

    [feat] add parameter components

---
+++ app/Http/Livewire/Parameters/Parameter/ParameterCreate.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Livewire\Parameters\Parameter;
+
+use App\Http\Requests\Parameters\Parameter\CreateParameterRequest;
+use App\Models\Parameters\Parameter;
+use Livewire\Component;
+
+class ParameterCreate extends Component
+{
+    public $module;
+    public $parameter_field;
+    public $value;
+    public $description;
+
+    public function render()
+    {
+        return view('livewire.parameters.parameter.parameter-create')->extends('layouts.app');
+    }
+
+    public function rules()
+    {
+        return (new CreateParameterRequest($this->module))->rules();
+    }
+
+    public function create()
+    {
+        $dataValidated = $this->validate();
+        $dataValidated['parameter'] = $dataValidated['parameter_field'];
+        Parameter::create($dataValidated);
+
+        session()->flash('success', 'El parámetro fue creado exitosamente.');
+        return redirect()->route('parameters.index');
+    }
+}
