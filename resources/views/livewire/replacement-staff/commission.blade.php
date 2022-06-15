7fffd31cd532a9c1a77ae5c13cdbbed89379d293
Jorge Miranda <mirandal.jorge@gmail.com>
2022-06-15 13:22:34 -0400

    Replacement Staff: Prueba en comisión

--- resources/views/livewire/replacement-staff/commission.blade.php
+++ resources/views/livewire/replacement-staff/commission.blade.php
@@ -10,6 +10,8 @@
       @endif
   </div>
 
+  {{-- dd($users) --}}
+
   <form method="POST" class="form-horizontal"
       action="{{ route('replacement_staff.request.technical_evaluation.commission.store', $technicalEvaluation) }}"/>
       @csrf
