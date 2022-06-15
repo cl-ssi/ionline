3a5b91cd9b3e276f35286e5a28e0922e7b0d6bf1
rafael1delgado <delgadorafael2011@gmail.com>
2022-06-15 16:09:37 -0400

    [feat] add barcode and search for unspsc

--- resources/views/warehouse/products/partials/form.blade.php
+++ resources/views/warehouse/products/partials/form.blade.php
@@ -10,7 +10,7 @@
         >
     </fieldset>
 
-    <fieldset class="form-group col-md-4">
+    <fieldset class="form-group col-md-3">
         <label for="name">Nombre</label>
         <input type="text"
             class="form-control @error('name') is-invalid @enderror"
@@ -21,6 +21,22 @@
             required
         >
         @error('name')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </fieldset>
+
+    <fieldset class="form-group col-md-3">
+        <label for="barcode">Código Barra</label>
+        <input type="text"
+            class="form-control @error('barcode') is-invalid @enderror"
+            id="barcode"
+            wire:model.debounce.1500ms="barcode"
+            placeholder="Ingresa el código de barra"
+            value="{{ old('barcode', optional($product)->barcode) }}"
+        >
+        @error('barcode')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
@@ -46,6 +62,32 @@
             @endforeach
         </select>
         @error('category_id')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </fieldset>
+</div>
+
+<div class="form-row">
+    <fieldset class="form-group col-md-3">
+        <label for="product-search">Buscar Producto o Servicio</label>
+        <input
+            wire:model.debounce.1500ms="search_unspsc_product"
+            id="product-search"
+            class="form-control"
+            type="text"
+        >
+    </fieldset>
+
+    <fieldset class="form-group col-md-6">
+        <label for="product-id">Selecciona un Producto o Servicio</label>
+        @livewire('unspsc.product-search')
+        <input
+            class="form-control @error('unspsc_product_id') is-invalid @enderror"
+            type="hidden"
+        >
+        @error('unspsc_product_id')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
