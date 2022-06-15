a8e4d1bc274e1e1cc394737d122df53c32e58fd8
rafael1delgado <delgadorafael2011@gmail.com>
2022-06-15 16:10:39 -0400

    [feat] Add button & script js

--- resources/views/livewire/warehouse/products/product-create.blade.php
+++ resources/views/livewire/warehouse/products/product-create.blade.php
@@ -5,6 +5,10 @@
         'product' => null
     ])
 
-    <button wire:click="createProduct" class="btn btn-primary">Guardar</button>
-    <a href="{{ route('warehouse.products.index', $store) }}" class="btn btn-outline-primary">Cancelar</a>
+    <button wire:click="createProduct" class="btn btn-primary">
+        Guardar
+    </button>
+    <a href="{{ route('warehouse.products.index', $store) }}" class="btn btn-outline-primary">
+        Cancelar
+    </a>
 </div>
