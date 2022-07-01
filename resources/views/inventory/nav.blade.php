5355588f6aab5736d201f66beba2cf5aff52f39f
rafael1delgado <delgadorafael2011@gmail.com>
2022-07-01 11:19:34 -0400

    [feat] Add inventory view

---
+++ resources/views/inventory/nav.blade.php
@@ -0,0 +1,35 @@
+<ul class="nav nav-tabs mb-3">
+    <li class="nav-item">
+        <a
+            class="nav-link {{ active('inventories.last-income') }}"
+            href="{{ route('inventories.last-income') }}"
+        >
+        <i class="fas fa-clock"></i> Últimos Ingresos a Bodega
+        </a>
+    </li>
+    <li class="nav-item">
+        <a
+            class="nav-link {{ active('inventories.pending-inventory') }}"
+            href="{{ route('inventories.pending-inventory') }}"
+        >
+            <i class="fas fa-hourglass"></i> Bandeja Pendiente de Inventario
+        </a>
+    </li>
+    <li class="nav-item">
+        <a
+            class="nav-link {{ active('inventories.details') }}"
+            href="{{ route('inventories.details') }}"
+        >
+            <i class="fas fa-shipping-fast"></i> Item Inventario
+        </a>
+    </li>
+    <li class="nav-item">
+        <a
+            class="nav-link {{ active('inventories.index') }}"
+            aria-current="page"
+            href="{{ route('inventories.index') }}"
+        >
+            <i class="fas fa-list-alt"></i> Inventario
+        </a>
+    </li>
+</ul>
