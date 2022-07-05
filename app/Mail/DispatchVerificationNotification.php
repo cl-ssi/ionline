2e3ac7e35bc247275c189618e6317eb308d2c507
sick <esteban.rojasg@redsalud.cl>
2022-07-05 15:05:53 -0400

    Nueva opción en farmacia para enviar correo solicitando confirmación de recepción de productos.

---
+++ app/Mail/DispatchVerificationNotification.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+
+use App\Pharmacies\Dispatch;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class DispatchVerificationNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $dispatch;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Dispatch $dispatch)
+    {
+        $this->dispatch = $dispatch;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $subject = "Nueva solicitud confirmación de recepción n°{$this->dispatch->id}";
+        return $this->view('pharmacies.mails.dispatch_verification_notificacion')->subject($subject);
+    }
+}
---
+++ app/Pharmacies/DispatchVerificationMailing.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Pharmacies;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DispatchVerificationMailing extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id','dispatch_id','status','sender_observation','delivery_date','receiver_observation','confirmation_date'
+    ];
+
+    use SoftDeletes;
+
+    protected $table = 'frm_dispatch_verification_mailings';
+
+    //relaciones
+    public function dispatch()
+    {
+      return $this->belongsTo('App\Pharmacies\Dispatch');
+    }
+
+    protected $dates = ['delivery_date','confirmation_date'];
+
+}
