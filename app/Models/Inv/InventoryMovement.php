95b2cff43462243d19cce6644cc929cea35aa024
rafael1delgado <delgadorafael2011@gmail.com>
2022-07-08 18:27:14 -0400

    [feat] Add InventoryMovement model

---
+++ app/Models/Inv/InventoryMovement.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models\Inv;
+
+use App\Models\Parameters\Place;
+use App\Rrhh\OrganizationalUnit;
+use App\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class InventoryMovement extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'inv_inventory_movements';
+
+    protected $fillable = [
+        'reception_confirmation',
+        'reception_date',
+        'installation_date',
+        'inventory_id',
+        'place_id',
+        'user_responsible_ou_id',
+        'user_responsible_id',
+        'user_using_ou_id',
+        'user_using_id',
+    ];
+
+    protected $dates = [
+        'reception_date',
+        'installation_date',
+    ];
+
+    public function inventory()
+    {
+        return $this->belongsTo(Inventory::class);
+    }
+
+    public function place()
+    {
+        return $this->belongsTo(Place::class);
+    }
+
+    public function responsibleOrganizationalUnit()
+    {
+        return $this->belongsTo(OrganizationalUnit::class, 'user_responsible_ou_id');
+    }
+
+    public function responsibleUser()
+    {
+        return $this->belongsTo(User::class, 'user_responsible_id');
+    }
+
+    public function usingOrganizationalUnit()
+    {
+        return $this->belongsTo(OrganizationalUnit::class, 'user_using_ou_id');
+    }
+
+    public function usingUser()
+    {
+        return $this->belongsTo(User::class, 'user_using_id');
+    }
+}
