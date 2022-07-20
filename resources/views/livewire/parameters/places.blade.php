c7234283047ddfd97125671a3ac2c4424451345d
rafael1delgado <delgadorafael2011@gmail.com>
2022-07-20 00:27:26 -0400

    [feat] add places view

---
+++ resources/views/livewire/parameters/places.blade.php
@@ -0,0 +1,79 @@
+<div>
+    @include('layouts.partials.flash_message')
+
+    @switch($view)
+        @case('index')
+            <div class="row">
+                <div class="col">
+                    <h3 class="mb-3">
+                        Lugares
+                    </h3>
+                </div>
+                <div class="col">
+                    <button
+                        class="btn btn-primary float-right"
+                        wire:click="create"
+                    >
+                        <i class="fas fa-plus"></i>
+                        Crear nuevo
+                    </button>
+                </div>
+            </div>
+            @include('parameters.places.index')
+            @break
+
+        @case('create')
+            <h3>Crear nuevo lugar</h3>
+            @include('parameters.places.form')
+                <button
+                    class="btn btn-primary"
+                    type="button"
+                    wire:target="store"
+                    wire:loading.attr="disabled"
+                    wire:click="store"
+                >
+                    <span
+                        class="spinner-border spinner-border-sm"
+                        role="status"
+                        wire:loading
+                        wire:target="store"
+                        aria-hidden="true"
+                    >
+                    </span>
+
+                    <span wire:loading.remove wire:target="store">
+                        <i class="fas fa-save"></i>
+                    </span>
+
+                    Crear
+                </button>
+                <button
+                    type="button"
+                    class="btn btn-outline-secondary"
+                    wire:click="index"
+                >
+                    Cancelar
+                </button>
+            @break
+
+        @case('edit')
+            <h3>Editar lugar</h3>
+            @include('parameters.places.form')
+            <button
+                type="button"
+                class="btn btn-primary"
+                wire:click="update({{$holiday}})"
+            >
+                Guardar
+            </button>
+            <button
+                type="button"
+                class="btn btn-outline-secondary"
+                wire:click="index"
+            >
+                Cancelar
+            </button>
+            @break
+
+    @endswitch
+</div>
