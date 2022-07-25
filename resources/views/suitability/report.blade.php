32a666ca8ec66c42a86d5c14384beeaa0426f929
tebiccr <tebiccr@gmail.com>
2022-07-25 15:15:47 -0400

    con filtro de meses en reporte de idoneidad

--- resources/views/suitability/report.blade.php
+++ resources/views/suitability/report.blade.php
@@ -5,7 +5,8 @@
 @section('content')
 @include('suitability.nav')
 
-<h3 class="mb-3">Reporte de Solicitudes de Idoneidad</h3>
+<h3 class="mb-3">Reporte de Solicitudes de Idoneidad {{ $request->year ?? '' }}  - {{ $request->month ?? '' }}
+</h3>
 <form method="GET" class="form-horizontal" action="{{ route('suitability.report') }}">
     <div class="form-row">
         <fieldset class="form-group col-sm-2">
@@ -14,6 +15,26 @@
                 <option value="">Selección...</option>
                 <option value="2021" @if($request->year == "2021") selected @endif>2021</option>
                 <option value="2022" @if($request->year == "2022") selected @endif>2022</option>
+            </select>
+        </fieldset>
+
+        <fieldset class="form-group col-sm-2">
+            <label>Mes</label>
+            <select class="form-control selectpicker show-tick" name="month" required>
+                <option value="">Selección...</option>
+                <option value="Todos" @if($request->month == "Todos") selected @endif>Todos</option>
+                <option value="1" @if($request->month == "1") selected @endif>Enero</option>
+                <option value="2" @if($request->month == "2") selected @endif>Febrero</option>
+                <option value="3" @if($request->month == "3") selected @endif>Marzo</option>
+                <option value="4" @if($request->month == "4") selected @endif>Abril</option>
+                <option value="5" @if($request->month == "5") selected @endif>Mayo</option>
+                <option value="6" @if($request->month == "6") selected @endif>Junio</option>
+                <option value="7" @if($request->month == "7") selected @endif>Julio</option>
+                <option value="8" @if($request->month == "8") selected @endif>Agosto</option>
+                <option value="9" @if($request->month == "9") selected @endif>Septiembre</option>
+                <option value="10" @if($request->month == "10") selected @endif>Octubre</option>
+                <option value="11" @if($request->month == "11") selected @endif>Noviembre</option>
+                <option value="12" @if($request->month == "12") selected @endif>Diciembre</option>
             </select>
         </fieldset>
 
