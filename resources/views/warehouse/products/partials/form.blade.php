702ceffc07eae20c23b6062e6443c78021e25dbe
rafael1delgado <delgadorafael2011@gmail.com>
2022-07-30 07:30:39 -0400

    [feat] update product/form view

--- resources/views/warehouse/products/partials/form.blade.php
+++ resources/views/warehouse/products/partials/form.blade.php
@@ -1,19 +1,46 @@
 <div class="form-row mt-3">
-    <fieldset class="form-group col-md-4">
+    <fieldset class="form-group col-md-3">
         <label for="store-id">Bodega</label>
         <input
+            class="form-control"
             type="text"
             id="store-id"
-            class="form-control"
             value="{{ optional($store)->name }}"
             disabled
         >
     </fieldset>
 
     <fieldset class="form-group col-md-3">
-        <label for="name">Nombre</label>
-        <input type="text"
+        <label for="product-search">Buscar Producto o Servicio</label>
+        <input
+            class="form-control"
+            type="text"
+            wire:model.debounce.1500ms="search_unspsc_product"
+            id="product-search"
+        >
+    </fieldset>
+
+    <fieldset class="form-group col-md-6">
+        <label for="product-id">Selecciona un Producto o Servicio*</label>
+        @livewire('unspsc.product-search')
+        <input
+            class="form-control @error('unspsc_product_id') is-invalid @enderror"
+            type="hidden"
+        >
+        @error('unspsc_product_id')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </fieldset>
+</div>
+
+<div class="form-row">
+    <fieldset class="form-group col-md-6">
+        <label for="name">Nombre* <small>(Especificación técnica)</small></label>
+        <input
             class="form-control @error('name') is-invalid @enderror"
+            type="text"
             id="name"
             wire:model.debounce.1500ms="name"
             placeholder="Ingresa el nombre"
@@ -29,8 +56,9 @@
 
     <fieldset class="form-group col-md-3">
         <label for="barcode">Código Barra</label>
-        <input type="text"
+        <input
             class="form-control @error('barcode') is-invalid @enderror"
+            type="text"
             id="barcode"
             wire:model.debounce.1500ms="barcode"
             placeholder="Ingresa el código de barra"
@@ -43,11 +71,11 @@
         @enderror
     </fieldset>
 
-    <fieldset class="form-group col-md-2">
+    <fieldset class="form-group col-md-3">
         <label for="category-id">Categoría</label>
         <select
-            id="category-id"
             class="form-control @error('category_id') is-invalid @enderror"
+            id="category-id"
             wire:model.debounce.1500ms="category_id"
             required
         >
@@ -62,32 +90,6 @@
             @endforeach
         </select>
         @error('category_id')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
-    </fieldset>
-</div>
-
-<div class="form-row">
-    <fieldset class="form-group col-md-3">
-        <label for="product-search">Buscar Producto o Servicio</label>
-        <input
-            wire:model.debounce.1500ms="search_unspsc_product"
-            id="product-search"
-            class="form-control"
-            type="text"
-        >
-    </fieldset>
-
-    <fieldset class="form-group col-md-6">
-        <label for="product-id">Selecciona un Producto o Servicio</label>
-        @livewire('unspsc.product-search')
-        <input
-            class="form-control @error('unspsc_product_id') is-invalid @enderror"
-            type="hidden"
-        >
-        @error('unspsc_product_id')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
