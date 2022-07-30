cee6a3b304e55301e076a6c9057557638c063ba0
rafael1delgado <delgadorafael2011@gmail.com>
2022-07-30 07:36:22 -0400

    [feat] order component & limit query

--- app/Http/Livewire/EstablishmentOuSearch.php
+++ app/Http/Livewire/EstablishmentOuSearch.php
@@ -6,27 +6,19 @@ use Livewire\Component;
 use App\Establishment;
 use App\Rrhh\OrganizationalUnit;
 
-use App\User;
-
 class EstablishmentOuSearch extends Component
 {
     public $establishments;
     public $establishment_id;
-    // public $ous;
-
 
     public $query;
     public $ous;
+
     /** Para cuando viene precargado */
     public $ou;
     public $selectedName;
     public $selected_id = 'organizationalunit';
     public $msg_too_many;
-
-    // public function mount()
-    // {
-    //     $this->establishments = Establishment::all();
-    // }
 
     public function resetx()
     {
@@ -40,28 +32,31 @@ class EstablishmentOuSearch extends Component
     {
         $this->establishments = Establishment::all();
 
-        if($this->ou) {
+        if($this->ou)
             $this->setOu($this->ou);
-        }
     }
 
-    public function setOu(organizationalunit $ou)
+    public function setOu(OrganizationalUnit $ou)
     {
         $this->resetx();
         $this->ou = $ou;
         $this->selectedName = $ou->name;
     }
 
-    public function change(){
-      $this->resetx();
+    public function change()
+    {
+        $this->resetx();
     }
 
     public function updatedQuery()
     {
-        $this->ous = organizationalunit::where('establishment_id',$this->establishment_id)
-                                         ->where('name','LIKE','%'.$this->query.'%')
-                                         ->orderBy('name','Asc')
-                                         ->get();
+        $this->ous = OrganizationalUnit::query()
+            ->where('establishment_id', $this->establishment_id)
+            ->where('name','LIKE', '%' . $this->query . '%')
+            ->orderBy('name')
+            ->limit(6)
+            ->get();
+
         $this->msg_too_many = false;
     }
 
