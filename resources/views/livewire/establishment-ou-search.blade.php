2552f9f826edb2500f00755cf280dfd381ecec2a
rafael1delgado <delgadorafael2011@gmail.com>
2022-07-30 07:33:01 -0400

    [feat] update & order view

--- resources/views/livewire/establishment-ou-search.blade.php
+++ resources/views/livewire/establishment-ou-search.blade.php
@@ -3,7 +3,16 @@
         <label for="for_establishment_id">Establecimiento</label>
 
         <div id="for-picker" wire:ignore>
-            <select wire:model="establishment_id" wire:change="change" class="form-control selectpicker" data-live-search="true" data-size="5" required data-container="#for-picker">
+            <select
+                class="form-control selectpicker"
+                id="for_establishment_id"
+                wire:model="establishment_id"
+                wire:change="change"
+                data-live-search="true"
+                data-size="5"
+                data-container="#for-picker"
+                required
+            >
                 <option value=""></option>
                 @foreach($establishments as $establishment)
                     <option value="{{$establishment->id}}">{{$establishment->name}}</option>
@@ -13,11 +22,12 @@
     </fieldset>
 
     <fieldset class="form-group col-12 col-md-8">
-        <label for="for_ou_id">Unidad organizacional</label>
+        <label for="for_ou_id">Unidad Organizacional</label>
         <div class="input-group">
             <input
                 type="text"
                 class="form-control"
+                id="for_ou_id"
                 placeholder="Nombre"
                 aria-label="Nombre"
                 wire:keydown.escape="resetx"
@@ -32,32 +42,40 @@
 
             <div class="input-group-append">
                 <a class="btn btn-outline-secondary" wire:click="resetx">
-                    <i class="fas fa-eraser"></i> Limpiar</a>
+                    <i class="fas fa-eraser"></i> Limpiar
+                </a>
             </div>
         </div>
 
         @if($ou)
-        <input type="hidden"  name="{{ $selected_id }}" value="{{ $ou->id }}" required>
+            <input type="hidden" name="{{ $selected_id }}" value="{{ $ou->id }}" required>
         @endif
 
         @if(!empty($query))
             <ul class="list-group col-12" style="z-index: 3; position: absolute;">
                 @if( count($ous) >= 1 )
                     @foreach($ous as $ou)
-                        <a wire:click="setOu({{$ou->id}})"
-                            class="list-group-item list-group-item-action"
-                        >{{ $ou->name }} </a>
+                        <a
+                            wire:click="setOu({{$ou->id}})"
+                            class="list-group-item list-group-item-action py-1"
+                        >
+                            {{ $ou->name }}
+                        </a>
                     @endforeach
                 @elseif($msg_too_many)
-                    <div class="list-group-item list-group-item-info">Hemos encontrado muchas coincidencias</div>
+                    <div class="list-group-item list-group-item-info py-1">
+                        Hemos encontrado muchas coincidencias
+                    </div>
                 @else
-                    <div class="list-group-item list-group-item-warning">No hay resultados</div>
+                    <div class="list-group-item list-group-item-warning py-1">
+                        No hay resultados
+                    </div>
                 @endif
+
             </ul>
         @endif
 
     </fieldset>
-
 
     <div wire:loading>
         Cargando...
