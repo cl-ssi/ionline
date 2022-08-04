f249dc46677d218f38a0b5c533b8bff2a5da865c
tebiccr <tebiccr@gmail.com>
2022-08-04 15:54:49 -0400

    lugares sin paginacion y ordenado por ubicacion

--- resources/views/parameters/places/index.blade.php
+++ resources/views/parameters/places/index.blade.php
@@ -11,27 +11,26 @@
         </thead>
         <tbody>
             @foreach($places as $place)
-                <tr>
-                    <td class="text-center">{{ $place->id }}</td>
-                    <td>{{ $place->name }}</td>
-                    <td>{{ $place->description }}</td>
-                    <td>{{ $place->location->name }}</td>
-                    <td>
-                        <button
-                            type="button"
-                            class="btn btn-sm btn-primary"
-                            wire:click="edit({{ $place }})"
-                        >
-                            <i class="fas fa-edit"></i>
-                        </button>
-                    </td>
-                </tr>
+            <tr>
+                <td class="text-center">{{ $place->id }}</td>
+                <td>{{ $place->name }}</td>
+                <td>{{ $place->description }}</td>
+                <td>{{ $place->location->name }}</td>
+                <td>
+                    <button type="button" class="btn btn-sm btn-primary" wire:click="edit({{ $place }})">
+                        <i class="fas fa-edit"></i>
+                    </button>
+                </td>
+            </tr>
             @endforeach
         </tbody>
         <caption>
+            {{--
             Total de resultados: {{ $places->total() }}
+            --}}
         </caption>
     </table>
 </div>
-
+{{--
 {{ $places->links() }}
+--}}
