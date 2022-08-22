a5be30898b775160f12a3944422d5d5c85eef8f7
rafael1delgado <delgadorafael2011@gmail.com>
2022-08-22 13:08:49 -0400

    [feat] add input to search

--- resources/views/livewire/inventory/inventory-pending.blade.php
+++ resources/views/livewire/inventory/inventory-pending.blade.php
@@ -7,14 +7,40 @@
         Bandeja Pendiente de Inventario
     </h3>
 
+    <div class="form-row">
+        <fieldset class="form-group col-md-3">
+            <label for="type-reception-id">Tipo de Ingreso</label>
+            <select wire:model="type_reception_id" id="type-reception-id" class="form-control">
+                <option value="">Todos</option>
+                <option value="{{ App\Models\Warehouse\TypeReception::receiving() }}">
+                    Ingreso Normal
+                </option>
+                <option value="{{ App\Models\Warehouse\TypeReception::purchaseOrder() }}">
+                    Orden de Compra
+                </option>
+            </select>
+        </fieldset>
+
+        <fieldset class="form-group col-md-9">
+            <label for="search">Buscar</label>
+            <input
+                class="form-control"
+                type="text"
+                id="search"
+                placeholder="Ingresa la orden de compra, origen, código de producto, producto o descripción técnica"
+                wire:model.debounce.1500ms="search"
+            >
+        </fieldset>
+    </div>
+
     <div class="table-responsive">
-        <table class="table table-bordered">
+        <table class="table table-sm table-bordered">
             <thead>
                 <tr>
                     <th class="text-center">ID</th>
                     <th class="text-center">Código</th>
                     <th>Producto</th>
-                    <th>Proveedor/OC</th>
+                    <th>Origen/OC</th>
                     <th class="text-center">Acciones</th>
                 </tr>
             </thead>
@@ -28,40 +54,44 @@
                     </td>
                 </tr>
                 @forelse($inventories as $inventory)
-                <tr>
-                    <td class="text-center">
-                        {{ $inventory->id }}
-                    </td>
-                    <td class="text-center">
-                        <small class="text-monospace">
-                            {{ $inventory->product->product->code }}
-                        </small>
-                    </td>
-                    <td>
-                        {{ $inventory->product->product->name }}
-                        <br>
-                        <small>
-                            {{ $inventory->product->name }}
-                        </small>
-                    </td>
-                    <td>
-                        @if($inventory->control->isPurchaseOrder())
-                            <span class="text-nowrap">
-                                {{ $inventory->control->po_code }}
-                            </span>
-                        @else
-                            {{ $inventory->control->origin->name }}
-                        @endif
-                    </td>
-                    <td class="text-center">
-                        <a
-                            class="btn btn-sm btn-primary"
-                            href="{{ route('inventories.edit', $inventory) }}"
-                        >
-                            <i class="fas fa-edit"></i>
-                        </a>
-                    </td>
-                </tr>
+                    <tr wire:loading.remove>
+                        <td class="text-center">
+                            {{ $inventory->id }}
+                        </td>
+                        <td class="text-center">
+                            <small class="text-monospace">
+                                {{ $inventory->product->product->code }}
+                            </small>
+                        </td>
+                        <td>
+                            {{ $inventory->product->product->name }}
+                            <br>
+                            <small>
+                                {{ $inventory->product->name }}
+                            </small>
+                        </td>
+                        <td>
+                            @if($inventory->control->isPurchaseOrder())
+                                <span class="text-nowrap">
+                                    {{ $inventory->control->po_code }}
+                                </span>
+                            @else
+                                {{ $inventory->control->origin->name }}
+                            @endif
+                            <br>
+                            <small>
+                                {{ optional($inventory->control->typeReception)->name }}
+                            </small>
+                        </td>
+                        <td class="text-center">
+                            <a
+                                class="btn btn-sm btn-primary"
+                                href="{{ route('inventories.edit', $inventory) }}"
+                            >
+                                <i class="fas fa-edit"></i>
+                            </a>
+                        </td>
+                    </tr>
                 @empty
                     <tr wire:loading.remove>
                         <td class="text-center" colspan="5">
