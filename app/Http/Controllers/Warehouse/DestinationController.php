6b8fe1549d04bad00583f2b8b92baa34040711f8
rafael1delgado <delgadorafael2011@gmail.com>
2022-08-22 12:55:57 -0400

    [feat] add nav for admin and user from warehouse

--- app/Http/Controllers/Warehouse/DestinationController.php
+++ app/Http/Controllers/Warehouse/DestinationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Warehouse;
 use App\Http\Controllers\Controller;
 use App\Models\Warehouse\Destination;
 use App\Models\Warehouse\Store;
+use Illuminate\Http\Request;
 
 class DestinationController extends Controller
 {
@@ -17,21 +18,25 @@ class DestinationController extends Controller
      * Display a listing of the resource.
      *
      * @param \App\Models\Warehouse\Store  $store
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function index(Store $store)
+    public function index(Store $store, Request $request)
     {
-        return view('warehouse.destinations.index', compact('store'));
+        $nav = $request->nav;
+        return view('warehouse.destinations.index', compact('store', 'nav'));
     }
 
     /**
      * Show the form for creating a new resource.
      * @param \App\Models\Warehouse\Store  $store
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function create(Store $store)
+    public function create(Store $store, Request $request)
     {
-        return view('warehouse.destinations.create', compact('store'));
+        $nav = $request->nav;
+        return view('warehouse.destinations.create', compact('store', 'nav'));
     }
 
     /**
@@ -39,10 +44,12 @@ class DestinationController extends Controller
      *
      * @param \App\Models\Warehouse\Store  $store
      * @param  \App\Models\Warehouse\Destination  $destination
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function edit(Store $store, Destination $destination)
+    public function edit(Store $store, Destination $destination, Request $request)
     {
-        return view('warehouse.destinations.edit', compact('store', 'destination'));
+        $nav = $request->nav;
+        return view('warehouse.destinations.edit', compact('store', 'destination', 'nav'));
     }
 }
