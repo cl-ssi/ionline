c6d2e23dbf652e71f7da035037a01039773c0f3d
rafael1delgado <delgadorafael2011@gmail.com>
2022-08-22 12:46:09 -0400

    [feat] delete type_destination

---
+++ database/migrations/2022_08_19_105758_delete_type_destination_to_wre_controls.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DeleteTypeDestinationToWreControls extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('wre_controls', function (Blueprint $table) {
+            $table->dropColumn('type_destination');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
