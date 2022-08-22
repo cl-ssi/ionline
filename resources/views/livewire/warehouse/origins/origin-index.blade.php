a3f022268cd05947cd0dc8967c5cdf8ad2037f8a
rafael1delgado <delgadorafael2011@gmail.com>
2022-08-22 13:15:44 -0400

    [feat] add indentation

--- resources/views/livewire/warehouse/origins/origin-index.blade.php
+++ resources/views/livewire/warehouse/origins/origin-index.blade.php
@@ -4,7 +4,10 @@
             <h5>Origenes: {{ $store->name }}</h5>
         </div>
         <div class="col text-right">
-            <a href="{{ route('warehouse.origins.create', $store) }}" class="btn btn-sm btn-primary">
+            <a
+                href="{{ route('warehouse.origins.create', ['store' => $store, 'nav' => $nav]) }}"
+                class="btn btn-sm btn-primary"
+            >
                 <i class="fa fa-plus"></i> Crear Origen
             </a>
         </div>
@@ -42,43 +45,45 @@
                     </td>
                 </tr>
                 @forelse($origins as $origin)
-                <tr wire:loading.remove>
-                    <td class="text-center">
-                        <a
-                            href="{{ route('warehouse.origins.edit', [
-                                'store' => $store,
-                                'origin' => $origin
-                            ]) }}"
-                            class="btn btn-sm btn-outline-secondary"
-                        >
-                            <i class="fas fa-edit"></i>
-                            {{ $origin->id }}
-                        </a>
-                    </td>
-                    <td>{{ $origin->name }}</td>
-                    <td class="text-center">
-                        <button
-                            class="btn btn-sm btn-outline-danger"
-                            wire:click="deleteOrigin({{ $origin }})"
-                        >
-                            <i class="fas fa-trash"></i>
-                        </button>
-                    </td>
-                </tr>
+                    <tr wire:loading.remove>
+                        <td class="text-center">
+                            <a
+                                href="{{ route('warehouse.origins.edit', [
+                                    'store' => $store,
+                                    'origin' => $origin,
+                                    'nav' => $nav,
+                                ]) }}"
+                                class="btn btn-sm btn-outline-secondary"
+                            >
+                                <i class="fas fa-edit"></i>
+                                {{ $origin->id }}
+                            </a>
+                        </td>
+                        <td>{{ $origin->name }}</td>
+                        <td class="text-center">
+                            <button
+                                class="btn btn-sm btn-outline-danger"
+                                wire:click="deleteOrigin({{ $origin }})"
+                            >
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </td>
+                    </tr>
                 @empty
-                <tr wire:loading.remove>
-                    <td class="text-center" colspan="3">
-                        <em>
-                            No hay resultados
-                        </em>
-                    </td>
-                </tr>
+                    <tr wire:loading.remove>
+                        <td class="text-center" colspan="3">
+                            <em>
+                                No hay resultados
+                            </em>
+                        </td>
+                    </tr>
                 @endforelse
             </tbody>
             <caption>
                 Total de Resultados: {{ $origins->total() }}
             </caption>
         </table>
+
         {{ $origins->links() }}
     </div>
 </div>
