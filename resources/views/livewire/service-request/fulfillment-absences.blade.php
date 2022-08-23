14cc7e75f502dcc5a5d1812d2a8a097b5cc3b0e6
sick <esteban.rojasg@redsalud.cl>
2022-08-23 11:44:18 -0400

    se quita permiso parental

--- resources/views/livewire/service-request/fulfillment-absences.blade.php
+++ resources/views/livewire/service-request/fulfillment-absences.blade.php
@@ -16,7 +16,7 @@
           <option value="Término de contrato anticipado">TÉRMINO DE CONTRATO ANTICIPADO (EFECTIVA DESDE)</option>
           <option value="Atraso">ATRASO</option>
           <!-- <option value="Fuero maternal">FUERO MATERNAL</option> -->
-          <option value="Permiso Parental">PERMISO PARENTAL</option>
+          <!-- <option value="Permiso Parental">PERMISO PARENTAL</option> -->
           <option value="Permiso Excepcional">PERMISO EXCEPCIONAL</option>
         </select>
       </fieldset>
