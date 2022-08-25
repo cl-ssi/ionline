fb133c8b003f1383735a4bce92a1a1a5d4f530ea
rafael1delgado <delgadorafael2011@gmail.com>
2022-08-25 08:48:35 -0400

    [feat] add up and down button

--- resources/views/profile/subrogation/index.blade.php
+++ resources/views/profile/subrogation/index.blade.php
@@ -9,21 +9,21 @@
         </tr>
     </thead>
     <tbody>
-        <tr class="table-{{ auth()->user()->subrogant == auth()->user() ? 'success':'warning'}}">
+        <tr class="table-{{ auth()->user()->subrogant == auth()->user() ? 'success' : 'warning'}}">
             <td>
                 @if($absent)
-                    <button type="button" class="btn btn-sm btn-success" 
-                    wire:click="toggleAbsent()"><i class="fas fa-building"></i> Activar</button>    
+                    <button type="button" class="btn btn-sm btn-success"
+                    wire:click="toggleAbsent()"><i class="fas fa-building"></i> Activar</button>
                 @else
-                    <button type="button" class="btn btn-sm btn-warning" 
-                    wire:click="toggleAbsent()"><i class="fas fa-cocktail"></i> Desactivar</button>    
+                    <button type="button" class="btn btn-sm btn-warning"
+                    wire:click="toggleAbsent()"><i class="fas fa-cocktail"></i> Desactivar</button>
                 @endif
             </td>
             <td>
                 @if($absent)
-                <i class="fas fa-cocktail"></i> Fuera de la oficina
+                    <i class="fas fa-cocktail"></i> Fuera de la oficina
                 @else
-                <i class="fas fa-building"></i> En la oficina
+                    <i class="fas fa-building"></i> En la oficina
                 @endif
             </td>
             <td>{{ auth()->user()->fullName }}</td>
@@ -31,30 +31,50 @@
             <td></td>
         </tr>
         @foreach($subrogations as $subrogation)
-            <tr class="table-{{ auth()->user()->subrogant == $subrogation->subrogant ? 'success':'warning'}}">
+            <tr class="table-{{ auth()->user()->subrogant == $subrogation->subrogant ? 'success' : 'warning'}}">
                 <td>
-                    <button type="button" class="btn btn-sm btn-primary">
+                    <button
+                        type="button"
+                        class="btn btn-sm btn-primary"
+                        wire:click="up({{ $subrogation }})"
+                        @if($subrogation->level == 1)
+                            disabled
+                        @endif
+                    >
                         <i class="fas fa-caret-up"></i>
                     </button>
-                    <button type="button" class="btn btn-sm btn-primary">
+
+                    <button
+                        type="button"
+                        class="btn btn-sm btn-primary"
+                        wire:click="down({{ $subrogation }})"
+                        @if($subrogation->level == $subrogations->count())
+                            disabled
+                        @endif
+                    >
                         <i class="fas fa-caret-down"></i>
                     </button>
-                    <!-- <button type="button" class="btn btn-sm btn-primary" 
+                    <!-- <button type="button" class="btn btn-sm btn-primary"
                         wire:click="edit({{$subrogation}})"><i class="fas fa-edit"></i></button> -->
                 </td>
                 <td>
                     @if($subrogation->subrogant->absent)
-                    <i class="fas fa-cocktail"></i> Fuera de la oficina
+                        <i class="fas fa-cocktail"></i> Fuera de la oficina
                     @else
-                    <i class="fas fa-building"></i> En la oficina
+                        <i class="fas fa-building"></i> En la oficina
                     @endif
                 </td>
                 <td>{{ $subrogation->subrogant->fullName }}</td>
                 <td>{{ $subrogation->level }}</td>
                 <td>
-                <button type="button" class="btn btn-sm btn-danger" 
-                    wire:click="delete({{$subrogation}})"><i class="fas fa-trash"></i></button>
-            </td>
+                    <button
+                        type="button"
+                        class="btn btn-sm btn-danger"
+                        wire:click="delete({{ $subrogation }})"
+                    >
+                        <i class="fas fa-trash"></i>
+                    </button>
+                </td>
             </tr>
         @endforeach
     </tbody>
