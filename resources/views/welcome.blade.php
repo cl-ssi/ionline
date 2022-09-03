a94fb6fd4b8be796ca30979dbb829f58978b6e74
AquaroTorres <alvarotorres@gmail.com>
2022-09-03 10:57:24 +0000

    Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,10 @@
     <title>{{ config('app.name') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+        crossorigin="anonymous">
     <link href="{{ asset('css/cu.min.css') }}" rel="stylesheet">
 
     <!-- Favicons -->
@@ -52,13 +55,13 @@
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm container bg-nav-gobierno">
         <h5 class="my-0 mr-md-auto font-weight-normal"> <img src="{{ asset('images/gob-header.svg') }}" alt="Logo del gobierno de chile"> </h5>
         <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2" href="http://www.saludiquique.cl">Servicio de Salud Iquique</a>
+            <a class="p-2 text-dark" href="http://www.saludiquique.cl">Web Servicio de Salud</a>
         </nav>
 
     </div>
 
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center container">
-        <h1 class="display-5 mb-3">{{ env('APP_SS') }}</h1>
+        <h1 class="display-5 mb-3">{{ env('APP_NAME') }} - {{ env('APP_SS') }}</h1>
         <div class="d-flex justify-content-center">
             <table class="align-self-center">
                 <tr>
@@ -74,12 +77,14 @@
     <div class="container">
         <div class="card-deck mb-3 text-center">
 
-
-		<div class="card shadow-sm">
+			<div class="card shadow-sm">
 				<div class="card-header">
-					<h4 class="my-0 font-weight-normal text-center">Ingreso al iOnline</h4>
+					<h4 class="my-0 font-weight-normal text-center">Ingreso al sistema</h4>
 				</div>
 				<div class="card-body">
+
+                    <h6 class="mb-3">Utilizando tu Clave Única</h6>
+
 					<!-- Código para visualizar botón oficial iniciar sesión con ClaveÚnica-->
 					<a class="btn-cu btn-m btn-color-estandar m-auto" 
 						href="{{ route('claveunica.autenticar') }}?redirect=L2NsYXZldW5pY2EvbG9naW4="
@@ -92,6 +97,8 @@
 				
 				<hr>
 				
+                <h6 class="mb-3">O con tu cuenta del sistema</h6>
+
 				<form method="POST" action="{{ route('login') }}">
 					@csrf
 					<div class="form-group row">
@@ -192,7 +199,7 @@
 						<!-- <a href="https://uni.saludtarapaca.gob.cl" class="btn btn-outline-info btn-block">UniSalud</a> -->
 					</li>
 					<li class="list-group-item">
-						<a href="https://esmeralda.saludtarapaca.org" class="btn btn-outline-info btn-block">Esmeralda</a>
+						<a href="https://esmeralda.saludtarapaca.gob.cl" class="btn btn-outline-info btn-block">Esmeralda</a>
 					</li>
 					<li class="list-group-item">
 						<a href="{{ route('claveunica.autenticar') }}?redirect=L3NpcmVteC9sb2dpbmN1" class="btn btn-outline-info btn-block">Sirmx</a>
@@ -204,21 +211,18 @@
 
         <footer class="pt-4 my-md-5 pt-md-5 border-top">
             <div class="row">
-                <div class="col-12 col-md">
+                <div class="col-3 col-md-3">
                     <img class="mb-2" src="{{ asset('images/logo_ssi_100px.png') }}" alt="Logo Servicio de Salud Iquique">
                 </div>
-                <div class="col-6 col-md">
+                <div class="col-1 col-md-6">
 
                 </div>
-                <div class="col-6 col-md">
-
-                </div>
-                <div class="col-6 col-md">
+                <div class="col-7 col-md-3">
                     <h5>Desarrollado por</h5>
                     <ul class="list-unstyled text-small">
                         <li>Departamento TIC del SSI</li>
                         <li><a class="text-muted" href="mailto:sistemas.ssi@redsalud.gobc.">sistemas.ssi@redsalud.gob.cl</a></li>
-                        <small class="d-block mb-3 text-muted"> 2021</small>
+                        <small class="d-block mb-3 text-muted">2021</small>
                     </ul>
                 </div>
             </div>
