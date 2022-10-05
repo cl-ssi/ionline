42f24e4aeac9e1a5cbe2ac8761374f4ccc88aefb
AquaroTorres <alvarotorres@gmail.com>
2022-10-05 23:39:46 +0000

    update filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -68,7 +68,7 @@ return [
         'gcs' => [
             'driver' => 'gcs',
             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'),
-            'key_file' => base_path().env('GOOGLE_CLOUD_SERVICE_ACCOUNT'), // optional: /path/to/service-account.json
+            'key_file' => env('GOOGLE_CLOUD_SERVICE_ACCOUNT',null), // optional: /path/to/service-account.json
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET'),
             //'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
             'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
