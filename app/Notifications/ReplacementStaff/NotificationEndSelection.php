7f4bb4adc86432339cb90462c2c9114ed8958a7e
mirandal <mirandal.jorge@gmail.com>
2022-10-05 12:45:36 -0300

    RS: Notification End

---
+++ app/Notifications/ReplacementStaff/NotificationEndSelection.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Notifications\ReplacementStaff;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+use App\Models\ReplacementStaff\RequestReplacementStaff;
+
+class NotificationEndSelection extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(RequestReplacementStaff $requestReplacementStaff, $to)
+    {
+        $this->requestReplacementStaff = $requestReplacementStaff;
+        $this->to = $to;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        if($this->to == 'reclutamiento'){
+            $action = 'replacement_staff.request.technical_evaluation.edit';
+        }
+        else{
+            $action = 'replacement_staff.request.technical_evaluation.show';
+        }
+
+        return [
+            'module'  => 'Solicitudes de Contratación',
+            'icon'    => '<i class="far fa-id-card"></i>',
+            'subject' => 'Fin proceso de selección',
+            'message' => 'Solicitud ID: '.$this->requestReplacementStaff->id,
+            'action'  => route($action, $this->requestReplacementStaff->id)
+        ];
+    }
+}
