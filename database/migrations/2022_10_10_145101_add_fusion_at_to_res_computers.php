12ecf967871b6a66bd2198d06e80a29544e0394e
rafael1delgado <delgadorafael2011@gmail.com>
2022-10-12 15:45:59 -0400

    feat: add fusion_at & inventory_id to res_computers

---
+++ database/migrations/2022_10_10_145101_add_fusion_at_to_res_computers.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFusionAtToResComputers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('res_computers', function (Blueprint $table) {
+            $table->timestamp('fusion_at')->after('place_id')->nullable();
+            $table->foreignId('inventory_id')
+                ->after('place_id')
+                ->nullable()
+                ->constrained('inv_inventories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('res_computers', function (Blueprint $table) {
+            //
+        });
+    }
+}
