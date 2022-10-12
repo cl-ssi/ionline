43687e665350009d51f23ab35ed955e5a46fd444
rafael1delgado <delgadorafael2011@gmail.com>
2022-10-12 15:56:41 -0400

    feat: add request

---
+++ app/Http/Requests/Resources/ComputerFusionRequest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Requests\Resources;
+
+use App\Models\Inv\Inventory;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ComputerFusionRequest extends FormRequest
+{
+    public $inventory;
+
+    public function __construct(Inventory $inventory)
+    {
+        $this->inventory = $inventory;
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'number_inventory'  => [
+                'required',
+                'string',
+                'min:2',
+                'max:255',
+                Rule::unique('inv_inventories', 'number')->ignore($this->inventory)
+            ],
+            'status'            => 'required',
+            'observations'      => 'nullable|string|max:5000',
+            'inventory_brand'           => 'required|string|max:255',
+            'inventory_model'           => 'required|string|max:255',
+            'inventory_serial_number'   => 'required|string|max:255',
+            'computer_brand'            => 'required|string|max:255',
+            'computer_model'            => 'required|string|max:255',
+            'computer_serial_number'    => 'required|string|max:255',
+            'hostname'          => 'nullable|string|max:255',
+            'domain'            => 'nullable|string|max:255',
+            'ip'                => 'required|string|ip',
+            'mac_address'       => 'required|string|max:255',
+            'ip_group'          => 'required|string|max:255',
+            'rack'              => 'nullable|string|max:255',
+            'vlan'              => 'nullable|string|max:255',
+            'network_segment'   => 'nullable|string|max:255',
+            'operating_system'  => 'required|string|max:255',
+            'processor'         => 'required|string|max:255',
+            'ram'               => 'required|string|max:255',
+            'hard_disk'         => 'required|string|max:255',
+            'intesis_id'        => 'nullable|string|max:255',
+            'comment'           => 'nullable|string|max:255',
+            'active_type'       => 'required|string|max:255',
+            'office_serial'     => 'nullable|string|max:255',
+            'windows_serial'    => 'nullable|string|max:255',
+        ];
+    }
+}
