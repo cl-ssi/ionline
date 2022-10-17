c099585704397e5954e5f5128e4de11415aee7b0
mirandal <mirandal.jorge@gmail.com>
2022-10-17 12:44:56 -0300

    RF: Se agrega buscador en inbox comprador

--- resources/views/livewire/request-form/search-requests.blade.php
+++ resources/views/livewire/request-form/search-requests.blade.php
@@ -86,19 +86,31 @@
             <fieldset class="form-group col-12 col-md-2">
                 <label for="for_purchaser">Comprador</label>
                 <input class="form-control form-control-sm" type="text" autocomplete="off" placeholder="NOMBRE / APELLIDOS"
-                    name="purchaser_search" wire:model.debounce.500ms="selectedPurchaser">
+                    name="purchaser_search" wire:model.debounce.500ms="selectedPurchaser" @if($inbox == 'purchase') disabled @endif>
             </fieldset>
             <fieldset class="form-group col-12 col-md-2">
                 <label for="for_purchaser">Programa</label>
                 <input class="form-control form-control-sm" type="text" autocomplete="off" placeholder=""
                     name="program_search" wire:model.debounce.500ms="selectedProgram">
             </fieldset>
+            {{--
+            <fieldset class="form-group col-12 col-md-2">
+                <label for="for_purchaser">N° O.C.</label>
+                <input class="form-control form-control-sm" type="text" autocomplete="off" placeholder=""
+                    name="purchase_order_search" wire:model.debounce.500ms="selectedPo">
+            </fieldset>
+            <fieldset class="form-group col-12 col-md-2">
+                <label for="for_purchaser">N° Licitación.</label>
+                <input class="form-control form-control-sm" type="text" autocomplete="off" placeholder=""
+                    name="bidding_search" wire:model.debounce.500ms="selectedBidding">
+            </fieldset>
+            --}}
         </div>
     </div>
 
     <br>
 
-    @if($request_forms->count() > 0)
+    @if($request_forms->count() > 0 && $inbox == 'all')
         <div class="row">
             <div class="col">
                 <p class="font-weight-lighter">Total de Registros: <b>{{ $request_forms->total() }}</b></p>
@@ -245,7 +257,88 @@
             </table>
         </div>
         {{ $request_forms->appends(request()->query())->links() }}
-    @else
+    @endif
+    
+    @if($request_forms->count() > 0 && $inbox == 'purchase')
+        <div class="row">
+            <div class="col">
+                <p class="font-weight-lighter">Total de Registros: <b>{{ $request_forms->total() }}</b></p>
+            </div>
+            <div class="col">
+                <a class="btn btn-success btn-sm mb-1 float-right" wire:click="export"><i class="fas fa-file-excel"></i> Exportar formularios</a></h6>
+            </div>
+        </div>
+                        
+        <div class="table-responsive">
+            <h6><i class="fas fa-inbox"></i> Mis Formularios asignados</h6>
+            <table class="table table-sm table-hover table-bordered small">
+                <thead>
+                    <tr class="text-center">
+                        <th>ID</th>
+                        <th>Folio</th>
+                        <th style="width: 7%">Fecha Creación</th>
+                        <th>Tipo / Mecanismo de Compra</th>
+                        <th>Descripción</th>
+                        <th>Usuario Gestor</th>
+                        <th>Items</th>
+                        <th>Presupuesto</th>
+                        <th>Vencimiento</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($request_forms as $requestForm)
+                    <tr>
+                        <td>{{ $requestForm->id }} <br>
+                            @if($requestForm->purchasingProcess)
+                            <span class="badge badge-{{$requestForm->purchasingProcess->getColor()}}">{{$requestForm->purchasingProcess->getStatus()}}</span>
+                            @else
+                            <span class="badge badge-warning">En proceso</span>
+                            @endif
+                        </td>
+                        <td>{{ $requestForm->folio }}</td>
+                        <td>
+                            {{ $requestForm->created_at->format('d-m-Y H:i') }}<br>
+                            {{ $requestForm->created_at->diffForHumans() }}
+                        </td>
+                        <td>{{ ($requestForm->purchaseMechanism) ? $requestForm->purchaseMechanism->PurchaseMechanismValue : '' }}<br>
+                            {{ $requestForm->SubtypeValue }}
+                        </td>
+                        <td>{{ $requestForm->name }}</td>
+                        <td>{{ $requestForm->user ? $requestForm->user->FullName : 'Usuario eliminado' }}<br>
+                            {{ $requestForm->user ? $requestForm->userOrganizationalUnit->name : 'Usuario eliminado' }}
+                        </td>
+                        <td>{{ $requestForm->quantityOfItems() }}</td>
+                        <td class="text-right">{{$requestForm->symbol_currency}}{{ number_format($requestForm->estimated_expense,$requestForm->precision_currency,",",".") }}</td>
+                        <td title="Aprobación: {{$requestForm->approvedAt}}" >
+                            {{ $requestForm->expireAt }}
+                            <div style="font-weight: bold">{{' (' . $requestForm->daysToExpire . ' días)' }}</div>
+                        </td>
+                        <td>
+                            @if($requestForm->signatures_file_id)
+                            <a class="btn btn-info btn-sm" title="Ver Formulario de Requerimiento firmado" href="{{ $requestForm->signatures_file_id == 11 ? route('request_forms.show_file', $requestForm->requestFormFiles->first() ?? 0) : route('request_forms.signedRequestFormPDF', [$requestForm, 1]) }}" target="_blank" title="Certificado">
+                                <i class="fas fa-file-contract"></i>
+                            </a>
+                            @endif
+
+                            @if($requestForm->old_signatures_file_id)
+                            <a class="btn btn-secondary btn-sm" title="Ver Formulario de Requerimiento Anterior firmado" href="{{ $requestForm->old_signatures_file_id == 11 ? route('request_forms.show_file', $requestForm->requestFormFiles->first() ?? 0) : route('request_forms.signedRequestFormPDF', [$requestForm, 0]) }}" target="_blank" title="Certificado">
+                                <i class="fas fa-file-contract"></i>
+                            </a>
+                            @endif
+                            <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="">
+                                <a href="{{ route('request_forms.supply.purchase', $requestForm) }}" class="btn btn-outline-secondary btn-sm"><i class="fas fa-shopping-cart"></i></a>
+                            </span>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        {{ $request_forms->appends(request()->query())->links() }}
+    @endif
+
+    @if($request_forms->count() == 0)
         <div class="row">
             <div class="col">
                 <p class="font-weight-lighter">Total de Registros: <b>{{ $request_forms->total() }}</b></p>
