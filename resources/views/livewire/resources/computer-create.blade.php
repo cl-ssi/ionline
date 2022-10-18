76eb778ca9e22550d3e2d2f8ed2b168e81214f1f
rafael1delgado <delgadorafael2011@gmail.com>
2022-10-18 09:56:45 -0400

    feat: update icon

--- resources/views/livewire/resources/computer-create.blade.php
+++ resources/views/livewire/resources/computer-create.blade.php
@@ -12,7 +12,7 @@
     <div class="form-row mb-3">
         <div class="col text-right">
             <button class="btn btn-primary" wire:click="create">
-                <i class="fas fa-fire"></i> Crear
+                <i class="fas fa-plus"></i> Crear
             </button>
         </div>
     </div>
