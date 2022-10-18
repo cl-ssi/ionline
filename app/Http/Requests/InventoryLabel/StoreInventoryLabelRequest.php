63890ca63cf466b8a148d954f6ea48ff7480d601
rafael1delgado <delgadorafael2011@gmail.com>
2022-10-18 09:51:43 -0400

    feat: add request for InventoryLabel

---
+++ app/Http/Requests/InventoryLabel/StoreInventoryLabelRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\InventoryLabel;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreInventoryLabelRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'  => [
+                'required',
+                'string',
+                'min:2',
+                'max:255',
+                Rule::unique('inv_labels', 'name')->where('module', $this->module)
+            ],
+            'color' => 'required'
+        ];
+    }
+}
