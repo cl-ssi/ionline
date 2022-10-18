582f77de8646ee04a3da51eebb16223bb79c6182
rafael1delgado <delgadorafael2011@gmail.com>
2022-10-18 09:41:18 -0400

    feat: add res_computer_label table

---
+++ database/migrations/2022_10_17_122607_create_res_computer_label.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResComputerLabel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('res_computer_label', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('computer_id')
+                ->nullable()
+                ->constrained('res_computers');
+
+            $table->foreignId('label_id')
+                ->nullable()
+                ->constrained('inv_labels');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('res_computer_label');
+    }
+}
