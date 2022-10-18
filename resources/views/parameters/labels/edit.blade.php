a6631ccde393f7ebc6f8cf7a3eae198c91bcfc69
rafael1delgado <delgadorafael2011@gmail.com>
2022-10-18 09:55:40 -0400

    feat: update labels view

--- resources/views/parameters/labels/edit.blade.php
+++ resources/views/parameters/labels/edit.blade.php
@@ -1,36 +1,36 @@
 @extends('layouts.app')
 
-@section('title', 'Etiquetas')
+@section('title', 'Editar Etiqueta')
 
 @section('content')
 
 <script src="{{ asset('js/colorpicker/jscolor.js') }}"></script>
 
-<h3 class="mb-3">Editar Etiqueta de {{$inventoryLabel->module}}</h3>
-
+<h3 class="mb-3">Editar Etiqueta de {{ $inventoryLabel->module }}</h3>
 
 <form method="POST" class="form-horizontal" action="{{ route('parameters.labels.update', $inventoryLabel) }}">
     @csrf
     @method('PUT')
-    <input type="hidden" class="form-control" id="for_module" name="module" value="{{$inventoryLabel->module}}">
+    <input type="hidden" class="form-control" id="for_module" name="module" value="{{ $inventoryLabel->module }}">
     <div class="row">
         <fieldset class="form-group col-3">
-            <label for="for_name">Nombre:</label>
-            <input type="text" class="form-control" id="for_name" name="name" required="" autocomplete="off" value="{{$inventoryLabel->name}}">
+            <label class="font-weight-bold" for="for_name">Nombre</label>
+            <input type="text" class="form-control" id="for_name" name="name" required="" autocomplete="off" value="{{ $inventoryLabel->name }}">
         </fieldset>
 
         <fieldset class="form-group col">
-            <label for="for_name">Color</label>
-            <input class="form-control jscolor" name="color" value="{{$inventoryLabel->color}}" required="" onchange="update(this.jscolor)">
+            <label class="font-weight-bold" for="for-color">Color</label>
+            <input class="form-control jscolor" name="color" value="{{ $inventoryLabel->color }}" required="" onchange="update(this.jscolor)" id="for-color">
         </fieldset>
 
     </div>
     <button type="submit" class="btn btn-primary">Crear</button>
+</form>
 
-    @endsection
+@endsection
 
-    @section('custom_js')
-  <script>
+@section('custom_js')
+    <script>
     $( document ).ready(function() {
       document.getElementById("for_name").focus();
     });
@@ -39,5 +39,5 @@
         // 'jscolor' instance can be used as a string
         document.getElementById('rect').style.backgroundColor = '#' + jscolor
     }
-  </script>
+    </script>
 @endsection
