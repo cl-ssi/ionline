d1b15a78e6c399f532444072f1241e6564c8a750
rafael1delgado <delgadorafael2011@gmail.com>
2022-10-19 15:02:33 -0400

    feat: add resources.computer.index component

--- resources/views/resources/computer/index.blade.php
+++ resources/views/resources/computer/index.blade.php
@@ -4,7 +4,26 @@
 
 @section('content')
 
-<h3 class="mb-3">Computadores</h3>
+<div class="row">
+    <div class="col">
+        <h3 class="mb-3">Computadores</h3>
+    </div>
+    <div class="col-md-4 text-right">
+        <a
+            class="btn btn-primary mx-1"
+            href="{{ route('resources.computer.create') }}"
+        >
+            <i class="fas fa-plus"></i> Agregar nuevo
+        </a>
+
+        <a
+            class="btn btn-success float-right"
+            href="{{ route('resources.computer.export') }}"
+        >
+            <i class="fas fa-file-excel"></i> Exportar Listado
+        </a>
+    </div>
+</div>
 
 <table class="table table-sm table-bordered text-center">
     <thead>
@@ -73,92 +92,20 @@
             </th>
             <th>{{ $computers->total() }}</th>
         </tr>
+        <tr>
+            <th></th>
+            <th>Fusionados</th>
+            <th>
+                {{ $totalMerged["merged"]}}
+            </th>
+            <th>No Fusionados</th>
+            <th>
+                {{ $totalMerged["not_merged"]}}
+            </th>
+        </tr>
     </tbody>
 </table>
 
-
-<div class="row">
-    <div class="col-sm-9">
-        <form class="form d-print-none" method="GET" action="{{ route('resources.computer.index') }}">
-            <fieldset class="form-group">
-                <div class="input-group">
-
-                    <div class="input-group-prepend">
-                        <a class="btn btn-primary" href="{{ route('resources.computer.create') }}">
-                            <i class="fas fa-plus"></i> Agregar nuevo</a>
-                    </div>
-
-                    <input type="text" class="form-control" id="forsearch" onkeyup="filter(3)"
-                        placeholder="Marca o Modelo o IP o Serial o Número de Inventario - Filtro por: Serial"
-                        name="search">
-
-                    <div class="input-group-append">
-                        <button class="btn btn-outline-secondary" type="submit">
-                            <i class="fas fa-search" aria-hidden="true"></i></button>
-                    </div>
-                </div>
-            </fieldset>
-        </form>
-    </div>
-    <div class="col-sm-3">
-        <a class="btn btn-success float-right" href="{{ route('resources.computer.export') }}">
-            <i class="fas fa-file-excel"></i> Exportar Listado
-        </a>
-    </div>
-</div>
-
-<div class="table-responsive">
-    <table class="table table-striped table-sm" id="TableFilter">
-        <thead>
-            <tr>
-                <th scope="col">Inventario</th>
-                <th scope="col">Marca</th>
-                <th scope="col">Modelo</th>
-                <th scope="col">Serial</th>
-                <th scope="col">IP</th>
-                <!-- <th scope="col">Comentario</th> -->
-                <th scope="col">Asignado a</th>
-                <th scope="col">Lugar</th>
-                <th scope="col">Accion</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($computers as $key => $computer)
-            <tr>
-                <td>{{ $computer->inventory_number }}</td>
-                <td>{{ $computer->brand }}</td>
-                <td>{{ $computer->model }}</td>
-                <td>{{ $computer->serial }}</td>
-                <td>{{ $computer->ip }}</td>
-                <!-- <td>{{ $computer->comment }}</td> -->
-                <td>
-                    @foreach($computer->users as $user)
-                         {{ $user->tinnyName }}<br>
-                    @endforeach
-                </td>
-                <td>{{ $computer->place ? $computer->place->name : 'Asignar Lugar' }}</td>
-                <td>
-                    <a
-                        @if($computer->inventory != null)
-                            class="btn btn-outline-primary btn-sm"
-                            href="{{ route('resources.computer.fusion', [
-                                'computer' => $computer,
-                                'inventory' => $computer->inventory
-                            ]) }}"
-                        @else
-                            class="btn btn-outline-secondary btn-sm"
-                            href="{{ route('resources.computer.edit', $computer) }}"
-                        @endif
-                    >
-                        <span class="fas fa-edit" aria-hidden="true"></span>
-                    </a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
-
-{{ $computers->links() }}
+@livewire('resources.computer-index')
 
 @endsection
