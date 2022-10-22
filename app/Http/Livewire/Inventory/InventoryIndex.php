40fec741c45f28e0d4e024076da924eb978a115a
rafael1delgado <delgadorafael2011@gmail.com>
2022-10-22 10:39:56 -0400

    feat: Add advanced filter in inventory

--- app/Http/Livewire/Inventory/InventoryIndex.php
+++ app/Http/Livewire/Inventory/InventoryIndex.php
@@ -3,6 +3,10 @@
 namespace App\Http\Livewire\Inventory;
 
 use App\Models\Inv\Inventory;
+use App\Models\Parameters\Location;
+use App\Models\Parameters\Place;
+use App\Models\Unspsc\Product as UnspscProduct;
+use App\User;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,41 +15,160 @@ class InventoryIndex extends Component
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
 
-    public $search;
+    public $products;
+    public $users;
+    public $responsibles;
+    public $places;
+    public $locations;
+    public $placeIds;
+
+    public $unspsc_product_id;
+    public $user_using_id;
+    public $user_responsible_id;
+    public $place_id;
+    public $location_id;
+
+    public $unspscProduct;
+    public $userUsing;
+    public $userResponsible;
+    public $place;
+    public $location;
+
+    public function mount()
+    {
+        $this->getProducts();
+        $this->getUsers();
+        $this->getResponsibles();
+        $this->getLocations();
+        $this->places = collect([]);
+    }
 
     public function render()
     {
         return view('livewire.inventory.inventory-index',[
-            'inventories' => $this->getInventories()
+            'inventories' => $this->getInventories(),
+
         ])->extends('layouts.app');
     }
 
     public function getInventories()
     {
-        $search = "%$this->search%";
-
         $inventories = Inventory::query()
-            ->when($this->search, function($query) use($search){
-                $query->where('number', 'like', $search)
-                    ->orWhereHas('responsible', function($subquery) use($search){
-                        $subquery->findByUser($this->search);
-                    })
-                    ->orWhereHas('place', function($subquery) use($search){
-                        $subquery->where('name', 'like', $search)
-                            ->orWhereHas('location', function($qu) use($search){
-                                $qu->where('name', 'like', $search);
-                            });
-                    })->orWhereHas('product', function ($query) use($search){
-                        $query->where('name', 'like', $search)
-                            ->orWhereHas('product', function($subquery) use($search) {
-                                $subquery->where('name', 'like', $search);
-                            });
-                    });
+            ->when($this->unspsc_product_id, function($query) {
+                $query->whereRelation('unspscProduct', 'id', '=', $this->unspsc_product_id);
+            })
+            ->when($this->user_using_id, function($query) {
+                $query->whereRelation('using', 'id', '=', $this->user_using_id);
+            })
+            ->when($this->user_responsible_id, function($query) {
+                $query->whereRelation('responsible', 'id', '=', $this->user_responsible_id);
+            })
+            ->when($this->place_id, function($query) {
+                $query->whereRelation('place', 'id', '=', $this->place_id);
+            })
+            ->when($this->location_id, function($query) {
+                $query->whereHas('place', function($query) {
+                    $query->whereRelation('location', 'id', '=', $this->location_id);
+                });
             })
             ->whereNotNull('number')
             ->orderByDesc('id')
             ->paginate(10);
 
         return $inventories;
+    }
+
+    public function getUsers()
+    {
+        $userIds = Inventory::query()
+            ->whereNotNull('user_using_id')
+            ->groupBy('user_using_id')
+            ->pluck('user_using_id');
+
+        $this->users = User::query()
+            ->whereIn('id', $userIds)
+            ->orderBy('name')
+            ->get(['id', 'name', 'fathers_family']);
+    }
+
+    public function getResponsibles()
+    {
+        $responsibleIds = Inventory::query()
+            ->whereNotNull('user_responsible_id')
+            ->groupBy('user_responsible_id')
+            ->pluck('user_responsible_id');
+
+        $this->responsibles = User::query()
+            ->whereIn('id', $responsibleIds)
+            ->orderBy('name')
+            ->get(['id', 'name', 'fathers_family']);
+    }
+
+    public function getProducts()
+    {
+        $productIds = Inventory::query()
+            ->whereNotNull('unspsc_product_id')
+            ->groupBy('unspsc_product_id')
+            ->pluck('unspsc_product_id');
+
+        $this->products = UnspscProduct::query()
+            ->whereIn('id', $productIds)
+            ->orderBy('name')
+            ->get(['id', 'name']);
+    }
+
+    public function getLocations()
+    {
+        $this->placeIds = Inventory::query()
+            ->whereNotNull('place_id')
+            ->groupBy('place_id')
+            ->pluck('place_id');
+
+        $places = Place::query()
+            ->whereIn('id', $this->placeIds)
+            ->orderBy('name')
+            ->get();
+
+        $this->locations = Location::whereIn('id', $places->pluck('location_id'))->get();
+    }
+
+    public function updatedUnspscProductId($unspsc_product_id)
+    {
+        $this->unspscProduct = null;
+        if($unspsc_product_id)
+            $this->unspscProduct = UnspscProduct::find($unspsc_product_id);
+    }
+
+    public function updatedUserUsingId($user_using_id)
+    {
+        $this->userUsing = null;
+        if($user_using_id)
+            $this->userUsing = User::find($user_using_id);
+    }
+
+    public function updatedUserResponsibleId($user_responsible_id)
+    {
+        $this->userResponsible = null;
+        if($this->user_responsible_id)
+            $this->userResponsible = User::find($user_responsible_id);
+    }
+
+    public function updatedLocationId($location_id)
+    {
+        $this->places = Place::query()
+            ->whereIn('id', $this->placeIds)
+            ->where('location_id', $location_id)
+            ->get();
+
+        $this->location = null;
+        if($location_id)
+            $this->location = Location::find($location_id);
+    }
+
+    public function updatedPlaceId($place_id)
+    {
+        $this->place = null;
+        if($place_id)
+            $this->place = Place::find($place_id);
     }
 }
--- resources/views/livewire/inventory/inventory-index.blade.php
+++ resources/views/livewire/inventory/inventory-index.blade.php
@@ -3,18 +3,148 @@
 
     @include('inventory.nav')
 
-    <div class="form-row my-3">
-        <fieldset class="col">
-            <label for="search" class="form-label">Buscador</label>
-            <input
-                type="text"
-                id="search"
-                class="form-control"
-                placeholder="Ingresa un número inventario, producto, ubicación, lugar o responsable"
-                wire:model.debounce.1500ms="search"
+    <div class="form-row g-2 my-3">
+        <fieldset class="form-group col-md-3">
+            <label for="products">Productos</label>
+            <select
+                wire:model.debounce.1500ms="unspsc_product_id"
+                id="products"
+                class="form-control form-control-sm"
             >
+                <option value="">Todos</option>
+                @foreach($products as $product)
+                    <option value="{{ $product->id }}">
+                        {{ $product->name }}
+                    </option>
+                @endforeach
+            </select>
+        </fieldset>
+
+        <fieldset class="form-group col-md-2">
+            <label for="locations">Ubicaciones</label>
+            <select
+                wire:model.debounce.1500ms="location_id"
+                id="locations"
+                class="form-control form-control-sm"
+            >
+                <option value="">Todos</option>
+                @foreach($locations as $itemLocation)
+                    <option value="{{ $itemLocation->id }}">
+                        {{ $itemLocation->name }}
+                    </option>
+                @endforeach
+            </select>
+        </fieldset>
+
+        <fieldset class="form-group col-md-2">
+            <label for="places">Lugares</label>
+            <select
+                wire:model.debounce.1500ms="place_id"
+                id="places"
+                class="form-control form-control-sm"
+            >
+                <option value="">Todos</option>
+                @foreach($places as $itemPlace)
+                    <option value="{{ $itemPlace->id }}">
+                        {{ $itemPlace->name }}
+                    </option>
+                @endforeach
+            </select>
+        </fieldset>
+
+        <fieldset class="form-group col-md-2">
+            <label for="responsibles">Responsables</label>
+            <select
+                wire:model.debounce.1500ms="user_responsible_id"
+                id="responsibles"
+                class="form-control form-control-sm"
+            >
+                <option value="">Todos</option>
+                @foreach($responsibles as $itemResponsible)
+                    <option value="{{ $itemResponsible->id }}">
+                        {{ $itemResponsible->tinny_name }}
+                    </option>
+                @endforeach
+            </select>
+        </fieldset>
+
+        <fieldset class="form-group col-md-2">
+            <label for="users">Usuarios</label>
+            <select
+                wire:model.debounce.1500ms="user_using_id"
+                id="users"
+                class="form-control form-control-sm"
+            >
+                <option value="">Todos</option>
+                @foreach($users as $itemUser)
+                    <option value="{{ $itemUser->id }}">
+                        {{ $itemUser->tinny_name }}
+                    </option>
+                @endforeach
+            </select>
+        </fieldset>
+
+        <fieldset class="form-group col-md-1">
+            <label for="">Filtro</label>
+            <button
+                class="btn btn-sm btn-primary btn-block"
+                wire:click="getInventories"
+            >
+                <i class="fas fa-filter"></i> Filtrar
+            </button>
         </fieldset>
     </div>
+
+    <h5>
+        Resumen de Filtro
+    </h5>
+
+    <div class="table-responsibe">
+        <table class="table table-bordered table-sm">
+            <tr>
+                <th>Producto</th>
+                <th>Ubicación</th>
+                <th>Lugar</th>
+                <th>Responsable</th>
+                <th>Usuario</th>
+                <th class="text-center">Total</th>
+            </tr>
+            <tr>
+                <td>
+                    @if($unspscProduct)
+                        {{ $unspscProduct->name }}
+                    @endif
+                </td>
+                <td>
+                    @if($location)
+                        {{ $location->name }}
+                    @endif
+                </td>
+                <td>
+                    @if($place)
+                        {{ $place->name }}
+                    @endif
+                </td>
+                <td>
+                    @if($userResponsible)
+                        {{ $userResponsible->tinny_name }}
+                    @endif
+                </td>
+                <td>
+                    @if($userUsing)
+                        {{ $userUsing->tinny_name }}
+                    @endif
+                </td>
+                <td class="text-center">
+                    {{ $inventories->total() }}
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <h5>
+        Inventario
+    </h5>
 
     <div class="table-responsive">
         <table class="table table-sm table-bordered">
@@ -24,9 +154,8 @@
                     <th>Producto</th>
                     <th>Ubicación</th>
                     <th>Lugar</th>
-                    <th>Fecha Entrega</th>
+                    <th>Responsable</th>
                     <th>Usuario</th>
-                    <th class="text-center">Valor</th>
                     <th></th>
                 </tr>
             </thead>
@@ -35,7 +164,7 @@
                     class="d-none"
                     wire:loading.class.remove="d-none"
                 >
-                    <td class="text-center" colspan="8">
+                    <td class="text-center" colspan="7">
                         @include('layouts.partials.spinner')
                     </td>
                 </tr>
@@ -66,15 +195,10 @@
                         {{ optional($inventory->place)->name }}
                     </td>
                     <td class="text-center">
-                        @if($inventory->deliver_date)
-                            {{ $inventory->deliver_date->format('Y-m-d') }}
-                        @endif
-                    </td>
-                    <td>
-                        {{ optional($inventory->using)->full_name }}
+                        {{ optional($inventory->responsible)->tinny_name }}
                     </td>
                     <td class="text-center">
-                        ${{ $inventory->price }}
+                        {{ optional($inventory->using)->tinny_name }}
                     </td>
                     <td class="text-center">
                         <a
@@ -87,15 +211,12 @@
                 </tr>
                 @empty
                 <tr class="text-center" wire:loading.remove>
-                    <td colspan="8">
+                    <td colspan="7">
                         <em>No hay registros</em>
                     </td>
                 </tr>
                 @endforelse
             </tbody>
-            <caption>
-                Total de resultados: {{ $inventories->total() }}
-            </caption>
         </table>
 
         {{ $inventories->links() }}
