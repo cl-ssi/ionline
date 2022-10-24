649c969c4cffb28d31aa06d7042c96685f19b2db
Esteban Miranda <tebiccr@gmail.com>
2022-10-24 10:29:48 -0300

    fix nombre del down

--- database/migrations/2022_10_20_171155_create_rem_users_table.php
+++ database/migrations/2022_10_20_171155_create_rem_users_table.php
@@ -31,6 +31,6 @@ class CreateRemUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_rem');
+        Schema::dropIfExists('rem_users');
     }
 }
