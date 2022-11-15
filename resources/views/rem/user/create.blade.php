7e252aef6d9c0c4c4717c26a382c09a48dc192dd
tebiccr <tebiccr@gmail.com>
2022-11-15 19:18:48 -0300

    con livewire al crear usuario

--- resources/views/rem/user/create.blade.php
+++ resources/views/rem/user/create.blade.php
@@ -18,13 +18,8 @@
         </fieldset>
 
         <fieldset class="form-group col">
-            <label for="for_user_id">Usuarios </label>
-            <select name="user_id" id="for_user_id" class="form-control selectpicker" data-live-search="true" title="Seleccione Usuario" required>
-                <option value="">Seleccionar Usuario</option> 
-                @foreach($users as $user)
-                <option value="{{ $user->id }}">{{ $user->fullname }}</option>
-                @endforeach
-            </select>
+            <label for="for_user_id">Usuarios </label>            
+                @livewire('search-select-user', ['required' => 'required'])            
         </fieldset>
 
     </div>
