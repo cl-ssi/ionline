e24ce964835f8159b002c88c69e5fd886bf88b44
AquaroTorres <alvarotorres@gmail.com>
2022-11-22 23:22:51 +0000

    Filesystem for testing

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,12 @@ return [
             'root' => storage_path('app'),
         ],
 
+        /** Driver for testing env (iOnline verde) */
+        'tmp-for-tests' => [
+            'driver' => 'local',
+            'root' => storage_path('app/livewire-tmp'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
