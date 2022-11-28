3fcfcb21744ad35d1da2690856556a6d3a1abfe6
mirandal <mirandal.jorge@gmail.com>
2022-11-28 16:12:33 -0300

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -1812,7 +1812,7 @@ Route::prefix('request_forms')->as('request_forms.')->middleware('auth')->group(
 Route::prefix('allowances')->as('allowances.')->middleware('auth')->group(function () {
 
     Route::get('/', [AllowanceController::class, 'index'])->name('index');
-    Route::get('all_index', [AllowanceController::class, 'all_index'])->name('all_index')->middleware('permission:Allowances: all');;
+    Route::get('all_index', [AllowanceController::class, 'all_index'])->name('all_index')->middleware('permission:Allowances: all');
     Route::get('sign_index', [AllowanceController::class, 'sign_index'])->name('sign_index');
     Route::get('create', [AllowanceController::class, 'create'])->name('create');
     Route::post('store', [AllowanceController::class,'store'])->name('store');
