441e2ef1f0e3e642d12638c51ebe67a0b12556ba
Esteban Miranda <tebiccr@gmail.com>
2022-12-05 20:57:02 -0300

    con softdeleted

--- app/Models/Rem/RemSerie.php
+++ app/Models/Rem/RemSerie.php
@@ -4,13 +4,17 @@ namespace App\Models\Rem;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RemSerie extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     public $table = 'rem_series';
 
     protected $fillable = [
+        'name',
         'period_id',
         'serie_id',
     ];
