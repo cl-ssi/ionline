ab316a9a38d754e1df46c1edfcb96e8fcc52cb60
sick <esteban.rojasg@redsalud.cl>
2022-12-21 10:27:01 -0300

    test

--- app/Http/Controllers/Requirements/EventController.php
+++ app/Http/Controllers/Requirements/EventController.php
@@ -263,8 +263,9 @@ class EventController extends Controller
 
     public function download(File $file)
     {
-        // return Storage::response($file->file, mb_convert_encoding($file->name, 'ASCII'));
+        // dd($file);
+        return Storage::response($file->file, mb_convert_encoding($file->name, 'ASCII'));
         // $file = $dispatch->files->first();
-        return Storage::disk('gcs')->response($file->file, mb_convert_encoding($file->name,'ASCII'));
+        // return Storage::disk('gcs')->response($file->file, mb_convert_encoding($file->name,'ASCII'));
     }
 }
