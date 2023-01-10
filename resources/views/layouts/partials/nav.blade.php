9a1bc415b40f97f6526da7391ecffe1d86f1236b
AquaroTorres <alvarotorres@gmail.com>
2023-01-10 15:51:38 -0300

    Eliminar arbol de pascua

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,8 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-dark shadow-sm bg-nav-gobierno">
     <div class="container">
         <a class="navbar-brand" href="{{ route('home') }}">
-            {{ config('app.name', 'Laravel') }} 
-            <i class="fas fa-holly-berry"></i>
+            {{ config('app.name', 'Laravel') }}
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
