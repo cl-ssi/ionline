5b9d6a4a65806a2d238ef6bc1d995d8fb7c6a1c3
Alvaro Lupa <alvaro.lupa@gmail.com>
2023-01-11 13:18:09 -0300

    [BD_RNI] opcion de subir archivo por comuna o establecimiento, dar acceso al archivo a funcionarios, entre otros.

---
+++ database/migrations/2022_12_26_131935_create_rni_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRniFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rni_files', function (Blueprint $table) {
+            $table->id();
+            $table->string('filename');
+            $table->string('size');
+            $table->text('file');
+            $table->foreignId('establishment_id')->constrained('establishments');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rni_files');
+    }
+}
