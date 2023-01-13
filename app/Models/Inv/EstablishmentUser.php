17771c516dd335f7ca8f70809b3cf835164495d9
rafael1delgado <delgadorafael2011@gmail.com>
2023-01-13 16:44:53 -0400

    feat: add pivot model Inv\EstablishmentUser

---
+++ app/Models/Inv/EstablishmentUser.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models\Inv;
+
+use App\Models\Establishment;
+use App\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Spatie\Permission\Models\Role;
+
+class EstablishmentUser extends Pivot
+{
+    use HasFactory;
+    
+    protected $table = 'inv_establishment_user';
+
+    protected $fillable = [
+        'establishment_id',
+        'role_id',
+        'user_id',
+    ];
+
+    public function establishment()
+    {
+        return $this->belongsTo(Establishment::class);
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
