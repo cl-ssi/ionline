943dcf4961966fa58f398e1465cb0a0026797bc0
rafael1delgado <delgadorafael2011@gmail.com>
2023-01-13 17:08:34 -0400

    feat: add establishment column

--- resources/views/parameters/places/index.blade.php
+++ resources/views/parameters/places/index.blade.php
@@ -1,28 +1,40 @@
 <div class="table-responsive">
-    <table class="table">
+    <table class="table table-sm table-bordered">
         <thead>
             <tr>
                 <th class="text-center">ID</th>
                 <th>Nombre</th>
                 <th>Descripción</th>
                 <th>Ubicación</th>
-                <th>Acciones</th>
+                <th>Establecimiento</th>
+                <th class="text-center">Acciones</th>
             </tr>
         </thead>
         <tbody>
-            @foreach($places as $place)
+            @forelse($places as $place)
             <tr>
                 <td class="text-center">{{ $place->id }}</td>
                 <td>{{ $place->name }}</td>
                 <td>{{ $place->description }}</td>
                 <td>{{ $place->location->name }}</td>
-                <td>
-                    <button type="button" class="btn btn-sm btn-primary" wire:click="edit({{ $place }})">
+                <td>{{ $place->establishment->name }}</td>
+                <td class="text-center">
+                    <button
+                        type="button"
+                        class="btn btn-sm btn-outline-primary"
+                        wire:click="edit({{ $place }})"
+                    >
                         <i class="fas fa-edit"></i>
                     </button>
                 </td>
             </tr>
-            @endforeach
+            @empty
+            <tr class="text-center">
+                <td colspan="6">
+                    <em>No hay lugares</em>
+                </td>
+            </tr>
+            @endforelse
         </tbody>
         <caption>
             {{--
