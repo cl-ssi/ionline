7794d64ab71583d2db0c47d258ed7c4dc24ce65d
rafael1delgado <delgadorafael2011@gmail.com>
2023-01-26 12:41:14 -0400

    feat: order SignatureFlow model

--- app/Models/ServiceRequests/SignatureFlow.php
+++ app/Models/ServiceRequests/SignatureFlow.php
@@ -12,6 +12,9 @@ class SignatureFlow extends Model implements Auditable
     use \OwenIt\Auditing\Auditable;
     use HasFactory;
     use SoftDeletes;
+    
+    protected $table = 'doc_signature_flow';
+
     /**
      * The attributes that are mass assignable.
      *
@@ -21,28 +24,30 @@ class SignatureFlow extends Model implements Auditable
         'id', 'ou_id', 'derive_date','responsable_id','user_id','service_request_id','resolution_id', 'sign_position', 'type', 'employee', 'observation', 'signature_date', 'status'
     ];
 
-    public function user(){
-      return $this->belongsTo('App\User','responsable_id')->withTrashed();
-    }
-
-    public function serviceRequest(){
-      return $this->belongsTo('App\ServiceRequests\ServiceRequest');
-    }
-
-    public function resolution(){
-      return $this->belongsTo('App\Models\Documents\Signature');
-    }
-
-    public function organizationalUnit(){
-      return $this->belongsTo('App\Rrhh\OrganizationalUnit','ou_id');
-    }
-
-    protected $table = 'doc_signature_flow';
-
     /**
      * The attributes that should be mutated to dates.
      *
      * @var array
      */
     protected $dates = ['signature_date'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User','responsable_id')->withTrashed();
+    }
+
+    public function serviceRequest()
+    {
+        return $this->belongsTo('App\ServiceRequests\ServiceRequest');
+    }
+
+    public function resolution()
+    {
+        return $this->belongsTo('App\Models\Documents\Signature');
+    }
+
+    public function organizationalUnit()
+    {
+        return $this->belongsTo('App\Rrhh\OrganizationalUnit','ou_id');
+    }
 }
