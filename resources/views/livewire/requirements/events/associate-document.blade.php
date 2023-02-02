d6c0462846be7b2e2ab67ef4dc3388c27a69a43e
AquaroTorres <alvarotorres@gmail.com>
2023-02-02 00:23:26 -0300

    SGR - Associate-document cols

--- resources/views/livewire/requirements/events/associate-document.blade.php
+++ resources/views/livewire/requirements/events/associate-document.blade.php
@@ -1,53 +1,53 @@
 @if($parte_id <> 0)
 
 <div class="form-row">
-	<fieldset class="form-group col-6">
-		<label for="for_doc_id">Asociar documentos</label>
-		<div class="input-group @if($message) is-invalid @endif">
+    <fieldset class="form-group col-12">
+        <label for="for_doc_id">Asociar documentos</label>
+        <div class="input-group @if($message) is-invalid @endif">
 
-			<input type="number" class="form-control" id="for_doc_id" wire:model="doc_id" placeholder="número interno">
-			
-			<div class="input-group-append">
-				<button type="button" class="btn btn-primary" wire:click="setDocument()">
-					<i class="fas fa-plus"></i>
-				</button>
-			</div>
-		</div>
-		<div class="invalid-feedback">
-			{{ $message }}
-		</div>
-	</fieldset>
+            <input type="number" class="form-control" id="for_doc_id" wire:model="doc_id" placeholder="número interno">
+            
+            <div class="input-group-append">
+                <button type="button" class="btn btn-primary" wire:click="setDocument()">
+                    <i class="fas fa-plus"></i>
+                </button>
+            </div>
+        </div>
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    </fieldset>
 
-	<fieldset class="form-group col-6">
-		<label for="for_documents">&nbsp;</label>
-		<input readonly class="form-control-plaintext" type='text' name="documents" wire:model='documents'>
-	</fieldset>
+    <fieldset class="form-group col-6">
+        <label for="for_documents">&nbsp;</label>
+        <input readonly class="form-control-plaintext" type='text' name="documents" wire:model='documents'>
+    </fieldset>
 </div>
 
 @else
-	<div class="col-12 col-md-5">
-		<div class="form-row">
-			<fieldset class="form-group col-12 col-md-6">
-				<label for="for_doc_id">Asociar documentos</label>
-				<div class="input-group @if($message) is-invalid @endif">
+    <div class="col-12">
+        <div class="form-row">
+            <fieldset class="form-group col-12 col-md-6">
+                <label for="for_doc_id">Asociar documentos</label>
+                <div class="input-group @if($message) is-invalid @endif">
 
-					<input type="number" class="form-control" id="for_doc_id" wire:model="doc_id" placeholder="número interno">
-					
-					<div class="input-group-append">
-						<button type="button" class="btn btn-primary" wire:click="setDocument()">
-							<i class="fas fa-plus"></i>
-						</button>
-					</div>
-				</div>
-				<div class="invalid-feedback">
-					{{ $message }}
-				</div>
-			</fieldset>
+                    <input type="number" class="form-control" id="for_doc_id" wire:model="doc_id" placeholder="número interno">
+                    
+                    <div class="input-group-append">
+                        <button type="button" class="btn btn-primary" wire:click="setDocument()">
+                            <i class="fas fa-plus"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            </fieldset>
 
-			<fieldset class="form-group col-12 col-md-6">
-				<label for="for_documents">&nbsp;</label>
-				<input readonly class="form-control-plaintext" type='text' name="documents" wire:model='documents'>
-			</fieldset>
-		</div>
-	</div>
+            <fieldset class="form-group col-12 col-md-6">
+                <label for="for_documents">&nbsp;</label>
+                <input readonly class="form-control-plaintext" type='text' name="documents" wire:model='documents'>
+            </fieldset>
+        </div>
+    </div>
 @endif
