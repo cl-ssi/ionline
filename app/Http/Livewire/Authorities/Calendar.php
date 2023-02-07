1b9fac3a1f6bc0641c8accafa18d37d8a0a56885
AquaroTorres <alvarotorres@gmail.com>
2023-02-07 16:15:12 -0300

    Authority Calendar maqueta

--- app/Http/Livewire/Authorities/Calendar.php
+++ app/Http/Livewire/Authorities/Calendar.php
@@ -3,11 +3,70 @@
 namespace App\Http\Livewire\Authorities;
 
 use Livewire\Component;
+use Carbon\Carbon;
+use App\User;
 
 class Calendar extends Component
 {
+    public $monthSelection;
+    public $startOfMonth;
+    public $data;
+
+    public $edit = true;
+    
+    /**
+    * Mount
+    */
+    public function mount()
+    {
+        $this->monthSelection = date('Y-m');
+    }
     public function render()
     {
+
+        $this->startOfMonth = Carbon::createFromFormat('Y-m', $this->monthSelection)->startOfMonth();
+        
+        /** Array de prueba */
+        $this->data = array();
+
+        $date = $this->startOfMonth->copy();
+        $this->data[$date->format('Y-m-d')]['date'] = $date;
+        $this->data[$date->format('Y-m-d')]['holliday'] = false;
+        $this->data[$date->format('Y-m-d')]['manager'] = User::find(15287582);
+        $this->data[$date->format('Y-m-d')]['secretary'] = User::find(15287582);
+
+        $date = $this->startOfMonth->copy()->addDays(1);
+        $this->data[$date->format('Y-m-d')]['date'] = $date;
+        $this->data[$date->format('Y-m-d')]['holliday'] = true;
+        $this->data[$date->format('Y-m-d')]['manager'] = User::find(15287582);
+        $this->data[$date->format('Y-m-d')]['secretary'] = User::find(15287582);
+        
+        $date = $this->startOfMonth->copy()->addDays(2);
+        $this->data[$date->format('Y-m-d')]['date'] = $date;
+        $this->data[$date->format('Y-m-d')]['holliday'] = false;
+        $this->data[$date->format('Y-m-d')]['manager'] = User::find(15287582);
+        $this->data[$date->format('Y-m-d')]['secretary'] = User::find(15287582);
+
+        $date = $this->startOfMonth->copy()->addDays(3);
+        $this->data[$date->format('Y-m-d')]['date'] = $date;
+        $this->data[$date->format('Y-m-d')]['holliday'] = false;
+        $this->data[$date->format('Y-m-d')]['manager'] = User::find(15287582);
+        $this->data[$date->format('Y-m-d')]['secretary'] = User::find(15287582);
+
+        $date = $this->startOfMonth->copy()->addDays(4);
+        $this->data[$date->format('Y-m-d')]['date'] = $date;
+        $this->data[$date->format('Y-m-d')]['holliday'] = false;
+        $this->data[$date->format('Y-m-d')]['manager'] = User::find(15287582);
+        $this->data[$date->format('Y-m-d')]['secretary'] = User::find(15287582);
+
+        $date = $this->startOfMonth->copy()->addDays(5);
+        $this->data[$date->format('Y-m-d')]['date'] = $date;
+        $this->data[$date->format('Y-m-d')]['holliday'] = false;
+        $this->data[$date->format('Y-m-d')]['manager'] = User::find(15287582);
+        $this->data[$date->format('Y-m-d')]['secretary'] = User::find(15287582);
+
+        // dd($this->data);
+
         return view('livewire.authorities.calendar');
     }
 }
--- resources/views/livewire/authorities/calendar.blade.php
+++ resources/views/livewire/authorities/calendar.blade.php
@@ -1,3 +1,83 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+
+    <div class="form-row">
+        <div class="col-6 col-md-3">
+
+            <div class="form-group">
+                <label for="date">Selecione el mes</label>
+                <input class="form-control" type="month" wire:model="monthSelection">
+            </div>
+        </div>
+    </div>
+
+    @if($edit)
+    <div class="card mb-4">
+        <div class="card-body">
+            <div class="form-row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="">Usuario</label>
+                        <select name="" id="" class="form-control">
+                            <option value="">Alvaro Torres</option>
+                            <option value="">Esteban Miranda</option>
+                            <option value="">Alvaro Lupa</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="">Desde</label>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="col-1">
+                    <div class="form-group">
+                        <label for="">&nbsp;</label>
+                        <button class="btn btn-primary form-control">
+                            <i class="fas fa-save"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
+
+    @for($i = 1; $i < $startOfMonth->dayOfWeek; $i++)
+    <div class="dia_calendario small p-2 text-center">
+        <br><br><br><br><br><br>
+    </div>
+    @endfor
+
+    @foreach($data as $date => $authority)
+        <div class="dia_calendario small p-2 text-center">
+
+            <span class="{{ ($authority['holliday'] OR $authority['date']->dayOfWeek == 0) ? 'text-danger': '' }}">
+                {{ $date }}
+            </span>
+
+            <hr class="mt-1 mb-1" >
+            {{ optional($authority['manager'])->tinnyName }} <br>
+            <em class="text-muted">{{ optional($authority['manager'])->position }}</em>
+            
+            <hr class="mt-1 mb-1" >
+            {{ optional($authority['secretary'])->tinnyName }} <br>
+            <em class="text-muted">{{ optional($authority['secretary'])->position }}</em>
+
+        </div>
+    @endforeach
+
+    @section('custom_css')
+    <style media="screen">
+        .dia_calendario {
+            display: inline-block;
+            border: solid 1px rgb(0, 0, 0, 0.125);
+            border-radius: 0.25rem;
+            width: 13.9%;
+            /* width: 154px; */
+            text-align: center;
+            margin-bottom: 5px;
+        }
+    </style>
+    @endsection
 </div>
