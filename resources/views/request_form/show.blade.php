f2709363cae234c29fd07cabe35ed35e2a95a33e
Alvaro Lupa <alvaro.lupa@gmail.com>
2023-02-24 13:12:57 -0300

    [FR] Auditoría interna para el proceso de firmas

--- resources/views/request_form/show.blade.php
+++ resources/views/request_form/show.blade.php
@@ -676,18 +676,65 @@
                     </div>
                 </div>
             </div>
+            <div class="card">
+                <div class="card-header" id="headingThree">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse"
+                                data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Firmas
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <h6 class="mt-3 mt-4">Historial de cambios</h6>
+                        <div class="table-responsive-md">
+                            <table class="table table-sm small text-muted mt-3">
+                                <thead>
+                                <tr>
+                                    <th>Fecha</th>
+                                    <th>Usuario</th>
+                                    <th>Modificaciones</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($requestForm->eventRequestForms as $eventRequestForm)
+                                    @if($eventRequestForm->audits->count() > 0)
+                                        @foreach($eventRequestForm->audits->sortByDesc('updated_at') as $audit)
+                                            <tr>
+                                                <td nowrap>{{ $audit->created_at }}</td>
+                                                <td nowrap>{{ optional($audit->user)->fullName }}</td>
+                                                <td>
+                                                    @foreach($audit->getModified() as $attribute => $modified)
+                                                        @if(isset($modified['old']) OR isset($modified['new']))
+                                                            <strong>{{ $attribute }}</strong>
+                                                            :  {{ isset($modified['old']) ? $modified['old'] : '' }}
+                                                            => {{ $modified['new'] }};
+                                                        @endif
+                                                    @endforeach
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    @endif
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
             @if($requestForm->purchasingProcess)
                 <div class="card">
-                    <div class="card-header" id="headingThree">
+                    <div class="card-header" id="headingFour">
                         <h2 class="mb-0">
                             <button class="btn btn-link btn-block text-left collapsed" type="button"
-                                    data-toggle="collapse" data-target="#collapseThree" aria-expanded="false"
-                                    aria-controls="collapseThree">
+                                    data-toggle="collapse" data-target="#collapseFour" aria-expanded="false"
+                                    aria-controls="collapseFour">
                                 Información de la Compra
                             </button>
                         </h2>
                     </div>
-                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
+                    <div id="collapseFour" class="collapse" aria-labelledby="headingFour"
                          data-parent="#accordionExample">
                         <div class="card-body">
                             <h6 class="mt-3 mt-4">Historial de cambios</h6>
@@ -727,16 +774,16 @@
                     </div>
                 </div>
                 <div class="card">
-                    <div class="card-header" id="headingFour">
+                    <div class="card-header" id="headingFive">
                         <h2 class="mb-0">
                             <button class="btn btn-link btn-block text-left collapsed" type="button"
-                                    data-toggle="collapse" data-target="#collapseFour" aria-expanded="false"
-                                    aria-controls="collapseThree">
+                                    data-toggle="collapse" data-target="#collapseFive" aria-expanded="false"
+                                    aria-controls="collapseFive">
                                 Historial de Procesos de Compra
                             </button>
                         </h2>
                     </div>
-                    <div id="collapseFour" class="collapse" aria-labelledby="headingFour"
+                    <div id="collapseFive" class="collapse" aria-labelledby="headingFive"
                          data-parent="#accordionExample">
                         <div class="card-body">
                             <h6 class="mt-3 mt-4">Historial de cambios</h6>
