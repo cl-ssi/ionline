5efc336e82908d930ff8c3690a9718a3ba31b5e8
rafael1delgado <delgadorafael2011@gmail.com>
2023-03-02 19:14:50 -0400

    feat: remove send from signature

--- app/Http/Livewire/Warehouse/Control/ControlReceiving.php
+++ app/Http/Livewire/Warehouse/Control/ControlReceiving.php
@@ -3,7 +3,6 @@
 namespace App\Http\Livewire\Warehouse\Control;
 
 use App\Models\Warehouse\Control;
-use App\Services\SignatureService;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -32,36 +31,5 @@ class ControlReceiving extends Component
             ->paginate(10);
 
         return $controls;
-    }
-
-    public function sendTechnicalRequest(Control $control)
-    {
-        $technicalSigner = $control->technicalSigner ?? null;
-
-        if($technicalSigner)
-        {
-            $technicalSignature = new SignatureService();
-            $technicalSignature->addResponsible($this->store->visator);
-            $technicalSignature->addSignature(
-                10,
-                "Acta de Recepción Técnica #$control->id",
-                "Recepción #$control->id",
-                'Visación en cadena de responsabilidad',
-                true
-            );
-            $technicalSignature->addView('warehouse.pdf.report-reception', [
-                'type' => '',
-                'control' => $control,
-                'store' => $control->store,
-                'act_type' => 'reception',
-            ]);
-            $technicalSignature->addVisators(collect([]));
-            $technicalSignature->addSignatures(collect([$technicalSigner]));
-            $technicalSignature = $technicalSignature->sendRequest();
-            $control->technicalSignature()->associate($technicalSignature);
-            $control->save();
-
-            session()->flash('success', "La solicitud de firma fue enviada a $technicalSigner->tinny_name.");
-        }
     }
 }
