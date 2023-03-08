c00eb50d58f1de9c2696c166f80428d42b005c6e
Alvaro Lupa <alvaro.lupa@gmail.com>
2023-03-08 12:08:10 -0300

    [Convenios] se habilita buscador por programa en el listado

--- resources/views/agreements/programs/index.blade.php
+++ resources/views/agreements/programs/index.blade.php
@@ -6,7 +6,25 @@
 
 @include('agreements/nav')
 
-<h3 class="mb-3">Programas</h3>
+<h3 class="mb-3">Programas 
+<form class="form-inline float-right mb-3" method="GET" action="{{ route('agreements.programs.index') }}">
+  <div class="input-group">
+    <input type="text" class="form-control" name="program_name" id="program_name" value="{{ request()->program_name }}" placeholder="Buscar por programa" aria-label="Buscar por programa" aria-describedby="button-addon2">
+    @if(request()->has('program_name') && strlen(request()->program_name) > 0)
+    <span class="input-group-append">
+        <button id="clear" class="btn bg-transparent" style="margin-left: -40px; z-index: 100;" type="button">
+            <i class="fa fa-times"></i>
+        </button>
+    </span>
+    @endif
+    <div class="input-group-append">
+        <button class="btn btn-outline-secondary" type="submit">
+            <i class="fas fa-search" aria-hidden="true"></i>
+        </button>
+    </div>
+	</div>
+</form>
+</h3>
 
 <table class="table table-sm table-hover">
     <thead>
@@ -35,4 +53,13 @@
     </tbody>
 </table>
 
+@endsection
+
+@section('custom_js')
+<script type="text/javascript">
+    $("#clear").on("click" , function() {
+        $("input[type=text]").val("");
+        $("span.input-group-append").hide();
+    });
+</script>
 @endsection
