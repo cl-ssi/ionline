975d7b45a4748617f455e10af0160229fa1a5cc0
rafael1delgado <delgadorafael2011@gmail.com>
2023-03-16 14:19:50 -0400

    feat: add the pdf for the precursor act

---
+++ resources/views/drugs/pdf/layouts.blade.php
@@ -0,0 +1,26 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>@yield('title')</title>
+        <meta name="description" content="Report Store">
+        <meta name="author" content="{{ env('APP_SS') }}">
+        <link href="/css/report.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="content">
+            <div style="padding-bottom: 0.3rem">
+                <img style="padding-bottom: 4px;" src="/images/logo_pluma.jpg"
+                width="120" alt="Logo {{ env('APP_SS') }}"><br>
+            </div>
+
+            @yield('content')
+
+            <div class="pie_pagina seis center" style="height: 20px;">
+                <span class="uppercase">{{ env('APP_SS') }}</span><br>
+                {{ env('APP_SS_ADDRESS') }} -
+                Fono: {{ env('APP_SS_TELEPHONE') }} -
+                {{ env('APP_SS_WEBSITE') }}
+            </div>
+        </div>
+    </body>
+</html>
