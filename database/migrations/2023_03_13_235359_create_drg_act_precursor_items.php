259a8274f692bb180708ebcfc0b90514dd6ada4b
rafael1delgado <delgadorafael2011@gmail.com>
2023-03-16 14:07:10 -0400

    feat: add act_precursors & act_precursor_items tables

---
+++ database/migrations/2023_03_13_235359_create_drg_act_precursor_items.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDrgActPrecursorItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drg_act_precursor_items', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('reception_item_id')->nullable()->constrained('drg_reception_items');
+            $table->foreignId('act_precursor_id')->nullable()->constrained('drg_act_precursors');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drg_act_precursor_items');
+    }
+}
