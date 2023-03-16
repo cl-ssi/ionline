ac5a92a5a42f548e2d1f93003e9f6f6537f97320
rafael1delgado <delgadorafael2011@gmail.com>
2023-03-16 14:08:14 -0400

    feat: add model ActPrecursor & ActPrecursorItem

---
+++ app/Models/Drugs/ActPrecursor.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models\Drugs;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ActPrecursor extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'drg_act_precursors';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'date',
+        'full_name_receiving',
+        'run_receiving',
+        'note',
+        'delivery_id',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'date'
+    ];
+
+    public function delivery()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function precursors()
+    {
+        return $this->hasMany(ActPrecursorItem::class);
+    }
+
+    public function getFormatDateAttribute()
+    {
+        return $this->date->day . ' de ' . $this->date->monthName . ' del ' . $this->date->year;
+    }
+}
