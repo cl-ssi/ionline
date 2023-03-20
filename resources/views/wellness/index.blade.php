5ac2a3aa1169a5346b118cf3146c01c3a827464c
AquaroTorres <alvarotorres@gmail.com>
2023-03-20 12:32:16 -0300

    Bienestar - Home

--- resources/views/wellness/index.blade.php
+++ resources/views/wellness/index.blade.php
@@ -6,18 +6,53 @@
 
 @include('wellness.nav')
 
-<div class="container my-5">
-    <h1 class="text-center">¡Bienvenidos al Módulo de Bienestar!</h1>
-    <hr>
-    <div class="row justify-content-center my-5">
-        <div class="col-md-8">
-            <p class="lead text-center">
-                En este espacio podrás visualizar a través de la plataforma del iOnline, todos los archivos que sean cargados desde la plataforma en Excel de Bienestar, y el balance correspondiente.
-            </p>
-            <p class="text-center">
-                Este módulo te permitirá tener un control más eficiente de los datos relacionados con el bienestar de los usuarios.
-            </p>
-        </div>
+
+<div class="row">
+    <div class="col-12 col-md-5">
+        <h3 class="mb-3">¡Bienvenidos al Módulo de Estado Financiero de Bienestar!</h3>
+        <p class="lead text-center">
+            Este módulo de iOnline permite visualizar el estado financiero en el tiempo del Servicio de Bienestar.<br>
+            Este panel de información incluye tanto gráficos de los fondos ejecutados y un resumen del balance presupuestario mes a mes del Servico de Bienestar.
+        </p>
+
+    </div>
+    <div class="col-12 col-md-7">
+        <div id="barchart" style="width: 900px; height: 500px;"></div>
     </div>
 </div>
+
+
+@endsection
+
+
+@section('custom_js')
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+        ['Ppto', 
+            '', { role: 'style' }, { role: 'annotation' }, 
+            '', { role: 'style' }, { role: 'annotation' } ],
+        ['Prespuesto', 800000000, '','Ppto Inicial', 100000000, '','Ajuste'],
+        ['Ejecutado', 300000000, '#1b9e77', 'Ejecutado',0, '',''],
+      ]);
+
+      var options = {
+        width: 600,
+        height: 400,
+        legend: { position: 'none', maxLines: 3 },
+        bar: { groupWidth: '75%' },
+        isStacked: true,
+        title: 'Estado de la ejecución presupuestaria',
+      };
+
+        var chart = new google.visualization.ColumnChart(document.getElementById('barchart'));
+
+        chart.draw(data, options);
+      }
+    </script>
 @endsection
--- resources/views/wellness/nav.blade.php
+++ resources/views/wellness/nav.blade.php
@@ -1,21 +1,27 @@
 <ul class="nav nav-tabs mb-3 d-print-none">
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('wellness.dosfile.index') }}">
-            <i class="fas fa-file-upload"></i> Carga archivo DOS
+        <a class="nav-link" href="{{ route('wellness.index') }}">
+            <i class="fas fa-home"></i> home
         </a>
     </li>
 
     <li class="nav-item">
         <a class="nav-link" href="{{ route('wellness.balances') }}">
-            <i class="fas fa-balance-scale"></i> Balances (DOS)
+            <i class="fas fa-balance-scale"></i> Balance
         </a>
     </li>
-    
+
     <li class="nav-item">
+        <a class="nav-link" href="{{ route('wellness.dosfile.index') }}">
+            <i class="fas fa-file-upload"></i> Carga archivo
+        </a>
+    </li>
+
+    <!--li class="nav-item">
         <a class="nav-link" href="{{ route('wellness.loans.index') }}">
             <i class="fas fa-file-excel"></i> Carga Excel de préstamos
         </a>
-    </li>
+    </li-->
 
     <li class="nav-item">
         <a class="nav-link" href="{{ route('wellness.report') }}">
