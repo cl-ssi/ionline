32f44b6b7066b39282d4784bb39857159bcc2da8
Esteban Miranda <tebiccr@gmail.com>
2023-04-05 04:47:01 -0400

    la logica de crear partes for director en Parte

--- app/Models/Documents/Parte.php
+++ app/Models/Documents/Parte.php
@@ -73,15 +73,16 @@ class Parte extends Model
     }
 
 
-    public function setCorrelative() {
-        abort_if(!isset($this->establishment_id), 501,'El parte no tiene establecimiento asociado');
+    public function setCorrelative()
+    {
+        abort_if(!isset($this->establishment_id), 501, 'El parte no tiene establecimiento asociado');
 
         /* Obtener el objeto correlativo según el tipo */
         $correlative = Correlative::whereNull('type_id')
-            ->where('establishment_id',$this->establishment_id)
+            ->where('establishment_id', $this->establishment_id)
             ->first();
 
-        if(!$correlative) {
+        if (!$correlative) {
             $correlative = Correlative::create([
                 'type_id' => null,
                 'correlative' => 1,
@@ -101,18 +102,81 @@ class Parte extends Model
     }
 
 
+    public static function createPartesForDirectors($destinatarios, SignaturesFlow $signaturesFlow)
+    {
+        // Limpiar los espacios en blanco alrededor de cada dirección de correo electrónico en el array de destinatarios
+        $destinatarios = array_map('trim', explode(',', $destinatarios));
+
+        // Preparar un array de correos electrónicos de la base de datos, separando los correos en el valor del campo mail_director por coma y limpiando los espacios en blanco
+        $mail_director_arr = Establishment::select('id', 'mail_director')
+            ->whereNotNull('mail_director')
+            ->get()
+            ->map(function ($est) {
+                return [
+                    'id' => $est['id'],
+                    'mail_director' => array_map('trim', explode(',', $est['mail_director']))
+                ];
+            })
+            ->toArray();
+
+        // Encontrar las coincidencias entre los vectores
+        $coincidencias = array_filter($destinatarios, function ($value) use ($mail_director_arr) {
+            return collect($mail_director_arr)->pluck('mail_director')->flatten()->contains($value);
+        });
+
+
+
+        // Buscar los establecimientos que tienen los correos electrónicos en la variable $coincidencias
+        $ids_establecimientos = [];
+        if (!empty($coincidencias)) {
+            foreach ($mail_director_arr as $mail_director) {
+                if (count(array_intersect($coincidencias, $mail_director['mail_director'])) > 0) {
+                    $ids_establecimientos[] = $mail_director['id'];
+                }
+            }
+        }
+
+        foreach ($ids_establecimientos as $id) {
+            $parte = Parte::create([
+                'entered_at' => Carbon::now(),
+                'type_id' => $signaturesFlow->signature->type_id,
+                'date' => $signaturesFlow->signature->request_date,
+                'subject' => $signaturesFlow->signature->subject,
+                'establishment_id' => $id,
+                'origin' => $signaturesFlow->signature->organizationalUnit->name . ' (Parte generado desde Solicitud de Firma N°' . $signaturesFlow->signature->id . ' por ' . $signaturesFlow->signature->responsable->fullname . ')',
+            ]);
+            $parte->setCorrelative();
+            $parte->save();
+
+            $distribucion = SignaturesFile::where('signature_id', $signaturesFlow->signature->id)
+                ->where('file_type', 'documento')
+                ->get();
+
+            ParteFile::create([
+                'parte_id' => $parte->id,
+                'file' => $distribucion->first()->file,
+                'name' => $distribucion->first()->id . '.pdf',
+                'signature_file_id' => $distribucion->first()->id,
+            ]);
+        }
+    }
+
+
+
+
     /* FIXME: Esto no es necesario */
     public function getCreationParteDateAttribute()
     {
         return Carbon::parse($this->date)->format('d-m-Y');
     }
 
-    public function scopeFilter($query, $column, $value) {
-        if(!empty($value)) {
-            switch($column) {
+    public function scopeFilter($query, $column, $value)
+    {
+        if (!empty($value)) {
+            switch ($column) {
                 case 'origin':
                 case 'subject':
-                    $query->where($column, 'LIKE', '%'.$value.'%');
+                    $query->where($column, 'LIKE', '%' . $value . '%');
                     break;
                 case 'id':
                 case 'number':
@@ -121,41 +185,40 @@ class Parte extends Model
                     break;
                 case 'without_sgr':
                     $query->whereDoesntHave('requirements');
-                    $query->whereDate('created_at', '>=', date('Y') - 1 .'-01-01');
+                    $query->whereDate('created_at', '>=', date('Y') - 1 . '-01-01');
                     break;
                 case 'important':
                     $query->whereNotNull('important');
                     break;
             }
         }
-
     }
 
     public function scopeSearch($query, Request $request)
     {
-        if($request->input('id') != "") {
-            $query->where('id', $request->input('id') );
+        if ($request->input('id') != "") {
+            $query->where('id', $request->input('id'));
         }
 
-        if($request->input('type') != "") {
-            $query->where('type', $request->input('type') );
+        if ($request->input('type') != "") {
+            $query->where('type', $request->input('type'));
         }
 
-        if($request->input('number') != "") {
-            $query->where('number', 'LIKE', '%'.$request->input('number').'%' );
+        if ($request->input('number') != "") {
+            $query->where('number', 'LIKE', '%' . $request->input('number') . '%');
         }
 
-        if($request->input('origin') != "") {
-            $query->where('origin', 'LIKE', '%'.$request->input('origin').'%' );
+        if ($request->input('origin') != "") {
+            $query->where('origin', 'LIKE', '%' . $request->input('origin') . '%');
         }
 
-        if($request->input('subject') != "") {
-            $query->where('subject', 'LIKE', '%'.$request->input('subject').'%' );
+        if ($request->input('subject') != "") {
+            $query->where('subject', 'LIKE', '%' . $request->input('subject') . '%');
         }
-        
-        if($request->input('without_sgr') != "") {
+
+        if ($request->input('without_sgr') != "") {
             $query->whereDoesntHave('requirements');
-            $query->whereDate('created_at', '>=', date('Y') - 1 .'-01-01');
+            $query->whereDate('created_at', '>=', date('Y') - 1 . '-01-01');
         }
 
         return $query;
@@ -164,12 +227,11 @@ class Parte extends Model
     /* FIXME: cambiar nombre */
     public function scopeSearch2($query, $request)
     {
-        if($request != "") {
-            $query->where('number','LIKE','%'.$request.'%')
-                  ->orWhere('origin','LIKE','%'.$request.'%');
+        if ($request != "") {
+            $query->where('number', 'LIKE', '%' . $request . '%')
+                ->orWhere('origin', 'LIKE', '%' . $request . '%');
         }
 
         return $query;
     }
-
 }
