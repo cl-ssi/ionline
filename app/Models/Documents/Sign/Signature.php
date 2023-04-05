e1821e097838bbdc30be17ce096fe4afbed93ad9
rafael1delgado <delgadorafael2011@gmail.com>
2023-04-05 00:02:26 -0400

    feat: add static functions

--- app/Models/Documents/Sign/Signature.php
+++ app/Models/Documents/Sign/Signature.php
@@ -98,4 +98,87 @@ class Signature extends Model
     {
         return $this->belongsTo(OrganizationalUnit::class, 'uo_id');
     }
+
+    public function makePayload($purpose, $run)
+    {
+        $entity = 'Subsecretaría General de La Presidencia';
+
+        $payload = [
+            "purpose" => $purpose,
+            "entity" => $entity,
+            "expiration" => now()->add(30, 'minutes')->format('Y-m-d\TH:i:s'),
+            "run" => $run
+        ];
+
+        return $payload;
+    }
+
+    public static function getUrl($modo)
+    {
+        switch ($modo)
+        {
+            case 0: 
+                $url = 'https://api.firma.test.digital.gob.cl/firma/v2/files/tickets';
+                break;
+            case 1:
+                $url = 'https://api.firma.test.digital.gob.cl/firma/v2/files/tickets';
+                break;
+            case 2:
+                $url = env('FIRMA_URL');
+                break;
+            case 3:
+                $url = env('FIRMA_URL');
+                break;
+            default:
+                $url = null;
+                break;
+        }
+        return $url;
+    }
+
+    public function getEntity($modo)
+    {
+        switch ($modo)
+        {
+            case 0:
+                $entity = 'Subsecretaría General de La Presidencia';
+                break;
+            case 1:
+                $entity = 'Subsecretaría General de La Presidencia';
+                break;
+            case 2:
+                $entity = 'Servicio de Salud Iquique';
+                break;
+            case 3:
+                $entity = 'Servicio de Salud Iquique';
+                break;
+            default:
+                $entity = null;
+                break;
+        }
+        return $entity;
+    }
+
+    public function getPurpose($modo)
+    {
+        switch ($modo)
+        {
+            case 0:
+                $purpose = 'Desatendido';
+                break;
+            case 1:
+                $purpose = 'Propósito General';
+                break;
+            case 2:
+                $purpose = 'Propósito General';
+                break;
+            case 3:
+                $purpose = 'Desatendido';
+                break;
+            default:
+                $purpose = null;
+                break;
+        }
+        return $purpose;
+    }
 }
