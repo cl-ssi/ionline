fddf04762be74435258c4296ceaaa8a24f43f55e
Alvaro Lupa <alvaro.lupa@gmail.com>
2023-04-10 17:44:12 -0400

    [Pro] Se visualiza todas las observaciones considerando las actividades indirectas no programadas, actividades pendientes solo acceso al SSI y no la perfil Comunal, otras mejoras

--- resources/views/programmings/reports/reportObservation.blade.php
+++ resources/views/programmings/reports/reportObservation.blade.php
@@ -14,13 +14,14 @@
                     <span class="small">Volver</span> 
     </a> Informe de Observaciones en Programación Númerica</h4>
 
-<!-- {{$reviewItems}} -->
+<h6><i>Total observaciones: {{$pendingItems->count() + $reviewItems->count()}}</i></h6>
 <table id="tblData" class="table table-striped  table-sm table-bordered table-condensed fixed_headers table-hover  ">
     <thead>
         <tr style="font-size:75%;">
             <th class="text-center align-middle" colspan="7">INFORME DE OBSERVACIONES </th>
         </tr>
         <tr class="small " style="font-size:60%;">
+            <th class="text-center align-middle">#</th>
             <th class="text-center align-middle">Nº REV.</th>
             <th class="text-center align-middle">TRAZADORA</th>
             <th class="text-center align-middle">PRESTACION O ACTIVIDAD</th>
@@ -35,8 +36,10 @@
         </tr>
     </thead>
     <tbody style="font-size:70%;">
+    @php($key = 1)
     @foreach($pendingItems as $pendingItem)
         <tr class="small">
+            <td class="text-center align-middle">{{$key}}</td>
             <td class="text-center align-middle"></td>
             <td class="text-center align-middle">{{ $pendingItem->activityItem->int_code ?? '' }}</td>
             <td class="text-center align-middle">{{ $pendingItem->activityItem->activity_name ?? '' }}</td>
@@ -49,9 +52,11 @@
             <td class="text-center align-middle">{{ $pendingItem->requestedBy->fullName ?? '' }}</td>
             <td class="text-center align-middle">@if($pendingItem->programming->status == 'active')<a href="{{ route('programmingitems.create', ['programming_id' => $pendingItem->programming_id, 'activity_search_id' => $pendingItem->activity_item_id]) }}" class="btn btb-flat btn-sm btn-light" title="Agregar item a la programación"><i class="fas fa-plus"></i></a>@endif</td>
         </tr>
+    @php($key++)
     @endforeach
     @foreach($reviewItems as $reviewItem)
         <tr class="small">
+            <td class="text-center align-middle">{{ $key }}</td>
             <td class="text-center align-middle">{{ $reviewItem->id }}</td>
             <td class="text-center align-middle">{{ $reviewItem->programItem->activityItem->int_code ?? '' }}</td>
             <td class="text-center align-middle">{{ $reviewItem->programItem->activityItem->activity_name ?? '' }}</td>
@@ -63,9 +68,10 @@
             <td class="text-center align-middle">{{ $reviewItem->observation }}</td>
             <td class="text-center align-middle">{{ $reviewItem->user->fullName ?? '' }}</td>
             @can('ProgrammingItem: evaluate')
-            <td class="text-center align-middle" ><a href="{{ route('reviewItems.index', ['programmingItem_id' => $reviewItem->programItem->id]) }}" class="btn btb-flat btn-sm btn-light"><i class="fas fa-clipboard-check"></i></a></td>
+            @if($pendingItem->programming->status == 'active')<td class="text-center align-middle" ><a href="{{ route('reviewItems.index', ['programmingItem_id' => $reviewItem->programItem->id]) }}" class="btn btb-flat btn-sm btn-light"><i class="fas fa-clipboard-check"></i></a>@endif</td>
             @endcan
         </tr>
+    @php($key++)
     @endforeach
     </tbody>
 </table>
