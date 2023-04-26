2f0fe2df7d6e0636b09f89e9e3535c3da0e54070
AquaroTorres <alvarotorres@gmail.com>
2023-04-26 13:17:47 -0300

    Cloud Task

--- config/queue.php
+++ config/queue.php
@@ -67,6 +67,19 @@ return [
             'block_for' => null,
         ],
 
+        'cloudtasks' => [
+            'driver' => 'cloudtasks',
+            'project' => env('STACKKIT_CLOUD_TASKS_PROJECT', ''),
+            'location' => env('STACKKIT_CLOUD_TASKS_LOCATION', ''),
+            'handler' => env('STACKKIT_CLOUD_TASKS_HANDLER', ''),
+            'queue' => env('STACKKIT_CLOUD_TASKS_QUEUE', 'default'),
+            'service_account_email' => env('STACKKIT_CLOUD_TASKS_SERVICE_EMAIL', ''),
+            // Optional: The deadline in seconds for requests sent to the worker. If the worker
+            // does not respond by this deadline then the request is cancelled and the attempt
+            // is marked as a DEADLINE_EXCEEDED failure.
+            'dispatch_deadline' => null,
+        ],
+
     ],
 
     /*
