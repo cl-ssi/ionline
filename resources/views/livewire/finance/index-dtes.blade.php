dad18c0ba2b191c8fcbdaecc0fffa549781c6fb2
AquaroTorres <alvarotorres@gmail.com>
2023-05-03 22:25:02 -0300

    DTES submenu

--- resources/views/livewire/finance/index-dtes.blade.php
+++ resources/views/livewire/finance/index-dtes.blade.php
@@ -1,21 +1,30 @@
 <div>
 
+    <ul class="nav nav-tabs mb-3">
+        <li class="nav-item">
+            <a class="nav-link active" href="{{ route('finance.dtes.index') }}">Ver dtes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('finance.dtes.upload') }}">Cargar archivo</a>
+        </li>
+    </ul>
+
+    
+    <h3 class="mb-3">Listado de dtes cargadas en sistema</h3>
+
     <div class="row">
-        <div class="col">
+        <div class="col-md-3">
             <div class="input-group mb-3">
-            <input type="text" class="form-control" wire:model.defer="filter.folio">
+            <input type="text" class="form-control" wire:model.defer="filter.folio" placeholder="folio">
             <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="button" wire:click="render()">Buscar</button>
+                <button class="btn btn-outline-secondary" type="button" wire:click="render()"> <i class="fas fa-search"></i> Buscar</button>
             </div>
         </div>
         </div>
         <div class="col-md-2">
-            <a href="{{ route('finance.dtes.upload') }}" class="btn btn-primary">Cargar archivo</a>
+           
         </div>
     </div>
-
-
-
 
     <table class="table table-sm table-bordered">
         <thead>
@@ -23,13 +32,7 @@
                 <th>Tipo documento</th>
                 <th>Folio</th>
                 <th>Emisor</th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
+                <th>Detalle</th>
             </tr>
         </thead>
         <tbody>
@@ -38,13 +41,16 @@
                 <td>{{ $dte->tipo_documento }}</td>
                 <td>{{ $dte->folio }}</td>
                 <td>{{ $dte->emisor }}</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td>
+                    <button class="btn btn-outline-secondary" type="button" data-toggle="collapse" data-target="#collapse{{$dte->id}}" aria-expanded="false" aria-controls="collapse{{$dte->id}}">
+                        Ver detalle
+                    </button>
+                    <div class="collapse width" id="collapse{{$dte->id}}">
+                        <pre>
+                            {{ print_r($dte->toArray()) }}
+                        </pre>
+                    </div>
+                </td>
             </tr>
             @endforeach
         </tbody>
