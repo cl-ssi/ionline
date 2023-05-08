a1c27c9430571652893b836be46a8cf6b466ce63
sick <esteban.rojasg@redsalud.cl>
2023-05-08 12:27:46 -0400

    fix para que aparezcan solitudes de honorarios a la creadora

--- app/Http/Controllers/ServiceRequests/ServiceRequestController.php
+++ app/Http/Controllers/ServiceRequests/ServiceRequestController.php
@@ -178,7 +178,7 @@ class ServiceRequestController extends Controller
     $serviceRequests = ServiceRequest::whereHas("SignatureFlows", function($subQuery) use($user_id){
         $subQuery->whereNull('status');
         $subQuery->where('responsable_id', $user_id);
-      $subQuery->orwhere('user_id', $user_id);
+        $subQuery->orwhere('user_id', $user_id);
     })
     ->wheredoesnthave("SignatureFlows", function($subQuery) {
         $subQuery->where('status',0);
@@ -196,7 +196,7 @@ class ServiceRequestController extends Controller
         if ($serviceRequest->SignatureFlows->where('status', '===', 0)->count() == 0) {
             foreach ($serviceRequest->SignatureFlows->sortBy('sign_position') as $key2 => $signatureFlow) {
                 //with responsable_id
-                if ($user_id == $signatureFlow->responsable_id) {
+                if ($user_id == $signatureFlow->responsable_id || $user_id == $signatureFlow->user_id) {
                     if ($signatureFlow->status == NULL) {
                         if ($serviceRequest->SignatureFlows->where('status', '!=', 2)->where('sign_position', $signatureFlow->sign_position - 1)->first()) {
                             if ($serviceRequest->SignatureFlows->where('status', '!=', 2)->where('sign_position', $signatureFlow->sign_position - 1)->first()->status == NULL) {
