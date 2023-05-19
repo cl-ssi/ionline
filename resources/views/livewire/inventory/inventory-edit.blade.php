5b0351a516f48f26f8969499f48b7cc34cc19ec4
AquaroTorres <alvarotorres@gmail.com>
2023-05-19 14:53:00 -0300

    Inventario re orden de edición

--- resources/views/livewire/inventory/inventory-edit.blade.php
+++ resources/views/livewire/inventory/inventory-edit.blade.php
@@ -85,7 +85,86 @@
             @enderror
         </fieldset>
 
-        <fieldset class="col-md-1">
+        <fieldset class="col-md-3">
+            <label for="brand" class="form-label">
+                Marca
+            </label>
+            <input
+                type="text"
+                class="form-control @error('brand') is-invalid @enderror"
+                id="brand"
+                wire:model.debounce.1500ms="brand"
+            >
+            @error('brand')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </fieldset>
+
+        <fieldset class="col-md-2">
+            <label for="model" class="form-label">
+                Modelo
+            </label>
+            <input
+                type="text"
+                class="form-control @error('model') is-invalid @enderror"
+                id="model"
+                wire:model.debounce.1500ms="model"
+            >
+            @error('model')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </fieldset>
+
+        <fieldset class="col-md-3">
+            <label for="serial-number" class="form-label">
+                Número de Serie
+            </label>
+            <input
+                type="text"
+                class="form-control @error('serial_number') is-invalid @enderror"
+                id="serial-number"
+                wire:model.debounce.1500ms="serial_number"
+            >
+            @error('serial_number')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </fieldset>
+
+        <fieldset class="col-md-2">
+            <label for="status" class="form-label">
+                Estado
+            </label>
+            <select
+                class="form-control @error('status') is-invalid @enderror"
+                id="status"
+                wire:model.debounce.1500ms="status"
+                >
+                <option value="">Seleccione un estado</option>
+                <option value="1">Bueno</option>
+                <option value="0">Regular</option>
+                <option value="-1">Malo</option>
+            </select>
+            @error('status')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </fieldset>
+        
+        
+
+    </div>
+
+    <div class="form-row mb-3">
+
+
+        <fieldset class="col-md-2">
             <label for="useful-life" class="form-label">
                 Vida útil
             </label>
@@ -96,27 +175,6 @@
                 wire:model.debounce.1500ms="useful_life"
             >
             @error('useful_life')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </fieldset>
-
-        <fieldset class="col-md-3">
-            <label for="status" class="form-label">
-                Estado
-            </label>
-            <select
-                class="form-control @error('status') is-invalid @enderror"
-                id="status"
-                wire:model.debounce.1500ms="status"
-            >
-                <option value="">Seleccione un estado</option>
-                <option value="1">Bueno</option>
-                <option value="0">Regular</option>
-                <option value="-1">Malo</option>
-            </select>
-            @error('status')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
@@ -140,59 +198,35 @@
             @enderror
         </fieldset>
 
-        <fieldset class="col-md-3">
-            <label for="serial-number" class="form-label">
-                Número de Serie
+        <fieldset class="col-md-2">
+            <label for="cost-center" class="form-label">
+                Cuenta contable
             </label>
             <input
                 type="text"
-                class="form-control @error('serial_number') is-invalid @enderror"
-                id="serial-number"
-                wire:model.debounce.1500ms="serial_number"
+                class="form-control"
+                id="cost-center"
+                value="{{ $inventory->accounting_code_id }}"
             >
-            @error('serial_number')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
         </fieldset>
     </div>
 
     <div class="form-row mb-3">
-        <fieldset class="col-md-3">
-            <label for="brand" class="form-label">
-                Marca
-            </label>
-            <input
-                type="text"
-                class="form-control @error('brand') is-invalid @enderror"
-                id="brand"
-                wire:model.debounce.1500ms="brand"
-            >
-            @error('brand')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </fieldset>
-
-        <fieldset class="col-md-3">
-            <label for="model" class="form-label">
-                Modelo
-            </label>
-            <input
-                type="text"
-                class="form-control @error('model') is-invalid @enderror"
-                id="model"
-                wire:model.debounce.1500ms="model"
-            >
-            @error('model')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </fieldset>
-
+        @if($inventory->control && $inventory->control->requestForm)
+            <fieldset class="col-md-3">
+                <label for="date-reception" class="form-label">
+                    Formulario Requerimiento
+                </label>
+                <br>
+                <a
+                    class="btn btn-primary btn-block"
+                    href="{{ route('request_forms.show', $inventory->control->requestForm) }}"
+                    target="_blank"
+                >
+                    <i class="fas fa-file-alt"></i> #{{ $inventory->control->requestForm->id }}
+                </a>
+            </fieldset>
+        @endif
 
         @if($inventory->po_id)
             <fieldset class="col-md-2">
@@ -211,7 +245,7 @@
 
             <fieldset class="col-md-2">
                 <label for="date-oc" class="form-label">
-                    Fecha Compra OC
+                    Fecha OC
                 </label>
                 <input
                     type="text"
@@ -225,7 +259,7 @@
 
             <fieldset class="col-md-2">
                 <label for="value-oc" class="form-label">
-                    Valor OC
+                    Valor
                 </label>
                 <input
                     type="text"
@@ -237,66 +271,30 @@
                 >
             </fieldset>
         @endif
-
-        <fieldset class="col-md-1">
-            <label for="subtitle" class="form-label">
-                Subtítulo
-            </label>
-            <input
-                type="text"
-                class="form-control"
-                id="subtitle"
-                value="{{ $inventory->subtitle }}"
-                disabled
-                readonly
-            >
-        </fieldset>
-
-        <fieldset class="col-md-2">
-            <label for="cost-center" class="form-label">
-                Centro Costo
-            </label>
-            <input
-                type="text"
-                class="form-control"
-                id="cost-center"
-                value="???"
-                disabled
-                readonly
-            >
-        </fieldset>
-
-        @if($inventory->control)
-            <fieldset class="col-md-2">
-                <label for="date-reception" class="form-label">
-                    Ingreso bodega
+        
+        @if($inventory->control && $inventory->control->requestForm)
+            <fieldset class="col-md-3">
+                <label for="financing" class="form-label">
+                    Programa
                 </label>
                 <input
                     type="text"
                     class="form-control"
-                    id="date-reception"
-                    value="{{ $inventory->control->date->format('Y-m-d') }}"
+                    id="financing"
+                    value="{{ $inventory->control->requestForm->associateProgram->name }}"
                     disabled
-                    readonly
                 >
             </fieldset>
         @endif
 
-        <fieldset class="col-md-3">
-            <label for="financing" class="form-label">
-                Financiamiento
-            </label>
-            <input
-                type="text"
-                class="form-control"
-                id="financing"
-                value="Donación o Compra"
-                disabled
-            >
-        </fieldset>
+    </div>
+
+
+    <div class="form-row mb-3">
+
 
         @if($inventory->control)
-            <fieldset class="col-md-4">
+            <fieldset class="col-md-5">
                 <label for="supplier" class="form-label">
                     @if($inventory->control->isPurchaseOrder())
                         Proveedor
@@ -318,6 +316,19 @@
             </fieldset>
         @endif
 
+        <fieldset class="col-md-2">
+            <label for="dte-number" class="form-label">
+                Número Factura
+            </label>
+            <input
+                type="text"
+                class="form-control"
+                id="dte-number"
+                value="{{ $inventory->dte_number }}"
+    
+            >
+        </fieldset>
+
         @if($inventory->control && $inventory->control->invoice_url)
             <fieldset class="col-md-2">
                 <label for="date-reception" class="form-label">
@@ -334,22 +345,23 @@
             </fieldset>
         @endif
 
-        @if($inventory->control && $inventory->control->requestForm)
-            <fieldset class="col-md-3">
+        @if($inventory->control)
+            <fieldset class="col-md-2">
                 <label for="date-reception" class="form-label">
-                    Formulario Requerimiento
+                    Ingreso bodega
                 </label>
-                <br>
-                <a
-                    class="btn btn-primary btn-block"
-                    href="{{ route('request_forms.show', $inventory->control->requestForm) }}"
-                    target="_blank"
+                <input
+                    type="text"
+                    class="form-control"
+                    id="date-reception"
+                    value="{{ $inventory->control->date->format('Y-m-d') }}"
+                    disabled
+                    readonly
                 >
-                    <i class="fas fa-file-alt"></i> #{{ $inventory->control->requestForm->id }}
-                </a>
             </fieldset>
         @endif
     </div>
+
 
     <div class="form-row mb-3">
         <fieldset class="col-md-12">
@@ -395,10 +407,10 @@
             <h5 class="mt-3">Historial del ítem</h5>
             @livewire('inventory.movement-index', ['inventory' => $inventory])
         </div>
-        @if($inventory->control)
+        @if($inventory->po_code)
             <div class="col">
-                <h5 class="mt-3">Facturas</h5>
-                @livewire('warehouse.invoices.list-invoices', ['control' => $inventory->control])
+                <h5 class="mt-3">Facturas relacionadas con la OC</h5>
+                @livewire('warehouse.invoices.list-invoices', ['inventory' => $inventory])
             </div>
         @endif
     </div>
