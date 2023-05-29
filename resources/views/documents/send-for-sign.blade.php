b6036570c7f69dabc46e9e01470721b33ea078aa
rafael1delgado <delgadorafael2011@gmail.com>
2023-05-28 21:34:52 -0400

    feat: add send-for-sign view

---
+++ resources/views/documents/send-for-sign.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Nuevo documento')
+
+@section('content')
+
+@include('documents.partials.nav')
+
+@livewire('sign.request-signature', [
+    'document' => $document
+])
+
+@endsection
