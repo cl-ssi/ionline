be5fcd4c778d48fbe34736c6031fa4d8fedea74b
mirandal <mirandal.jorge@gmail.com>
2023-05-30 17:29:04 -0400

    Ind: Metas Ley N° 18.834

--- resources/views/indicators/19664/partials/card.blade.php
+++ resources/views/indicators/19664/partials/card.blade.php
@@ -3,9 +3,10 @@
         <strong>Metas Sanitarias Ley N° 19.664</strong>
     </div>
     <ul class="list-group list-group-flush">
+        <li class="list-group-item"><a href="{{ route('indicators.health_goals.list', [19664, 2023]) }}">2023 <span class="badge badge-secondary">Próximamente</span></a></li>
         <li class="list-group-item"><a href="{{ route('indicators.health_goals.list', [19664, 2022]) }}">2022</a></li>
         <li class="list-group-item"><a href="{{ route('indicators.health_goals.list', [19664, 2021]) }}">2021</a></li>
-        <li class="list-group-item"><a href="{{ route('indicators.19664.2020.index') }}">2020</a></li>
+        <!-- <li class="list-group-item"><a href="{{ route('indicators.19664.2020.index') }}">2020</a></li> -->
         <!-- <li class="list-group-item"><a href="{{ route('indicators.19664.2019.index') }}">2019</a></li> -->
         <li class="list-group-item">
             <a href="{{ route('indicators.19664.index') }}">Años anteriores</a>
