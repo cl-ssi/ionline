a2bc367faafebe74c26621a85ee24f11a6bd91f9
AquaroTorres <alvarotorres@gmail.com>
2023-06-01 01:01:59 -0300

    Fix siremx

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -217,7 +217,7 @@
                         <a href="https://esmeralda.saludtarapaca.gob.cl" class="btn btn-outline-info btn-block">Esmeralda</a>
                     </li>
                     <li class="list-group-item">
-                        <a href="{{ route('claveunica.autenticar') }}?L3NpcmVteC9sb2dpbmN1" class="btn btn-outline-info btn-block">Siremx</a>
+                        <a href="{{ route('claveunica.autenticar') }}/L3NpcmVteC9sb2dpbmN1" class="btn btn-outline-info btn-block">Siremx</a>
                     </li>
                 </ul>
             </div>
