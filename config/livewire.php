14f7a32ab52f09fec4082e555752aa11bb1287f0
Alvaro Lupa <alvaro.lupa@gmail.com>
2023-06-07 12:53:14 -0400

    Livewire upload config updated

--- config/livewire.php
+++ config/livewire.php
@@ -96,9 +96,9 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => null,        // Example: 'local', 's3'              Default: 'default'
+        'disk' => env('TMP_FILE_UPLOAD_DISK', null),        // Example: 'local', 's3'              Default: 'default'
         'rules' => null,       // Example: ['file', 'mimes:png,jpg']  Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => null,   // Example: 'tmp'                      Default  'livewire-tmp'
+        'directory' => env('TMP_FILE_UPLOAD_DIRECTORY', null),   // Example: 'tmp'                      Default  'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs.
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
