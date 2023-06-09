f4f293848742fd4ac9c80021369b52ec58615b00
rafael1delgado <delgadorafael2011@gmail.com>
2023-06-09 09:00:47 -0400

    feat: add functions and attributes

--- app/Models/Documents/Sign/SignatureAnnex.php
+++ app/Models/Documents/Sign/SignatureAnnex.php
@@ -5,6 +5,7 @@ namespace App\Models\Documents\Sign;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class SignatureAnnex extends Model
 {
@@ -32,5 +33,26 @@ class SignatureAnnex extends Model
     public function signature()
     {
         return $this->belongsTo(Signature::class);
+    }
+
+    public function isFile()
+    {
+        return $this->type == 'file';
+    }
+
+    public function isLink()
+    {
+        return $this->type == 'link';
+    }
+
+    public function getLinkFileAttribute()
+    {
+        $link = null;
+        if($this->isFile() && Storage::disk('gcs')->exists($this->file))
+        {
+            $link = Storage::disk('gcs')->url($this->file);
+        }
+
+        return $link;
     }
 }
