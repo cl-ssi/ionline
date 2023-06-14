45231d41ea55c509daa054588885a67497594e75
Esteban Miranda <tebiccr@gmail.com>
2023-06-13 22:15:48 -0400

    mejor nombre migracion

--- database/migrations/2023_06_13_205732_create_sum_summaries_table.php
+++ database/migrations/2023_06_13_205732_create_sum_summaries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSummariesTable extends Migration
+class CreateSumSummariesTable extends Migration
 {
     /**
      * Run the migrations.
