82c65471c1417a07e48bfa41c6e7a0d7882fee93
AquaroTorres <alvarotorres@gmail.com>
2023-06-15 12:47:14 -0300

    Maqueta solicitud AmiPass Fixes #780

---
+++ app/Http/Livewire/Welfare/AmiPass/NewBeneficiaryRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Welfare\AmiPass;
+
+use Livewire\Component;
+
+class NewBeneficiaryRequest extends Component
+{
+    public function render()
+    {
+        return view('livewire.welfare.ami-pass.new-beneficiary-request');
+    }
+}
