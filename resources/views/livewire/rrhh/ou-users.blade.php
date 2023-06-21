ee5170a56fed59f35e420aa9773d7e3f3d8c664a
Esteban Miranda <tebiccr@gmail.com>
2023-06-20 20:22:33 -0400

    con seleccion  de establecimiento y crear firma

---
+++ resources/views/livewire/rrhh/ou-users.blade.php
@@ -0,0 +1,18 @@
+<div>
+    {{-- The best athlete wants his opponent at his best. --}}
+    <select class="form-control" name="user_signer" required>
+        <option value="" > Seleccionar Usuario</option>
+        @foreach ($users as $user)
+            <option value="{{ $user->id }}"
+                @if ($authority_id == $user->id)
+                    selected
+                @endif
+                >
+                @if ($authority_id == $user->id)
+                    👑
+                @endif
+                {{ $user->shortName ?? '' }}
+            </option>
+        @endforeach
+    </select>
+</div>
