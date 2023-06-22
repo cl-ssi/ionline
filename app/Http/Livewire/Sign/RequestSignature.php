d2e8033b025fe3c2bb4a04614025edd752787fd4
AquaroTorres <alvarotorres@gmail.com>
2023-06-22 14:00:52 -0300

    Firmas 2.0 comentar tipo

--- app/Http/Livewire/Sign/RequestSignature.php
+++ app/Http/Livewire/Sign/RequestSignature.php
@@ -144,27 +144,27 @@ class RequestSignature extends Component
 
         $myColumns = $this->myColumns->keys();
 
-        if($myColumns->count() == 0)
-        {
-            session()->flash('danger', 'El Tipo de Firma es un campo obligatorio.');
-            return;
-        }
+        // if($myColumns->count() == 0)
+        // {
+        //     session()->flash('danger', 'El Tipo de Firma es un campo obligatorio.');
+        //     return;
+        // }
 
-        if($myColumns->contains('left') && $this->column_left_endorse == null)
-        {
-            session()->flash('danger', "El Tipo de Firma en la columna izquierda es obligatorio.");
-            return;
-        }
-        elseif($myColumns->contains('center') && $this->column_center_endorse == null)
-        {
-            session()->flash('danger', "El Tipo de Firma en la columna central es obligatorio.");
-            return;
-        }
-        elseif($myColumns->contains('right') && $this->column_right_endorse == null)
-        {
-            session()->flash('danger', "El Tipo de Firma en la columna derecha es obligatorio.");
-            return;
-        }
+        // if($myColumns->contains('left') && $this->column_left_endorse == null)
+        // {
+        //     session()->flash('danger', "El Tipo de Firma en la columna izquierda es obligatorio.");
+        //     return;
+        // }
+        // elseif($myColumns->contains('center') && $this->column_center_endorse == null)
+        // {
+        //     session()->flash('danger', "El Tipo de Firma en la columna central es obligatorio.");
+        //     return;
+        // }
+        // elseif($myColumns->contains('right') && $this->column_right_endorse == null)
+        // {
+        //     session()->flash('danger', "El Tipo de Firma en la columna derecha es obligatorio.");
+        //     return;
+        // }
 
         /**
          * Map the recipients
