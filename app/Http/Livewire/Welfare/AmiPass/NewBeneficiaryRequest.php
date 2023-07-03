c65a3f0b54a85b66e195764e1c7befd9f3549e94
Esteban Miranda <tebiccr@gmail.com>
2023-07-03 12:39:15 -0400

    save() solicitud amipass

--- app/Http/Livewire/Welfare/AmiPass/NewBeneficiaryRequest.php
+++ app/Http/Livewire/Welfare/AmiPass/NewBeneficiaryRequest.php
@@ -4,16 +4,37 @@ namespace App\Http\Livewire\Welfare\AmiPass;
 
 use Livewire\Component;
 use App\Models\Establishment;
+use App\Models\Welfare\AmiPass\BeneficiaryRequest;
 
 class NewBeneficiaryRequest extends Component
 {
 
     public $jefatura = '';
     public $correoElectronico = '';
-    public $cargoUnidad = '';    
+    public $cargoUnidad = '';
     public $selectedOption = '';
     public $establecimientos;
     public $selectedEstablishmentId = '';
+
+
+    //Campos del BeneficiaryRequest       
+    public $motivoRequerimiento = '';
+    public $nombreFuncionarioReemplazar = '';
+    public $nombreCompleto = '';
+    public $rutFuncionario = '';
+    public $dondeCumpliraFunciones = '';
+    public $correoPersonal = '';
+    public $celular = '';
+    public $fechaInicioContrato = '';
+    public $fechaTerminoContrato = '';
+    public $jornadaLaboral = '';
+    public $residencia = '';
+    public $haUtilizadoAmipass = '';
+    public $fechaNacimiento = '';
+    public $establecimiento = '';
+    public $estado = '';
+    public $amiManagerId = '';
+    public $amiManagerAt = '';
 
 
     public function render()
@@ -36,13 +57,41 @@ class NewBeneficiaryRequest extends Component
         $this->establecimientos = Establishment::whereIn('id', $establishments_ids)->orderBy('official_name')->get();
     }
 
-
-
     public function resetFields()
     {
         $this->jefatura = '';
         $this->correoElectronico = '';
         $this->cargoUnidad = '';
         $this->selectedEstablishmentId = '';
+    }
+
+
+    public function save()
+    {
+        // Aquí puedes realizar la lógica necesaria para guardar los datos en el store
+
+        // Por ejemplo, puedes utilizar el método `create` del modelo correspondiente:
+        BeneficiaryRequest::create([
+            'nombre_jefatura' => $this->jefatura,
+            'cargo_unidad_departamento' => $this->cargoUnidad,
+            'correo_jefatura' => $this->correoElectronico,
+            'establecimiento' => $this->selectedEstablishmentId,
+            'motivo_requerimiento' => $this->motivoRequerimiento,
+            'nombre_funcionario_reemplazar' => $this->nombreFuncionarioReemplazar,
+            'nombre_completo' => $this->nombreCompleto,
+            'rut_funcionario' => $this->rutFuncionario,
+            'fecha_nacimiento' => $this->fechaNacimiento,
+            'correo_personal' => $this->correoPersonal,
+            'celular' => $this->celular,
+            'fecha_inicio_contrato' => $this->fechaInicioContrato,
+            'fecha_termino_contrato' => $this->fechaTerminoContrato,
+            'donde_cumplira_funciones' => $this->dondeCumpliraFunciones,
+            'jornada_laboral' => $this->jornadaLaboral,
+            'residencia' => $this->residencia,
+            'ha_utilizado_amipass' => $this->haUtilizadoAmipass,
+            
+        ]);
+
+        
     }
 }
--- resources/views/livewire/welfare/ami-pass/new-beneficiary-request.blade.php
+++ resources/views/livewire/welfare/ami-pass/new-beneficiary-request.blade.php
@@ -31,9 +31,9 @@
 
             <div class="form-row">
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1"
+                    <input class="form-check-input" type="radio" name="exampleRadios2" id="exampleRadios2"
                         value="yo" wire:model="selectedOption">
-                    <label class="form-check-label" for="exampleRadios1">
+                    <label class="form-check-label" for="exampleRadios2">
                         Yo:
                     </label>
                 </div>
@@ -46,9 +46,9 @@
 
             <div class="form-row">
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1"
+                    <input class="form-check-input" type="radio" name="exampleRadios2" id="exampleRadios2"
                         value="otro" wire:model="selectedOption">
-                    <label class="form-check-label" for="exampleRadios1">
+                    <label class="form-check-label" for="exampleRadios2">
                         Otro:
                     </label>
                 </div>
@@ -99,63 +99,63 @@
 
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1"
-                                value="option1">
+                                value="Suplencia o Reemplazo por Renuncia" wire:model="motivoRequerimiento">
                             <label class="form-check-label" for="exampleRadios1">
                                 Suplencia o Reemplazo por Renuncia
                             </label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2"
-                                value="option2">
+                                value="Suplencia o Reemplazo por Licencia Medica" wire:model="motivoRequerimiento">
                             <label class="form-check-label" for="exampleRadios2">
                                 Suplencia o Reemplazo por Licencia Medica
                             </label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3"
-                                value="option3">
+                                value="Suplencia o Reemplazo por Permiso sin Goce de Sueldo" wire:model="motivoRequerimiento">
                             <label class="form-check-label" for="exampleRadios3">
                                 Suplencia o Reemplazo por Permiso sin Goce de Sueldo
                             </label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3"
-                                value="option3">
+                                value="Suplencia o reemplazo por Feriado Legal" wire:model="motivoRequerimiento">
                             <label class="form-check-label" for="exampleRadios3">
                                 Suplencia o reemplazo por Feriado Legal
                             </label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3"
-                                value="option3">
+                                value="Creacion de Cargo" wire:model="motivoRequerimiento">
                             <label class="form-check-label" for="exampleRadios3">
                                 Creacion de Cargo
                             </label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3"
-                                value="option3">
+                                value="Continuidad" wire:model="motivoRequerimiento">
                             <label class="form-check-label" for="exampleRadios3">
                                 Continuidad
                             </label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3"
-                                value="option3">
+                                value="Contrato transitorio" wire:model="motivoRequerimiento">
                             <label class="form-check-label" for="exampleRadios3">
                                 Contrato transitorio
                             </label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3"
-                                value="option3">
+                                value="Ingreso de Funcionario en Comisión de Servicios en la DSST" wire:model="motivoRequerimiento">
                             <label class="form-check-label" for="exampleRadios3">
                                 Ingreso de Funcionario en Comisión de Servicios en la DSST
                             </label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3"
-                                value="option3">
+                                value="Otro" wire:model="motivoRequerimiento">
                             <label class="form-check-label" for="exampleRadios3">
                                 Otro
                             </label>
@@ -166,7 +166,7 @@
                 </div>
                 <div class="form-group col-6">
                     <label for="text3">Nombre del funcionario/a A REEMPLAZAR, según motivo del requerimiento</label>
-                    <input id="text3" name="text3" type="text" class="form-control">
+                    <input id="text3" name="text3" type="text" class="form-control" wire:model="nombreFuncionarioReemplazar">
                     <small id="emailHelp" class="form-text text-muted">Si no es una suplencia o reemplazo, indique NO
                         CORRESPONDE</small>
                 </div>
@@ -191,46 +191,46 @@
                 <div class="form-group col-3">
                     <label for="text5">RUT de Funcionario/a*</label>
                     <input id="text5" name="text5" type="text" class="form-control"
-                        placeholder="Ejemplo: 12345678-9">
+                        placeholder="Ejemplo: 12345678-9" wire:model="rutFuncionario">
                     <small id="emailHelp" class="form-text text-muted">SIN PUNTOS Y CON GUION</small>
                 </div>
                 <div class="form-group col-6">
                     <label for="text6">Nombre Completo (nombres y apellidos) *</label>
-                    <input id="text6" name="text6" type="text" class="form-control">
+                    <input id="text6" name="text6" type="text" class="form-control" wire:model="nombreCompleto">
                 </div>
                 <div class="form-group col-3">
                     <label for="text9">Fecha de Nacimiento *</label>
-                    <input id="text9" name="text9" type="date" required="required" class="form-control">
+                    <input id="text9" name="text9" type="date" required="required" class="form-control" wire:model="fechaNacimiento">
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col">
                     <label for="text7">Correo electrónico "PERSONAL" *</label>
-                    <input id="text7" name="text7" type="text" class="form-control">
+                    <input id="text7" name="text7" type="text" class="form-control" wire:model="correoPersonal">
                 </div>
                 <div class="form-group col">
                     <label for="text8">Número de celular (9-12345678) *</label>
-                    <input id="text8" name="text8" type="text" required="required" class="form-control">
+                    <input id="text8" name="text8" type="text" required="required" class="form-control" wire:model="celular">
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-3">
                     <label for="text11">Fecha de inicio de contrato</label>
-                    <input id="text11" name="text11" type="date" class="form-control">
+                    <input id="text11" name="text11" type="date" class="form-control" wire:model="fechaInicioContrato">
                     <small id="emailHelp" class="form-text text-muted">Contrata, suplencia y/o reemplazo</small>
                 </div>
                 <div class="form-group col-3">
                     <label for="text12">Fecha de termino de contrato</label>
-                    <input id="text12" name="text12" type="date" class="form-control">
+                    <input id="text12" name="text12" type="date" class="form-control" wire:model="fechaTerminoContrato">
                     <small id="emailHelp" class="form-text text-muted">De acuerdo a los respaldos existentes Ej.
                         duración de Licencia Médica, suplencia, etc.</small>
                 </div>
 
                 <div class="form-group col-6">
                     <label for="text10">Donde cumplirá funciones (especificar)</label>
-                    <input id="text10" name="text10" type="text" class="form-control">
+                    <input id="text10" name="text10" type="text" class="form-control" wire:model="dondeCumpliraFunciones">
                     <small id="emailHelp" class="form-text text-muted">Unidad organizacional, Establecimiento o
                         Residencia</small>
                 </div>
@@ -242,52 +242,52 @@
                 <div>
                     <div class="custom-control custom-radio custom-control-inline">
                         <input name="radio1" id="radio1_0" type="radio" class="custom-control-input"
-                            value="rabbit">
+                            value="Diurna (44 hrs.)" wire:model="jornadaLaboral">
                         <label for="radio1_0" class="custom-control-label">Diurna (44 hrs.)</label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
                         <input name="radio1" id="radio1_1" type="radio" class="custom-control-input"
-                            value="duck">
+                            value="Diurna (33 hrs.)" wire:model="jornadaLaboral">
                         <label for="radio1_1" class="custom-control-label">Diurna (33 hrs.)</label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
                         <input name="radio1" id="radio1_2" type="radio" class="custom-control-input"
-                            value="fish">
+                            value="3° Turno" wire:model="jornadaLaboral">
                         <label for="radio1_2" class="custom-control-label">3° Turno</label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
                         <input name="radio1" id="radio1_3" type="radio" class="custom-control-input"
-                            value="">
+                            value="4° Turno" wire:model="jornadaLaboral">
                         <label for="radio1_3" class="custom-control-label">4° Turno</label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
                         <input name="radio1" id="radio1_4" type="radio" class="custom-control-input"
-                            value="">
+                            value="SAMU Periférico" wire:model="jornadaLaboral">
                         <label for="radio1_4" class="custom-control-label">SAMU Periférico</label>
                     </div>
                 </div>
             </div>
             <div class="form-group">
                 <label for="text14">Sólo Jornada en Turno: Especificar Dependencias.</label>
-                <input id="text14" name="text14" type="text" class="form-control">
+                <input id="text14" name="text14" type="text" class="form-control" wire:model="residencia">
             </div>
             <div class="form-group">
                 <label>Ha utilizado antes el beneficio en SSI</label>
                 <div>
                     <div class="custom-control custom-radio custom-control-inline">
                         <input name="radio2" id="radio2_0" type="radio" class="custom-control-input"
-                            value="rabbit">
+                            value="Sí" wire:model="haUtilizadoAmipass">
                         <label for="radio2_0" class="custom-control-label">Sí</label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
                         <input name="radio2" id="radio2_1" type="radio" class="custom-control-input"
-                            value="duck">
+                            value="No" wire:model="haUtilizadoAmipass">
                         <label for="radio2_1" class="custom-control-label">No</label>
                     </div>
                 </div>
             </div>
             <div class="form-group">
-                <button name="submit" type="submit" class="btn btn-primary">Guardar</button>
+                <button wire:click="save" type="button" class="btn btn-primary">Guardar</button>
             </div>
             </p>
         </div>
