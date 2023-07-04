17936b726c5094db984531396ad8a9cc0c7770a3
AquaroTorres <alvarotorres@gmail.com>
2023-07-04 12:40:30 -0300

    Actualizar a laravel 9 Fixes #812

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,7 +17,8 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Fruitcake\Cors\HandleCors::class,
+        /** Update Laravel 9.0 */
+        // \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
