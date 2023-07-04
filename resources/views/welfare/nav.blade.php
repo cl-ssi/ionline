38679e55323adc4a5b017889aeb296ec30418013
Esteban Miranda <tebiccr@gmail.com>
2023-07-04 17:56:51 -0400

    fix permisos

--- resources/views/welfare/nav.blade.php
+++ resources/views/welfare/nav.blade.php
@@ -32,7 +32,7 @@
         </li>
     @endcanany
 
-    @canany([, 'be god'])
+    @canany(['be god'])
         <li class="nav-item dropdown ">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
