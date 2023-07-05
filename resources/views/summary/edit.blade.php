77c31b4a96eee5ef517efe26ee184912d5dea56d
AquaroTorres <alvarotorres@gmail.com>
2023-07-05 14:02:10 -0300

    Sumario file input show file name

--- resources/views/summary/edit.blade.php
+++ resources/views/summary/edit.blade.php
@@ -21,3 +21,15 @@
     @endif
     
 @endsection
+
+@section('custom_js')
+<script type="text/javascript">
+
+    $('.custom-file input').change(function (e) {
+        if (e.target.files.length) {
+            $(this).next('.custom-file-label').html(e.target.files[0].name);
+        }
+    });
+
+</script>
+@endsection
