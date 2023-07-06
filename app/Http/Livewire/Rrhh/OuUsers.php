b6707bda2a0ff7437bb6ac38fd8ec7ab479082a6
AquaroTorres <alvarotorres@gmail.com>
2023-07-06 11:19:46 -0300

    FIx: ou-users currentManager is null

--- app/Http/Livewire/Rrhh/OuUsers.php
+++ app/Http/Livewire/Rrhh/OuUsers.php
@@ -7,22 +7,18 @@ use Livewire\Component;
 
 class OuUsers extends Component
 {
-    public $ou_id = 20;
     public $users = [];
-    public $authority_id=0;
+    public $authority_id = null;
     public $listeners = ["getOuId" => "getUsersFromOu"];
-
-
 
     public function render()
     {
-
         return view('livewire.rrhh.ou-users');
     }
 
     public function getUsersFromOu(OrganizationalUnit $ou)
     {
         $this->users = $ou->users;
-        $this->authority_id = $ou->currentManager->user_id;
+        $this->authority_id = $ou->currentManager->user_id ?? null;
     }
 }
