5262508247d2231240c42ddfd8efeb9b8b04f8ea
AquaroTorres <alvarotorres@gmail.com>
2023-07-12 18:48:11 -0300

    SR perfil lista de años con contrato

--- app/Http/Controllers/ServiceRequests/PerfilController.php
+++ app/Http/Controllers/ServiceRequests/PerfilController.php
@@ -15,6 +15,32 @@ class PerfilController extends Controller
 
         $user = User::findOrFail($run);
 
+        /** Años en que tiene contratos */
+        $yearsWithServiceRequests = ServiceRequest::where('user_id', $user->id)
+            ->distinct()
+            ->select(DB::raw('YEAR(start_date) as year'))
+            ->pluck('year')
+            ->toArray();
+
+        /** Array con rango de años entre el 2020 y el actual, todos seteados en false */
+        $yearsRange = array_fill_keys(range(2020, date('Y')), false);
+
+        /** Dejamos en true sólo aquellos que tiene contratos */
+        foreach($yearsRange as $year => $value) {
+            $yearsRange[$year] = in_array($year, $yearsWithServiceRequests) ??  false;
+        }
+
+        /**
+         * Ej: dd($yearsRange) 
+         * array:4 [
+         *   2020 => false
+         *   2021 => false
+         *   2022 => true
+         *   2023 => true
+         * ]
+         */
+
+
         $workingDayTypes = ServiceRequest::where('user_id', $run)->whereYear('request_date', $year)
             ->distinct()
             ->pluck('working_day_type');
