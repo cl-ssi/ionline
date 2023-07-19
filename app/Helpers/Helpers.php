29e9261b9e2fe509b4d400363ac4af16e5a29c15
Esteban Miranda <tebiccr@gmail.com>
2023-07-19 15:36:32 -0400

    tipos horas-mensual y helper de solapado

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -18,3 +18,29 @@ function trashed($user) {
     if($user->trashed())
         echo '<i class="fas fa-user-slash" title="Usuario eliminado"></i>';
 }
+
+function fechasSeSolapan($inicio1, $fin1, $inicio2, $fin2) {
+    // Convertir las fechas a objetos DateTime
+    $inicio1 = new DateTime($inicio1);
+    $fin1 = new DateTime($fin1);
+    $inicio2 = new DateTime($inicio2);
+    $fin2 = new DateTime($fin2);
+
+    // Verificar si se solapan
+    if ($inicio1 <= $fin2 && $fin1 >= $inicio2) {
+        return true; // Los rangos se solapan
+    } else {
+        return false; // Los rangos no se solapan
+    }
+}
+
+// Función para verificar las superposiciones en un array de fechas
+function isOverlapping($dates, $start_date, $end_date) {
+    foreach ($dates as $date) {
+        $date = new DateTime($date);
+        if ($date >= $start_date && $date <= $end_date) {
+            return true;
+        }
+    }
+    return false;
+}
--- resources/views/livewire/service-request/contracts-by-working-day-type.blade.php
+++ resources/views/livewire/service-request/contracts-by-working-day-type.blade.php
@@ -1,6 +1,7 @@
 <ul class="nav mb-3">
     @php
         $previousEndDate = null;
+        $dates = []; // Creamos un array para almacenar las fechas de inicio y fin de los contratos
     @endphp
 
     @foreach ($serviceRequests as $serviceRequest)
@@ -8,6 +9,9 @@
             $hasNotContinuity = $previousEndDate && $previousEndDate->addDay() != $serviceRequest->start_date;
             $previousEndDate = $serviceRequest->end_date;
             $isCurrentContract = $serviceRequest->start_date <= now() && now() <= $serviceRequest->end_date;
+            
+            $dates[] = $serviceRequest->start_date->format('Y-m-d');
+            $dates[] = $serviceRequest->end_date->format('Y-m-d');
         @endphp
         @if ($hasNotContinuity)
             <li>
@@ -18,7 +22,7 @@
             </li>
         @endif
         <li class="nav-item">
-            <a class="nav-link @if ($serviceRequest->id == $service_request_id) font-weight-bold @endif"
+            <a class="nav-link @if ($serviceRequest->id == $service_request_id) font-weight-bold @endif @if (isOverlapping($dates, $serviceRequest->start_date, $serviceRequest->end_date)) text-danger @endif" 
                 href="{{ route('rrhh.service-request.show', ['user' => $user_id, 'year' => $year, 'type' => $type, 'serviceRequest' => $serviceRequest]) }}">
                 <span style="font-size: 19px;"><i>id: {{ $serviceRequest->id }}</i></span><br>
                 {{ $serviceRequest->start_date->format('Y-m-d') }} <br>
