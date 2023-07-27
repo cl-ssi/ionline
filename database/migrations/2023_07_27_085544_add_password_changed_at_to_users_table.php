ff775f64c1c3f25e15df001663ec02f4ebab2f91
AquaroTorres <alvarotorres@gmail.com>
2023-07-27 11:27:03 -0300

    Middleware para Obligar a cambiar password Fixes #960

---
+++ database/migrations/2023_07_27_085544_add_password_changed_at_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->datetime('password_changed_at')->after('password')->nullable();
+        });
+
+        DB::select('UPDATE users SET password_changed_at = created_at');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->removeColumn('password_changed_at');
+        });
+    }
+};
