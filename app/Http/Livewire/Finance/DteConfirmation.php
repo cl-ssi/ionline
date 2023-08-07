52d4c6ddbf98f9d5a63cbd4389b8c747b4d82e28
rafael1delgado <delgadorafael2011@gmail.com>
2023-08-07 08:30:09 -0400

    feat: update dte component

--- app/Http/Livewire/Finance/DteConfirmation.php
+++ app/Http/Livewire/Finance/DteConfirmation.php
@@ -9,28 +9,15 @@ class DteConfirmation extends Component
 {
     public $dte;
     public $confirmation_observation;
+
     /**
-    * Mount
-    */
+     * Mount
+     */
     public function mount(Dte $dte)
     {
         $this->dte = $dte;
     }
 
-    /**
-    * Confirmation
-    */
-    public function saveConfirmation($status)
-    {
-        $this->dte->confirmation_status = $status;
-        $this->dte->confirmation_user_id = auth()->id();
-        $this->dte->confirmation_ou_id = auth()->user()->organizational_unit_id;
-        $this->dte->confirmation_observation = $this->confirmation_observation;
-        $this->dte->confirmation_at = now();
-        $this->dte->save();
-
-    }
-    
     public function render()
     {
         // $this->dte->fresh();
