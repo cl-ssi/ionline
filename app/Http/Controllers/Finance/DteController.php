c7637fde3b5f599f72c34f3775e917a83074e8f7
rafael1delgado <delgadorafael2011@gmail.com>
2023-08-07 09:50:21 -0400

    feat: add DteController

---
+++ app/Http/Controllers/Finance/DteController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Finance;
+
+use App\Http\Controllers\Controller;
+use App\Models\Finance\Dte;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class DteController extends Controller
+{
+    /**
+     * Callback after accepting a DTE
+     *
+     * @param  Dte $dte
+     * @return void
+     */
+    public function store(Dte $dte, Request $request)
+    {
+        $dte->update([
+            'confirmation_status' => 1,
+            'confirmation_user_id' => auth()->id(),
+            'confirmation_ou_id' => auth()->user()->organizational_unit_id,
+            'confirmation_observation' => '',
+            'confirmation_at' => now(),
+            'confirmation_signature_file' => $request->folder.$request->filename.'.pdf',
+        ]);
+
+        return redirect()->back()->with('success', 'El dte fue aceptado exitosamente.');
+    }
+
+    public function pdf(Dte $dte)
+    {
+        return Storage::disk('gcs')->download($dte->confirmation_signature_file);
+    }
+}
