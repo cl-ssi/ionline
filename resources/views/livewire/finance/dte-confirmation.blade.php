3a2d58012df569656171f2f96493439b1d504b78
AquaroTorres <alvarotorres@gmail.com>
2023-08-09 18:35:49 -0300

    DTE:Index:Maqueta nuevo diseño

--- resources/views/livewire/finance/dte-confirmation.blade.php
+++ resources/views/livewire/finance/dte-confirmation.blade.php
@@ -1,20 +1,18 @@
 <div>
     <h3>Estimado/a
-        {{--
-        @if ($dte->requestForm)
+        @if($dte->requestForm)
             {{ $dte->requestForm->contractManager->shortName }}
         @endif
-        --}}
     </h3>
 
     <p>
         El siguiente DTE (documento tributario electrónico) fué recepcionado en portal DIPRES Acepta.<br>
-        {{--
-        @if ($dte->requestForm)
+
+        @if($dte->requestForm)
             Está asociado al formulario de requerimiento Fólio Nº: <strong>{{ $dte->requestForm->folio }}</strong> de
             <strong>{{ $dte->requestForm->contractOrganizationalUnit->name }}</strong>. <br>
         @endif
-        --}}
+
         Solicito favor señalar aceptación o reclamo del documento:
     </p>
 
@@ -22,17 +20,14 @@
         <thead>
             <tr>
                 <th></th>
-                <th>TIPO DOCUMENTO</th>
-                <th>FOLIO</th>
+                <th>DOCUMENTO</th>
                 <th>RUT</th>
                 <th>RAZON SOCIAL EMISOR</th>
                 <th>PUBLICACION</th>
                 <th>EMISION</th>
                 <th>MONTO TOTAL</th>
-                <th>ACTAS DE INGRESOS</th>
                 <th>FECHA MÁXIMO PARA RECLAMAR</th>
                 <th>FOLIO OC</th>
-                <th>ACTA RECEPCION CONFORME</th>
             </tr>
         </thead>
         <tbody>
@@ -40,9 +35,8 @@
                 <td>
                     @switch($dte->confirmation_status)
                         @case('0')
-                            <i class="fas fa-fw fa-hand-middle-finger text-danger"></i>
-                        @break
-
+                            <i class="fas fa-fw fa-thumbs-down text-danger"></i>
+                            @break
                         @case('1')
                             <i class="fas fa-fw fa-thumbs-up text-success"></i>
                         @break
@@ -52,10 +46,13 @@
                         @break
                     @endswitch
                 </td>
-                <td>{{ $dte->tipo_documento }}</td>
                 <td>
-                    <a href="http://dipres2303.acepta.com/ca4webv3/PdfView?url={{ $dte->uri }}" target="_blank"
-                        class="btn btn-sm mb-1 btn-outline-secondary">
+                    {{ $dte->tipo_documento }}
+                    <a
+                        href="http://dipres2303.acepta.com/ca4webv3/PdfView?url={{ $dte->uri }}"
+                        target="_blank"
+                        class="btn btn-sm mb-1 btn-outline-secondary"
+                    >
                         <i class="fas fa-file-pdf text-danger"></i> {{ $dte->folio }}
                     </a>
                 </td>
@@ -65,28 +62,24 @@
                 <td>{{ $dte->emision }}</td>
                 <td class="text-right">$ {{ money($dte->monto_total) }}</td>
                 <td>
-                    @foreach ($dte->controls as $control)
-                        <a class="btn btn-sm btn-primary" href="{{ route('warehouse.control.show', $control) }}"
-                            target="_blank">
-                            #{{ $control->id }}
-                        </a>
-                    @endforeach
                 </td>
                 <td>{{ optional(optional($dte->publicacion)->add('+3 days'))->format('Y-m-d') }}</td>
                 <td>{{ $dte->folio_oc }}</td>
-                <td>
-                    @if (isset($dte->confirmation_signature_file))
-                        <a class="btn btn-sm btn-outline-danger"
-                            href="{{ route('finance.dtes.confirmation.pdf', $dte) }}"
-                            title="Acta de Recepción Conforme de Factura">
-                            <i class="fas fa-file-pdf text-danger"></i>
-                        </a>
-                    @endif
-                </td>
             </tr>
         </tbody>
     </table>
 
+    @foreach($dte->controls as $control)
+    <li>
+        <a
+            class="btn btn-sm btn-primary"
+            href="{{ route('warehouse.control.show', $control) }}"
+            target="_blank"
+            >
+            #{{ $control->id }}
+        </a>
+    </li>
+    @endforeach
 
     @if (is_null($dte->confirmation_status))
         <div class="form-group">
@@ -184,10 +177,19 @@
 
         <br>
 
-        <p>
-            En el evento de aceptar el documento tributario electrónico se solicita remitir Acta de Recepcion conforme
-            al Depto. de Abastecimiento y Logística.
-        </p>
+    <p>
+        En el evento de aceptar el documento tributario electrónico se solicita remitir Acta de Recepcion conforme al Depto. de Abastecimiento y Logística.
+    </p>
+    @else
+        @if(isset($dte->confirmation_signature_file))
+            <a
+                class="btn btn-sm btn-outline-danger"
+                href="{{ route('finance.dtes.confirmation.pdf', $dte) }}"
+                title="Acta de Recepción Conforme de Factura"
+            >
+                <i class="fas fa-file-pdf text-danger"></i>
+            </a>
+        @endif
     @endif
 
     <b>
--- resources/views/livewire/finance/dte-send-confirmation.blade.php
+++ resources/views/livewire/finance/dte-send-confirmation.blade.php
@@ -1,9 +1,22 @@
 <div>
     @if(is_null($dte->confirmation_send_at))
-    <button type="button" class="btn btn-primary" wire:click="sendConfirmation()">
-        <i class="fas fa-paper-plane"></i>
-    </button>
+        <button type="button" class="btn btn-primary" wire:click="sendConfirmation()">
+            <i class="fas fa-paper-plane"></i>
+        </button>
     @else
-        {{ $dte->confirmation_send_at }}
+    <i class="fas fa-paper-plane"></i> {{ $dte->confirmation_send_at }}
+        <br>
+        @switch($dte->confirmation_status)
+            @case('0')
+                <i class="fas fa-fw fa-thumbs-down text-danger"></i>
+                @break
+            @case('1')
+                <i class="fas fa-fw fa-thumbs-up text-success"></i>
+                @break
+            @case(null)
+                <i class="fas fa-fw fa-hourglass-start"></i>
+                @break
+        @endswitch
+        {{ $dte->confirmation_at }}
     @endif
 </div>
