8f718f7c70052432c5dbf5c780f8af091170be2c
rafael1delgado <delgadorafael2011@gmail.com>
2023-08-11 17:20:36 -0400

    feat: minor changes

--- app/Http/Livewire/Finance/DteSendConfirmation.php
+++ app/Http/Livewire/Finance/DteSendConfirmation.php
@@ -11,22 +11,23 @@ class DteSendConfirmation extends Component
 {
     public $dte;
     public $user;
-    
+
     /**
-    * mount
-    */
+     * mount
+     */
     public function mount(Dte $dte, User $user)
     {
         $this->dte = $dte;
         $this->user = $user;
     }
+
     /**
-    * Send Confirmation
-    */
+     * Send Confirmation
+     */
     public function sendConfirmation()
-    {        
+    {
         $this->user->notify(new DteConfirmation($this->dte));
-        
+
         $this->dte->confirmation_sender_id = auth()->id();
         $this->dte->confirmation_send_at = now();
         $this->dte->save();
