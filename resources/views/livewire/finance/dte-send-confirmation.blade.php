96216fc331ea23312452c80511b8c78f6ab32425
AquaroTorres <alvarotorres@gmail.com>
2023-08-12 01:43:53 -0300

    DTES:FEAT: Link a formulario firmados

--- resources/views/livewire/finance/dte-send-confirmation.blade.php
+++ resources/views/livewire/finance/dte-send-confirmation.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if(is_null($dte->confirmation_send_at))
-        <button type="button" class="btn btn-primary" wire:click="sendConfirmation()">
+        <button type="button" class="btn btn-sm btn-primary" wire:click="sendConfirmation()">
             <i class="fas fa-paper-plane"></i>
         </button>
     @else
--- resources/views/livewire/finance/index-dtes.blade.php
+++ resources/views/livewire/finance/index-dtes.blade.php
@@ -127,10 +127,38 @@
                     </td>
                     <td class="small">
                         @if ($dte->requestForm)
-                            <a class="btn btn-sm btn-outline-primary btn-block"
+                            <!-- <a class="btn btn-sm btn-outline-primary btn-block"
                                 href="{{ route('request_forms.show', $dte->requestForm->id) }}" target="_blank">
-                                <i class="fas fa-file-alt"></i> {{ $dte->requestForm->folio }}
-                            </a>
+                                <i class="fas fa-file-alt"></i> 
+                            </a> -->
+                            {{ $dte->requestForm->folio }}<br>
+                            @if($dte->requestForm->signatures_file_id)
+                                <a class="btn btn-info btn-sm" title="Ver Formulario de Requerimiento firmado" 
+                                    href="{{ $dte->requestForm->signatures_file_id == 11 ? 
+                                        route('request_forms.show_file', $dte->requestForm->requestFormFiles->first() ?? 0) : 
+                                        route('request_forms.signedRequestFormPDF', [$dte->requestForm, 1]) }}" target="_blank" title="Certificado">
+                                <i class="fas fa-file-contract"></i>
+                                </a>
+                            @endif
+
+                            @if($dte->requestForm->old_signatures_file_id)
+                                <a class="btn btn-secondary btn-sm" title="Ver Formulario de Requerimiento Anterior firmado" 
+                                    href="{{ $dte->requestForm->old_signatures_file_id == 11 ? 
+                                        route('request_forms.show_file', $dte->requestForm->requestFormFiles->first() ?? 0) : 
+                                        route('request_forms.signedRequestFormPDF', [$dte->requestForm, 0]) }}" target="_blank" title="Certificado">
+                                <i class="fas fa-file-contract"></i>
+                                </a>
+                                @endif
+
+                            @if($dte->requestForm->signedOldRequestForms->isNotEmpty())
+                                <a class="btn btn-secondary btn-sm" title="Ver Formulario de Requerimiento Anteriores firmados" 
+                                    href="{{ $dte->requestForm->old_signatures_file_id == 11 ? 
+                                        route('request_forms.show_file', $dte->requestForm->requestFormFiles->first() ?? 0) : 
+                                        route('request_forms.signedRequestFormPDF', [$dte->requestForm, 0]) }}" target="_blank" data-toggle="modal" data-target="#history-fr-{{$dte->requestForm->id}}">
+                                <i class="fas fa-file-contract"></i>
+                                </a>
+                                @include('request_form.partials.modals.old_signed_request_forms')
+                            @endif
                         @endif
                     </td>
                     <td class="small">
