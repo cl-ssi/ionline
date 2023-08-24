a9c2d8ee88a78159c38a1a07049b92ba0f4c4347
Esteban Miranda <tebiccr@gmail.com>
2023-08-24 16:46:48 -0400

    con buscador de funcionario

--- app/Http/Livewire/Rrhh/NoAttendanceRecordIndex.php
+++ app/Http/Livewire/Rrhh/NoAttendanceRecordIndex.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Rrhh;
 use Livewire\WithPagination;
 use Livewire\Component;
 use App\Models\Rrhh\NoAttendanceRecord;
+use App\User; 
 
 class NoAttendanceRecordIndex extends Component
 {
@@ -13,10 +14,14 @@ class NoAttendanceRecordIndex extends Component
 
     public $rejectForm;
     public $rrhh_observation;
+    public $filter = '';
+    
+
+
 
     /**
-    * Registrado por rrhh
-    */
+     * Registrado por rrhh
+     */
     public function setOk(NoAttendanceRecord $noAttendanceRecord)
     {
         $noAttendanceRecord->rrhh_user_id = auth()->id();
@@ -26,24 +31,24 @@ class NoAttendanceRecordIndex extends Component
     }
 
     /**
-    * Reject Form
-    */
+     * Reject Form
+     */
     public function reject(NoAttendanceRecord $noAttendanceRecord)
     {
         $this->rejectForm = $noAttendanceRecord->id;
     }
 
     /**
-    * Close Reject Form
-    */
+     * Close Reject Form
+     */
     public function closeRejectForm()
     {
         $this->rejectForm = null;
     }
 
     /**
-    * Save Reject Form
-    */
+     * Save Reject Form
+     */
     public function saveRejectForm(NoAttendanceRecord $noAttendanceRecord)
     {
         $noAttendanceRecord->rrhh_user_id = auth()->id();
@@ -54,12 +59,26 @@ class NoAttendanceRecordIndex extends Component
         $noAttendanceRecord->save();
         $this->closeRejectForm();
     }
-    
+
+
+    public function searchFuncionary()
+    {
+        $this->resetPage();
+        $this->render();
+    }
+
+
+
     public function render()
     {
-        return view('livewire.rrhh.no-attendance-record-index',[
+        return view('livewire.rrhh.no-attendance-record-index', [
             'records' => NoAttendanceRecord::whereNotNull('status')
-                ->where('establishment_id',auth()->user()->organizationalUnit->establishment_id)
+                ->where('establishment_id', auth()->user()->organizationalUnit->establishment_id)
+                ->when($this->filter, function ($query) {
+                    $query->whereHas('user', function ($userQuery) {
+                        $userQuery->search($this->filter);
+                    });
+                })
                 ->latest()
                 ->paginate(50),
         ]);
