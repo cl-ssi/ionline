ec5cfd0c3cb7564c95c8783faca4788fc2bb14cf
Alvaro Lupa <alvaro.lupa@gmail.com>
2023-09-01 16:07:33 -0400

    [Amipass] personal consolidated amipass regularizations and new charges

--- app/Http/Livewire/Welfare/AmiPass/ChargeIndex.php
+++ app/Http/Livewire/Welfare/AmiPass/ChargeIndex.php
@@ -3,6 +3,8 @@
 namespace App\Http\Livewire\Welfare\Amipass;
 
 use App\Models\Welfare\AmiPass\Charge;
+use App\Models\Welfare\AmiPass\NewCharge;
+use App\Models\Welfare\AmiPass\Regularization;
 use Livewire\Component;
 
 class ChargeIndex extends Component
@@ -10,8 +12,9 @@ class ChargeIndex extends Component
     public function render()
     {
         return view('livewire.welfare.amipass.charge-index', [
-            'records' => Charge::where('rut', auth()->id())
-                ->paginate(50),
+            'records' => Charge::where('rut', auth()->id())->get(),
+            'regularizations' => Regularization::where('rut', auth()->id())->get(),
+            'new_records' => NewCharge::where('rut', auth()->id())->get()
         ]);
     }
 }
--- resources/views/livewire/welfare/amipass/charge-index.blade.php
+++ resources/views/livewire/welfare/amipass/charge-index.blade.php
@@ -17,7 +17,7 @@
             </tr>
         </thead>
         <tbody>
-            @forelse($records as $record)
+            @foreach($records as $record)
             <tr>
                 <td>{{ $record->rut }}-{{ $record->dv }}</td>
                 <td>{{ $record->nombre }}</td>
@@ -31,11 +31,7 @@
                 <td class="text-right">{{ number_format($record->valor_debia_cargarse, 0, ",", ".") ?? '-' }}</td>
                 <td class="{{$record->diferencia_color}} text-right">{{ number_format($record->diferencia, 0, ",", ".") ?? '-' }}</td>
             </tr>
-            @empty
-            <tr>
-                <td colspan="12" class="text-center">No presenta registros de cargas efectivas en Amipass</td>
-            </tr>
-            @endforelse
+            @endforeach
         </tbody>
         <tfoot>
             <tr class="font-weight-bold text-right">
@@ -50,25 +46,82 @@
             </tr>
         </tfoot>
     </table>
-    {{ $records->links() }}
 
     <div class="alert alert-info" role="alert">
-        A la fecha presenta un saldo {{$records->sum('diferencia') > 0 ? 'a favor' : 'en contra' }} de ${{number_format(abs($records->sum('diferencia')), 0, ",", ".")}} entre todas las cargas autorizadas el cual será regularizada en la(s) próxima(s) carga(s) programada(s).
+        A la fecha de Agosto-23 presentaba un saldo {{$records->sum('diferencia') > 0 ? 'a favor' : 'en contra' }} de ${{number_format(abs($records->sum('diferencia')), 0, ",", ".")}} entre todas las cargas autorizadas el cual será regularizada en la(s) próxima(s) carga(s) programada(s).
     </div>
     @else
     <fieldset>No presenta registros de cargas efectivas en Amipass</fieldset>
     @endif
+
+    @if($regularizations->count() > 0)
+    <table class="table table-sm table-bordered table-hover" style="font-size: 12px;">
+        <thead>
+            <tr>
+                <th colspan="5">Regularizaciones registradas</th>
+            </tr>
+            <tr>
+                <th width="95px" scope="col">Rut</th>
+                <th scope="col">Funcionario</th>
+                <th scope="col">Lugar desempeño</th>
+                <th scope="col">Fecha registro</th>
+                <th scope="col">Total regularizado</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($regularizations as $record)
+            <tr>
+                <td>{{ $record->rut }}-{{ $record->dv }}</td>
+                <td>{{ $record->nombre }}</td>
+                <td>{{ $record->lugar_desempeño }}</td>
+                <td>{{ Str::after($record->fecha, '-') }}</td>
+                <td class="{{ $record->total_real_cargado > 0 ? 'text-success' : 'text-danger' }} font-weight-bold text-right">{{ number_format($record->total_real_cargado, 0, ",", ".") ?? '-' }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    @else
+    <fieldset>No presenta registros de regularizaciones</fieldset>
+    @endif
+
+    @if($new_records->count() > 0)
+    <table class="table table-sm table-bordered table-hover" style="font-size: 12px;">
+        <thead>
+            <tr>
+                <th colspan="10">Mis cargas de Sept-23 a la fecha</th>
+            </tr>
+            <tr>
+                <th width="95px" scope="col">Rut</th>
+                <th scope="col">Funcionario</th>
+                <th scope="col">Lugar desempeño</th>
+                <th scope="col">Fecha registro</th>
+                <th scope="col">Días hábiles del mes</th>
+                <th scope="col">Días de ausentismos</th>
+                <th scope="col">Valor día</th>
+                <th scope="col">Subtotal mes</th>
+                <th scope="col">Valor regularizado</th>
+                <th scope="col">Valor a cargar</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($new_records as $record)
+            <tr>
+                <td>{{ $record->rut }}-{{ $record->dv }}</td>
+                <td>{{ $record->nombre }}</td>
+                <td>{{ $record->lugar_desempeño }}</td>
+                <td>{{ Str::after($record->fecha, '-') }}</td>
+                <td class="text-right">{{ $record->dias_habiles_mes ?? '-' }}</td>
+                <td class="text-right">{{ $record->dias_ausentismo ?? '-' }}</td>
+                <td class="text-right">{{ number_format($record->valor_dia, 0, ",", ".") ?? '-' }}</td>
+                <td class="text-right">{{ number_format($record->subtotal, 0, ",", ".") ?? '-' }}</td>
+                <td class="text-right">{{ number_format($record->total_regularizado, 0, ",", ".") ?? '-' }}</td>
+                <td class="text-right font-weight-bold">{{ number_format($record->valor_a_cargar, 0, ",", ".") ?? '-' }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    @else
+    <fieldset>No presenta registros de cargas efectivas en Amipass</fieldset>
+    @endif
     
-    {{--
-    @if($record->rrhh_at)
-                            {{ $record->rrhh_at }}
-                        @elseif($record->status === 1)
-                            <button type="button" class="btn btn-sm btn-success" wire:click="setOk({{$record}})">
-                                <i class="fas fa-check"></i> 
-                            </button>
-                            <button type="button" class="btn btn-sm btn-danger" wire:click="reject({{$record}})">
-                                <i class="fas fa-ban"></i>
-                            </button>
-                        @endif
-                        --}}
 </div>
