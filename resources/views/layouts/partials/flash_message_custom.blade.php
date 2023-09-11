43293b47b36f6351ab9373dab8bce3e5c662fdc4
AquaroTorres <alvarotorres@gmail.com>
2023-09-11 15:15:45 -0300

    Flash Message Custom

--- resources/views/layouts/partials/flash_message_custom.blade.php
+++ resources/views/layouts/partials/flash_message_custom.blade.php
@@ -1,8 +1,20 @@
-@foreach (['danger-custom', 'warning-custom', 'success-custom', 'info-custom'] as $key)
-    @if(session()->has($key))
-        <div class="alert alert-{{ str_replace('-custom','',$key) }} alert-dismissable">
-            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            {!! session()->get($key) !!}
-        </div>
-    @endif
-@endforeach
+@if(session()->has($name))
+    <div class="alert alert-{{ $type ?? 'primary' }} alert-dismissable">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        {!! session()->get($name) !!}
+    </div>
+@endif
+
+<!--
+    Ejemplo de uso:
+
+    Incluir en el blade:
+    @include('layouts.partials.flash_message_custom',[
+        'name' => 'custom_name',  // debe ser único
+        'type' => 'primary' // optional: 'primary' (default), 'danger', 'warning', 'success', 'info'
+    ])
+
+
+    En el controller:
+    session()->flash('custom_name','Mensaje que se quiere mostrar');
+-->
