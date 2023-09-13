3d23604f0f3484313d8d7236ea174309bfb9b259
AquaroTorres <alvarotorres@gmail.com>
2023-09-13 11:11:08 -0300

    Cambio de noticia en home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,19 +22,19 @@
 
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <!-- <li data-target="#carouselExampleIndicators" data-slide-to="1" class=""></li> -->
+            <!-- <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li> -->
+            <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
             <!-- <li data-target="#carouselExampleIndicators" data-slide-to="2" class=""></li> -->
         </ol>
         <div class="carousel-inner">
 
-            <div class="carousel-item active">
+            <!-- <div class="carousel-item active">
                 <img src="{{ asset('images/news/new1.png') }}" alt="">
-            </div>
-
-            <!-- <div class="carousel-item">
-                <img src="{{ asset('images/news/new2.png') }}" alt="">
             </div> -->
+
+            <div class="carousel-item active">
+                <img src="{{ asset('images/news/new2.png') }}" alt="">
+            </div>
 
             <!-- <div class="carousel-item">
                 <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Third slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#555"></rect><text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text></svg>
