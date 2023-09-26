823365a4bef811693e71080006b6c7e351c83cd1
sick <esteban.rojasg@redsalud.cl>
2023-09-26 14:59:08 -0300

    Reunión profile-honorarios Carol de RRHH-SST: Eliminar una solicitud - implementar en profile#1160

--- app/Http/Livewire/ServiceRequest/ShowCompact.php
+++ app/Http/Livewire/ServiceRequest/ShowCompact.php
@@ -8,6 +8,13 @@ use App\Models\ServiceRequests\ServiceRequest;
 class ShowCompact extends Component
 {
     public ServiceRequest $serviceRequest;
+
+    public function deleteRequest(ServiceRequest $serviceRequest){
+        // dd($serviceRequest->user_id);
+        $serviceRequest->delete();
+        session()->flash('success', 'Se eliminó la solicitud.');
+        return redirect()->route('rrhh.service-request.show',['user' => $serviceRequest->user_id]);
+    }
     
     public function render()
     {
