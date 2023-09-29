0c216c525701ef5b4f146493b65f1932277ee229
AquaroTorres <alvarotorres@gmail.com>
2023-09-29 17:47:00 -0300

    DOC:FEAT:Formato resolución

--- resources/views/documents/templates/partials/distribution_and_responsible.blade.php
+++ resources/views/documents/templates/partials/distribution_and_responsible.blade.php
@@ -1,5 +1,9 @@
 @if($document->distribution OR $document->responsible)
-    <table class="seis" style="margin-left: -3px; padding-top: 160px; width: 100%">
+    @if($document->type->name == 'Oficio' OR $document->type->name == 'Resolución')
+        <table class="seis" style="margin-left: -3px; width: 100%; padding-top: 160px;">
+    @else
+        <table class="seis" style="margin-left: -3px; width: 100%">
+    @endif
         <tr style="vertical-align: top;">
             @include('documents.templates.partials.distribution')
             @include('documents.templates.partials.responsible')
