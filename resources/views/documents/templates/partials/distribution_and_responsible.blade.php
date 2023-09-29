80aa2352b5b8f769f781f32763afd73edaa2a50c
AquaroTorres <alvarotorres@gmail.com>
2023-09-29 16:46:57 -0300

    DOC:FIX:No mostrar responsables o distribución si no tien

--- resources/views/documents/templates/partials/distribution_and_responsible.blade.php
+++ resources/views/documents/templates/partials/distribution_and_responsible.blade.php
@@ -1,6 +1,8 @@
-<table class="seis" style="margin-left: -3px; padding-top: 160px; width: 100%">
-    <tr style="vertical-align: top;">
-        @include('documents.templates.partials.distribution')
-        @include('documents.templates.partials.responsible')
-    </tr>
-</table>
+@if($document->distribution OR $document->responsible)
+    <table class="seis" style="margin-left: -3px; padding-top: 160px; width: 100%">
+        <tr style="vertical-align: top;">
+            @include('documents.templates.partials.distribution')
+            @include('documents.templates.partials.responsible')
+        </tr>
+    </table>
+@endif
