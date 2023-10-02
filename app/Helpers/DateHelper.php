9b92a9f6cced7520f7a26e2db8a604c37b800376
sick <esteban.rojasg@redsalud.cl>
2023-10-02 15:34:54 -0300

    fix error getBusinessDaysByDateRange

--- app/Helpers/DateHelper.php
+++ app/Helpers/DateHelper.php
@@ -3,7 +3,7 @@
 namespace App\Helpers;
 
 use Illuminate\Support\Facades\Cache;
-use App\Models\Parameters\Holiday;
+// use App\Models\Parameters\Holiday;
 
 class DateHelper
 {
@@ -14,11 +14,11 @@ class DateHelper
      * @param  \Illuminate\Support\Carbon  $endDate
      * @return \Illuminate\Support\Collection
      */
-    public static function getBusinessDaysByDateRange($startDate, $endDate, $holidays)
+    public static function getBusinessDaysByDateRange($startDate, $endDate)
     {
-        // $holidays = Cache::remember('holidays', 600, function () use($startDateHoliday, $endDateHoliday) {
-        //     return Holiday::whereBetween('date', [$startDateHoliday, $endDateHoliday])->get();
-        // });
+        $holidays = Cache::remember('holidays', 600, function () use($startDateHoliday, $endDateHoliday) {
+            return Holiday::whereBetween('date', [$startDateHoliday, $endDateHoliday])->get();
+        });
 
         // $holidays = Holiday::whereBetween('date', [$startDate, $endDate])->get();
 
