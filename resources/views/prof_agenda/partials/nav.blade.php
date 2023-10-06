cfccbd64d76efcfdbcf806cb8ebb0507029b6aa1
sick <esteban.rojasg@redsalud.cl>
2023-10-05 21:27:45 -0300

    Agenda UST: se crean perfiles de usuarios

--- resources/views/prof_agenda/partials/nav.blade.php
+++ resources/views/prof_agenda/partials/nav.blade.php
@@ -7,62 +7,57 @@
         </a>
     </li>
 
-    <li class="nav-item">
-        <a class="nav-link {{ active('prof_agenda.agenda.index') }}"
-            href="{{ route('prof_agenda.agenda.index') }}">
-            <i class="fa fa-calendar"></i> Gestor de agenda
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link {{ active('prof_agenda.open_hour.index') }}"
-            href="{{ route('prof_agenda.open_hour.index') }}">
-            <i class="fa fa-book"></i> Reservas
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link {{ active('prof_agenda.proposals.index') }}"
-            href="{{ route('prof_agenda.proposals.index') }}">
-            <i class="fa fa-list"></i> Propuestas
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link {{ active('prof_agenda.proposals.open_calendar') }}"
-            href="{{ route('prof_agenda.proposals.open_calendar') }}">
-            <i class="fa fa-folder-open"></i> Aperturar agenda
-        </a>
-    </li>
-
-    <!-- <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle {{active('prof_agenda.activity_types.*')}}"
-        data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-clipboard-check"></i> Reportes
-        </a>
-        <div class="dropdown-menu">
-
-            <a class="dropdown-item {{ active('prof_agenda.activity_types.index') }}"
-                href="{{ route('prof_agenda.activity_types.index') }}">
-                <i class="fas fa-clipboard-check"></i> Reservas
+    @canany(['Agenda UST: Administrador','Agenda UST: Funcionario','Agenda UST: Secretaria'])
+        <li class="nav-item">
+            <a class="nav-link {{ active('prof_agenda.agenda.index') }}"
+                href="{{ route('prof_agenda.agenda.index') }}">
+                <i class="fa fa-calendar"></i> Gestor de agenda
             </a>
+        </li>
+    @endcanany
 
-        </div>
-    </li> -->
-
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle {{active('prof_agenda.activity_types.*')}}"
-        data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-clipboard-check"></i> Mantenedores
-        </a>
-        <div class="dropdown-menu">
-
-            <a class="dropdown-item {{ active('prof_agenda.activity_types.index') }}"
-                href="{{ route('prof_agenda.activity_types.index') }}">
-                <i class="fas fa-clipboard-check"></i> Tipos de actividad
+    @canany(['Agenda UST: Administrador','Agenda UST: Funcionario','Agenda UST: Secretaria'])
+        <li class="nav-item">
+            <a class="nav-link {{ active('prof_agenda.open_hour.index') }}"
+                href="{{ route('prof_agenda.open_hour.index') }}">
+                <i class="fa fa-book"></i> Reservas
             </a>
+        </li>
+    @endcanany
 
-        </div>
-    </li>
+    @canany(['Agenda UST: Administrador','Agenda UST: Funcionario'])
+        <li class="nav-item">
+            <a class="nav-link {{ active('prof_agenda.proposals.index') }}"
+                href="{{ route('prof_agenda.proposals.index') }}">
+                <i class="fa fa-list"></i> Propuestas
+            </a>
+        </li>
+    @endcanany
+
+    @canany(['Agenda UST: Administrador','Agenda UST: Funcionario'])
+        <li class="nav-item">
+            <a class="nav-link {{ active('prof_agenda.proposals.open_calendar') }}"
+                href="{{ route('prof_agenda.proposals.open_calendar') }}">
+                <i class="fa fa-folder-open"></i> Aperturar agenda
+            </a>
+        </li>
+    @endcanany
+
+    @canany(['Agenda UST: Administrador'])
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle {{active('prof_agenda.activity_types.*')}}"
+            data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-clipboard-check"></i> Mantenedores
+            </a>
+            <div class="dropdown-menu">
+
+                <a class="dropdown-item {{ active('prof_agenda.activity_types.index') }}"
+                    href="{{ route('prof_agenda.activity_types.index') }}">
+                    <i class="fas fa-clipboard-check"></i> Tipos de actividad
+                </a>
+
+            </div>
+        </li>
+    @endcanany
 
 </ul>
