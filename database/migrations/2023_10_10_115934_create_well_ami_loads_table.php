57dea6d85632fd871426983f5742bcbae5b43530
sick <esteban.rojasg@redsalud.cl>
2023-10-11 10:19:39 -0300

    Amipass: vista importación ami_loads + migración

---
+++ database/migrations/2023_10_10_115934_create_well_ami_loads_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('well_ami_loads', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_amipass')->nullable();
+            $table->string('centro_de_costo')->nullable();
+            $table->string('sucursal')->nullable();
+            $table->integer('n_factura')->nullable();
+            $table->string('tipo')->nullable();
+            $table->datetime('fecha')->nullable();
+            $table->bigInteger('n_tarjeta')->nullable();
+            $table->string('nombre_empleado')->nullable();
+            $table->foreignId('run')->nullable()->constrained('users');
+            $table->string('dv')->nullable();
+            $table->string('tipo_empleado')->nullable();
+            $table->integer('monto')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('well_ami_loads');
+    }
+};
