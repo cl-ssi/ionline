7c37ca5533dd3f9a887535296727f7e3ae15cdc9
rafael1delgado <delgadorafael2011@gmail.com>
2023-10-12 14:57:21 -0400

    feat: add requets to create and edit organizational units

---
+++ app/Http/Requests/Parameters/OrganizationalUnit/UpdateOrganizationalUnitRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Parameters\OrganizationalUnit;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+
+class UpdateOrganizationalUnitRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @param  Request  $request
+     * @return array<string, mixed>
+     */
+    public function rules(Request $request)
+    {
+        return [
+            'name'                      => 'required|string|max:255',
+            'establishment_id'          => 'nullable|exists:establishments,id',
+            'organizational_unit_id'    => 'required|exists:organizational_units,id',
+            'sirh_function'             => 'required|integer',
+            'sirh_ou_id'                => 'required|unique:organizational_units,sirh_ou_id,'.$request->organizationalUnitId,
+            'sirh_cost_center'          => 'required|integer'
+        ];
+    }
+}
