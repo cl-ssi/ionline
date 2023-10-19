12d8a7d8394813a9fc3e96573ea8deebc9111c43
sick <esteban.rojasg@redsalud.cl>
2023-10-19 15:23:09 -0300

    test

--- app/Http/Livewire/Rrhh/AbscencesImport.php
+++ app/Http/Livewire/Rrhh/AbscencesImport.php
@@ -34,6 +34,7 @@ class AbscencesImport extends Component
 
         $file = $this->file;
         $collection = Excel::toCollection(new AbscencesImportFile, $file);
+        dd($collection);
 
         $total_count = $collection->first()->count()+1;
         $count_inserts = 0;
