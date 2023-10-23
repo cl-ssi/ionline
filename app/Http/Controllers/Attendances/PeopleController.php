5ff918acc405a47b10ae2436cb253ce29f2ac644
m-r-2021 <m.soto.r.2021@gmail.com>
2023-10-23 19:13:57 -0300

    Attendance: Modificar pagina para jornada de cuidados paliativos Fixes #1352

--- app/Http/Controllers/Attendances/PeopleController.php
+++ app/Http/Controllers/Attendances/PeopleController.php
@@ -14,12 +14,33 @@ class PeopleController extends Controller
 
         $persona = People::where('rut', $request->rut)->first();
 
+        $jornada = $request->jornada;
+
+        $fecha = $request->fecha;
+
         if ($persona != null) {
-            $persona->asistencia = true;
+            if ($fecha == "3") {
+                if ($jornada == 'dia') {
+                    if (!$persona->asistencia_dia_2) {
+                        $persona->asistencia_dia_2 = true;
+                    }
+                } else if ($jornada == 'tarde') {
+                    if (!$persona->asistencia_tarde_2) {
+                        $persona->asistencia_tarde_2 = true;
+                    }
+                }
+            } else if ($fecha == "2") {
+                if (!$persona->asistencia_dia_1) {
+                    $persona->asistencia_dia_1 = true;
+                }
+            }
+
+            
+
             $persona->save();
 
             //return redirect('main')->with('nombre', $persona->nombre)->withSuccess('Ingresado correctamente');
-            return view('attendances.main', ["nombre" => $persona->nombre, "asistencia" => $persona->asistencia]);
+            return view('attendances.main', ["nombre" => $persona->nombre, "asistencia" => $persona->asistencia, "persona" => $persona]);
         } else {
 
             $unregisteredPerson = new UnregisteredPeople();
