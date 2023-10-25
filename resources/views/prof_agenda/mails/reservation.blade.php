6fe441107d75e6db1f2fde0d79e4726795235e9a
sick <esteban.rojasg@redsalud.cl>
2023-10-25 17:31:56 -0300

    Agenda ust: fix hora reserva mail

--- resources/views/prof_agenda/mails/reservation.blade.php
+++ resources/views/prof_agenda/mails/reservation.blade.php
@@ -5,7 +5,7 @@
     <div style="text-align: justify;">
         <p> Estimado(a) <b>{{$openHour->patient->shortName}}</b>,</p>
         <p> Se ha reservado una hora de {{$openHour->activityType->name}} con el profesional <b>{{$openHour->profesional->shortName}}</b> </p>
-        <p> La reserva se encuentra realizada para <b>{{$openHour->start_date->format('Y-m-d')}}</b> a las <b>{{$openHour->start_date->format('H:s')}}</b>. Se solicita llegar puntual a su hora.</p>
+        <p> La reserva se encuentra realizada para <b>{{$openHour->start_date->format('Y-m-d')}}</b> a las <b>{{$openHour->start_date->format('H:i')}}</b>. Se solicita llegar puntual a su hora.</p>
         <p> Si no puede asistir, rogamos contactar a la Unidad de Salud del Trabajador para reagendar o cancelar su hora.</p>
     </div>
 
