af11ef9ff73b97a6d431c5d4a5200dff97ff5744
Esteban Miranda <tebiccr@gmail.com>
2023-10-30 19:20:56 -0300

    con el formulario de estado/rechazar

--- resources/views/finance/payments/rejected.blade.php
+++ resources/views/finance/payments/rejected.blade.php
@@ -32,7 +32,7 @@
                     </td>
                     <td class="small">
                         {{ $dte->confirmation_at }} <br>
-                        {{ $dte->confirmationUser->shortName }}:<br>
+                        {{ $dte->confirmationUser->shortName ?? '' }}:<br>
                         <i class="text-danger">
                             {{ $dte->confirmation_observation }}
                         </i>
