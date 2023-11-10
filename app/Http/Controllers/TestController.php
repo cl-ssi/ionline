8879e650d88bcc6a8d185fb86d051d70f0edef94
AquaroTorres <alvarotorres@gmail.com>
2023-11-10 12:16:54 -0400

    TEST IP

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -19,19 +19,21 @@ class TestController extends Controller
 {
     public function getIp()
     {
-        if ( !empty($_SERVER['HTTP_CLIENT_IP']) ) {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
             // Check IP from internet.
             // 'ip' => request()->getClientIp(),
             $ip = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+            logger()->info('HTTP_CLIENT_IP: ' . $_SERVER['HTTP_CLIENT_IP']);
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             // Check IP is passed from proxy.
             $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            logger()->info('HTTP_X_FORWARDED_FOR: ' . $_SERVER['HTTP_X_FORWARDED_FOR']);
         } else {
             // Get IP address from remote address.
             $ip = $_SERVER['REMOTE_ADDR'];
+            logger()->info('REMOTE_ADDR: ' . $_SERVER['REMOTE_ADDR']);
         }
-        
-        logger()->info($ip);
+
         //Storage::disk('local')->prepend('log_ips.txt', $ip);
         return $ip;
     }
@@ -51,7 +53,7 @@ class TestController extends Controller
         $user = \App\User::find(1528758);
         echo $user->name;
         Log::info('primer log');
-        echo "Primer log";
+        echo 'Primer log';
     }
 
     public function info()
@@ -60,30 +62,26 @@ class TestController extends Controller
     }
 
     /**
-    * No cargar 
-    */
+     * No cargar
+     */
     public function loopLivewire()
     {
         $ous = OrganizationalUnit::all();
 
-        foreach($ous as $ou)
-        {
+        foreach ($ous as $ou) {
             // @livewire( parametro ou)
             $estab = Establishment::where('id', $ou->establishment_id)->first();
             // echo $estab->id."<br>";
-            if($estab->type == 'HOSPITAL')
-            {
-                echo $estab->name."<br>";
-            } 
+            if ($estab->type == 'HOSPITAL') {
+                echo $estab->name . '<br>';
+            }
             // fin de livewire
         }
-
     }
 
-
     /**
-    * Test Job
-    */
+     * Test Job
+     */
     public function job()
     {
         TestJob::dispatch(auth()->user())
@@ -91,16 +89,15 @@ class TestController extends Controller
             ->delay(15);
 
         return view('mails.test', [
-            'user' => auth()->user()
+            'user' => auth()->user(),
         ]);
-        
+
         // return "Test Job Dispatch";
     }
 
-
     /**
-    * Test de cards en teams
-    */
+     * Test de cards en teams
+     */
     public function SendCardToTeams()
     {
         $wh = env('LOG_TEAMS_WEBHOOK_URL_ATORRES');
@@ -108,130 +105,122 @@ class TestController extends Controller
         // $data['text'] = 'Hello world';
 
         $data = [
-            "@type" => "MessageCard",
-            "@context" => "http://schema.org/extensions",
-            "themeColor" => "0076D7",
-            "summary" => "Larry Bryant created a new task",
-            "sections" => [
+            '@type' => 'MessageCard',
+            '@context' => 'http://schema.org/extensions',
+            'themeColor' => '0076D7',
+            'summary' => 'Larry Bryant created a new task',
+            'sections' => [
                 [
-                    "activityTitle" => "Larry Bryant created a new task",
-                    "activitySubtitle" => "On Project Tango",
-                    "activityImage" =>
-                        "https://teamsnodesample.azurewebsites.net/static/img/image5.png",
-                    "facts" => [
+                    'activityTitle' => 'Larry Bryant created a new task',
+                    'activitySubtitle' => 'On Project Tango',
+                    'activityImage' => 'https://teamsnodesample.azurewebsites.net/static/img/image5.png',
+                    'facts' => [
                         [
-                            "name" => "Assigned to",
-                            "value" => "Unassigned",
+                            'name' => 'Assigned to',
+                            'value' => 'Unassigned',
                         ],
                         [
-                            "name" => "Due date",
-                            "value" =>
-                                "Mon May 01 2017 17:07:18 GMT-0700 (Pacific Daylight Time)",
+                            'name' => 'Due date',
+                            'value' => 'Mon May 01 2017 17:07:18 GMT-0700 (Pacific Daylight Time)',
                         ],
                         [
-                            "name" => "Status",
-                            "value" => "Not started",
+                            'name' => 'Status',
+                            'value' => 'Not started',
                         ],
                     ],
-                    "markdown" => true,
+                    'markdown' => true,
                 ],
             ],
-            "potentialAction" => [
+            'potentialAction' => [
                 [
-                    "@type" => "ActionCard",
-                    "name" => "Add a comment",
-                    "inputs" => [
+                    '@type' => 'ActionCard',
+                    'name' => 'Add a comment',
+                    'inputs' => [
                         [
-                            "@type" => "TextInput",
-                            "id" => "comment",
-                            "isMultiline" => false,
-                            "title" => "Add a comment here for this task",
+                            '@type' => 'TextInput',
+                            'id' => 'comment',
+                            'isMultiline' => false,
+                            'title' => 'Add a comment here for this task',
                         ],
                     ],
-                    "actions" => [
+                    'actions' => [
                         [
-                            "@type" => "HttpPOST",
-                            "name" => "Add comment",
-                            "target" =>
-                                "https://learn.microsoft.com/outlook/actionable-messages",
+                            '@type' => 'HttpPOST',
+                            'name' => 'Add comment',
+                            'target' => 'https://learn.microsoft.com/outlook/actionable-messages',
                         ],
                     ],
                 ],
                 [
-                    "@type" => "ActionCard",
-                    "name" => "Set due date",
-                    "inputs" => [
+                    '@type' => 'ActionCard',
+                    'name' => 'Set due date',
+                    'inputs' => [
                         [
-                            "@type" => "DateInput",
-                            "id" => "dueDate",
-                            "title" => "Enter a due date for this task",
+                            '@type' => 'DateInput',
+                            'id' => 'dueDate',
+                            'title' => 'Enter a due date for this task',
                         ],
                     ],
-                    "actions" => [
+                    'actions' => [
                         [
-                            "@type" => "HttpPOST",
-                            "name" => "Save",
-                            "target" =>
-                                "https://learn.microsoft.com/outlook/actionable-messages",
-                        ],
-                    ],
-                ],
-                [
-                    "@type" => "OpenUri",
-                    "name" => "Post x More",
-                    "targets" => [
-                        [
-                            "os" => "default",
-                            "uri" =>
-                                "http://localhost:8000/postx",
+                            '@type' => 'HttpPOST',
+                            'name' => 'Save',
+                            'target' => 'https://learn.microsoft.com/outlook/actionable-messages',
                         ],
                     ],
                 ],
                 [
-                    "@type" => "ActionCard",
-                    "name" => "Change status",
-                    "inputs" => [
+                    '@type' => 'OpenUri',
+                    'name' => 'Post x More',
+                    'targets' => [
                         [
-                            "@type" => "MultichoiceInput",
-                            "id" => "list",
-                            "title" => "Select a status",
-                            "isMultiSelect" => "false",
-                            "choices" => [
+                            'os' => 'default',
+                            'uri' => 'http://localhost:8000/postx',
+                        ],
+                    ],
+                ],
+                [
+                    '@type' => 'ActionCard',
+                    'name' => 'Change status',
+                    'inputs' => [
+                        [
+                            '@type' => 'MultichoiceInput',
+                            'id' => 'list',
+                            'title' => 'Select a status',
+                            'isMultiSelect' => 'false',
+                            'choices' => [
                                 [
-                                    "display" => "In Progress",
-                                    "value" => "1",
+                                    'display' => 'In Progress',
+                                    'value' => '1',
                                 ],
                                 [
-                                    "display" => "Active",
-                                    "value" => "2",
+                                    'display' => 'Active',
+                                    'value' => '2',
                                 ],
                                 [
-                                    "display" => "Closed",
-                                    "value" => "3",
+                                    'display' => 'Closed',
+                                    'value' => '3',
                                 ],
                             ],
                         ],
                     ],
-                    "actions" => [
+                    'actions' => [
                         [
-                            "@type" => "HttpPOST",
-                            "name" => "Save",
-                            "target" =>
-                                "https://learn.microsoft.com/outlook/actionable-messages",
+                            '@type' => 'HttpPOST',
+                            'name' => 'Save',
+                            'target' => 'https://learn.microsoft.com/outlook/actionable-messages',
                         ],
                     ],
                 ],
             ],
         ];
-        
-    
+
         $response = Http::withBody(json_encode($data), 'application/json')->post($wh);
 
         if ($response->failed()) {
             dd(json_decode($response->getBody(), true));
-         } else {
+        } else {
             dd('success');
-         }
+        }
     }
-
 }
