11d401dac32ea5c8fd60ea95d55b30dcc883d462
AquaroTorres <alvarotorres@gmail.com>
2023-11-20 12:12:40 -0400

    Filable:Feat:Modelo Fixes #1518

--- app/Models/File.php
+++ app/Models/File.php
@@ -11,14 +11,53 @@ class File extends Model
     use HasFactory;
 
     /**
+     * Get the file model.
+     */
+    // public function file(): MorphOne
+    // {
+    //     return $this->morphOne(File::class, 'fileable');
+    // }
+
+    /**
+     * Get all of the files of a model.
+     */
+    // public function files(): MorphMany
+    // {
+    //     return $this->morphMany(File::class, 'fileable');
+    // }
+
+
+    // $modelo->file()->create([
+    //     'storage_path' => 'ionline/documents/23234.pdf',
+    //     'stored' => true,
+    //     'name' => 'Mi documento.pdf',
+    //     'input_title' => 'Archivo pulento',
+    //     'input_name' => 'reception',
+    //     'required' => false,
+    //     'valid_types' => json_encode(["pdf", "xls"]),
+    //     'max_file_size' => 10,
+    //     'stored_by_id' => 1,
+    // ]);
+
+    /**
     * The attributes that are mass assignable.
     *
     * @var array
     */
     protected $fillable = [
+        'storage_path',
+        'stored',
+
         'name',
-        'path',
-        'type',
+
+        'input_title',
+        'input_name',
+        'required',
+        'valid_types',
+        'max_file_size',
+
+        'stored_by_id',
+
         'fileable_type',
         'fileable_id',
     ];
