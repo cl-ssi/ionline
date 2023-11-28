2862be13a059d53d8dc031364aa58b1b64784da6
sick <esteban.rojasg@redsalud.cl>
2023-11-28 17:36:17 -0300

    Amipass: creación reporte por funcionario

--- app/Http/Livewire/Welfare/Amipass/ReportByEmployee.php
+++ app/Http/Livewire/Welfare/Amipass/ReportByEmployee.php
@@ -2,14 +2,60 @@
 
 namespace App\Http\Livewire\Welfare\Amipass;
 
+use App\Models\Welfare\Amipass\Charge;
+use App\Models\Welfare\Amipass\NewCharge;
+use App\Models\Welfare\Amipass\Regularization;
+use App\User;
+
+use Carbon\Carbon;
+
 use Livewire\Component;
 
 // use App\Models\Rrhh\CompensatoryDay;
 
 class ReportByEmployee extends Component
 {
+    public $records;
+    public $regularizations;
+    public $new_records;
+    public $user_id;
+
+    protected $listeners = ['reportByEmployeeEmit', 'reportByEmployeeEmit'];
+
+    public function reportByEmployeeEmit(User $user_id)
+    {
+        $this->user_id = $user_id->id;
+    }
+
     public function search(){
-        dd("");
+        $this->records = Charge::where('rut', $this->user_id)->get();
+        $meses[] = ['ene','feb','mar','abr','may','jun','jul','ago','sep','oct','nov','dic'];
+        foreach($this->records as $key => $record){
+            list($day, $month, $year) = explode('-', $record->fecha);
+            // si no es la fecha actual, se elimina de la colecctión
+            if($year != Carbon::now()->format('y')){
+                unset($this->records[$key]);
+            }
+        }
+
+        $this->regularizations = Regularization::where('rut', $this->user_id)->get();
+        foreach($this->regularizations as $key => $regularization){
+            list($day, $month, $year) = explode('-', $regularization->fecha);
+            // si no es la fecha actual, se elimina de la colecctión
+            if($year != Carbon::now()->format('y')){
+                unset($this->regularization[$key]);
+            }
+        }
+
+        $this->new_records = NewCharge::where('rut', $this->user_id)->get();
+        foreach($this->new_records as $key => $new_record){
+            list($day, $month, $year) = explode('-', $new_record->fecha);
+            // si no es la fecha actual, se elimina de la colecctión
+            if($year != Carbon::now()->format('y')){
+                unset($this->new_record[$key]);
+            }
+        }
+
     }
 
     public function render()
--- resources/views/livewire/welfare/amipass/report-by-employee.blade.php
+++ resources/views/livewire/welfare/amipass/report-by-employee.blade.php
@@ -3,12 +3,16 @@
 
     <h4>Reporte Amipass por funcionario</h4>
 
-    <div class="form-row">
-        <div class="form-group col-3">
+    <div class="row">
+        <div class="group col-3">
             <label for="text11">Funcionario</label>
-            
+            @livewire('search-select-user', [
+                'selected_id' => 'user_id',
+                'required'    => 'required',
+                'emit_name' => 'reportByEmployeeEmit'
+            ])
         </div>
-        <div class="form-group">
+        <div class="group col-3">
             <label for="text12"><br></label>
             <div wire:loading.remove>
                 <button wire:click="search" type="button" class="form-control btn btn-primary">Generar</button>
@@ -18,6 +22,131 @@
             </div>
 
         </div>
+    </div><br><br>
+
+    @if($records && $records->count() > 0)
+    <table class="table table-sm table-bordered table-hover" style="font-size: 12px;">
+        <thead>
+            <tr>
+                <th width="95px" scope="col">Rut</th>
+                <th scope="col">Funcionario</th>
+                <th scope="col">Lugar desempeño</th>
+                <th scope="col">Fecha registro</th>
+                <th scope="col">Total real cargado</th>
+                <th scope="col">Días de ausentismos</th>
+                <th scope="col">Días hábiles del mes</th>
+                <th scope="col">Días a cargar</th>
+                <th scope="col">Valor día</th>
+                <th scope="col">Valor que debía cargarse</th>
+                <th scope="col">Diferencia</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($records as $record)
+            <tr>
+                <td>{{ $record->rut }}-{{ $record->dv }}</td>
+                <td>{{ $record->nombre }}</td>
+                <td>{{ $record->lugar_desempeño }}</td>
+                <td>{{ Str::after($record->fecha, '-') }}</td>
+                <td class="text-right">{{ number_format($record->total_real_cargado, 0, ",", ".") ?? '-' }}</td>
+                <td class="text-right">{{ $record->dias_ausentismo ?? '-' }}</td>
+                <td class="text-right">{{ $record->dias_habiles_mes ?? '-' }}</td>
+                <td class="text-right">{{ $record->dias_a_cargar ?? '-' }}</td>
+                <td class="text-right">{{ number_format($record->valor_dia, 0, ",", ".") ?? '-' }}</td>
+                <td class="text-right">{{ number_format($record->valor_debia_cargarse, 0, ",", ".") ?? '-' }}</td>
+                <td class="{{$record->diferencia_color}} text-right">{{ number_format($record->diferencia, 0, ",", ".") ?? '-' }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+        <tfoot>
+            <tr class="font-weight-bold text-right">
+                <td colspan="4" class="text-right">Totales $</td>
+                <td>{{number_format($records->sum('total_real_cargado'), 0, ",", ".")}}</td>
+                <td>{{$records->sum('dias_ausentismo')}}</td>
+                <td>{{$records->sum('dias_habiles_mes')}}</td>
+                <td>{{$records->sum('dias_a_cargar')}}</td>
+                <td></td>
+                <td>{{number_format($records->sum('valor_debia_cargarse'), 0, ",", ".")}}</td>
+                <td class="{{ $records->sum('diferencia') > 0 ? 'text-success' : 'text-danger' }}">{{number_format($records->sum('diferencia'), 0, ",", ".")}}</td>
+            </tr>
+        </tfoot>
+    </table>
+
+    <div class="alert alert-info" role="alert">
+        A la fecha de Agosto-23 presentaba un saldo {{$records->sum('diferencia') > 0 ? 'a favor' : 'en contra' }} de ${{number_format(abs($records->sum('diferencia')), 0, ",", ".")}} entre todas las cargas autorizadas el cual será regularizada en la(s) próxima(s) carga(s) programada(s).
     </div>
+    @else
+    <fieldset>No presenta registros de cargas efectivas en Amipass</fieldset>
+    @endif
+
+    @if($regularizations && $regularizations->count() > 0)
+    <table class="table table-sm table-bordered table-hover" style="font-size: 12px;">
+        <thead>
+            <tr>
+                <th colspan="5">Regularizaciones registradas</th>
+            </tr>
+            <tr>
+                <th width="95px" scope="col">Rut</th>
+                <th scope="col">Funcionario</th>
+                <th scope="col">Lugar desempeño</th>
+                <th scope="col">Fecha registro</th>
+                <th scope="col">Total regularizado</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($regularizations as $record)
+            <tr>
+                <td>{{ $record->rut }}-{{ $record->dv }}</td>
+                <td>{{ $record->nombre }}</td>
+                <td>{{ $record->lugar_desempeño }}</td>
+                <td>{{ Str::after($record->fecha, '-') }}</td>
+                <td class="{{ $record->total_real_cargado > 0 ? 'text-success' : 'text-danger' }} font-weight-bold text-right">{{ number_format($record->total_real_cargado, 0, ",", ".") ?? '-' }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    @else
+    <fieldset>No presenta registros de regularizaciones</fieldset>
+    @endif
+
+    @if($new_records && $new_records->count() > 0)
+    <table class="table table-sm table-bordered table-hover" style="font-size: 12px;">
+        <thead>
+            <tr>
+                <th colspan="10">Mis cargas de Oct-23 a la fecha</th>
+            </tr>
+            <tr>
+                <th width="95px" scope="col">Rut</th>
+                <th scope="col">Funcionario</th>
+                <th scope="col">Lugar desempeño</th>
+                <th scope="col">Fecha registro</th>
+                <th scope="col">Días hábiles del mes</th>
+                <th scope="col">Días de ausentismos</th>
+                <th scope="col">Valor día</th>
+                <th scope="col">Subtotal mes</th>
+                <th scope="col">Valor regularizado</th>
+                <th scope="col">Valor a cargar</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($new_records as $record)
+            <tr>
+                <td>{{ $record->rut }}-{{ $record->dv }}</td>
+                <td>{{ $record->nombre }}</td>
+                <td>{{ $record->lugar_desempeño }}</td>
+                <td>{{ Str::after($record->fecha, '-') }}</td>
+                <td class="text-right">{{ $record->dias_habiles_mes ?? '-' }}</td>
+                <td class="text-right">{{ $record->dias_ausentismo ?? '-' }}</td>
+                <td class="text-right">{{ number_format($record->valor_dia, 0, ",", ".") ?? '-' }}</td>
+                <td class="text-right">{{ number_format($record->subtotal, 0, ",", ".") ?? '-' }}</td>
+                <td class="text-right">{{ number_format($record->total_regularizado, 0, ",", ".") ?? '-' }}</td>
+                <td class="text-right font-weight-bold">{{ number_format($record->valor_a_cargar, 0, ",", ".") ?? '-' }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    @else
+    <fieldset>No presenta registros de cargas efectivas en Amipass</fieldset>
+    @endif
 
 </div>
