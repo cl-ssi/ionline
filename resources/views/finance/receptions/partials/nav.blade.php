b6f6208262aa2d73689ad350a8e1d94239deaba1
AquaroTorres <alvarotorres@gmail.com>
2023-12-05 10:24:13 -0400

    Reception:nav

--- resources/views/finance/receptions/partials/nav.blade.php
+++ resources/views/finance/receptions/partials/nav.blade.php
@@ -1,20 +1,32 @@
 <ul class="nav nav-tabs mb-3">
     <li class="nav-item">
         <a class="nav-link {{ active('finance.receptions.index') }}"
-            href="{{ route('finance.receptions.index') }}">Listado de actas</a>
+            href="{{ route('finance.receptions.index') }}">
+            <i class="bi bi-list"></i> 
+            Listado de actas
+        </a>
     </li>
     <li class="nav-item">
         <a class="nav-link {{ active('finance.receptions.create') }}"
             aria-current="page"
-            href="{{ route('finance.receptions.create') }}">Nueva acta con OC</a>
+            href="{{ route('finance.receptions.create') }}">
+            <i class="bi bi-file-plus"></i> 
+            Nueva acta con OC
+        </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link"
-            href="#">Nueva acta sin OC</a>
+        <a class="nav-link disabled"
+            href="#">
+            <i class="bi bi-receipt"></i> 
+            Nueva acta sin OC
+        </a>
     </li>
     <li class="nav-item">
         <a class="nav-link {{ active('finance.receptions.reject') }}"
             aria-current="page"
-            href="{{ route('finance.receptions.reject') }}">Rechazar</a>
+            href="{{ route('finance.receptions.reject') }}">
+            <i class="bi bi-x-octagon"></i> 
+            Rechazar
+        </a>
     </li>
 </ul>
