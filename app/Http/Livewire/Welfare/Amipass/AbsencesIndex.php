8a1ab3f2b223d6346ec62c2b11eea56f001a46e3
sick <esteban.rojasg@redsalud.cl>
2023-12-28 18:48:17 -0300

    Amipass: se agrega año en vista de amipass

--- app/Http/Livewire/Welfare/Amipass/AbsencesIndex.php
+++ app/Http/Livewire/Welfare/Amipass/AbsencesIndex.php
@@ -2,16 +2,31 @@
 
 namespace App\Http\Livewire\Welfare\Amipass;
 
-use App\Models\Welfare\Amipass\Absence;
+use App\Models\Welfare\Amipass\Absence; //tudela
+use App\Models\Rrhh\Absenteeism; // rrhh (nuevo)
 use Livewire\Component;
 
 class AbsencesIndex extends Component
 {
+    public $year;
+    
+    public function mount(){
+        $this->year = now()->format('Y');
+    }
+
     public function render()
     {
-        return view('livewire.welfare.amipass.absences-index', [
-            'records' => Absence::where('rut', auth()->id())
-                ->paginate(50),
-        ]);
+        if($this->year == 2023){
+            return view('livewire.welfare.amipass.absences-index', [
+                'records' => Absence::where('rut', auth()->id())->whereYear('fecha_inicio',2023)
+                    ->paginate(50),
+            ]);
+        }else{
+            return view('livewire.welfare.amipass.absences-index', [
+                'records' => Absenteeism::where('rut', auth()->id())->whereYear('finicio',2023)
+                    ->paginate(50),
+            ]);
+        }
+        
     }
 }
--- app/Http/Livewire/Welfare/Amipass/ChargeIndex.php
+++ app/Http/Livewire/Welfare/Amipass/ChargeIndex.php
@@ -5,16 +5,106 @@ namespace App\Http\Livewire\Welfare\Amipass;
 use App\Models\Welfare\Amipass\Charge;
 use App\Models\Welfare\Amipass\NewCharge;
 use App\Models\Welfare\Amipass\Regularization;
+use App\Models\Rrhh\AbsenteeismType;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use App\Models\Parameters\Holiday;
+use App\User;
+
 use Livewire\Component;
 
 class ChargeIndex extends Component
 {
+    public $year;
+
+    public function mount(){
+        $this->year = now()->format('Y');
+    }
+
     public function render()
     {
-        return view('livewire.welfare.amipass.charge-index', [
-            'records' => Charge::where('rut', auth()->id())->get(),
-            'regularizations' => Regularization::where('rut', auth()->id())->get(),
-            'new_records' => NewCharge::where('rut', auth()->id())->get()
-        ]);
+        if($this->year == 2023){
+            return view('livewire.welfare.amipass.charge-index', [
+                'records' => Charge::where('rut', auth()->id())->get(),
+                'regularizations' => Regularization::where('rut', auth()->id())->get(),
+                'new_records' => NewCharge::where('rut', auth()->id())->get()
+            ]);
+        }
+        else{
+            $this->calculatedData = [];
+            $this->userWithContracts = null;
+            
+            // aqui obtener info como en report by dates
+            // $start = $this->year.'-01-01';
+            // $end = $this->year.'-12-31';
+            $start = '2023-01-01';
+            $end = '2023-12-31';
+            
+            $compensatoryAbsenteeismType = AbsenteeismType::find(5); // dias compensatorios
+            $periods = CarbonPeriod::create($start, '1 month', $end);
+    
+            foreach($periods as $period){
+                $startDate = $period->copy();
+                $endDate = $period->endOfMonth();
+    
+                // dd($startDate, $endDate);
+    
+                $holidays = Holiday::whereBetween('date', [$startDate, $endDate])->get();
+                
+    
+                /* Obtener los usuarios que tienen contratos en un rango de fecha con sus ausentismos */
+                $this->userWithContracts = User::with([
+                    'contracts' => function ($query) use ($startDate, $endDate) {
+                        $query->where(function ($query) use ($startDate, $endDate) {
+                            $query->whereDate('fecha_inicio_contrato', '<=', $endDate)
+                                    ->whereDate('fecha_termino_contrato', '>=', $startDate);
+                                    // ->where('shift',0);
+                        });
+                    },
+                    'absenteeisms' => function ($query) use ($startDate, $endDate) {
+                        $query->where(function ($query) use ($startDate, $endDate) {
+                            $query->whereDate('finicio', '<=', $endDate)
+                                ->whereDate('ftermino', '>=', $startDate)
+                                ->where('absenteeism_type_id','!=',5);
+                            });
+                    },
+                    'amiLoads' => function ($query) use ($startDate, $endDate) {
+                        $query->where(function ($query) use ($startDate, $endDate) {
+                            $query->whereMonth('fecha',$startDate->month);
+                            });
+                    },
+                    'shifts' => function ($query) use ($startDate, $endDate) {
+                        $query->where(function ($query) use ($startDate, $endDate) {
+                            $query->where('year',$startDate->year)
+                                    ->where('month',$startDate->month);
+                            });
+                    },
+                    'compensatoryDays' => function ($query) use ($startDate, $endDate) {
+                        $query->where(function ($query) use ($startDate, $endDate) {
+                            $query->whereDate('start_date', '<=', $endDate)
+                                ->whereDate('end_date', '>=', $startDate);
+                            });
+                    },
+                    'absenteeisms.type'
+                ])
+                ->whereHas('contracts', function ($query) use ($startDate, $endDate) {
+                    $query->where(function ($query) use ($startDate, $endDate) {
+                        $query->whereDate('fecha_inicio_contrato', '<=', $endDate)
+                            ->whereDate('fecha_termino_contrato', '>=', $startDate);
+                            // ->where('shift',0); se traen todos, abajo se hace el filtro
+                    });
+                })
+                ->whereIn('id',[auth()->id()])
+                ->get();  
+    
+                if($this->userWithContracts->count() > 0){
+                    $this->calculatedData[$startDate->format('Y-m')] = $this->userWithContracts->first()->getAmipassData($startDate,$endDate,$holidays,$compensatoryAbsenteeismType);   
+                }
+                
+            }
+
+            return view('livewire.welfare.amipass.charge-index');
+        }
+        
     }
 }
