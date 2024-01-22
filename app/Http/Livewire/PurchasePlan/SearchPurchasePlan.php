3b019d3a79c9eac21a20a16850f0adde143f055b
mirandal <mirandal.jorge@gmail.com>
2024-01-22 12:05:54 -0300

    PurchasePlan: se agrega RRFF u.o. inferiores

--- app/Http/Livewire/PurchasePlan/SearchPurchasePlan.php
+++ app/Http/Livewire/PurchasePlan/SearchPurchasePlan.php
@@ -37,12 +37,19 @@ class SearchPurchasePlan extends Component
         //             ->toSql();
         // dd($result);
         if($this->index == 'own'){
-            //PLANES DE COMPRAS: OWN INDEX 
+            //PLANES DE COMPRAS: OWN INDEX
+            $organizationalUnitIn = array();
+            $organizationalUnitIn[] = Auth::user()->organizationalUnit->id;
+
+            if(Auth::user()->organizationalUnit->id == Parameter::get('ou', 'ControlEquipos') || Auth::user()->organizationalUnit->id == Parameter::get('ou', 'ControlInfraestructura')){
+                $organizationalUnitIn[] = Parameter::get('ou', 'DeptoRRFF');
+            }
+
             return view('livewire.purchase-plan.search-purchase-plan', [
                 'purchasePlans' => PurchasePlan::with('organizationalUnit.establishment', 'approvals.sentToOu', 'userResponsible', 'userCreator')->latest()
                     ->where('user_creator_id', Auth::user()->id)
                     ->orWhere('user_responsible_id', Auth::user()->id)
-                    ->orWhere('organizational_unit_id', Auth::user()->organizationalUnit->id)
+                    ->orWhereIn('organizational_unit_id', $organizationalUnitIn)
                     ->when(Auth::user()->organizationalUnit->id == Parameter::get('ou', 'SaludMentalSSI'),
                         fn($q) => $q->orwhereHas('organizationalUnit', 
                             fn($q2) => $q2->whereIn('establishment_id', explode(',', Parameter::get('establishment', 'EstablecimientosDispositivos')))))
