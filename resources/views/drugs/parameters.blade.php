399d50836a0abc837647ed124ccda7cbd3fac3c4
AquaroTorres <alvarotorres@gmail.com>
2024-01-31 13:21:28 -0300

    Drugs:Fix: parameters, agregar key a los livewires

--- resources/views/drugs/parameters.blade.php
+++ resources/views/drugs/parameters.blade.php
@@ -12,37 +12,37 @@
     'module' => 'drugs',
     'parameterName' => 'Jefe',
     'type' => 'user'
-])
+], key('Jefe'))
 
 @livewire('parameters.parameter.single-manager',[
     'module'=>'drugs',
     'parameterName' => 'CargoJefe',
     'type' => 'value'
-])
+], key('Jefe'))
 
 @livewire('parameters.parameter.single-manager',[
     'module'=>'drugs',
     'parameterName'=>'MinistroDeFe',
     'type' => 'user'
-])
+], key('MinistroDeFe'))
 
 @livewire('parameters.parameter.single-manager',[
     'module'=>'drugs',
     'parameterName'=>'MinistroDeFeJuridico',
     'type' => 'user'
-])
+], key('MinistroDeFeJuridico'))
 
 @livewire('parameters.parameter.single-manager',[
     'module'=>'drugs',
     'parameterName'=>'Mandatado',
     'type' => 'user'
-])
+], key('Mandatado'))
 
 @livewire('parameters.parameter.single-manager',[
     'module'=>'drugs',
     'parameterName'=>'MandatadoResolucion',
     'type' => 'value'
-])
+], key('MandatadoResolucion'))
 
 
 @endsection
