8f46f2eb8741aabe7e19bd2d0393914f665fc688
AquaroTorres <alvarotorres@gmail.com>
2024-02-15 16:35:07 -0300

    Documents:Feat: Report por OU y usuario

--- resources/views/documents/report.blade.php
+++ resources/views/documents/report.blade.php
@@ -6,38 +6,21 @@
 
 @include('documents.partials.nav')
 
-<h3 class="mb-3">Reporte de documentos
-    <small>Creados: <strong>{{ $ct }}</strong></small>
+<h3 class="mb-3">
+    Ranking de documentos creados en iOnline
 </h3>
+<h4>
+    {{ auth()->user()->organizationalUnit->establishment->name }}
+    :  {{ number_format($ct,0,',','.') }} documentos
+</h4>
 
 <div class="row">
-    <div class="col-md-6 col-12">
-
-        <table class="table table-sm">
-            <thead>
-                <tr>
-                    <th>Usuario</th>
-                    <th class="text-center">Total</th>
-                </tr>
-            </thead>
-            @foreach($users as $user)
-            <tbody>
-                <tr>
-                    <td>{{ $user->fullName }}</td>
-                    <td class="text-center">{{ $user->documents_count }}</td>
-                    <td class="text-center"></td>
-                </tr>
-            </tbody>
-            @endforeach
-        </table>
-    </div>
-    
-    <div class="col-md-6 col-12">
+    <div class="col-md-7 col-12">
         
         <table class="table table-sm">
             <thead>
                 <tr>
-                    <th>Unidad Organizacional</th>
+                    <th>Por Unidad Organizacional</th>
                     <th class="text-center">Total</th>
                 </tr>
             </thead>
@@ -45,7 +28,26 @@
             <tbody>
                 <tr>
                     <td>{{ $ou->name }}</td>
-                    <td class="text-center">{{ $ou->documents_count }}</td>
+                    <td class="text-center">{{ number_format($ou->documents_count,0,',','.') }}</td>
+                </tr>
+            </tbody>
+            @endforeach
+        </table>
+    </div>
+
+    <div class="col-md-5 col-12">
+        <table class="table table-sm">
+            <thead>
+                <tr>
+                    <th>Por Funcionario</th>
+                    <th class="text-center">Total</th>
+                </tr>
+            </thead>
+            @foreach($users as $user)
+            <tbody>
+                <tr>
+                    <td>{{ $user->shortName }}</td>
+                    <td class="text-center">{{ number_format($user->documents_count,0,',','.') }}</td>
                 </tr>
             </tbody>
             @endforeach
