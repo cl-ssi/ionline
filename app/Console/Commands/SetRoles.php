626f93e9564a7fca34f7e048f40a6056fe9a7680
AquaroTorres <alvarotorres@gmail.com>
2024-02-21 17:02:30 -0300

    Command: Set rol a usuarios que tengan los permisos contenidos en ese rol

---
+++ app/Console/Commands/SetRoles.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+use Spatie\Permission\Models\Role;
+
+class SetRoles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'set:roles {rol_name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $rol_name = $this->argument('rol_name');
+
+        // Get all permissions from rol {rol_name}
+        $permissions = Role::findByName($rol_name)->permissions;
+
+        // Obtener todos los usuarios que tengan el conjunto de permisos $permissions
+        $users = User::permission($permissions)->get();
+
+        foreach ($users as $user) {
+            // set the rol {rol_name} to the user
+            $user->assignRole($rol_name);
+            echo "Set rol " . $rol_name . " to user: " . $user->name . "\n";
+        }
+
+        // Get all users whe have the rol {rol_name}
+        $users = User::role($rol_name)->get();
+
+        foreach ($users as $user) {
+            foreach ($permissions as $permission) {
+                $user->revokePermissionTo($permission);
+            }
+            echo "Revoked all permissions from user: " . $user->name . "\n";
+        }
+
+        return Command::SUCCESS;
+    }
+}
