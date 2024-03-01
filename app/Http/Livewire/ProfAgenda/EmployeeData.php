e5a55ea55b3dbedcc7104c85705db9cb250fcd0c
sickiqq <sick_iqq@hotmail.com>
2024-03-01 13:10:10 -0300

    Agenda ust: validación para verificar funcionarios con 3 faltas consecutivas en los ultimos 2 meses

--- app/Http/Livewire/ProfAgenda/EmployeeData.php
+++ app/Http/Livewire/ProfAgenda/EmployeeData.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\ProfAgenda;
 use Livewire\Component;
 use App\Models\ClCommune;
 use App\User;
+use App\Models\ProfAgenda\OpenHour;
 
 class EmployeeData extends Component
 {
@@ -12,6 +13,7 @@ class EmployeeData extends Component
     public $dv;
     public $email;
     public $message;
+    public $flag_more_than_3_faults = false;
 
     // public function mount(){
     //     dd($this->user_id);
@@ -32,19 +34,40 @@ class EmployeeData extends Component
 
         $this->message = "";
         if ($this->user_id > 3000000) {
-          $user = User::find($this->user_id);
-          $this->emit('renderFromEmployeeData');
-          if ($user) {
-            $this->email = $user->email;
-          }else{
-            // validación correo
-            if ($this->email != null) {
-              $user = User::where('email',$this->email)->first();
-              if ($user != null) {
-                $this->message = "No es posible utilizar el coreo " . $this->email . ", ya está siendo utilizado por " . $user->getFullNameUpperAttribute();
-              }
+            $user = User::find($this->user_id);
+            $this->emit('renderFromEmployeeData');
+
+            // validación para verificar funcionarios con 3 faltas consecutivas en los ultimos 2 meses
+            $twoMonthsAgo = now()->subMonths(2);
+            $openHours = OpenHour::where('patient_id',$user->id)
+                                ->orderBy('start_date')
+                                ->where('start_date', '>=', $twoMonthsAgo)
+                                ->get();
+            $count = 0;
+            foreach($openHours as $openHour){
+                if($openHour->assistance === 0){
+                    $count += 1;
+                }
+                if($openHour->assistance !== 0){
+                    $count = 0;
+                }
+                if($count >= 3){
+                    $this->flag_more_than_3_faults = true;
+                }
             }
-          }
+
+            // validación datos de usuario
+            if ($user) {
+                $this->email = $user->email;
+            }else{
+                // validación correo
+                if ($this->email != null) {
+                $user = User::where('email',$this->email)->first();
+                if ($user != null) {
+                    $this->message = "No es posible utilizar el coreo " . $this->email . ", ya está siendo utilizado por " . $user->getFullNameUpperAttribute();
+                }
+                }
+            }
         }
 
         return view('livewire.prof-agenda.employee-data',compact('user','communes'));
--- resources/views/livewire/prof-agenda/employee-data.blade.php
+++ resources/views/livewire/prof-agenda/employee-data.blade.php
@@ -1,35 +1,35 @@
 <div>
     <div class="form-row">
 
-    <fieldset class="form-group col-5 col-md-2">
-        <label for="for_run">Run (sin DV)</label>
-        <input type="number" min="1" max="50000000" class="form-control" id="for_user_id" name="user_id" wire:model.lazy="user_id" required>
-    </fieldset>
+        <fieldset class="form-group col-5 col-md-2">
+            <label for="for_run">Run (sin DV)</label>
+            <input type="number" min="1" max="50000000" class="form-control" id="for_user_id" name="user_id" wire:model.lazy="user_id" required>
+        </fieldset>
 
-    <fieldset class="form-group col-3 col-md-1">
-        <label for="for_dv">Digito</label>
-        <input type="text" class="form-control" id="for_dv" name="dv" wire:model.lazy="dv" required>
-    </fieldset>
+        <fieldset class="form-group col-3 col-md-1">
+            <label for="for_dv">Digito</label>
+            <input type="text" class="form-control" id="for_dv" name="dv" wire:model.lazy="dv" required>
+        </fieldset>
 
-    <!-- <fieldset class="form-group col-3 col-md-1">
-        <label for="">&nbsp;</label>
-        <button type="button" id="btn_fonasa" class="btn btn-outline-info">Fonasa&nbsp;</button>
-    </fieldset> -->
+        <!-- <fieldset class="form-group col-3 col-md-1">
+            <label for="">&nbsp;</label>
+            <button type="button" id="btn_fonasa" class="btn btn-outline-info">Fonasa&nbsp;</button>
+        </fieldset> -->
 
-    <fieldset class="form-group col-12 col-md-3">
-        <label for="for_name">Nombres</label>
-        <input type="text" class="form-control" id="for_name" name="name" required="required" @if($user) value="{{$user->name}}" @endif>
-    </fieldset>
+        <fieldset class="form-group col-12 col-md-3">
+            <label for="for_name">Nombres</label>
+            <input type="text" class="form-control" id="for_name" name="name" required="required" @if($user) value="{{$user->name}}" @endif>
+        </fieldset>
 
-    <fieldset class="form-group col-12 col-md">
-        <label for="for_name">Apellido Paterno</label>
-        <input type="text" class="form-control" id="for_fathers_family" name="fathers_family" required="required" @if($user) value="{{$user->fathers_family}}" @endif>
-    </fieldset>
+        <fieldset class="form-group col-12 col-md">
+            <label for="for_name">Apellido Paterno</label>
+            <input type="text" class="form-control" id="for_fathers_family" name="fathers_family" required="required" @if($user) value="{{$user->fathers_family}}" @endif>
+        </fieldset>
 
-    <fieldset class="form-group col-12 col-md">
-        <label for="for_name">Apellido Materno</label>
-        <input type="text" class="form-control" id="for_mothers_family" name="mothers_family" required="required" @if($user) value="{{$user->mothers_family}}" @endif>
-    </fieldset>
+        <fieldset class="form-group col-12 col-md">
+            <label for="for_name">Apellido Materno</label>
+            <input type="text" class="form-control" id="for_mothers_family" name="mothers_family" required="required" @if($user) value="{{$user->mothers_family}}" @endif>
+        </fieldset>
 
     </div>
 
@@ -53,35 +53,41 @@
 
     <div class="form-row">
 
-    <fieldset class="form-group col-12 col-md-5">
-        <label for="for_address">Dirección</label>
-        <input type="text" class="form-control" id="foraddress" placeholder="Dirección, comuna"
-            name="address" @if($user) value="{{$user->address}}" @endif>
-    </fieldset>
+        <fieldset class="form-group col-12 col-md-5">
+            <label for="for_address">Dirección</label>
+            <input type="text" class="form-control" id="foraddress" placeholder="Dirección, comuna"
+                name="address" @if($user) value="{{$user->address}}" @endif>
+        </fieldset>
 
-    <fieldset class="form-group col-12 col-md-2">
-        <label for="for_commune_id">Comuna</label>
-        <select name="commune_id" class="form-control">
-        <option value=""></option>
-        @foreach($communes as $key => $commune)
-            <option value="{{$commune->id}}" @selected($user && $user->commune_id == $commune->id)>{{$commune->name}}</option>
-        @endforeach
-        </select>
-    </fieldset>
+        <fieldset class="form-group col-12 col-md-2">
+            <label for="for_commune_id">Comuna</label>
+            <select name="commune_id" class="form-control">
+            <option value=""></option>
+            @foreach($communes as $key => $commune)
+                <option value="{{$commune->id}}" @selected($user && $user->commune_id == $commune->id)>{{$commune->name}}</option>
+            @endforeach
+            </select>
+        </fieldset>
 
-    <fieldset class="form-group col-12 col-md-2">
-        <label for="for_phone_number">N.telefónico</label>
-        <input type="text" class="form-control" id="for_phone_number"
-            name="phone_number" @if($user) value="{{$user->phone_number}}" @endif>
-    </fieldset>
+        <fieldset class="form-group col-12 col-md-2">
+            <label for="for_phone_number">N.telefónico</label>
+            <input type="text" class="form-control" id="for_phone_number"
+                name="phone_number" @if($user) value="{{$user->phone_number}}" @endif>
+        </fieldset>
 
-    <fieldset class="form-group col-12 col-md-3">
-        <label for="for_email">Email institucional*</label>
-        <input type="email" class="form-control" id="for_email" wire:model.lazy="email"
-            name="email" @if($user) value="{{$user->email}}" @endif>
-    </fieldset>
+        <fieldset class="form-group col-12 col-md-3">
+            <label for="for_email">Email institucional*</label>
+            <input type="email" class="form-control" id="for_email" wire:model.lazy="email"
+                name="email" @if($user) value="{{$user->email}}" @endif>
+        </fieldset>
 
     </div>
+
+    @if($flag_more_than_3_faults)
+        <div class="alert alert-danger" role="alert">
+            Atención!! Hemos detectado que el funcionario tiene 3 faltas consecutivas en los últimos dos meses
+        </div>
+    @endif
 
     <br>
     @if($message != "")
