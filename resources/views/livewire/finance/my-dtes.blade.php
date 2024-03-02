248b2339f9ad535094f963791085205912563270
AquaroTorres <alvarotorres@gmail.com>
2024-03-02 10:24:58 -0300

    Dtes:Feat:Bande "Mis Dtes" Fixes #1999

---
+++ resources/views/livewire/finance/my-dtes.blade.php
@@ -0,0 +1,63 @@
+<div>
+    @include('finance.payments.partials.nav')
+
+    <h3 class="mb-3">Mis Documentos Tributarios Electrónicos</h3>
+
+
+    <table class="table table-sm table-bordered" wire:loading.class="text-muted">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th width="55px">Estb.</th>
+                <th>Documento</th>
+                <th width="140px">OC</th>
+                <th>FR</th>
+                <th>Recepción</th>
+                <th>Bodega</th>
+                <th>Pagado</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($dtes as $dte)
+            <tr class="{{ $dte->rowClass }}">
+                    <td class="text-center">
+                        {{ $dte->id }}
+                    </td>
+                    <td>
+                        {{ $dte->establishment?->alias }}
+                    </td>
+                    <td class="small">
+                        @include('finance.payments.partials.dte-info')
+                    </td>
+                    <td class="small">
+                        @livewire('finance.get-purchase-order', ['dte' => $dte], key($dte->id))
+                    </td>
+                    <td class="small">
+                        @include('finance.payments.partials.fr-info')
+
+                        {{ $dte->requestForm?->contractManager?->tinnyName }} 
+                        {{ $dte->contractManager?->tinnyName }}
+                        <br>
+                        {{ $dte->estado_reclamo }}
+                    </td>
+                    <td class="small">
+                        <!-- Nuevo módulo de Recepciones -->
+                        @include('finance.payments.partials.receptions-info')
+                    </td>
+
+                    <td class="small">
+                        -- pendiente
+                    </td>
+                    <td>
+                        @if($dte->tgrPayedDte)
+                            <i class="fas fa-thumbs-up text-success"></i>
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    {{ $dtes->links() }}
+
+</div>
