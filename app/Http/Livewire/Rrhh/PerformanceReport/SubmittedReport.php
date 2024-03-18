57226d764ca9b3a879b0b428aeaaf9b2f83e8db9
Esteban Miranda <tebiccr@gmail.com>
2024-03-18 16:45:51 -0300

    calificaciones: avance mas operativo cabecera

--- app/Http/Livewire/Rrhh/PerformanceReport/SubmittedReport.php
+++ app/Http/Livewire/Rrhh/PerformanceReport/SubmittedReport.php
@@ -14,21 +14,27 @@ class SubmittedReport extends Component
     public $organizationalUnit;
     public $year;
     public $periods;
+    public $selectedUserData;
+    public $selectedUser = null;    
+    public $selectedPeriod = null;
 
     public function mount()
     {
         $loggedInUser = auth()->user();
         $this->organizationalUnit = $loggedInUser->organizationalUnit->name;
-        $this->users = User::where('organizational_unit_id', $loggedInUser->organizational_unit_id)
-                   ->orderBy('name')
-                   ->get();
+        $this->users = User::whereHas('contracts')
+        ->where('organizational_unit_id', $loggedInUser->organizational_unit_id)
+        ->where('id', '!=', $loggedInUser->id)
+        ->orderBy('name')
+        ->get();
 
                 // Obtener el año seleccionado
         // Obtener el año seleccionado
         $year = $this->year ?? now()->year;
         $this->periods = PerformanceReportPeriod::where('year', $year)->get();
         $this->periods = $this->periods->sortBy('start_at');
-
+        $this->selectedUser = null;
+        $this->selectedPeriod = null;
     }
 
 
@@ -36,4 +42,12 @@ class SubmittedReport extends Component
     {
         return view('livewire.rrhh.performance-report.submitted-report');
     }
+
+    public function showForm($userId, $periodId)
+    {
+        $this->selectedUser = User::find($userId);
+        $this->selectedPeriod = PerformanceReportPeriod::find($periodId);
+    }
+
+
 }
