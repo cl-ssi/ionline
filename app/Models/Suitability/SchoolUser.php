a01dd88826c5a491fbbdf173117a01ffa39ab8fe
Esteban Miranda <tebiccr@gmail.com>
2024-03-21 17:16:53 -0300

    Idoneidad: use SoftDeletes;

--- app/Models/Suitability/SchoolUser.php
+++ app/Models/Suitability/SchoolUser.php
@@ -4,10 +4,12 @@ namespace App\Models\Suitability;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SchoolUser extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     public $table = 'school_users_external';
 
     protected $dates = [
