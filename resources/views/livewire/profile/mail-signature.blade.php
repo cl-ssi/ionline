d9a1a14e62e38a8ada70749bfe23ed007b56b2d1
AquaroTorres <alvarotorres@gmail.com>
2024-03-21 17:20:25 -0300

    Plantilla Firma 100 años de salud y seguridad social

--- resources/views/livewire/profile/mail-signature.blade.php
+++ resources/views/livewire/profile/mail-signature.blade.php
@@ -99,17 +99,13 @@
         </span>
         <br>
 
-        <a href="{{ route('redirect-url.signature') }}">
-            <img src="{{ asset('images/firma_banner_400px.gif') }}"
-                alt="Banner Firma"
-            >
-        </a>
 
     </address>
 
 
     <br><br><br>
 
+    <!--
     <strong>Ejemplo de firma 1</strong>
 
     <address class="border p-2 mb-3">
@@ -183,80 +179,86 @@
     <br><br><br>
 
 
+    -->
 
-
-    <strong>Ejemplo de firma 2</strong>
+    <strong>Formato SST</strong>
     <address class="border p-2 mb-3">
-    Estimado {{ auth()->user()->shortName }}<br><br>
-    El presente correo electrónico es para lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut est sint iure minus accusantium quidem, eligendi in aut ab tempore nihil, modi iusto quasi. Tempora asperiores quas rem libero iusto?
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis deleniti, aspernatur, autem pariatur dolores magni, soluta fugiat nostrum omnis voluptatum voluptas ipsum ut dolore eum libero! Accusantium odio omnis ipsam.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt nostrum ratione repellendus dolor, eligendi necessitatibus saepe odit, illum, voluptatum eaque odio culpa minus nisi ullam voluptatem perferendis error labore expedita.
-    <br><br>
-    <div class="row">
-        <div class="ml-3 mr-3 mt-4" >
-            <img src="/images/firma_camp.png" 
-                width="190" 
-                alt="Logo Vacuna Dupla Influenza y Covid19">
-        </div>
-        <div class="">
-            <span class="raya_azul">━━━</span><span class="raya_rojo">━━━━━</span><br>
-            <span class="small">
-                <strong>{{ $user->shortName }}
-                    @if($pronom)
-                    ({{ $pronom }})
-                    @endif
-                </strong>
-                <br>
-            </span>
-
-            @if($user->position)
-                <span class="text-muted small">
-                    @if($user->position == 'Jefe' OR
-                        $user->position == 'Director' OR
-                        $user->position == 'Jefa' OR
-                        $user->position == 'Directora')
-                            {{ $user->position }}
-                    @elseif($user->position != NULL)
-                        <em>{{ $user->position }}</em>
-                    @endif
+        Estimado {{ auth()->user()->shortName }}<br><br>
+        El presente correo electrónico es para lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut est sint iure minus accusantium quidem, eligendi in aut ab tempore nihil, modi iusto quasi. Tempora asperiores quas rem libero iusto?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis deleniti, aspernatur, autem pariatur dolores magni, soluta fugiat nostrum omnis voluptatum voluptas ipsum ut dolore eum libero! Accusantium odio omnis ipsam.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt nostrum ratione repellendus dolor, eligendi necessitatibus saepe odit, illum, voluptatum eaque odio culpa minus nisi ullam voluptatem perferendis error labore expedita.
+        <br><br>
+        <div class="row">
+            <div class="ml-3 mr-3 mt-4" >
+                <img src="/images/logo_100_años_salud_y_seguridad_social.png" 
+                    width="190" 
+                    alt="100 Años Salud y Seguridad Social">
+            </div>
+            <div class="">
+                <span class="raya_azul">━━━</span><span class="raya_rojo">━━━━━</span><br>
+                <span class="small">
+                    <strong>{{ $user->shortName }}
+                        @if($pronom)
+                        ({{ $pronom }})
+                        @endif
+                    </strong>
+                    <br>
                 </span>
-            @endif
 
-            @if($user->organizationalUnit)
-                <span class="small">{{ $user->organizationalUnit->name }}</span>
+                @if($user->position)
+                    <span class="text-muted small">
+                        @if($user->position == 'Jefe' OR
+                            $user->position == 'Director' OR
+                            $user->position == 'Jefa' OR
+                            $user->position == 'Directora')
+                                {{ $user->position }}
+                        @elseif($user->position != NULL)
+                            <em>{{ $user->position }}</em>
+                        @endif
+                    </span>
+                    <br>
+                @endif
+
+                @if($user->organizationalUnit)
+                    <span class="small">{{ $user->organizationalUnit->name }}</span>
+                    <br>
+                @endif
+
+
+                @foreach($user->telephones as $telephone)
+                    <span class="small">Teléfono: <a href="tel:+56{{ $telephone->number }}">+56 {{ $telephone->number }}</a> /  
+                    Anexo: {{ $telephone->minsal }}</span>
+                    <br>
+                @endforeach
+
+                @if($personalPhone)
+                    <span class="small">Teléfono: <a href="tel:+56{{ $personalPhone }}">+56 {{ $personalPhone }}</a></span>
+                    <br>
+                @endif
+
+                @if($user->email)
+                    <span class="small"><a href="mailto:{{ $user->email }}">{{ $user->email }}</a></span>
+                    <br>
+                @endif
+
+
+                <span class="small">
+                    <strong class="text-muted">
+                    <br>
+                    {{ optional($user->organizationalUnit)->establishment->official_name ?? '' }}<br>
+                    Gobierno de Chile
+                    </strong>
+                </span>
+
                 <br>
-            @endif
 
-
-            @foreach($user->telephones as $telephone)
-                <span class="small">Teléfono: <a href="tel:+56{{ $telephone->number }}">+56 {{ $telephone->number }}</a> /  
-                Anexo: {{ $telephone->minsal }}</span>
-                <br>
-            @endforeach
-
-            @if($personalPhone)
-                <span class="small">Teléfono: <a href="tel:+56{{ $personalPhone }}">+56 {{ $personalPhone }}</a></span>
-                <br>
-            @endif
-
-            @if($user->email)
-                <span class="small"><a href="mailto:{{ $user->email }}">{{ $user->email }}</a></span>
-                <br>
-            @endif
-
-
-            <span class="small">
-                <strong class="text-muted"><br>
-                {{ optional($user->organizationalUnit)->establishment->official_name ?? '' }}<br>
-                Gobierno de Chile
-                </strong>
-            </span>
-
-            <br>
+            </div>
         </div>
-    </div>
-
-
+        <a href="{{ route('redirect-url.signature') }}">
+            <img src="{{ asset('images/firma_banner_400px.gif') }}"
+                alt="Banner Firma"
+                >
+        </a>
     </address>
 
 
