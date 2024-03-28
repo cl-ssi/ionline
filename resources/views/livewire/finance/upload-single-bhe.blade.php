6b904e49c7b5b361c695dc8caf73c6c282fb51f1
AquaroTorres <alvarotorres@gmail.com>
2024-03-28 15:51:13 -0300

    Upload Single BHE - Formato

--- resources/views/livewire/finance/upload-single-bhe.blade.php
+++ resources/views/livewire/finance/upload-single-bhe.blade.php
@@ -46,6 +46,37 @@
             </div>
 
             <div class="form-group col-2">
+                <label for="emision">Fecha</label>
+                <input type="date" class="form-control" id="emision" wire:model.defer="dte.emision" autocomplete="off">
+                <div id="emision-helper" class="form-text">
+                    @if($dte->isDirty('emision'))
+                        {{ $dte->getOriginal('emision') }}
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group col-2">
+                <label for="monto_total">Monto Total*</label>
+                <input type="text" class="form-control" id="montoTotal" wire:model.defer="dte.monto_total"
+                    autocomplete="off" min="1">
+                @error('dte.monto_total') <span class="text-danger">{{ $message }}</span> @enderror
+                <div id="monto_total-helper" class="form-text">
+                    @if($dte->isDirty('monto_total'))
+                        {{ $dte->getOriginal('monto_total') }}
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group col-1">
+                <label for="folio_oc">Documento</label>
+                <a class="btn btn-outline-danger form-control" target="_blank" href="{{ $dte->uri }}">
+                        <i class="bi bi-file-pdf"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="row g-2 mb-3">
+            <div class="form-group col-2">
                 <label for="emisor">RUT Emisor</label>
                 <input type="text" class="form-control" id="emisor" wire:model.defer="dte.emisor"
                     autocomplete="off">
@@ -57,37 +88,13 @@
                 </div>
             </div>
 
-            <div class="form-group col-4">
+            <div class="form-group col-6">
                 <label for="razonSocial">Razón Social</label>
                 <input type="text" class="form-control" id="razon_social_emisor" wire:model.defer="dte.razon_social_emisor"
                     autocomplete="off">
                 <div id="razon_social_emisor-helper" class="form-text">
                     @if($dte->isDirty('razon_social_emisor'))
                         {{ $dte->getOriginal('razon_social_emisor') }}
-                    @endif
-                </div>
-            </div>
-
-            <div class="form-group col-2">
-                <label for="emision">Fecha</label>
-                <input type="date" class="form-control" id="emision" wire:model.defer="dte.emision" autocomplete="off">
-                <div id="emision-helper" class="form-text">
-                    @if($dte->isDirty('emision'))
-                        {{ $dte->getOriginal('emision') }}
-                    @endif
-                </div>
-            </div>
-        </div>
-
-        <div class="row g-2 mb-3">
-            <div class="form-group col-2">
-                <label for="monto_total">Monto Total*</label>
-                <input type="text" class="form-control" id="montoTotal" wire:model.defer="dte.monto_total"
-                    autocomplete="off" min="1">
-                @error('dte.monto_total') <span class="text-danger">{{ $message }}</span> @enderror
-                <div id="monto_total-helper" class="form-text">
-                    @if($dte->isDirty('monto_total'))
-                        {{ $dte->getOriginal('monto_total') }}
                     @endif
                 </div>
             </div>
@@ -109,19 +116,8 @@
                 @error('dte.folio_oc') <span class="text-danger">{{ $message }}</span> @enderror
             </div>
 
-            <div class="form-group col-1">
-                <label for="folio_oc">Documento</label>
-                <a class="btn btn-outline-danger form-control" target="_blank" href="{{ $dte->uri }}">
-                        <i class="bi bi-file-pdf"></i>
-                </a>
-            </div>
-
-            <div class="form-group offset-3 col-2">
-                <label for="btn-save">&nbsp;</label>
-                <button class="btn btn-primary form-control" wire:click="save">Guardar</button>
-            </div>
-
         </div>
+        <button class="btn btn-primary" wire:click="save">Guardar</button>
     @endif
 
     @if($bhe_to_text)
