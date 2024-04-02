7d16858b7e46c47bd2edfc71a38b1c5c0a02c782
mirandal <mirandal.jorge@gmail.com>
2024-04-02 09:58:14 -0300

    Plan de Compras: Editar en approvals

--- resources/views/purchase_plan/show_approval.blade.php
+++ resources/views/purchase_plan/show_approval.blade.php
@@ -1,7 +1,8 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" data-mutate-approach="sync"></script>
 
 <div class="row">
-    <div class="col-12">
+    <div class="col-6">
         <h5 class="mb-3">
             <i class="fas fa-shopping-cart"></i> Plan de Compra: ID {{ $purchasePlan->id }}
             @switch($purchasePlan->status)
@@ -16,6 +17,12 @@
                     ''
             @endswitch
         </h5>
+    </div>
+    <div class="col-6">
+        @if($purchasePlan->canEdit())
+            <a href="{{ route('purchase_plan.edit', $purchasePlan->id) }}" target="_blank"
+                class="btn btn-primary btn-sm mb-1"><i class="fas fa-edit fa-fw"></i> Editar</a>
+        @endif
     </div>
 </div>
 
