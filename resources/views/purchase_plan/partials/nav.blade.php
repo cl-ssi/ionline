dbdbe52baf4f5a6642513729efd1770bd34dccf9
mirandal <mirandal.jorge@gmail.com>
2024-04-02 15:14:50 -0300

    Plan de Compras: se habilita creación

--- resources/views/purchase_plan/partials/nav.blade.php
+++ resources/views/purchase_plan/partials/nav.blade.php
@@ -7,8 +7,8 @@
                 <li><a class="dropdown-item" href="{{ route('purchase_plan.all_index') }}"><i class="fas fa-inbox"></i> Todos los Plan de Compras</a></li>
             @endcan
             <li><a class="dropdown-item" href="{{ route('purchase_plan.pending_index') }}"><i class="fas fa-inbox"></i> Pendientes de firmar</a></li>
-            {{--<li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="{{ route('purchase_plan.create') }}"><i class="fas fa-plus"></i> Nuevo Plan de Compras</a></li>--}}
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="{{ route('purchase_plan.create') }}"><i class="fas fa-plus"></i> Nuevo Plan de Compras</a></li>
         </ul>
     </li>
     
