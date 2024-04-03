c61e467319234134b016f66c3c7321781fcafb42
m.soto.r.2021 <m.soto.r.2021@gmail.com>
2024-04-03 06:54:26 -0300

    Crear Calendario Vehiculos Fixes #2275

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,7 @@ use App\Http\Controllers\JobPositionProfiles\JobPositionProfileController;
 use App\Http\Controllers\JobPositionProfiles\JobPositionProfileSignController;
 use App\Http\Controllers\JobPositionProfiles\MessageController;
 // use App\Http\Controllers\Lobby\MeetingController;
+use App\Http\Controllers\CalendarEventController;
 use App\Http\Controllers\Mammography\MammographyController;
 use App\Http\Controllers\Meeting\CommitmentController;
 use App\Http\Controllers\Meeting\MeetingController;
@@ -1449,6 +1450,13 @@ Route::prefix('requirements')->as('requirements.')->middleware(['auth', 'must.ch
 });
 
 Route::view('calendars', 'calendars.index')->name('calendars');
+
+# Calendario Vehiculos
+Route::get('/vehicleCalendar', [CalendarEventController::class, 'index']);
+
+Route::post('/vehicleCalendar', [CalendarEventController::class, 'store']);
+Route::patch('/vehicleCalendar/edit/{event}', [CalendarEventController::class, 'update']);
+Route::delete('/vehicleCalendar/destroy/{event}', [CalendarEventController::class, 'destroy']);
 
 Route::prefix('indicators')->as('indicators.')->group(function () {
     Route::get('/', function () {
