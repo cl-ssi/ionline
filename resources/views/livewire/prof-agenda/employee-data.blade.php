8a04f084a09b559ed180abd6daed8495a5619f13
sickiqq <sick_iqq@hotmail.com>
2024-04-05 11:41:06 -0300

    agenda ust: fix error livewire carga datos de pacientes (campo email).

--- resources/views/livewire/prof-agenda/employee-data.blade.php
+++ resources/views/livewire/prof-agenda/employee-data.blade.php
@@ -77,7 +77,7 @@
 
         <fieldset class="form-group col-12 col-md-3">
             <label for="for_email">Email institucional*</label>
-            <input type="email" class="form-control" id="for_email" wire:model.lazy="email"
+            <input type="email" class="form-control" id="for_email" wire:model.defer="email"
                 name="email" @if($user) value="{{$user->email}}" @endif>
         </fieldset>
 
