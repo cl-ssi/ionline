5214e5459eb7af7896a0d943dc19ece0d4b89c7d
AquaroTorres <alvarotorres@gmail.com>
2024-04-10 11:23:35 -0400

    Meeting:Feat:Relaciones en formato laravel nuevo

--- app/Models/Meetings/Grouping.php
+++ app/Models/Meetings/Grouping.php
@@ -4,6 +4,7 @@ namespace App\Models\Meetings;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
 
@@ -17,8 +18,9 @@ class Grouping extends Model implements Auditable
         'type', 'name', 'meeting_id'
     ];
 
-    public function meeting() {
-        return $this->belongsTo('App\Models\Meetings\Meeting');
+    public function meeting(): BelongsTo
+    {
+        return $this->belongsTo(Meeting::class);
     }
 
     protected $hidden = [
