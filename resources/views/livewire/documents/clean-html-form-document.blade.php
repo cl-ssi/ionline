499b9a883df9c032fceaed880f684805d41711fc
AquaroTorres <alvarotorres@gmail.com>
2024-04-19 01:28:11 -0400

    CodeFormat: Mensaje descriptivo

--- resources/views/livewire/documents/clean-html-form-document.blade.php
+++ resources/views/livewire/documents/clean-html-form-document.blade.php
@@ -1,6 +1,6 @@
 <div>
     <button class="btn btn-sm btn-outline-warning" 
         wire:click="clean"  
-        onclick="confirm('Esto limpiara el formato del documento y las tablas (el contenido del documento no será modificado)') || event.stopImmediatePropagation()"
+        onclick="confirm('Guarde su documento antes de ejecutar esta opción, que limpiara el formato del documento y las tablas (el contenido del documento no será modificado)') || event.stopImmediatePropagation()"
         >Limpiar formato</button>
 </div>
