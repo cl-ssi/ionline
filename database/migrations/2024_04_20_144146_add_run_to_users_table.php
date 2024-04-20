3b3f3ef2dbcb3cbe5b159fae86a6858ebff8f250
AquaroTorres <alvarotorres@gmail.com>
2024-04-20 15:35:26 -0400

    Users:Feat:Add calculated run column

---
+++ database/migrations/2024_04_20_144146_add_run_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('run', 10)
+                  ->virtualAs("UPPER(CONCAT(CAST(id AS CHAR), TRIM(dv)))")
+                  ->after('dv')
+                  ->stored();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('run');
+        });
+    }
+};
