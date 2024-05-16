990f095aeda4d4f2cd186732df08d081b44b9e29
AquaroTorres <alvarotorres@gmail.com>
2024-05-16 11:33:07 -0400

    Drogas:Ultimos accesos y roles

--- resources/views/drugs/users.blade.php
+++ resources/views/drugs/users.blade.php
@@ -8,32 +8,46 @@
 
 <h3 class="mb-3">Usuarios con acceso al módulo de drogas</h3>
 
-<ul>
-    @foreach($users as $user)
-        <li>{{ $user->fullName }}</li>
-    @endforeach
-</ul>
+<div class="row">
 
-<h3 class="mb-3">Ultimos accesos ({{ $daysAgo }} días)</h3>
+    <div class="col">
 
-<table class="table table-sm table-bordered">
-    <thead>
-        <tr>
-            <th>Fecha de acceso</th>
-            <th>Usuario</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($lastLogs as $log )
-        <tr>
-            <td>{{ $log->created_at }}</td>
-            <td>{{ $log->user->shortName }} @if($log->user->deleted_at) <b>Eliminado</b> @endif</td>
-            <td>{{ $log->switch_id }}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+        <ul>
+            @foreach($users as $user)
+            <li>
+                {{ $user->fullName }} <br>
+                {!! $user->hasRole('Drogas: Jefe de unidad') ? '- Drogas: Jefe de unidad<br>': '' !!}
+                {!! $user->hasRole('Drogas: Recepcionista') ? '- Drogas: Recepcionista<br>': '' !!}
+                {!! $user->hasRole('Drogas: Usuario administrativo') ? '- Drogas: Usuario administrativo<br>': '' !!}
+            </li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="col">
+        <h3 class="mb-3">Ultimos accesos {{--( $daysAgo días)--}} </h3>
+
+        <table class="table table-sm table-bordered">
+            <thead>
+                <tr>
+                    <th>Fecha de acceso</th>
+                    <th>Usuario</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($lastLogs as $log )
+                <tr>
+                    <td>{{ $log->created_at }}</td>
+                    <td>{{ $log->user->shortName }} @if($log->user->deleted_at) <b>Eliminado</b> @endif</td>
+                    <td>{{ $log->switch_id }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
 
 @endsection
 
