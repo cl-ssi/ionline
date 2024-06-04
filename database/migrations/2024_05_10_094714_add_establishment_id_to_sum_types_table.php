0c999ed941bbc3424b954c6aac323c5dd0a1986b
AquaroTorres <alvarotorres@gmail.com>
2024-06-04 12:23:01 -0400

    SumType:Fix:Migration establishment_id nullable

--- database/migrations/2024_05_10_094714_add_establishment_id_to_sum_types_table.php
+++ database/migrations/2024_05_10_094714_add_establishment_id_to_sum_types_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('sum_types', function (Blueprint $table) {
-            $table->foreignId('establishment_id')->after('name')->default(38)->constrained('establishments');
+            $table->foreignId('establishment_id')->nullable()->after('name')->constrained('establishments');
         });
     }
 
