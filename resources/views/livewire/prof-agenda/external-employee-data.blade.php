e871ba05e1280f4687cd29667434c3b5db99c5c2
sickiqq <sick_iqq@hotmail.com>
2024-07-22 12:39:42 -0400

    agenda ust: fix error al guardar reserva.

--- resources/views/livewire/prof-agenda/external-employee-data.blade.php
+++ resources/views/livewire/prof-agenda/external-employee-data.blade.php
@@ -3,12 +3,12 @@
 
         <fieldset class="form-group col-5 col-md-2">
             <label for="for_run">Run (sin DV)</label>
-            <input type="number" min="1" max="50000000" class="form-control" id="for_user_id" name="user_id" wire:model.lazy="user_id" required>
+            <input type="number" min="1" max="50000000" class="form-control" id="for_user_id" name="user_id" wire:model.lazy="externaluser_id" required>
         </fieldset>
 
         <fieldset class="form-group col-3 col-md-1">
             <label for="for_dv">Digito</label>
-            <input type="text" class="form-control" id="for_dv" name="dv" @if($user) value="{{$user->dv}}" @endif required>
+            <input type="text" class="form-control" id="for_dv" name="dv" @if($externaluser) value="{{$externaluser->dv}}" @endif required>
         </fieldset>
 
         <!-- <fieldset class="form-group col-3 col-md-1">
@@ -20,27 +20,27 @@
             <label for="for_name">Sexo</label>
             <select name="gender" id="" class="form-control">
                 <option value=""></option>
-                <option value="male" @if($user) @selected($user->gender == "male") @endif>Masculino</option>
-                <option value="female" @if($user) @selected($user->gender == "female") @endif>Femenino</option>
+                <option value="male" @if($externaluser) @selected($externaluser->gender == "male") @endif>Masculino</option>
+                <option value="female" @if($externaluser) @selected($externaluser->gender == "female") @endif>Femenino</option>
             </select>
         </fieldset>
 
         <fieldset class="form-group col-12 col-md-3">
             <label for="for_name">Nombres</label>
             <input type="text" class="form-control" id="for_name" name="name" required="required" 
-                @if($user) value="{{$user->name}}" @endif>
+                @if($externaluser) value="{{$externaluser->name}}" @endif>
         </fieldset>
 
         <fieldset class="form-group col-12 col-md-2">
             <label for="for_name">A.Paterno</label>
             <input type="text" class="form-control" id="for_fathers_family" name="fathers_family" required="required" 
-                @if($user) value="{{$user->fathers_family}}" @endif>
+                @if($externaluser) value="{{$externaluser->fathers_family}}" @endif>
         </fieldset>
 
         <fieldset class="form-group col-12 col-md-2">
             <label for="for_name">A.Materno</label>
             <input type="text" class="form-control" id="for_mothers_family" name="mothers_family" required="required" 
-                @if($user) value="{{$user->mothers_family}}" @endif>
+                @if($externaluser) value="{{$externaluser->mothers_family}}" @endif>
         </fieldset>
 
     </div>
@@ -52,25 +52,25 @@
         <fieldset class="form-group col-12 col-md-4">
             <label for="for_address">Dirección</label>
             <input type="text" class="form-control" id="foraddress" placeholder="Dirección, comuna"
-                name="address" @if($user) value="{{$user->address}}" @endif>
+                name="address" @if($externaluser) value="{{$externaluser->address}}" @endif>
         </fieldset>
 
         <fieldset class="form-group col-12 col-md-2">
             <label for="for_phone_number">N.telefónico</label>
             <input type="text" class="form-control" id="for_phone_number"
-                name="phone_number" @if($user) value="{{$user->phone_number}}" @endif>
+                name="phone_number" @if($externaluser) value="{{$externaluser->phone_number}}" @endif>
         </fieldset>
 
         <fieldset class="form-group col-12 col-md-3">
             <label for="for_email">Email institucional*</label>
             <input type="email" class="form-control" id="for_email" wire:model.defer="email"
-                name="email" @if($user) value="{{$user->email}}" @endif>
+                name="email" @if($externaluser) value="{{$externaluser->email}}" @endif>
         </fieldset>
 
         <fieldset class="form-group col-12 col-md-3">
             <label for="for_birthday">F.Nacimiento</label>
             <input type="date" class="form-control" id="for_birthday"
-                name="birthday" @if($user) @if($user->birthday) value="{{$user->birthday->format('Y-m-d')}}" @endif @endif>
+                name="birthday" @if($externaluser) @if($externaluser->birthday) value="{{$externaluser->birthday->format('Y-m-d')}}" @endif @endif>
         </fieldset>
 
     </div>
