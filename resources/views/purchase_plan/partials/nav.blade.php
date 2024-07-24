2374e4f2676c3120a25622059eeb0322baa88822
Jorge Miranda <mirandal.jorge@gmail.com>
2024-07-24 14:31:39 -0400

    Plan de Compras: Acceso a todos los planes a Finanzas

--- resources/views/purchase_plan/partials/nav.blade.php
+++ resources/views/purchase_plan/partials/nav.blade.php
@@ -3,9 +3,9 @@
         <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"><i class="fas fa-inbox"></i> Solicitudes</a>
         <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="{{ route('purchase_plan.own_index') }}"><i class="fas fa-inbox"></i> Mis Plan de Compras</a></li>
-            @can('Purchase Plan: all')
+            @if(auth()->user()->organizational_unit_id == App\Models\Parameters\Parameter::get('ou','FinanzasSSI') || auth()->user()->hasPermissionTo('Purchase Plan: all'))
                 <li><a class="dropdown-item" href="{{ route('purchase_plan.all_index') }}"><i class="fas fa-inbox"></i> Todos los Plan de Compras</a></li>
-            @endcan
+            @endif
             <li><a class="dropdown-item" href="{{ route('purchase_plan.pending_index') }}"><i class="fas fa-inbox"></i> Pendientes de firmar</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="{{ route('purchase_plan.create') }}"><i class="fas fa-plus"></i> Nuevo Plan de Compras</a></li>
