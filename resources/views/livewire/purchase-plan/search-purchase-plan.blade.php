bd4b56a296f6fc22c1dc395ffaebc4ab23d3fc56
Jorge Miranda <mirandal.jorge@gmail.com>
2024-08-12 14:36:40 -0400

    Plan de Compras: permitir editar en todo momento

--- resources/views/livewire/purchase-plan/search-purchase-plan.blade.php
+++ resources/views/livewire/purchase-plan/search-purchase-plan.blade.php
@@ -141,7 +141,7 @@
                                 <td class="text-center">
                                     <a href="{{ route('purchase_plan.show', $purchasePlan) }}"
                                         class="btn btn-outline-secondary btn-sm mb-1"><i class="fas fa-eye fa-fw"></i></a>
-                                    @if($purchasePlan->canEdit() && $index == 'own')
+                                    @if($index == 'own')
                                         <a href="{{ route('purchase_plan.edit', $purchasePlan) }}"
                                             class="btn btn-outline-secondary btn-sm mb-1"><i class="fas fa-edit fa-fw"></i> </a>
                                     @endif
