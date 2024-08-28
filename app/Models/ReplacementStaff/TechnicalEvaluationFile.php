c5c9ac281a56475669f66dcd3bb70fb9f1ab8120
AquaroTorres <alvarotorres@gmail.com>
2024-08-28 00:43:19 -0400

    Ordenar ReplacementStaff

--- app/Models/ReplacementStaff/TechnicalEvaluationFile.php
+++ app/Models/ReplacementStaff/TechnicalEvaluationFile.php
@@ -2,30 +2,60 @@
 
 namespace App\Models\ReplacementStaff;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TechnicalEvaluationFile extends Model
 {
-    use HasFactory;
-    use softDeletes;
+    use HasFactory, SoftDeletes;
 
-    protected $fillable = [
-        'name', 'file'
-    ];
-
-    public function user() {
-        return $this->belongsTo('App\Models\User')->withTrashed();
-    }
-
-    public function technicalEvaluation() {
-        return $this->belongsTo('App\Models\ReplacementStaff\TechnicalEvaluation');
-    }
-
-    protected $hidden = [
-        'created_at', 'updated_at'
-    ];
-
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'rst_technical_evaluation_files';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'file'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Get the user that owns the document.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class)->withTrashed();
+    }
+
+    /**
+     * Get the technical evaluation that owns the file.
+     *
+     * @return BelongsTo
+     */
+    public function technicalEvaluation(): BelongsTo
+    {
+        return $this->belongsTo(TechnicalEvaluation::class);
+    }
 }
