298f1757e7cdb1258e9e11fcf6fbc34ff3048045
sickiqq <sick_iqq@hotmail.com>
2024-08-28 11:52:56 -0400

    fix error

--- app/Models/ServiceRequests/Fulfillment.php
+++ app/Models/ServiceRequests/Fulfillment.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Carbon\Carbon;
 use DateInterval;
 use DatePeriod;
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
