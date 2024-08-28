ff0f92838b16c7f8b826e2453c0e5e2f16b562bb
AquaroTorres <alvarotorres@gmail.com>
2024-08-27 23:10:24 -0400

    Ordenar Inventory Movement

--- app/Models/Inv/InventoryMovement.php
+++ app/Models/Inv/InventoryMovement.php
@@ -2,22 +2,33 @@
 
 namespace App\Models\Inv;
 
-use OwenIt\Auditing\Contracts\Auditable;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use App\Models\User;
-use App\Models\Rrhh\OrganizationalUnit;
-use App\Notifications\Inventory\ItemReception;
 use App\Models\Parameters\Place;
+use App\Models\Rrhh\OrganizationalUnit;
+use App\Models\User;
+use App\Notifications\Inventory\ItemReception;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
 
 class InventoryMovement extends Model implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
     use HasFactory, SoftDeletes;
 
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'inv_inventory_movements';
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'reception_confirmation',
         'reception_date',
@@ -29,59 +40,101 @@ class InventoryMovement extends Model implements Auditable
         'user_responsible_id',
         'user_using_ou_id',
         'user_using_id',
-        'user_sender_id',
+        'user_sender_id'
     ];
 
-    protected $dates = [
-        'installation_date',
-    ];
-
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
     protected $casts = [
-        'reception_date' => 'date:Y-m-d H:i:s'
+        'reception_date' => 'date:Y-m-d H:i:s',
+        'installation_date' => 'date'
     ];
 
-    public function inventory()
+    /**
+     * Get the inventory that owns the inventory movement.
+     *
+     * @return BelongsTo
+     */
+    public function inventory(): BelongsTo
     {
         return $this->belongsTo(Inventory::class);
     }
 
-    public function place()
+    /**
+     * Get the place that owns the inventory movement.
+     *
+     * @return BelongsTo
+     */
+    public function place(): BelongsTo
     {
         return $this->belongsTo(Place::class);
     }
 
-    public function responsibleOrganizationalUnit()
+    /**
+     * Get the responsible organizational unit that owns the inventory movement.
+     *
+     * @return BelongsTo
+     */
+    public function responsibleOrganizationalUnit(): BelongsTo
     {
         return $this->belongsTo(OrganizationalUnit::class, 'user_responsible_ou_id');
     }
 
-    public function responsibleUser()
+    /**
+     * Get the responsible user that owns the inventory movement.
+     *
+     * @return BelongsTo
+     */
+    public function responsibleUser(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'user_responsible_id')->withTrashed();;
+        return $this->belongsTo(User::class, 'user_responsible_id')->withTrashed();
     }
 
-    public function usingOrganizationalUnit()
+    /**
+     * Get the using organizational unit that owns the inventory movement.
+     *
+     * @return BelongsTo
+     */
+    public function usingOrganizationalUnit(): BelongsTo
     {
         return $this->belongsTo(OrganizationalUnit::class, 'user_using_ou_id');
     }
 
-    public function usingUser()
+    /**
+     * Get the using user that owns the inventory movement.
+     *
+     * @return BelongsTo
+     */
+    public function usingUser(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'user_using_id')->withTrashed();;
+        return $this->belongsTo(User::class, 'user_using_id')->withTrashed();
     }
 
-    public function senderUser()
+    /**
+     * Get the sender user that owns the inventory movement.
+     *
+     * @return BelongsTo
+     */
+    public function senderUser(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'user_sender_id')->withTrashed();;
+        return $this->belongsTo(User::class, 'user_sender_id')->withTrashed();
     }
 
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
     protected static function boot()
     {
         parent::boot();
 
         static::created(function ($movement) {
             /** Enviar notificación al responsable  */
-            if($movement->user_responsible_id) {
+            if ($movement->user_responsible_id) {
                 $movement->responsibleUser?->notify(new ItemReception($movement));
             }
         });
