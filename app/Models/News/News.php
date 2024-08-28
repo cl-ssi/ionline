55e072d366046b608441d6e531319122a7348172
AquaroTorres <alvarotorres@gmail.com>
2024-08-28 00:41:17 -0400

    Ordenar News

--- app/Models/News/News.php
+++ app/Models/News/News.php
@@ -2,41 +2,69 @@
 
 namespace App\Models\News;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
 
 class News extends Model implements Auditable
 {
-    use HasFactory;
-    use softDeletes;
-    use \OwenIt\Auditing\Auditable;
+    use HasFactory, SoftDeletes, \OwenIt\Auditing\Auditable;
 
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'news';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-        'type', 'title', 'subtitle', 'image', 'lead', 'body', 'publication_date_at', 'until_at', 'publication_date_at', 'user_id'
+        'type',
+        'title',
+        'subtitle',
+        'image',
+        'lead',
+        'body',
+        'publication_date_at',
+        'until_at',
+        'user_id'
     ];
 
-    public function user() {
-        return $this->belongsTo('App\Models\User')->withTrashed();
-    }
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at'          => 'datetime',
+        'publication_date_at' => 'datetime',
+        'until_at'            => 'datetime'
+    ];
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-
-     protected $hidden = [
-        'created_at', 'updated_at'
+    protected $hidden = [
+        'created_at',
+        'updated_at'
     ];
 
-    protected $casts = [
-        'created_at'            => 'datetime',
-        'publication_date_at'   => 'datetime',
-        'until_at'              => 'datetime',
-    ];
-
-    protected $table = 'news';
+    /**
+     * Get the user that owns the document.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class)->withTrashed();
+    }
 }
