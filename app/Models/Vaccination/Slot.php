67a1929c8b183a91d85e951716acf4cdd47873f4
AquaroTorres <alvarotorres@gmail.com>
2024-08-27 23:30:54 -0400

    Update Slot

--- app/Models/Vaccination/Slot.php
+++ app/Models/Vaccination/Slot.php
@@ -4,22 +4,49 @@ namespace App\Models\Vaccination;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Slot extends Model
 {
     use HasFactory;
 
-    public function day() {
-        return $this->belongsTo('\App\Models\Vaccination\Day');
-    }
-
-    protected $fillable = [
-        'start_at','end_at','available','used','day_id'
-    ];
-
-    protected $dates = [
-        'start_at','end_at'
-    ];
-
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'vac_slots';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'start_at',
+        'end_at',
+        'available',
+        'used',
+        'day_id'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'start_at' => 'datetime',
+        'end_at'   => 'datetime'
+    ];
+
+    /**
+     * Get the day that owns the slot.
+     *
+     * @return BelongsTo
+     */
+    public function day(): BelongsTo
+    {
+        return $this->belongsTo(Day::class);
+    }
 }
