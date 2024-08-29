04ecc32c094e0aa19a4cc3d6dd7c202a12b795a3
sickiqq <sick_iqq@hotmail.com>
2024-08-29 10:22:08 -0400

    farmacia: fix problema cargar nombre de la farmacia.

--- resources/views/pharmacies/index.blade.php
+++ resources/views/pharmacies/index.blade.php
@@ -7,8 +7,11 @@
 @include('pharmacies.nav')
 
 <h3 class="mb-3">Bienvenido al módulo de bodegas</h3>
-<h4>Bodega selecionada: {{auth()->user()->pharmacies->firstWhere('id', session('pharmacy_id'))->name}}</h4>
--
+@if(auth()->user()->pharmacies->firstWhere('id', session('pharmacy_id')))
+    <h4>Bodega selecionada: {{auth()->user()->pharmacies->firstWhere('id', session('pharmacy_id'))->name}}</h4>
+@else
+    <h4>Problema al cargar información. Ingrese nuevamente al módulo.</h4>
+@endif
 
 @endsection
 
