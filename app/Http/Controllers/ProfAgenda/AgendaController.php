8c5e9ddcbf885a9d0a067ae7564a2ff0e3ce9d56
sickiqq <sick_iqq@hotmail.com>
2024-08-29 11:06:52 -0400

    agenda ust: fix permisos

--- app/Http/Controllers/ProfAgenda/AgendaController.php
+++ app/Http/Controllers/ProfAgenda/AgendaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\ProfAgenda;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 use App\Models\ProfAgenda\ActivityType;
 use App\Models\ProfAgenda\Proposal;
@@ -12,13 +13,19 @@ class AgendaController extends Controller
 {
     public function index(Request $request)
     {
-        $activity_types = ActivityType::all();
-        $user_id_param = $request->user_id;
-        $profession_id = $request->profession_id;
-        $proposals = Proposal::where('user_id',$user_id_param)
-                            ->where('profession_id',$profession_id)
-                            ->get();
-
+        if(auth()->user()->can('Agenda UST: Administrador') || auth()->user()->can('Agenda UST: Funcionario') || auth()->user()->can('	Agenda UST: Secretaria')){
+            $activity_types = ActivityType::all();
+            $user_id_param = $request->user_id;
+            $profession_id = $request->profession_id;
+            $proposals = Proposal::where('user_id',$user_id_param)
+                                ->where('profession_id',$profession_id)
+                                ->get();
+        }else{
+            // Redirigir hacia atrás con un mensaje si no tiene permisos
+            return redirect()->back()->with('warning', 'No tiene permisos para acceder a esta sección.');
+        }
+        
+        
         return view('prof_agenda.agenda',compact('request','proposals','activity_types'));
     }
 
