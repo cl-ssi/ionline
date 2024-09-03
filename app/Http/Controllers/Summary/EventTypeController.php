777c087c01c49b54895aaa46efacc0b43953d86d
AquaroTorres <alvarotorres@gmail.com>
2024-09-03 13:46:56 -0400

    SummaryEvenType:Fix:Eagger loading

--- app/Http/Controllers/Summary/EventTypeController.php
+++ app/Http/Controllers/Summary/EventTypeController.php
@@ -18,7 +18,11 @@ class EventTypeController extends Controller
      */
     public function index()
     {
-        $summaryTypes = Type::where('establishment_id',auth()->user()->establishment_id)->get();
+        $summaryTypes = Type::with([
+            'eventTypes',
+            'eventTypes.actor',
+        ])
+            ->where('establishment_id',auth()->user()->establishment_id)->get();
         return view('summary.events.index', compact('summaryTypes'));
     }
 
