efc3c7d4ba9ebc2e1cf052cde66562aa27d259b1
AquaroTorres <alvarotorres@gmail.com>
2024-09-10 18:27:17 -0300

    News:Action para borrar imagen

--- app/Filament/Clusters/Parameters/Resources/NewsResource/Pages/EditNews.php
+++ app/Filament/Clusters/Parameters/Resources/NewsResource/Pages/EditNews.php
@@ -5,6 +5,7 @@ namespace App\Filament\Clusters\Parameters\Resources\NewsResource\Pages;
 use App\Filament\Clusters\Parameters\Resources\NewsResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Storage;
 
 class EditNews extends EditRecord
 {
@@ -13,6 +14,19 @@ class EditNews extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('deleteImage')
+                ->label('Borrar imágen')
+                ->action(function () {
+                    $record = $this->record;
+                    if (Storage::exists($record->image)) {
+                        Storage::delete($record->image);
+                    }
+                    $record->image = null;
+                    $record->save();
+                })
+                ->requiresConfirmation()
+                ->color('danger')
+                ->icon('heroicon-o-trash'),
             Actions\DeleteAction::make(),
         ];
     }
