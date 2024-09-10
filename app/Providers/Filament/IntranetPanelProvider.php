4b5d63f320a3b50cf23862e3d3094b608ae293c4
AquaroTorres <alvarotorres@gmail.com>
2024-09-10 18:31:45 -0300

    Favicon

--- app/Providers/Filament/IntranetPanelProvider.php
+++ app/Providers/Filament/IntranetPanelProvider.php
@@ -33,6 +33,7 @@ class IntranetPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Blue,
             ])
+            ->favicon(asset('favicon.ico'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters')
