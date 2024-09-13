126d76310579b1aa068695eaee461fa25b5e001b
Alvaro Torres F. <alvarotorres@gmail.com>
2024-09-13 15:58:46 -0300

    DocManual Model

---
+++ app/Models/Documents/Manual.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Documents;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Manual extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2024_09_13_154957_create_doc_manuals_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doc_manuals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('log_module_id')->constrained();
+            $table->text('content');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doc_manuals');
+    }
+};
