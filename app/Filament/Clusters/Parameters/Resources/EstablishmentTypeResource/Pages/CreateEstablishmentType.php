29ac88fc9e222a34f4f23a336e1b8e6e5010c777
m.soto.r.2021 <m.soto.r.2021@gmail.com>
2024-09-13 11:28:58 -0300

    Agrega EstablishmentType a iOnline Plus Fixes #3031

---
+++ app/Filament/Clusters/Parameters/Resources/EstablishmentTypeResource/Pages/CreateEstablishmentType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Clusters\Parameters\Resources\EstablishmentTypeResource\Pages;
+
+use App\Filament\Clusters\Parameters\Resources\EstablishmentTypeResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateEstablishmentType extends CreateRecord
+{
+    protected static string $resource = EstablishmentTypeResource::class;
+}
