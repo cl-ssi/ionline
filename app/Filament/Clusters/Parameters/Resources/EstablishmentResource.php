55c7c0888a23ab616b70029f6ddc570a4acc0f4c
m.soto.r.2021 <m.soto.r.2021@gmail.com>
2024-09-13 12:25:38 -0300

    Agrega Establishment Type Policy a iOnline Plus Fixes #3034

--- app/Filament/Clusters/Parameters/Resources/EstablishmentResource.php
+++ app/Filament/Clusters/Parameters/Resources/EstablishmentResource.php
@@ -1,218 +1,80 @@
 <?php
 
-namespace App\Filament\Clusters\Parameters\Resources;
+namespace App\Policies\Parameters;
 
-use App\Filament\Clusters\Parameters;
-use App\Filament\Clusters\Parameters\Resources\EstablishmentResource\Pages;
-use App\Filament\Clusters\Parameters\Resources\EstablishmentResource\RelationManagers;
-use App\Models\Establishment;
-use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
-use Filament\Tables;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Models\Parameters\EstablishmentType;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
 
-class EstablishmentResource extends Resource
+class EstablishmentTypePolicy
 {
-    protected static ?string $model = Establishment::class;
-
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-
-    protected static ?string $cluster = Parameters::class;
-
-    protected static ?string $modelLabel = 'establecimiento';
-
-    protected static ?string $pluralModelLabel = 'establecimientos';
-
-    public static function form(Form $form): Form
+    /**
+     * Perform pre-authorization checks.
+     * For administrative purposes, the user with the 'be god' ability can do anything.
+     * If null is returned, the authorization check will fall through to the policy method
+     */
+    public function before(User $user): ?bool
     {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('alias')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('type')
-                    ->required(),
-                Forms\Components\Select::make('establishment_type_id')
-                    ->relationship('establishmentType', 'name')
-                    ->default(null),
-                Forms\Components\TextInput::make('deis')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('new_deis')
-                    ->numeric()
-                    ->default(null),
-                Forms\Components\TextInput::make('mother_code')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('new_mother_code')
-                    ->numeric()
-                    ->default(null),
-                Forms\Components\TextInput::make('sirh_code')
-                    ->numeric()
-                    ->default(null),
-                Forms\Components\Select::make('commune_id')
-                    ->relationship('commune', 'name')
-                    ->required(),
-                Forms\Components\TextInput::make('dependency')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\Select::make('health_service_id')
-                    ->relationship('healthService', 'name')
-                    ->default(null),
-                Forms\Components\TextInput::make('official_name')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('administrative_dependency')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('level_of_care')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('street_type')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('street_number')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('address')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('url')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('telephone')
-                    ->tel()
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('emergency_service')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('latitude')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('longitude')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('level_of_complexity')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('provider_type_health_system')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('mail_director')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('father_organizational_unit_id')
-                    ->numeric()
-                    ->default(null),
-            ]);
+        if ($user->can('be god')) {
+            return true;
+        }
+
+        return null;
     }
 
-    public static function table(Table $table): Table
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
     {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('name')
-                    ->searchable(),
-                // Tables\Columns\TextColumn::make('alias')
-                //     ->searchable(),
-                Tables\Columns\TextColumn::make('type'),
-                // Tables\Columns\TextColumn::make('establishmentType.name')
-                //     ->numeric()
-                //     ->sortable(),
-                Tables\Columns\TextColumn::make('deis')
-                    ->searchable(),
-                // Tables\Columns\TextColumn::make('new_deis')
-                //     ->numeric()
-                //     ->sortable(),
-                // Tables\Columns\TextColumn::make('mother_code')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('new_mother_code')
-                //     ->numeric()
-                //     ->sortable(),
-                // Tables\Columns\TextColumn::make('sirh_code')
-                //     ->numeric()
-                //     ->sortable(),
-                Tables\Columns\TextColumn::make('commune.name')
-                    ->numeric()
-                    ->sortable(),
-                // Tables\Columns\TextColumn::make('dependency')
-                //     ->searchable(),
-                Tables\Columns\TextColumn::make('healthService.name')
-                    ->numeric()
-                    ->sortable(),
-                // Tables\Columns\TextColumn::make('official_name')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('administrative_dependency')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('level_of_care')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('street_type')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('street_number')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('address')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('url')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('telephone')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('emergency_service')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('latitude')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('longitude')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('level_of_complexity')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('provider_type_health_system')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('mail_director')
-                //     ->searchable(),
-                // Tables\Columns\TextColumn::make('father_organizational_unit_id')
-                //     ->numeric()
-                //     ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+        return false;
     }
 
-    public static function getRelations(): array
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, EstablishmentType $EstablishmentType): bool
     {
-        return [
-            //
-        ];
+        return false;
     }
 
-    public static function getPages(): array
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
     {
-        return [
-            'index' => Pages\ListEstablishments::route('/'),
-            'create' => Pages\CreateEstablishment::route('/create'),
-            'edit' => Pages\EditEstablishment::route('/{record}/edit'),
-        ];
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, EstablishmentType $EstablishmentType): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, EstablishmentType $EstablishmentType): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, EstablishmentType $EstablishmentType): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, EstablishmentType $EstablishmentType): bool
+    {
+        return false;
     }
 }
