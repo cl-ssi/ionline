d654abbc02b1c594456d353a471eae771371a3d3
Alvaro Torres F. <alvarotorres@gmail.com>
2024-09-16 17:41:59 -0300

    Account widget

---
+++ app/Filament/Widgets/AccountWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class AccountWidget extends Widget
+{
+    protected static string $view = 'filament.widgets.account-widget';
+}
--- app/Providers/Filament/IntranetPanelProvider.php
+++ app/Providers/Filament/IntranetPanelProvider.php
@@ -42,8 +42,6 @@ class IntranetPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
