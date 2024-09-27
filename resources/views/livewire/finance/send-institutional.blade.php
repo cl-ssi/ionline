740ca3c4a860e7ff1a9de7729723dc87277d984d
Ignacio Miranda <piranga8@gmail.com>
2024-09-27 12:08:37 -0300

    fix: Payment Ready Dte and added indicator

--- resources/views/livewire/finance/send-institutional.blade.php
+++ resources/views/livewire/finance/send-institutional.blade.php
@@ -1,6 +1,7 @@
 <div>
     <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" wire:model.live="checked" id="institutionalPaymentSwitch-{{$dteId}}">
+        <input class="form-check-input" type="checkbox" checked="{{$checked}}" wire:model.live="checked" id="institutionalPaymentSwitch-{{$dteId}}">
         <label class="form-check-label" for="institutionalPaymentSwitch-{{$dteId}}">Manual</label>
     </div>
+    <i class="fa fa-spinner fa-spin" wire:loading></i>
 </div>
