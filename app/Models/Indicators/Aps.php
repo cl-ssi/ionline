f6cca47b747ead7ed0299984f1a80b787e0017b6
Alvaro Torres F. <alvarotorres@gmail.com>
2024-10-10 10:45:47 -0300

    Indicators Cluster

--- app/Models/Indicators/Aps.php
+++ app/Models/Indicators/Aps.php
@@ -4,14 +4,22 @@ namespace App\Models\Indicators;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Aps extends Model
 {
     use HasFactory;
-    protected $table = 'ind_aps';
-    protected $fillable = ['name', 'year', 'slug', 'number'];
 
-    public function indicators()
+    protected $table = 'ind_aps';
+
+    protected $fillable = [
+        'year',
+        'number',
+        'name',
+        'slug',
+    ];
+
+    public function indicators(): MorphMany
     {
         return $this->morphMany(Indicator::class, 'indicatorable');
     }
