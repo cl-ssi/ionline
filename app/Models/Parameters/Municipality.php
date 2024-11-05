5612e51e7363216a10d5f42e24e8cd3dba3dafd5
Alvaro Torres F. <alvarotorres@gmail.com>
2024-11-05 15:58:47 -0300

    Municipality

--- app/Models/Parameters/Municipality.php
+++ app/Models/Parameters/Municipality.php
@@ -11,54 +11,54 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Municipality extends Model
 {
     /** Cambio a nueva tabla pendiente */
-    // protected $table = 'cfg_municipalities';
-
-    // protected $fillable = [
-    //     'name',
-    //     'rut',
-    //     'address',
-    //     'emails',
-    //     'commune_id'
-    // ];
-
-    // protected $casts = [
-    //     'emails' => 'array'
-    // ];
-
-    // public function commune(): BelongsTo
-    // {
-    //     return $this->belongsTo(ClCommune::class);
-    // }
-
-    // public function alcaldes(): HasMany
-    // {
-    //     return $this->hasMany(Alcalde::class);
-    // }
+    protected $table = 'cfg_municipalities';
 
     protected $fillable = [
         'name',
-        'name_municipality',
-        'rut_municipality',
-        'adress_municipality',
-        'appellative_representative',
-        'decree_representative',
-        'name_representative',
-        'rut_representative',
-        'name_representative_surrogate',
-        'rut_representative_surrogate', 
-        'decree_representative_surrogate',
-        'email_municipality',
-        'email_municipality_2',
-        'appellative_representative_surrogate'
+        'rut',
+        'address',
+        'emails',
+        'commune_id'
     ];
 
-    protected $hidden = [
-        'created_at',
-        'updated_at'
+    protected $casts = [
+        'emails' => 'array'
     ];
 
     public function commune(): BelongsTo
     {
-        return $this->belongsTo(Commune::class);
+        return $this->belongsTo(ClCommune::class);
     }
+
+    public function alcaldes(): HasMany
+    {
+        return $this->hasMany(Alcalde::class);
+    }
+
+    // protected $fillable = [
+    //     'name',
+    //     'name_municipality',
+    //     'rut_municipality',
+    //     'adress_municipality',
+    //     'appellative_representative',
+    //     'decree_representative',
+    //     'name_representative',
+    //     'rut_representative',
+    //     'name_representative_surrogate',
+    //     'rut_representative_surrogate', 
+    //     'decree_representative_surrogate',
+    //     'email_municipality',
+    //     'email_municipality_2',
+    //     'appellative_representative_surrogate'
+    // ];
+
+    // protected $hidden = [
+    //     'created_at',
+    //     'updated_at'
+    // ];
+
+    // public function commune(): BelongsTo
+    // {
+    //     return $this->belongsTo(Commune::class);
+    // }
 }
