026d5d8ebae5d3708c7f1f4a6c5350c42bfcf2e3
Alvaro Torres F. <alvarotorres@gmail.com>
2024-11-07 16:25:18 -0300

    FK en formato laravel 10+

--- database/migrations/2020_08_25_163619_create_frm_establishments_table.php
+++ database/migrations/2020_08_25_163619_create_frm_establishments_table.php
@@ -18,11 +18,10 @@ class CreateFrmEstablishmentsTable extends Migration
             $table->string('name')->unique();
             $table->text('email')->nullable();
             //se añade por defecto 1 para la relación con la bodega
-            $table->unsignedBigInteger('pharmacy_id')->default(1);
+            $table->foreignId('pharmacy_id')->nullable()->constrained('frm_pharmacies');
             $table->timestamps();
             $table->softDeletes();
 
-            $table->foreign('pharmacy_id')->references('id')->on('frm_pharmacies');
         });
     }
 
