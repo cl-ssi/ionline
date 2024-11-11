755623c40ed86b5bdafa2e384616df6ab6ae962b
Jorge Miranda L <mirandal.jorge@gmail.com>
2024-11-11 20:27:00 -0300

    Drogas: Observer

---
+++ app/Observers/Drugs/ProtocolObserver.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Observers\Drugs;
+
+use App\Models\Drugs\Protocol;
+
+class ProtocolObserver
+{
+    /**
+     * Handle the Protocol "created" event.
+     */
+    
+    public function created(Protocol $protocol): void
+    {
+        if($protocol->result == 'Positivo' && $protocol->receptionItem->result_substance_id == null){
+            $protocol->receptionItem->result_substance_id = $protocol->receptionItem->substance_id;
+            $protocol->receptionItem->save();
+        }
+    }
+
+    /**
+     * Handle the Protocol "updated" event.
+     */
+    public function updated(Protocol $protocol): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Protocol "deleted" event.
+     */
+    public function deleted(Protocol $protocol): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Protocol "restored" event.
+     */
+    public function restored(Protocol $protocol): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Protocol "force deleted" event.
+     */
+    public function forceDeleted(Protocol $protocol): void
+    {
+        //
+    }
+}
