9718554d108b222bfdcad8d32b18bed86a34b8c9
AlexisCanoFlores <130815043+Alexis-CF@users.noreply.github.com>
2024-11-12 12:39:16 -0300

    Correcion de Tablas rst

--- database/migrations/2021_03_30_153838_create_rst_request_replacement_staff_table.php
+++ database/migrations/2021_03_30_153838_create_rst_request_replacement_staff_table.php
@@ -17,12 +17,11 @@ class CreateRstRequestReplacementStaffTable extends Migration
             $table->id();
             $table->string('form_type')->nullable();
             $table->string('name');
-            $table->foreignId('profile_manage_id')>nullable()->cosntrained('rst_profile_manages');
+            $table->foreignId('profile_manage_id')->nullable()->cosntrained('rst_profile_manages');
             $table->string('law')->nullable();
             $table->integer('degree')->nullable();
             $table->date('start_date')->nullable();
             $table->date('end_date')->nullable();
-
             //LEGAL QUALITY
             $table->foreignId('legal_quality_manage_id')->nullable()->cosntrained('rst_legal_quality_manages');
             $table->integer('salary')->nullable();
@@ -32,31 +31,23 @@ class CreateRstRequestReplacementStaffTable extends Migration
             $table->unsignedInteger('run')->nullable();
             $table->char('dv',1)->nullable();
             $table->string('other_fundament')->nullable();
-
             $table->enum('work_day',['diurnal', 'third shift', 'fourth shift', 'other']);
             $table->string('other_work_day')->nullable();
             $table->integer('charges_number')->nullable();
             $table->string('job_profile_file')->nullable();
             $table->string('request_verification_file');
-            $table->foreignId('ou_of_performance_id');
-            $table->foreignId('replacement_staff_id')->nullable()->constrained'rst_replacement_staff');
-
+            $table->foreignId('ou_of_performance_id')->constrained('organizational_units');
+            $table->foreignId('replacement_staff_id')->nullable()->constrained('rst_replacement_staff');
             $table->string('request_status');
             $table->boolean('sirh_contract')->nullable()->after('request_status');
-
-            $table->foreignId('user_id');
-            $table->foreignId('organizational_unit_id');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('organizational_unit_id')->constrained('organizational_units');
             $table->foreignId('establishment_id')->constrained('establishments');
             $table->foreignId('requester_id')->nullable()->constrained('users');
             $table->foreignId('requester_ou_id')->nullable()->constrained('organizational_units');
-            $table->foreignId('request_id')->nullable();
+            $table->foreignId('request_id')->nullable()->constrained('rst_request_replacement_staff');
             $table->foreignId('budget_item_id')->nullable()->constrained('cfg_budget_items');
             $table->foreignId('signatures_file_id')->nullable()->constrained('doc_signatures_files');
-
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('organizational_unit_id')->references('id')->on('organizational_units');
-            $table->foreign('request_id')->references('id')->on('rst_request_replacement_staff');
-            $table->foreign('ou_of_performance_id')->references('id')->on('organizational_units');
 
             $table->timestamps();
             $table->softDeletes();
