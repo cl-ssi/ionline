841c434eee22d73bb707857dd6b3a6aea8bc6967
lucasmason123 <lucas.mason@inacapmail.cl>
2024-11-12 17:13:45 -0300

    Talent Management: Creacion del recurso JobPositionProfile

---
+++ app/Filament/Clusters/TalentManagement.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Clusters;
+
+use Filament\Clusters\Cluster;
+
+class TalentManagement extends Cluster
+{
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+
+    protected static ?string $title = 'Gestión del Talento';
+}
