7322c26aede61d5edf721ee5dd4ac43a5aa6c283
m.soto.r.2021 <m.soto.r.2021@gmail.com>
2024-11-11 23:22:29 -0300

    Agregar Drogas:PoliceUnit iOnline Plus Fixes #3423

---
+++ app/Filament/Clusters/Documents/Resources/Drugs/PoliceUnitResource/Pages/CreatePoliceUnit.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Clusters\Documents\Resources\Drugs\PoliceUnitResource\Pages;
+
+use App\Filament\Clusters\Documents\Resources\Drugs\PoliceUnitResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePoliceUnit extends CreateRecord
+{
+    protected static string $resource = PoliceUnitResource::class;
+}
