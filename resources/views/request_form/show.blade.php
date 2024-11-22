f866a6c3c7c98183f0ca171e6a47baa9f4fa6e77
Alvaro Torres F. <alvarotorres@gmail.com>
2024-11-22 13:09:06 -0300

    Requestform Audit formato como los otros items

--- resources/views/request_form/show.blade.php
+++ resources/views/request_form/show.blade.php
@@ -639,7 +639,36 @@
                 <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
                      data-parent="#accordionExample">
                     <div class="card-body">
-                        @include('partials.audit', ['audits' => $requestForm->audits()])
+                        {{-- @include('partials.audit', ['audits' => $requestForm->audits()]) --}}
+                        <h6 class="mt-3 mt-4">Historial de cambios</h6>
+                        <div class="table-responsive-md">
+                            <table class="table table-sm small text-muted mt-3">
+                                <thead>
+                                <tr>
+                                    <th>Fecha</th>
+                                    <th>Usuario</th>
+                                    <th>Modificaciones</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                        @foreach($requestForm->audits->sortByDesc('updated_at') as $audit)
+                                            <tr>
+                                                <td nowrap>{{ $audit->created_at }}</td>
+                                                <td nowrap>{{ optional($audit->user)->fullName }}</td>
+                                                <td>
+                                                    @foreach($audit->getModified() as $attribute => $modified)
+                                                        @if(isset($modified['old']) OR isset($modified['new']))
+                                                            <strong>{{ $attribute }}</strong>
+                                                            :  {{ isset($modified['old']) ? $modified['old'] : '' }}
+                                                            => {{ $modified['new'] ?? '' }};
+                                                        @endif
+                                                    @endforeach
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
