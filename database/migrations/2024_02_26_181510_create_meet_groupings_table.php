23e7ccb90906a1b915ecff5bed948932e75474ca
AlexisCanoFlores <130815043+Alexis-CF@users.noreply.github.com>
2024-11-25 15:19:56 -0300

    Migracion tablas restantes Fixes #3547

--- database/migrations/2024_02_26_181510_create_meet_groupings_table.php
+++ database/migrations/2024_02_26_181510_create_meet_groupings_table.php
@@ -15,11 +15,10 @@ return new class extends Migration
     {
         Schema::create('meet_groupings', function (Blueprint $table) {
             $table->id();
-
             $table->string('type')->nullable();
             $table->string('name')->nullable();
+            $table->foreignId('user_id')->nullable()->constrained();
             $table->foreignId('meeting_id')->nullable()->constrained('meet_meetings');
-
             $table->timestamps();
             $table->softDeletes();
         });
