34a1b03dacafe364f1ad2e29b1754e3211bcd17d
AlexisCanoFlores <130815043+Alexis-CF@users.noreply.github.com>
2024-11-27 10:41:04 -0300

    Calendario en nuevo Nav Fixes #3558

--- app/Providers/Filament/IntranetPanelProvider.php
+++ app/Providers/Filament/IntranetPanelProvider.php
@@ -64,7 +64,10 @@ class IntranetPanelProvider extends PanelProvider
             ->navigationItems([
                 NavigationItem::make('iOnline Clásico')
                     ->url(fn(): string => route('home'))
-                    ->icon('heroicon-o-link')
+                    ->icon('heroicon-o-link'),
+                NavigationItem::make('Calendario')
+                    ->url(fn(): string => route('calendars'))
+                    ->icon('heroicon-o-calendar')
             ])
             ->defaultAvatarProvider(GravatarProvider::class)
             ->plugins([
