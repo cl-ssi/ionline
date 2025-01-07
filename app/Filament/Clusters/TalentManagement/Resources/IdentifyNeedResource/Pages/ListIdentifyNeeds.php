0b0883614cec7c1c20ff51f3bc86f9da9648ded2
Jorge Miranda L <mirandal.jorge@gmail.com>
2025-01-07 16:30:23 -0300

    DNC: Tabs todas y mis form.

--- app/Filament/Clusters/TalentManagement/Resources/IdentifyNeedResource/Pages/ListIdentifyNeeds.php
+++ app/Filament/Clusters/TalentManagement/Resources/IdentifyNeedResource/Pages/ListIdentifyNeeds.php
@@ -5,15 +5,36 @@ namespace App\Filament\Clusters\TalentManagement\Resources\IdentifyNeedResource\
 use App\Filament\Clusters\TalentManagement\Resources\IdentifyNeedResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListIdentifyNeeds extends ListRecords
 {
     protected static string $resource = IdentifyNeedResource::class;
 
+    /*
     protected function getHeaderActions(): array
     {
         return [
             Actions\CreateAction::make(),
         ];
+    }
+    */
+
+    public function getTabs(): array
+    {
+        $tabs = [];
+
+        $tabs['Todas'] = Tab::make()
+            ->modifyQueryUsing(function (Builder $query): Builder {
+                return $query; // No aplica ningún filtro
+        });
+
+        $tabs['Mis Formularios'] = Tab::make()
+            ->modifyQueryUsing(function (Builder $query): Builder {
+                return $query->where('user_id', auth()->id());
+        });
+
+        return $tabs;
     }
 }
