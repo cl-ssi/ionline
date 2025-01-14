64075a9b2a0585df7bd8a716469e90a58f404ba8
Jorge Miranda L <mirandal.jorge@gmail.com>
2025-01-14 12:57:58 -0300

    Staff de Reemplazos: se corrige LW de Adjuntos

--- resources/views/livewire/replacement-staff/files.blade.php
+++ resources/views/livewire/replacement-staff/files.blade.php
@@ -1,15 +1,6 @@
 <div>
-    <div class="form-row">
-        <fieldset class="form-group">
-            <label for="">&nbsp;</label>
-            <button class="btn text-white btn-info float-right" wire:click.prevent="add({{$i}})">Agregar <i class="fas fa-plus"></i></button>
-        </fieldset>
-    </div>
-
     <form method="POST" class="form-horizontal" action="{{ route('replacement_staff.request.technical_evaluation.file.store', $technicalEvaluation) }}" enctype="multipart/form-data"/>
         @csrf
-        @method('POST')
-        @foreach($inputs as $key => $value)
             <div class="form-row">
                 <fieldset class="form-group col mt">
                     <fieldset class="form-group">
@@ -23,17 +14,10 @@
                       <input class="form-control" type="file" name="file[]" accept="application/pdf" required>
                     </div>
                 </fieldset>
-
-                <fieldset class="form-group col-md-2">
-                    <label for="for_button"><br></label>
-                    <button class="btn btn-danger btn-block" wire:click.prevent="remove({{$key}})">Remover</button>
-                </fieldset>
             </div>
-            <hr>
-        @endforeach
 
-        @if($count>0)
-        <button type="submit" class="btn btn-primary float-right">Guardar</button>
-        @endif
+            <div class="form-row">
+                <button type="submit" class="btn btn-primary float-right">Guardar</button>
+            </div>
     </form>
 </div>
