fc8a315087d8195f1c60676422778aa77a3bcb38
Alvaro Torres F. <alvarotorres@gmail.com>
2025-01-20 08:27:30 -0300

    Convenios:Feat:Agr add softdeletes

--- database/migrations/2020_09_03_100190_create_agr_signers_table.php
+++ database/migrations/2020_09_03_100190_create_agr_signers_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->text('decree'); // DECRETO DIRECTOR
             $table->foreignId('user_id')->constrained('users'); // DIRECTOR USER ID
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
