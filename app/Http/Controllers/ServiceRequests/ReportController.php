92f0658313c9caea37a2e22a694a8d0d65af6e04
sickiqq <sick_iqq@hotmail.com>
2025-02-04 13:38:09 -0300

    honorarios: fix report to pay 13.75 a 14.5

--- app/Http/Controllers/ServiceRequests/ReportController.php
+++ app/Http/Controllers/ServiceRequests/ReportController.php
@@ -354,7 +354,7 @@ class ReportController extends Controller
         return redirect()->back();
       }
 
-      $totalToPay = $fulfillment->total_to_pay - round($fulfillment->total_to_pay * 0.1375);
+      $totalToPay = $fulfillment->total_to_pay - round($fulfillment->total_to_pay * 0.145);
       $txt .=
         $fulfillment->serviceRequest->employee->id . strtoupper($fulfillment->serviceRequest->employee->dv) . "\t" .
         strtoupper(trim($fulfillment->serviceRequest->employee->fullName)) . "\t" .
