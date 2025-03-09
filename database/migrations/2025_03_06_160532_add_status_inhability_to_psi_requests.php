4bc55afa6f7493833eb55d1493f4953ceddb3077
Ignacio Miranda <piranga8@gmail.com>
2025-03-09 19:22:16 -0300

    Idoniedad: Agregando cambios a tabla psi_request

---
+++ database/migrations/2025_03_06_160532_add_status_inhability_to_psi_requests.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('psi_requests', function (Blueprint $table) {
+            $table->enum('status_inhability', ['none', 'in_progress', 'enabled', 'disabled'])->default('none')->nullable()->after('disability');
+            $table->date('signed_at')->nullable()->after('status_inhability');
+            $table->date('test_send_at')->nullable()->after('signed_at');
+            $table->date('approved_at')->nullable()->after('test_send_at');
+            $table->date('rejected_at')->nullable()->after('approved_at');
+            $table->date('certificated_at')->nullable()->after('rejected_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+
+
+     public function down(): void
+     {
+         Schema::table('psi_request', function (Blueprint $table) {
+             $table->dropColumn([
+                 'status_inhability',
+                 'signed_at',
+                 'test_send_at',
+                 'approved_at',
+                 'rejected_at',
+                 'certificated_at'
+             ]);
+         });
+     }
+};
