86e7f123a548349dba040afe445d1c48b45edc5a
Jorge Miranda L <mirandal.jorge@gmail.com>
2025-03-25 15:27:31 -0300

    DNC: Se restringe acceso a planificación

--- app/Policies/IdentifyNeeds/AuthorizeAmountPolicy.php
+++ app/Policies/IdentifyNeeds/AuthorizeAmountPolicy.php
@@ -13,7 +13,7 @@ class AuthorizeAmountPolicy
      */
     public function viewAny(User $user): bool
     {
-        return true;
+        return ($user->can('DNC: authorize amount'));
     }
 
     /**
