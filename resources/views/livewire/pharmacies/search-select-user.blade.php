e11c8e84e3c64442c0878d0af094286feba7cdf8
sickiqq <sick_iqq@hotmail.com>
2025-04-11 15:28:11 -0400

    fraccionamiento: posicion ui listado en lw3

--- resources/views/livewire/pharmacies/search-select-user.blade.php
+++ resources/views/livewire/pharmacies/search-select-user.blade.php
@@ -14,7 +14,7 @@
 
     <fieldset class="form-group col">
         <label for="for_patient">Paciente</label>
-        <div class="input-group">
+        <div class="input-group" style="position: relative;"> <!-- Add relative positioning -->
             <input
                 type="text"
                 class="form-control {{ $small_option ? 'form-control-sm' : '' }}"
@@ -31,35 +31,34 @@
             onkeydown="return event.key != 'Enter';"
             />
 
-                @if($addUsers)
-                <a class="btn btn-outline-secondary {{ $small_option ? 'btn-sm' : '' }}" wire:click="addUser">
-                    <i class="fas fa-user"></i></a>
-                @endif
-                <a class="btn btn-outline-secondary {{ $small_option ? 'btn-sm' : '' }} {{ $disabled }}" wire:click="resetx">
-                    <i class="fas fa-eraser"></i></a>
+            @if($addUsers)
+            <a class="btn btn-outline-secondary {{ $small_option ? 'btn-sm' : '' }}" wire:click="addUser">
+                <i class="fas fa-user"></i></a>
+            @endif
+            <a class="btn btn-outline-secondary {{ $small_option ? 'btn-sm' : '' }} {{ $disabled }}" wire:click="resetx">
+                <i class="fas fa-eraser"></i></a>
+
+            @if(!empty($query))
+                <ul class="list-group" style="z-index: 3; position: absolute; top: 100%; left: 0; width: 100%;"> <!-- Adjust styles -->
+                    @if( count($users) >= 1 )
+                        @foreach($users as $user)
+                            @if(!$user->trashed())
+                            <a wire:click="setUser({{$user->id}})" wire:click.prevent="addSearchedUser({{ $user }})"
+                                class="list-group-item list-group-item-action"
+                            >{{ $user->full_name }} </a>
+                            @endif
+                        @endforeach
+                    @elseif($msg_too_many)
+                        <div class="list-group-item list-group-item-info">Hemos encontrado muchas coincidencias</div>
+                    @else
+                        <div class="list-group-item list-group-item-warning">No hay resultados</div>
+                    @endif
+                </ul>
+            @endif
         </div>
     </fieldset>
     
     <input type="text" name="{{ $selected_id }}" value="{{ optional($user)->id }}" style="display:none;" {{ $required }} {{$disabled}}>
-
-    <!-- TODO revisar si es necesario en los modulos o en que parte traer los eliminados para cambiar el comportamiento del modelo y evitar este if(!$user->trashed()) -->
-    @if(!empty($query))
-        <ul class="list-group" style="z-index: 3; position: absolute;">
-            @if( count($users) >= 1 )
-                @foreach($users as $user)
-                    @if(!$user->trashed())
-                    <a wire:click="setUser({{$user->id}})" wire:click.prevent="addSearchedUser({{ $user }})"
-                        class="list-group-item list-group-item-action"
-                    >{{ $user->full_name }} </a>
-                    @endif
-                @endforeach
-            @elseif($msg_too_many)
-                <div class="list-group-item list-group-item-info">Hemos encontrado muchas coincidencias</div>
-            @else
-                <div class="list-group-item list-group-item-warning">No hay resultados</div>
-            @endif
-        </ul>
-    @endif
 
     <fieldset class="form-group col">
         <label for="for_origin_establishment_name">Centro de salud</label>
