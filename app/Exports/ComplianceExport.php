ce8dfd36d32d7dc7dfaa8c73297d562d9755f6f6
sickiqq <sick_iqq@hotmail.com>
2025-04-15 16:53:37 -0400

    honorarios: agrega columnas excel report cumplimientos.

--- app/Exports/ComplianceExport.php
+++ app/Exports/ComplianceExport.php
@@ -7,12 +7,8 @@ use App\Models\ServiceRequests\Fulfillment;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Illuminate\Http\Request;
 
-
 class ComplianceExport implements FromCollection, WithHeadings, WithMapping, ShouldAutoSize
 {
-
-    // private $request;
-    
     use Exportable;
 
     public function __construct(Request $request)
@@ -20,29 +16,43 @@ class ComplianceExport implements FromCollection, WithHeadings, WithMapping, Sho
         $this->request = $request;
     }
 
-
-
     public function collection()
     {
-        
         return Fulfillment::Search($this->request)
-                            ->whereHas('ServiceRequest')
-                            ->orderBy('id','Desc')
-                            ->get();        
+                         ->whereHas('ServiceRequest')
+                         ->orderBy('id','Desc')
+                         ->get();        
     }
-
 
     public function headings(): array
     {
         return [
-          'ID',
-          'Rut',
-          'Nombre',
-          'Unidad',
-          'Periodo',
-          'Tipo',
-          'Tipo de Contrato',
-          'Tipo de Jornada',
+            'ID',
+            'N° Solicitud',
+            'Rut',
+            'Nombre',
+            'Unidad',
+            'Período',
+            'Teléfono',
+            'Email',
+            'Programa',
+            'Profesión',
+            'Hrs. Semanales',
+            'Tipo',
+            'Tipo de Contrato',
+            'Tipo de Jornada',
+            'Monto Bruto',
+            'N° Boleta',
+            'Total Hrs. Pagadas',
+            'Total Pagado', 
+            'Fecha Pago',
+            'Estado',
+            'Tiene Resolución',
+            'Tiene Certificado',
+            'Apr. Responsable',
+            'Apr. RRHH',
+            'Apr. Finanzas',
+            'Tiene Boleta'
         ];
     }
 
@@ -50,15 +60,47 @@ class ComplianceExport implements FromCollection, WithHeadings, WithMapping, Sho
     {
         return [
             $fulfillment->id,
+            $fulfillment->servicerequest?->id ?? '',
             $fulfillment->servicerequest?->employee?->runFormat() ?? '',
             $fulfillment->servicerequest?->employee?->fullname 
                 ? strtoupper($fulfillment->servicerequest->employee->fullname) 
                 : '',
             $fulfillment->servicerequest?->responsabilityCenter?->name ?? '',
-            $fulfillment->year . '-' . $fulfillment->month,  // Fixed period format
+            $fulfillment->year . '-' . $fulfillment->month,
+            $fulfillment->servicerequest?->employee?->phone_number ?? '',
+            $fulfillment->servicerequest?->employee?->email ?? '',
+            $fulfillment->servicerequest?->programm_name ?? '',
+            $fulfillment->servicerequest?->profession?->name ?? '',
+            $fulfillment->servicerequest?->weekly_hours ?? '',
             $fulfillment->servicerequest?->type ?? '',
             $fulfillment->servicerequest?->program_contract_type ?? '',
-            $fulfillment->servicerequest?->working_day_type ?? ''
+            $fulfillment->servicerequest?->working_day_type ?? '',
+            $fulfillment->servicerequest?->gross_amount ?? '',
+            $fulfillment->bill_number ?? '',
+            $fulfillment->total_hours_paid ?? '',
+            $fulfillment->total_paid ?? '',
+            $fulfillment->payment_date ? $fulfillment->payment_date->format('Y-m-d') : '',
+            $this->getStatus($fulfillment),
+            $fulfillment->serviceRequest?->has_resolution_file ? 'Sí' : 'No',
+            $fulfillment->signatures_file_id ? 'Sí' : 'No',
+            $fulfillment->responsable_approbation ? 'Sí' : 'No',
+            $fulfillment->rrhh_approbation ? 'Sí' : 'No',
+            $fulfillment->finances_approbation ? 'Sí' : 'No',
+            $fulfillment->has_invoice_file ? 'Sí' : 'No'
         ];
+    }
+
+    private function getStatus($fulfillment)
+    {
+        if ($fulfillment->quit_status() == 'Sí') {
+            return 'Renunció';
+        }
+        if ($fulfillment->payment_date) {
+            return 'Pagado';
+        }
+        if ($fulfillment->has_invoice_file) {
+            return 'Con Boleta';
+        }
+        return 'En Proceso';
     }
 }
